01491341c689dedc78c965573a8e0e09d732d718
kec <kec>
2007-04-03 19:14:52 +0000

    Modified to support only executing for specified goals.

--- src/main/java/org/dwfa/mojo/GenerateBerkeleyVodb.java
+++ src/main/java/org/dwfa/mojo/GenerateBerkeleyVodb.java
@@ -8,9 +8,11 @@ import java.util.Set;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
+import org.apache.maven.execution.MavenSession;
 import org.apache.maven.model.Dependency;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.dwfa.maven.MojoUtil;
 import org.dwfa.vodb.LoadSourcesFromJars;
 
 /**
@@ -22,9 +24,10 @@ import org.dwfa.vodb.LoadSourcesFromJars;
  * @requiresDependencyResolution compile
  */
 public class GenerateBerkeleyVodb extends AbstractMojo {
-    private static final String fileSep = System.getProperty("file.separator", "/");
+	private static final String fileSep = System.getProperty("file.separator",
+			"/");
 
-    /**
+	/**
 	 * Location of the build directory.
 	 * 
 	 * @parameter expression="${project.build.directory}/classes/berkeley-db"
@@ -32,12 +35,12 @@ public class GenerateBerkeleyVodb extends AbstractMojo {
 	 */
 	File outputDirectory;
 
-    /**
-     * @parameter expression="${project.dependencies}"
-     * @required
-     */
-    private List<Dependency> dependencies;
-	
+	/**
+	 * @parameter expression="${project.dependencies}"
+	 * @required
+	 */
+	private List<Dependency> dependencies;
+
 	/**
 	 * @parameter expression="${settings.localRepository}"
 	 * @required
@@ -45,63 +48,77 @@ public class GenerateBerkeleyVodb extends AbstractMojo {
 	private String localRepository;
 
 	/**
-     * @parameter
-     * @required
-     */
-    private  List<DependencySpec> dependenciesToProcess;
-    
-	public void execute() throws MojoExecutionException {
-		String[] args = new String[3];
-		int argIndex = 0;
-		args[argIndex++] = outputDirectory.getAbsolutePath();
-		outputDirectory.mkdirs();
-		try {
-		Set<String> processSet = new HashSet<String>(dependenciesToProcess.size());
-		for (DependencySpec ds: dependenciesToProcess) {
-			processSet.add(ds.getGroupId() + ds.getArtifactId() + ds.getVersion());
-		}
-		getLog().info("Here are the dependencies...");
-		
-        for (Dependency d: dependencies) {
-            getLog().info("Project Dependency: " + d);
-            if (processSet.contains(d.getGroupId()+ d.getArtifactId() + d.getVersion())) {
-                getLog().info("    Process this one: " + d);
-                String path = dependencyToPath(d);
-                args[argIndex++] = path;
-				JarFile jf = new JarFile(path);
+	 * @parameter
+	 * @required
+	 */
+	private List<DependencySpec> dependenciesToProcess;
 
-				Enumeration<JarEntry> jarEnum = jf.entries();
-				while (jarEnum.hasMoreElements()) {
-					JarEntry je = jarEnum.nextElement();
-					getLog().info(je.toString());
+	private String[] allowedGoals = new String[] { "install" };
+
+	/**
+	 * The maven session
+	 * 
+	 * @parameter expression="${session}"
+	 * @required
+	 */
+	private MavenSession session;
+
+	public void execute() throws MojoExecutionException {
+		if (MojoUtil.allowedGoal(getLog(), session.getGoals(), allowedGoals)) {
+			String[] args = new String[3];
+			int argIndex = 0;
+			args[argIndex++] = outputDirectory.getAbsolutePath();
+			outputDirectory.mkdirs();
+			try {
+				Set<String> processSet = new HashSet<String>(
+						dependenciesToProcess.size());
+				for (DependencySpec ds : dependenciesToProcess) {
+					processSet.add(ds.getGroupId() + ds.getArtifactId()
+							+ ds.getVersion());
 				}
-            }
-        }
-        
-        LoadSourcesFromJars.main(args);
-        
-		} catch (Exception ex) {
-			throw new MojoExecutionException(
-					"Error processing dependency. Reason: " +
-					ex.getMessage(),
-					ex);			
-		} 
+				getLog().info("Here are the dependencies...");
+
+				for (Dependency d : dependencies) {
+					getLog().info("Project Dependency: " + d);
+					if (processSet.contains(d.getGroupId() + d.getArtifactId()
+							+ d.getVersion())) {
+						getLog().info("    Process this one: " + d);
+						String path = dependencyToPath(d);
+						args[argIndex++] = path;
+						JarFile jf = new JarFile(path);
+
+						Enumeration<JarEntry> jarEnum = jf.entries();
+						while (jarEnum.hasMoreElements()) {
+							JarEntry je = jarEnum.nextElement();
+							getLog().info(je.toString());
+						}
+					}
+				}
+
+				LoadSourcesFromJars.main(args);
+
+			} catch (Exception ex) {
+				throw new MojoExecutionException(
+						"Error processing dependency. Reason: "
+								+ ex.getMessage(), ex);
+			}
+		}
 	}
 
-    public String dependencyToPath(Dependency dep) {
-        StringBuffer buff = new StringBuffer();
-        buff.append(localRepository);
-        buff.append(fileSep); 
-        buff.append(dep.getGroupId().replace('.', fileSep.charAt(0)));
-        buff.append(fileSep); 
-        buff.append(dep.getArtifactId());
-        buff.append(fileSep); 
-        buff.append(dep.getVersion());
-        buff.append(fileSep); 
-        buff.append(dep.getArtifactId());
-        buff.append("-"); 
-        buff.append(dep.getVersion());
-        buff.append(".jar"); 
-        return buff.toString();
-    }
+	public String dependencyToPath(Dependency dep) {
+		StringBuffer buff = new StringBuffer();
+		buff.append(localRepository);
+		buff.append(fileSep);
+		buff.append(dep.getGroupId().replace('.', fileSep.charAt(0)));
+		buff.append(fileSep);
+		buff.append(dep.getArtifactId());
+		buff.append(fileSep);
+		buff.append(dep.getVersion());
+		buff.append(fileSep);
+		buff.append(dep.getArtifactId());
+		buff.append("-");
+		buff.append(dep.getVersion());
+		buff.append(".jar");
+		return buff.toString();
+	}
 }
