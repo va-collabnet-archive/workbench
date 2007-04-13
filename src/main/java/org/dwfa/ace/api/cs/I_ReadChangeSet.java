c3d7e79e262706c2a74c114a879166ed20f28fc9
kec <kec>
2007-04-13 16:47:18 +0000

    Updated the change set API.

--- src/main/java/org/dwfa/ace/api/cs/I_ReadChangeSet.java
+++ src/main/java/org/dwfa/ace/api/cs/I_ReadChangeSet.java
@@ -1,8 +1,9 @@
 package org.dwfa.ace.api.cs;
 
 import java.io.IOException;
+import java.io.Serializable;
 
-public interface I_ReadChangeSet {
+public interface I_ReadChangeSet extends Serializable {
 	
 	public void read() throws IOException;
 
--- src/main/java/org/dwfa/ace/api/cs/I_WriteChangeSet.java
+++ src/main/java/org/dwfa/ace/api/cs/I_WriteChangeSet.java
@@ -1,10 +1,11 @@
 package org.dwfa.ace.api.cs;
 
 import java.io.IOException;
+import java.io.Serializable;
 
 import org.dwfa.ace.api.I_GetConceptData;
 
-public interface I_WriteChangeSet {
+public interface I_WriteChangeSet extends Serializable {
 
 	public void open() throws IOException;
 
