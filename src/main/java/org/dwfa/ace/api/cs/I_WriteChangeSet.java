d8f5ad3b393dc5194c4b753582ede189e04bc7ef
kec <kec>
2007-04-13 04:24:05 +0000

    Updated the API to allow use of temporary files and a commit step.

--- src/main/java/org/dwfa/ace/api/cs/I_WriteChangeSet.java
+++ src/main/java/org/dwfa/ace/api/cs/I_WriteChangeSet.java
@@ -5,7 +5,12 @@ import java.io.IOException;
 import org.dwfa.ace.api.I_GetConceptData;
 
 public interface I_WriteChangeSet {
-	
-	public void writeChanges(I_GetConceptData conceptBean, int version) throws IOException;
+
+	public void open() throws IOException;
+
+	public void writeChanges(I_GetConceptData conceptBean, int version)
+			throws IOException;
+
+	public void commit() throws IOException;
 
 }
