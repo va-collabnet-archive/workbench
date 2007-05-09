e0af5dc55720492c6730a5bf59119108292dcf8a
chrissy <chrissy>
2007-05-09 06:08:51 +0000

    added getChainedTransform() method - used to nest transforms

--- dev/src/main/java/org/dwfa/maven/transform/AbstractTransform.java
+++ dev/src/main/java/org/dwfa/maven/transform/AbstractTransform.java
@@ -4,13 +4,19 @@ import org.dwfa.maven.I_ReadAndTransform;
 import org.dwfa.maven.Transform;
 
 public abstract class AbstractTransform implements I_ReadAndTransform {
-	
+
 	private String name;
-	
+
 	private String lastTransform;
-	
+
 	private int columnId = -1;
-	
+
+    private I_ReadAndTransform chainedTransform; //
+
+    public I_ReadAndTransform getChainedTransform() {
+        return chainedTransform;
+    }
+
 	public String toString() {
 		return getClass().getSimpleName() + ": " + name;
 	}
@@ -33,7 +39,7 @@ public abstract class AbstractTransform implements I_ReadAndTransform {
 	public void setColumnId(int columnId) {
 		this.columnId = columnId;
 	}
-	
+
 	public final void setup(Transform transformer) {
 		if (columnId == -1) {
 			columnId = transformer.getNextColumnId();
