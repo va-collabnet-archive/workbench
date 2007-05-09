bd84aa2b4af6f043603336a46a71f87110cdf985
chrissy <chrissy>
2007-05-09 06:07:57 +0000

    added getChainedTransform() method - used to nest transforms

--- dev/src/main/java/org/dwfa/maven/I_ReadAndTransform.java
+++ dev/src/main/java/org/dwfa/maven/I_ReadAndTransform.java
@@ -3,6 +3,7 @@ package org.dwfa.maven;
 public interface I_ReadAndTransform {
 	public void setup(Transform transformer);
 	public String transform(String input) throws Exception;
+	public I_ReadAndTransform getChainedTransform();
 	public String getLastTransform();
 	public String getName();
 	public void setName(String name);
