4aa47a1f7933313449000206d7f455914e0eee30
kec <kec>
2007-06-12 00:46:17 +0000

    Fixed incorrect code in BinaryChangeSetRead.

--- src/main/java/org/dwfa/mojo/BinaryChangeSetRead.java
+++ src/main/java/org/dwfa/mojo/BinaryChangeSetRead.java
@@ -8,7 +8,7 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.dwfa.ace.api.I_TermFactory;
 import org.dwfa.ace.api.LocalVersionedTerminology;
-import org.dwfa.ace.api.cs.I_WriteChangeSet;
+import org.dwfa.ace.api.cs.I_ReadChangeSet;
 
 /**
  * 
@@ -40,8 +40,8 @@ public class BinaryChangeSetRead extends AbstractMojo {
 	public void execute() throws MojoExecutionException, MojoFailureException {
 		I_TermFactory termFactory = LocalVersionedTerminology.get();
 		try {
-			I_WriteChangeSet writer = termFactory.newBinaryChangeSetWriter(new File(changeSetDir, changeSetFileName));
-			termFactory.addChangeSetWriter(writer);
+			I_ReadChangeSet reader = termFactory.newBinaryChangeSetReader(new File(changeSetDir, changeSetFileName));
+			reader.read();
 		} catch (Exception e) {
 			throw new MojoExecutionException(e.getLocalizedMessage(), e);
 		}		
