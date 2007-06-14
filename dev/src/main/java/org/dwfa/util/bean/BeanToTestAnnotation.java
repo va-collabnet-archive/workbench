79d6363f6b28f75d6ef718f4ea3fc8b43b7cbac3
kec <kec>
2007-06-14 18:41:54 +0000

    Added bean to test annotation and bean writing-- made serlializable.

--- dev/src/main/java/org/dwfa/util/bean/BeanToTestAnnotation.java
+++ dev/src/main/java/org/dwfa/util/bean/BeanToTestAnnotation.java
@@ -1,6 +1,13 @@
 package org.dwfa.util.bean;
 
+import java.io.Serializable;
+
 @BeanList(specs = { @Spec(directory = "temp/test/", type = BeanType.GENERIC_BEAN) })
-public class BeanToTestAnnotation {
+public class BeanToTestAnnotation implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 }
