70f84507074ba5e6fe65254cb69a22f0c7266cec
scastillo <scastillo>
2007-06-18 22:46:25 +0000

    Added ACTIVE_CONCEPT_UUID

--- src/main/java/org/dwfa/ace/task/ProcessAttachmentKeys.java
+++ src/main/java/org/dwfa/ace/task/ProcessAttachmentKeys.java
@@ -3,7 +3,8 @@ package org.dwfa.ace.task;
 public enum ProcessAttachmentKeys {
     I_GET_CONCEPT_DATA,
     ACTIVE_CONCEPT, NEW_STATUS, REL_TYPE,
-    DEFAULT_CONCEPT_LIST, REL_PARENT, DEFAULT_FILE;
+    DEFAULT_CONCEPT_LIST, REL_PARENT, DEFAULT_FILE,
+    HTML_STR, ACTIVE_CONCEPT_UUID;
 
     public String getAttachmentKey() {
         return "A: " + this.name();
