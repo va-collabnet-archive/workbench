99eb1e3a2e23496e27e5c084449a670664c9ce3d
kec <kec>
2007-06-19 01:14:59 +0000

    Fixed conflict in ProcessAttachmentKeys

--- src/main/java/org/dwfa/ace/task/ProcessAttachmentKeys.java
+++ src/main/java/org/dwfa/ace/task/ProcessAttachmentKeys.java
@@ -3,9 +3,9 @@ package org.dwfa.ace.task;
 public enum ProcessAttachmentKeys {
     I_GET_CONCEPT_DATA,
     ACTIVE_CONCEPT, NEW_STATUS, REL_TYPE,
-    DEFAULT_CONCEPT_LIST, REL_PARENT, DEFAULT_FILE,
+    DEFAULT_CONCEPT_LIST, REL_PARENT, DEFAULT_FILE, WORKING_PROFILE, USERNAME, PASSWORD, ADMIN_USERNAME, ADMIN_PASSWORD,
     HTML_STR, ACTIVE_CONCEPT_UUID;
-
+    
     public String getAttachmentKey() {
         return "A: " + this.name();
     }
