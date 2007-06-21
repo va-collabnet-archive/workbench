b3865ad8cffe91ce7b20552495457b1a19a4aed6
scastillo <scastillo>
2007-06-21 05:21:37 +0000

    created a task to get selected addresses from the address book...

---
+++ src/main/java/org/dwfa/ace/task/gui/SelectAddressesInAddressBook.java
@@ -0,0 +1,93 @@
+package org.dwfa.ace.task.gui;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Collection;
+import java.util.List;
+
+import org.dwfa.ace.api.I_ConfigAceFrame;
+import org.dwfa.ace.task.WorkerAttachmentKeys;
+import org.dwfa.bpa.process.Condition;
+import org.dwfa.bpa.process.I_EncodeBusinessProcess;
+import org.dwfa.bpa.process.I_Work;
+import org.dwfa.bpa.process.TaskFailedException;
+import org.dwfa.bpa.tasks.AbstractTask;
+import org.dwfa.util.bean.BeanList;
+import org.dwfa.util.bean.BeanType;
+import org.dwfa.util.bean.Spec;
+
+/**
+ * Task which gets the addresses selected by the user in the address book
+ * @author Susan Castillo
+ *
+ */
+@BeanList(specs = { @Spec(directory = "tasks/ace/gui", type = BeanType.TASK_BEAN) })
+
+public class SelectAddressesInAddressBook extends AbstractTask {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    private static final int dataVersion = 1;
+    
+    private List<String> addressesSelected;
+
+    private void writeObject(ObjectOutputStream out) throws IOException {
+        out.writeInt(dataVersion);
+        out.writeObject(addressesSelected);
+    }
+
+    private void readObject(ObjectInputStream in) throws IOException,
+            ClassNotFoundException {
+        int objDataVersion = in.readInt();
+        if (objDataVersion == dataVersion) {
+        addressesSelected = (List<String>) in.readObject();
+
+        } else {
+            throw new IOException(
+                    "Can't handle dataversion: " + objDataVersion);
+        }
+    }
+
+    public void complete(I_EncodeBusinessProcess process, I_Work worker)
+                         throws TaskFailedException {
+        // Nothing to do
+    }
+
+    public Condition evaluate(I_EncodeBusinessProcess process, I_Work worker)
+                                throws TaskFailedException {
+        try {
+            I_ConfigAceFrame configFrame = (I_ConfigAceFrame) worker
+                .readAttachement(WorkerAttachmentKeys.ACE_FRAME_CONFIG.name());
+            boolean showAddressBook = true;
+            configFrame.setShowAddresses(showAddressBook);
+            
+            addressesSelected = configFrame.getSelectedAddresses();
+            
+            return Condition.CONTINUE;
+        } catch (IllegalArgumentException e) {
+            throw new TaskFailedException(e);
+        }
+    }
+
+    public int[] getDataContainerIds() {
+        return new int[] {};
+    }
+
+    public Collection<Condition> getConditions() {
+        return AbstractTask.CONTINUE_CONDITION;
+    }
+
+
+	public List<String> getAddressesSelected() {
+		return addressesSelected;
+	}
+
+	public void setAddressesSelected(List<String> addressesSelected) {
+		this.addressesSelected = addressesSelected;
+	}
+}
+
---
+++ src/main/java/org/dwfa/ace/task/gui/SelectAddressesInAddressBookBeanInfo.java
@@ -0,0 +1,47 @@
+package org.dwfa.ace.task.gui;
+
+import java.beans.BeanDescriptor;
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
+
+import org.dwfa.bpa.tasks.editor.CheckboxEditor;
+
+/**
+ * Bean info to SelectAddressesInAddressBook class.
+ * @author Susan Castillo	
+ *
+ */
+public class SelectAddressesInAddressBookBeanInfo {
+
+    /**
+     *
+     */
+    public SelectAddressesInAddressBookBeanInfo() {
+        super();
+     }
+
+    public PropertyDescriptor[] getPropertyDescriptors() {
+        try {
+            PropertyDescriptor addressesSelected =
+                new PropertyDescriptor("addressesSelected", SelectAddressesInAddressBook.class);
+            addressesSelected.setBound(true);
+            addressesSelected.setPropertyEditorClass(CheckboxEditor.class);
+            addressesSelected.setDisplayName("<html><font color='green'>Select Addresses<br>in Address Book");
+            addressesSelected.setShortDescription("Select Addresses");
+
+            PropertyDescriptor rv[] = { addressesSelected };
+            return rv;
+        } catch (IntrospectionException e) {
+             throw new Error(e.toString());
+        }
+    }
+    /**
+     * @see java.beans.BeanInfo#getBeanDescriptor()
+     */
+    public BeanDescriptor getBeanDescriptor() {
+        BeanDescriptor bd = new BeanDescriptor(SelectAddressesInAddressBook.class);
+        bd.setDisplayName("<html><font color='green'><center>Show or Hide <br>Select Addresses<br>Address Book");
+        return bd;
+    }
+
+}
