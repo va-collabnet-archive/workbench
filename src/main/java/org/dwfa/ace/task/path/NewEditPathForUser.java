36d60f578f7925f065bf9823cf96334129e59c87
kec <kec>
2007-06-25 21:39:05 +0000

    Resolved conflict.

--- src/main/java/org/dwfa/ace/task/path/NewEditPathForUser.java
+++ src/main/java/org/dwfa/ace/task/path/NewEditPathForUser.java
@@ -6,9 +6,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Collection;
-import java.util.UUID;
 
-import org.dwfa.ace.api.I_ConfigAceFrame;
 import org.dwfa.ace.api.I_GetConceptData;
 import org.dwfa.ace.api.I_Path;
 import org.dwfa.ace.api.LocalVersionedTerminology;
