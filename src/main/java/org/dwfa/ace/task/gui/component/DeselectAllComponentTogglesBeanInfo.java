547ec865505e18b4ec1faf33b97003950afbb103
kec <kec>
2007-07-31 12:05:13 +0000

    Added tasks to deselect the component toggles.

---
+++ src/main/java/org/dwfa/ace/task/gui/component/DeselectAllComponentTogglesBeanInfo.java
@@ -0,0 +1,38 @@
+package org.dwfa.ace.task.gui.component;
+
+import java.beans.BeanDescriptor;
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
+import java.beans.SimpleBeanInfo;
+
+import org.dwfa.ace.task.gui.ShowConceptTab;
+import org.dwfa.bpa.tasks.editor.IncrementEditor;
+
+public class DeselectAllComponentTogglesBeanInfo extends SimpleBeanInfo {
+
+    public PropertyDescriptor[] getPropertyDescriptors() {
+
+        try {
+            PropertyDescriptor index =
+                new PropertyDescriptor("index", ShowConceptTab.class);
+            index.setBound(true);
+            index.setPropertyEditorClass(IncrementEditor.class);
+            index.setDisplayName("<html><font color='green'>Concept tab:");
+            index.setShortDescription("Index of tab deselect all toggles. ");
+            PropertyDescriptor rv[] = { index };
+            return rv;
+        } catch (IntrospectionException e) {
+            throw new Error(e.toString());
+       }
+    }
+    /**
+     * @see java.beans.BeanInfo#getBeanDescriptor()
+     */
+    public BeanDescriptor getBeanDescriptor() {
+        BeanDescriptor bd = new BeanDescriptor(ShowConceptTab.class);
+        bd.setDisplayName("<html><font color='green'><center>Deselect all toggles<br>"
+                + "for concept tab");
+        return bd;
+    }
+
+}
