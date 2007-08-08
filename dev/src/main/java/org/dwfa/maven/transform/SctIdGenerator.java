3fcb05557535b0512705067cfd635766edc79d33
kec <kec>
2007-08-08 12:04:08 +0000

    Changed to static methods.

--- dev/src/main/java/org/dwfa/maven/transform/SctIdGenerator.java
+++ dev/src/main/java/org/dwfa/maven/transform/SctIdGenerator.java
@@ -1,8 +1,8 @@
 package org.dwfa.maven.transform;
 
 public class SctIdGenerator {
-   
-   public enum NAMESPACE {
+      
+   public static enum NAMESPACE {
       NEHTA("1000036");
       
       private String digits;
@@ -14,7 +14,7 @@ public class SctIdGenerator {
       }
    };
    
-   public enum PROJECT {
+   public static enum PROJECT {
       AMT("01"), PATHOLOGY("02"), ALLERGIES_AND_ADVERSE_REACTIONS("03"), RADIOLOGY("04"), DIAGNOSIS("05");
       
       private String digits;
@@ -26,7 +26,7 @@ public class SctIdGenerator {
       }
    };
    
-   public enum TYPE {
+   public static enum TYPE {
       CONCEPT("10"), DESCRIPTION("11"), RELATIONSHIP("12"), SUBSET("13");
       private String digits;
       private TYPE(String digits) {
@@ -37,7 +37,7 @@ public class SctIdGenerator {
       }
    }
 
-   private int[][] FnF = {{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+   private static int[][] FnF = {{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
          {1, 5, 7, 6, 2, 8, 3, 0, 9, 4},
          {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
          {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -47,7 +47,7 @@ public class SctIdGenerator {
          {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
          {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
 
-   private int[][] Dihedral = {
+   private static int[][] Dihedral = {
          { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 },
          { 1, 2, 3, 4, 0, 6, 7, 8, 9, 5 },
          { 2, 3, 4, 0, 1, 7, 8, 9, 5, 6 },
@@ -60,9 +60,9 @@ public class SctIdGenerator {
          { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 } };
 
 
-   private int[] InverseD5= { 0, 4, 3, 2, 1, 5, 6, 7, 8, 9 };
+   private static int[] InverseD5= { 0, 4, 3, 2, 1, 5, 6, 7, 8, 9 };
 
-   public SctIdGenerator() {
+   static {
       for ( int i = 2; i < 8; i++ ) {
          for ( int j = 0; j < 10; j++ ) {
             FnF[ i ][ j ] = FnF[ i - 1 ][ FnF[ 1 ][ j ]];
@@ -70,7 +70,7 @@ public class SctIdGenerator {
       }
    }
 
-   public String generate(long sequence, PROJECT project, NAMESPACE namespace, TYPE type) throws Exception {
+   public static String generate(long sequence, PROJECT project, NAMESPACE namespace, TYPE type) throws Exception {
       
       if (sequence <= 0) {
          throw new Exception("sequence must be > 0");
@@ -81,7 +81,7 @@ public class SctIdGenerator {
       return mergedid + verhoeffCompute(mergedid);
    }
 
-   public boolean verhoeffCheck(String idAsString) {
+   public static  boolean verhoeffCheck(String idAsString) {
       int check = 0;
       
       for ( int i=idAsString.length()-1; i >=0; i--) {
@@ -94,7 +94,7 @@ public class SctIdGenerator {
       }
    }
 
-   public long verhoeffCompute(String idAsString){
+   public static long verhoeffCompute(String idAsString){
       int check = 0;
       for ( int i = idAsString.length()-1; i >=0; i-- ) {
          check = Dihedral[ check ][ FnF [((idAsString.length()-i) % 8) ][ new Integer(new String(new char[]{idAsString.charAt(i)}))]];
