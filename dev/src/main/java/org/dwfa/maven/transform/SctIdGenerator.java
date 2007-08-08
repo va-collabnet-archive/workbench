12d5349d9f6b655c205a6382fae60f4374d92786
kec <kec>
2007-08-08 08:05:20 +0000

    Added an SCT ID generator and test class.

---
+++ dev/src/main/java/org/dwfa/maven/transform/SctIdGenerator.java
@@ -0,0 +1,105 @@
+package org.dwfa.maven.transform;
+
+public class SctIdGenerator {
+   
+   public enum NAMESPACE {
+      NEHTA("1000036");
+      
+      private String digits;
+      private NAMESPACE(String digits) {
+         this.digits = digits;
+      }
+      public String getDigits() {
+         return digits;
+      }
+   };
+   
+   public enum PROJECT {
+      AMT("01"), PATHOLOGY("02"), ALLERGIES_AND_ADVERSE_REACTIONS("03"), RADIOLOGY("04"), DIAGNOSIS("05");
+      
+      private String digits;
+      private PROJECT(String digits) {
+         this.digits = digits;
+      }
+      public String getDigits() {
+         return digits;
+      }
+   };
+   
+   public enum TYPE {
+      CONCEPT("10"), DESCRIPTION("11"), RELATIONSHIP("12"), SUBSET("13");
+      private String digits;
+      private TYPE(String digits) {
+         this.digits = digits;
+      }
+      public String getDigits() {
+         return digits;
+      }
+   }
+
+   private int[][] FnF = {{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+         {1, 5, 7, 6, 2, 8, 3, 0, 9, 4},
+         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+
+   private int[][] Dihedral = {
+         { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+         { 1, 2, 3, 4, 0, 6, 7, 8, 9, 5 },
+         { 2, 3, 4, 0, 1, 7, 8, 9, 5, 6 },
+         { 3, 4, 0, 1, 2, 8, 9, 5, 6, 7 },
+         { 4, 0, 1, 2, 3, 9, 5, 6, 7, 8 },
+         { 5, 9, 8, 7, 6, 0, 4, 3, 2, 1 },
+         { 6, 5, 9, 8, 7, 1, 0, 4, 3, 2 },
+         { 7, 6, 5, 9, 8, 2, 1, 0, 4, 3 },
+         { 8, 7, 6, 5, 9, 3, 2, 1, 0, 4 },
+         { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 } };
+
+
+   private int[] InverseD5= { 0, 4, 3, 2, 1, 5, 6, 7, 8, 9 };
+
+   public SctIdGenerator() {
+      for ( int i = 2; i < 8; i++ ) {
+         for ( int j = 0; j < 10; j++ ) {
+            FnF[ i ][ j ] = FnF[ i - 1 ][ FnF[ 1 ][ j ]];
+         }
+      }
+   }
+
+   public String generate(long sequence, PROJECT project, NAMESPACE namespace, TYPE type) throws Exception {
+      
+      if (sequence <= 0) {
+         throw new Exception("sequence must be > 0");
+      }
+      
+      String mergedid =  Long.toString(sequence) + project.digits + namespace.digits + type.digits;
+      
+      return mergedid + verhoeffCompute(mergedid);
+   }
+
+   public boolean verhoeffCheck(String idAsString) {
+      int check = 0;
+      
+      for ( int i=idAsString.length()-1; i >=0; i--) {
+         check = Dihedral[ check ][ FnF [ (idAsString.length()-i-1) % 8 ] [ new Integer(new String(new char[]{idAsString.charAt(i)}))]];
+      }
+      if ( check != 0 ) {
+         return false;
+      } else {
+         return true;
+      }
+   }
+
+   public long verhoeffCompute(String idAsString){
+      int check = 0;
+      for ( int i = idAsString.length()-1; i >=0; i-- ) {
+         check = Dihedral[ check ][ FnF [((idAsString.length()-i) % 8) ][ new Integer(new String(new char[]{idAsString.charAt(i)}))]];
+         
+         }
+      return InverseD5[ check ];
+   }
+}
---
+++ dev/src/test/java/org/dwfa/maven/transform/SctIdGeneratorTest.java
@@ -0,0 +1,56 @@
+package org.dwfa.maven.transform;
+
+import junit.framework.TestCase;
+
+public class SctIdGeneratorTest extends TestCase {
+   SctIdGenerator generator = new SctIdGenerator();
+
+   
+   //Concept table
+   //369445005 0  Chronic proctocolitis (disorder) XUU7a D5-45285 1
+
+   //Description table  
+   //513502016 8  133947006   Occupations 0  3  en
+
+   //Rel table
+   //100000028 280844000   116680003   71737002 0  0  0
+   //1000036
+
+   
+   public void testGenerate() {
+      try {
+         String generatedId = generator.generate(0, SctIdGenerator.PROJECT.AMT, SctIdGenerator.NAMESPACE.NEHTA, SctIdGenerator.TYPE.CONCEPT);
+         fail("Generator should have thrown an error");
+      } catch (Exception e) {
+            // expected exception at 0 index;
+      }
+      try {
+         assertEquals("1011000036106", generator.generate(1, SctIdGenerator.PROJECT.AMT, SctIdGenerator.NAMESPACE.NEHTA, SctIdGenerator.TYPE.CONCEPT));
+         
+      
+      } catch (Exception e) {
+         fail("exception: " + e.getLocalizedMessage());
+      }
+      
+   }
+
+   public void testVerhoeffCheck() {
+      // From http://en.wikipedia.org/wiki/Verhoeff_algorithm#Example
+      assertEquals(true, generator.verhoeffCheck("1428570"));
+      assertEquals(false, generator.verhoeffCheck("1428571"));
+      assertEquals(false, generator.verhoeffCheck("1428572"));
+      assertEquals(false, generator.verhoeffCheck("1428573"));
+      assertEquals(false, generator.verhoeffCheck("1428574"));
+      assertEquals(false, generator.verhoeffCheck("1428575"));
+      assertEquals(false, generator.verhoeffCheck("1428576"));
+      assertEquals(false, generator.verhoeffCheck("1428577"));
+      assertEquals(false, generator.verhoeffCheck("1428578"));
+      assertEquals(false, generator.verhoeffCheck("1428579"));
+   }
+
+   public void testVerhoeffCompute() {
+      // From http://en.wikipedia.org/wiki/Verhoeff_algorithm#Example
+      assertEquals(0, generator.verhoeffCompute("142857"));
+   }
+
+}
