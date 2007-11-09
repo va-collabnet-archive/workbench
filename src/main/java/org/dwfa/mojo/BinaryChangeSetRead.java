83377106357b57a5fd980986164956b601f79ac0
chrissy <chrissy>
2007-11-09 04:48:24 +0000

    added option to validate a change-set prior to processing (default is false)

--- src/main/java/org/dwfa/mojo/BinaryChangeSetRead.java
+++ src/main/java/org/dwfa/mojo/BinaryChangeSetRead.java
@@ -9,36 +9,43 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.dwfa.ace.api.I_TermFactory;
 import org.dwfa.ace.api.LocalVersionedTerminology;
+import org.dwfa.ace.api.cs.ComponentValidator;
 import org.dwfa.ace.api.cs.I_ReadChangeSet;
 import org.dwfa.maven.MojoUtil;
 
 /**
  * Read a binary change set, and apply the results of that change set to the
- * open database. 
+ * open database.
  * @goal bcs-read
- * 
+ *
  * @phase process-resources
  * @requiresDependencyResolution compile
  */
 
 
 public class BinaryChangeSetRead extends AbstractMojo {
-	/**
-	 * The change set directory
-	 * 
-	 * @parameter default-value="${project.build.directory}/generated-resources/changesets/"
-	 */
-	File changeSetDir;
-	
-	/**
-	 * The change set file name
-	 * 
-	 * @parameter 
-	 * @required
-	 */
-	String changeSetFileName;
+    /**
+     * The change set directory
+     *
+     * @parameter default-value="${project.build.directory}/generated-resources/changesets/"
+     */
+    File changeSetDir;
 
-	public void execute() throws MojoExecutionException, MojoFailureException {
+    /**
+     * The change set file name
+     *
+     * @parameter
+     * @required
+     */
+    String changeSetFileName;
+
+    /**
+     * Whether to validate the change set first or not.
+     * @parameter
+     */
+    boolean validate = false;
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
         try {
             if (MojoUtil.alreadyRun(getLog(), this.getClass().getCanonicalName() + changeSetDir.getCanonicalPath() + changeSetFileName)) {
                 return;
@@ -48,12 +55,17 @@ public class BinaryChangeSetRead extends AbstractMojo {
         } catch (IOException e) {
             throw new MojoExecutionException(e.getLocalizedMessage(), e);
         }
-		I_TermFactory termFactory = LocalVersionedTerminology.get();
-		try {
-			I_ReadChangeSet reader = termFactory.newBinaryChangeSetReader(new File(changeSetDir, changeSetFileName));
-			reader.read();
-		} catch (Exception e) {
-			throw new MojoExecutionException(e.getLocalizedMessage(), e);
-		}		
-	}
+        I_TermFactory termFactory = LocalVersionedTerminology.get();
+        try {
+            I_ReadChangeSet reader = termFactory.newBinaryChangeSetReader(new File(changeSetDir,
+                    changeSetFileName));
+            if (validate) {
+                getLog().info("******* Validating changeset before importing. ********");
+                reader.getValidators().add(new ComponentValidator());
+            }
+            reader.read();
+        } catch (Exception e) {
+            throw new MojoExecutionException(e.getLocalizedMessage(), e);
+        }
+    }
 }
