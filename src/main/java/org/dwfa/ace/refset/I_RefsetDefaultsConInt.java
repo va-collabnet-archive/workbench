1f63e52a47a1dcd8b135509bfb5e1fe42d762a75
kec <kec>
2007-12-08 23:35:45 +0000

    Added con int refsets.

---
+++ src/main/java/org/dwfa/ace/refset/I_RefsetDefaultsConInt.java
@@ -0,0 +1,7 @@
+package org.dwfa.ace.refset;
+
+public interface I_RefsetDefaultsConInt extends I_RefsetDefaultsConcept {
+	   public int getDefaultForIntegerValue();
+	   public Integer[] getIntegerPopupItems();
+
+}
