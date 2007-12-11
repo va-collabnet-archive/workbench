cde85cb3c96127aba480e8629779f73058b159d9
lawley <lawley>
2007-12-11 04:06:40 +0000

    Classification stub code

---
+++ src/main/java/org/dwfa/mojo/Classify.java
@@ -0,0 +1,45 @@
+package org.dwfa.mojo;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.dwfa.ace.api.I_TermFactory;
+import org.dwfa.ace.api.LocalVersionedTerminology;
+import org.dwfa.maven.MojoUtil;
+
+/**
+ * 
+ * 
+ * @goal classify
+ * 
+ * @author michaellawley
+ *
+ */
+public class Classify extends AbstractMojo {
+    
+    /**
+     * The classify factory name
+     *
+     * @parameter
+     */
+    String classifyFactory = "java.lang.String";
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        try {
+            if (MojoUtil.alreadyRun(getLog(), this.getClass().getCanonicalName() + classifyFactory)) {
+                return;
+            }
+        } catch (NoSuchAlgorithmException e) {
+            throw new MojoExecutionException(e.getLocalizedMessage(), e);
+        } catch (IOException e) {
+            throw new MojoExecutionException(e.getLocalizedMessage(), e);
+        }
+        I_TermFactory termFactory = LocalVersionedTerminology.get();
+        
+        getLog().info("classify mojo stub");
+    }
+    
+}
