eb8f744c62e37f54b5148f1dc0bafe614393fc79
kec <kec>
2008-01-22 20:01:35 +0000

    Update to DwfaSetLog.

--- dev/src/main/java/org/dwfa/maven/DwfaSetLog.java
+++ dev/src/main/java/org/dwfa/maven/DwfaSetLog.java
@@ -17,13 +17,15 @@ import org.apache.maven.plugin.MojoFailureException;
 public class DwfaSetLog extends AbstractMojo {
     
     /**
-     * Specifications to apply to the specified logs
+     * Specifications to apply to the specified logs. If you want to specify the root log, use the
+     * logger name of "root". 
      * @parameter 
      */
     private SetLogSpec[] logSettings;
     
     /**
-     * Specifications to apply to all handlers associated with the specified logs
+     * Specifications to apply to all handlers associated with the specified logs. If you want to specify the root log, use the
+     * logger name of "root". 
      * @parameter 
      */
     private SetLogSpec[] handlerSettings;
@@ -33,13 +35,19 @@ public class DwfaSetLog extends AbstractMojo {
             for (SetLogSpec logSpec: logSettings) {
                 Logger log = Logger.getLogger(logSpec.getLogger());
                 log.setLevel(Level.parse(logSpec.getLevel()));
+                getLog().info("Setting " + logSpec.getLogger() + " to:" + Level.parse(logSpec.getLevel()));
             }
         }
         
         if (handlerSettings != null) {
             for (SetLogSpec handlerSpec: handlerSettings) {
-                Logger log = Logger.getLogger(handlerSpec.getLogger());
+                String spec = handlerSpec.getLogger();
+                if (spec==null || spec.equals(" ") || spec.equals("root")) {
+                    spec = "";
+                }
+                Logger log = Logger.getLogger(spec);
                 for (Handler h: log.getHandlers()) {
+                    getLog().info("Setting " + handlerSpec.getLogger() + " handler: " + h + " to:" + Level.parse(handlerSpec.getLevel()));
                     h.setLevel(Level.parse(handlerSpec.getLevel()));
                 }
             }
