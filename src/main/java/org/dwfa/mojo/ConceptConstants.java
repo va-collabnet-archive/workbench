334405fed46b9c9846d39f1700bcae5c6602f02f
dionm <dionm>
2008-02-04 08:24:28 +0000

    changed constants

--- src/main/java/org/dwfa/mojo/ConceptConstants.java
+++ src/main/java/org/dwfa/mojo/ConceptConstants.java
@@ -11,6 +11,6 @@ public class ConceptConstants {
 
 	public static final ConceptSpec GENERATES_REL = new ConceptSpec("generates", "e22745a4-c59b-4ebe-b703-bd3fffaa4e80");
 	public static final ConceptSpec CREATES_MEMBERSHIP_TYPE = new ConceptSpec("creates membership type", "6873fb42-95b1-4835-994d-9745263393fb");
-	public static final ConceptSpec SNOMED_IS_A = new ConceptSpec("is a", "c93a30b9-ba77-3adb-a9b8-4589c9f8fb25");
+	public static final ConceptSpec SNOMED_IS_A = new ConceptSpec("Is a (attribute)", "c93a30b9-ba77-3adb-a9b8-4589c9f8fb25");
 
 }
