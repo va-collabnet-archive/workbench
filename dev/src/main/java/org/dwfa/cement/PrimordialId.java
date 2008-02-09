ae67e4a67a71f811e9443f2c6f9066687560785c
kec <kec>
2008-02-09 08:03:59 +0000

    Changed primordial ids to historical values.

--- dev/src/main/java/org/dwfa/cement/PrimordialId.java
+++ dev/src/main/java/org/dwfa/cement/PrimordialId.java
@@ -18,8 +18,8 @@ public enum PrimordialId {
 	NOT_REFINABLE_ID(9, UUID.fromString("e4cde443-8fb6-11db-b606-0800200c9a66")),
    PREFERED_TERM_ID(10, UUID.fromString("d8e3b37d-7c11-33ef-b1d0-8769e2264d44")),
    EXTENSION_TABLE_ID(11, UUID.fromString("0cbed8ca-650d-11dc-8314-0800200c9a66")),
-   STATED_CHARACTERISTIC_ID(12, UUID.fromString("7cf285d6-a3b7-11dc-8314-0800200c9a66")),
-   INFERRED_CHARACTERISTIC_ID(13, UUID.fromString("1ebef994-a3b8-11dc-8314-0800200c9a66"));
+   STATED_CHARACTERISTIC_ID(12, UUID.fromString("3fde38f6-e079-3cdc-a819-eda3ec74732d")),
+   INFERRED_CHARACTERISTIC_ID(13, UUID.fromString("d8fb4fb0-18c3-3352-9431-4919193f85bc"));
 
 	private int sequenceRelativeId;
 	private Collection<UUID> uids;
