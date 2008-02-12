ab343f7b618fe62263beb9192b59c67dd00b2443
dionm <dionm>
2008-02-12 09:52:32 +0000

    fixed uuids for regenerated concepts

--- src/main/java/org/dwfa/mojo/ConceptConstants.java
+++ src/main/java/org/dwfa/mojo/ConceptConstants.java
@@ -9,8 +9,8 @@ import org.dwfa.tapi.spec.ConceptSpec;
  */
 public class ConceptConstants {
 
-	public static final ConceptSpec GENERATES_REL = new ConceptSpec("generates", "e22745a4-c59b-4ebe-b703-bd3fffaa4e80");
-	public static final ConceptSpec CREATES_MEMBERSHIP_TYPE = new ConceptSpec("creates membership type", "6873fb42-95b1-4835-994d-9745263393fb");
+	public static final ConceptSpec GENERATES_REL = new ConceptSpec("generates", "ca77a82c-ffa8-4dc1-8d31-6b137b4607ca");
+	public static final ConceptSpec CREATES_MEMBERSHIP_TYPE = new ConceptSpec("creates membership type", "adccc8cc-cfe0-4947-b2b2-2c493034ac3a");
 	public static final ConceptSpec SNOMED_IS_A = new ConceptSpec("Is a (attribute)", "c93a30b9-ba77-3adb-a9b8-4589c9f8fb25");
 
 }
