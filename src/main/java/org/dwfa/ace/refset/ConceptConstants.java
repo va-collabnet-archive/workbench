3579bfc364f9a0e93a5c1ad62ddd69f77fca98fa
tore <tore>
2008-03-05 00:14:12 +0000

    updated uuid to concept constant marked parent member

--- src/main/java/org/dwfa/ace/refset/ConceptConstants.java
+++ src/main/java/org/dwfa/ace/refset/ConceptConstants.java
@@ -12,5 +12,5 @@ public class ConceptConstants {
 	public static final ConceptSpec GENERATES_REL = new ConceptSpec("generates", "ca77a82c-ffa8-4dc1-8d31-6b137b4607ca");
 	public static final ConceptSpec CREATES_MEMBERSHIP_TYPE = new ConceptSpec("creates membership type", "adccc8cc-cfe0-4947-b2b2-2c493034ac3a");
 	public static final ConceptSpec SNOMED_IS_A = new ConceptSpec("Is a (attribute)", "c93a30b9-ba77-3adb-a9b8-4589c9f8fb25");
-	public static final ConceptSpec PARENT_MARKER = new ConceptSpec("marked parent member", "818297c2-34b7-4cc1-81d0-c003c8fe9477");
+	public static final ConceptSpec PARENT_MARKER = new ConceptSpec("marked parent member", "125f3d04-de17-490e-afec-1431c2a39e29");
 }
