41e778681db1eba79ca72e7259458df9369b064a
dionm <dionm>
2008-03-26 05:05:04 +0000

    removed the hard coded uuid and replaced with a concept constant

--- src/main/java/org/dwfa/ace/refset/ConceptConstants.java
+++ src/main/java/org/dwfa/ace/refset/ConceptConstants.java
@@ -9,6 +9,7 @@ import org.dwfa.tapi.spec.ConceptSpec;
  */
 public class ConceptConstants {
 
+	public static final ConceptSpec AU_CT_EDIT_PATH = new ConceptSpec("au-ct edit path", "c65b08ce-8512-52fa-be06-0844bd7310d6");
 	public static final ConceptSpec GENERATES_REL = new ConceptSpec("generates", "ca77a82c-ffa8-4dc1-8d31-6b137b4607ca");
 	public static final ConceptSpec CREATES_MEMBERSHIP_TYPE = new ConceptSpec("creates membership type", "adccc8cc-cfe0-4947-b2b2-2c493034ac3a");
 	public static final ConceptSpec SNOMED_IS_A = new ConceptSpec("Is a (attribute)", "c93a30b9-ba77-3adb-a9b8-4589c9f8fb25");
--- src/main/java/org/dwfa/ace/task/refset/RunMemberRefsetCalculation.java
+++ src/main/java/org/dwfa/ace/task/refset/RunMemberRefsetCalculation.java
@@ -9,6 +9,7 @@ import java.util.UUID;
 
 import org.dwfa.ace.api.I_TermFactory;
 import org.dwfa.ace.api.LocalVersionedTerminology;
+import org.dwfa.ace.refset.ConceptConstants;
 import org.dwfa.ace.refset.MemberRefsetCalculator;
 import org.dwfa.bpa.process.Condition;
 import org.dwfa.bpa.process.I_EncodeBusinessProcess;
@@ -53,8 +54,10 @@ public class RunMemberRefsetCalculation extends AbstractTask {
 
 			MemberRefsetCalculator cal = new MemberRefsetCalculator();
 			System.out.println("Setting outputdirectory to: "+ new File(".").getAbsolutePath());
-			cal.setOutputDirectory(new File("."));			
-			cal.setPathConcept(tf.getConcept(new UUID[]{UUID.fromString("c65b08ce-8512-52fa-be06-0844bd7310d6")}));
+			cal.setOutputDirectory(new File("."));
+			
+			int nid = ConceptConstants.AU_CT_EDIT_PATH.localize().getNid();
+			cal.setPathConcept(tf.getConcept(nid));
 			cal.run();
 			
 		} catch (Exception e) {
