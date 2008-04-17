062af929fda6d36185923c79ec80a88f4416d2cd
dionm <dionm>
2008-04-17 13:47:43 +0000

    changed to allow validation settings to be modified

--- src/main/java/org/dwfa/mojo/BinaryChangeSetReadAll.java
+++ src/main/java/org/dwfa/mojo/BinaryChangeSetReadAll.java
@@ -8,6 +8,7 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.dwfa.ace.api.I_TermFactory;
 import org.dwfa.ace.api.LocalVersionedTerminology;
+import org.dwfa.ace.api.cs.ComponentValidator;
 import org.dwfa.ace.task.cs.ImportAllChangeSets;
 import org.dwfa.bpa.process.TaskFailedException;
 import org.dwfa.maven.MojoUtil;
@@ -29,6 +30,13 @@ public class BinaryChangeSetReadAll extends AbstractMojo {
      * @parameter default-value="${project.build.directory}/generated-resources/changesets/"
      */
     String changeSetDir;
+    
+    /**
+     * List of validators to use when validating change sets if validate = true
+     * 
+     * @parameter
+     */
+    private String[] validators = new String[]{ComponentValidator.class.getName()};
 
     /**
      * Whether to validate the change set first or not. Default value is true; 
@@ -50,6 +58,7 @@ public class BinaryChangeSetReadAll extends AbstractMojo {
         
         ImportAllChangeSets importAllChangeSetsTask = new ImportAllChangeSets();
         importAllChangeSetsTask.setValidateChangeSets(validate);
+        importAllChangeSetsTask.setValidators(validators);
         importAllChangeSetsTask.setRootDirStr(changeSetDir);
         try {
         	importAllChangeSetsTask.importAllChangeSets(new LoggerAdaptor(getLog()));
