5124c881e9dd2d6ce82f68d16671e3e6f12854df
kec <kec>
2008-05-31 20:29:16 +0000

    Added compress function to db.

--- src/main/java/org/dwfa/ace/api/I_ImplementTermFactory.java
+++ src/main/java/org/dwfa/ace/api/I_ImplementTermFactory.java
@@ -6,6 +6,12 @@ public interface I_ImplementTermFactory extends I_TermFactory {
 	public void setup(Object envHome, boolean readOnly, Long cacheSize) throws IOException;
 	public void setup(Object envHome, boolean readOnly, Long cacheSize, DatabaseSetupConfig databaseSetupConfig) throws IOException;
 	public void checkpoint() throws IOException;
+	/**
+	 * 
+	 * @param minUtilization a number between 0 and 90 reflecting the desired minimum percent utilization of the log files. 
+	 * @throws IOException
+	 */
+	public void compress(int minUtilization) throws IOException;
 	public void close() throws IOException;
     public I_ConfigAceDb newAceDbConfig();
 }
