ef3cdefd603bcd1dfca027b983cd7d064a58a977
mzhang <mzhang>
2008-06-24 02:56:09 +0000

    add Classify current editing

---
+++ src/main/java/org/dwfa/ace/task/classify/ClassifyCurrentEditingBeanInfo.java
@@ -0,0 +1,21 @@
+package org.dwfa.ace.task.classify;
+
+import java.beans.BeanDescriptor;
+import java.beans.PropertyDescriptor;
+import java.beans.SimpleBeanInfo;
+
+public class ClassifyCurrentEditingBeanInfo extends SimpleBeanInfo {
+    
+    public PropertyDescriptor[] getPropertyDescriptors() {
+        return new PropertyDescriptor[0];
+    }
+    /**
+     * @see java.beans.BeanInfo#getBeanDescriptor()
+     */
+    public BeanDescriptor getBeanDescriptor() {
+        BeanDescriptor bd = new BeanDescriptor(ClassifyCurrentEditing.class);
+        bd.setDisplayName("<html><font color='green'><center>Classify Current editing");
+        return bd;
+    }
+
+}
