80c062127a17168a8d6f094a8d70a8e6e40d6a61
chrissy <chrissy>
2008-08-11 06:47:21 +0000

    added handler for concept-double refsets

--- src/main/java/org/dwfa/mojo/refset/RefsetType.java
+++ src/main/java/org/dwfa/mojo/refset/RefsetType.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package org.dwfa.mojo.refset;
 
@@ -14,7 +14,7 @@ import org.dwfa.ace.api.ebr.I_ThinExtByRefPartConcept;
 import org.dwfa.ace.api.ebr.I_ThinExtByRefPartConceptInt;
 import org.dwfa.ace.api.ebr.I_ThinExtByRefPartInteger;
 import org.dwfa.ace.api.ebr.I_ThinExtByRefPartString;
-import org.dwfa.ace.api.ebr.I_ThinExtByRefTuple;
+import org.dwfa.ace.api.ebr.I_ThinExtByRefPartMeasurement;
 import org.dwfa.cement.RefsetAuxiliary.Concept;
 import org.dwfa.mojo.file.FileHandler;
 import org.dwfa.mojo.refset.writers.BooleanRefsetHandler;
@@ -23,128 +23,135 @@ import org.dwfa.mojo.refset.writers.ConceptRefsetHandler;
 import org.dwfa.mojo.refset.writers.IntegerRefsetHandler;
 import org.dwfa.mojo.refset.writers.MemberRefsetHandler;
 import org.dwfa.mojo.refset.writers.StringRefsetHandler;
+import org.dwfa.mojo.refset.writers.ConceptDoubleRefsetHandler;
 
-enum RefsetType {	
-	
-	CONCEPT(
-		Concept.CONCEPT_EXTENSION,  
-		ConceptRefsetHandler.class, 
-		".concept.refset"
-	),
-	
-	INTEGER(
-		Concept.INT_EXTENSION, 
-		IntegerRefsetHandler.class, 
-		".integer.refset"
-	),
-	
-	STRING(
-		Concept.STRING_EXTENSION, 
-		StringRefsetHandler.class, 
-		".string.refset"
-	),
-	
-	BOOLEAN(
-		Concept.BOOLEAN_EXTENSION,
-		BooleanRefsetHandler.class,
-		".boolean.refset"
-	),
-	
-	CONCEPT_INTEGER(
-		Concept.CONCEPT_INT_EXTENSION, 
-		ConceptIntegerRefsetHandler.class,
-		".concept.integer.refset"
-	);
-	
+enum RefsetType {
 
-	
-	private Class<? extends MemberRefsetHandler> refsetWriterClass;
-	private MemberRefsetHandler refsetWriter = null;
-	private Concept auxiliaryConcept;
-	private String fileExtension = null;
-	
-	RefsetType(Concept auxiliaryConcept, Class<? extends MemberRefsetHandler> refsetWriterClass, String fileExtension) {
-		this.auxiliaryConcept = auxiliaryConcept;
-		this.refsetWriterClass = refsetWriterClass;		
-		this.fileExtension = fileExtension;
-	}
-	
-	public static RefsetType findByExtension(I_ThinExtByRefPart part) {
-		if (part instanceof I_ThinExtByRefPartBoolean) {
-			return BOOLEAN;
-		} else if (part instanceof I_ThinExtByRefPartConcept) {
-			return CONCEPT;
-		} else if (part instanceof I_ThinExtByRefPartConceptInt) {
-			return CONCEPT_INTEGER;
-		} else if (part instanceof I_ThinExtByRefPartInteger) {
-			return INTEGER;
-		} else if (part instanceof I_ThinExtByRefPartString) {
-			return STRING;
-		}
-		
-		throw new EnumConstantNotPresentException(RefsetType.class, "No refset type for the class " 
-				+ part.getClass() + " exists. Full object is " + part);
-	}
+    CONCEPT(
+        Concept.CONCEPT_EXTENSION,
+        ConceptRefsetHandler.class,
+        ".concept.refset"
+    ),
 
-	/**
-	 * @param filename 
-	 * @return The appropriate refset type based on the file name. 
-	 * @throws EnumConstantNotPresentException where the filename cannot be matched
-	 */
-	public static RefsetType findByFilename(String filename) {
-		for (RefsetType t : RefsetType.values()) {
-			if (t.matches(filename)) {
-				return t;
-			}
-		} 
-		throw new EnumConstantNotPresentException(RefsetType.class, "No refset type for " + filename + " exists");
-	}	
-	
-	private boolean matches(String filename) {
-		return (filename.toLowerCase().endsWith(this.fileExtension));
-	}	
-	
-	public MemberRefsetHandler getRefsetHandler() throws InstantiationException, IllegalAccessException {
-		if (refsetWriter == null) {
-			refsetWriter = refsetWriterClass.newInstance();
-		}
-	
-		return refsetWriter;
-	}
+    INTEGER(
+        Concept.INT_EXTENSION,
+        IntegerRefsetHandler.class,
+        ".integer.refset"
+    ),
 
-	public Concept getAuxiliaryConcept() {
-		return this.auxiliaryConcept;
-	}
-	
-	public String getFileExtension() {
-		return fileExtension;
-	}
+    STRING(
+        Concept.STRING_EXTENSION,
+        StringRefsetHandler.class,
+        ".string.refset"
+    ),
 
-	public static FilenameFilter getFileNameFilter() {
-		
-		return new FilenameFilter() {
-			List<String> filenameExtensions;
-			
-			public boolean accept(File dir, String name) {
-				if (filenameExtensions == null) {
-					filenameExtensions = new ArrayList<String>();
-					for (RefsetType refsetType : RefsetType.values()) {
-						filenameExtensions.add(refsetType.getFileExtension());
-					}
-				}
-				
-				for (String extension : filenameExtensions) {
-					if (name.endsWith(extension)) {
-						return true;
-					}
-				}
-				return false;
-			}
-			
-		};
-	}
+    BOOLEAN(
+        Concept.BOOLEAN_EXTENSION,
+        BooleanRefsetHandler.class,
+        ".boolean.refset"
+    ),
 
-	public static FileHandler<I_ThinExtByRefPart> getHandlerForFile(File file) throws InstantiationException, IllegalAccessException {
-		return findByFilename(file.getName()).getRefsetHandler();
-	}
+    CONCEPT_INTEGER(
+        Concept.CONCEPT_INT_EXTENSION,
+        ConceptIntegerRefsetHandler.class,
+        ".concept.integer.refset"
+    ),
+
+    CONCEPT_DOUBLE(
+            Concept.MEASUREMENT_EXTENSION,
+            ConceptDoubleRefsetHandler.class,
+            ".concept.double.refset"
+    );
+
+    private Class<? extends MemberRefsetHandler> refsetWriterClass;
+    private MemberRefsetHandler refsetWriter = null;
+    private Concept auxiliaryConcept;
+    private String fileExtension = null;
+
+    RefsetType(Concept auxiliaryConcept, Class<? extends MemberRefsetHandler> refsetWriterClass, String fileExtension) {
+        this.auxiliaryConcept = auxiliaryConcept;
+        this.refsetWriterClass = refsetWriterClass;
+        this.fileExtension = fileExtension;
+    }
+
+    public static RefsetType findByExtension(I_ThinExtByRefPart part) {
+        if (part instanceof I_ThinExtByRefPartBoolean) {
+            return BOOLEAN;
+        } else if (part instanceof I_ThinExtByRefPartConcept) {
+            return CONCEPT;
+        } else if (part instanceof I_ThinExtByRefPartConceptInt) {
+            return CONCEPT_INTEGER;
+        } else if (part instanceof I_ThinExtByRefPartInteger) {
+            return INTEGER;
+        } else if (part instanceof I_ThinExtByRefPartString) {
+            return STRING;
+        } else if (part instanceof I_ThinExtByRefPartMeasurement) {
+            return CONCEPT_DOUBLE;
+        }
+
+        throw new EnumConstantNotPresentException(RefsetType.class, "No refset type for the class "
+                + part.getClass() + " exists. Full object is " + part);
+    }
+
+    /**
+     * @param filename
+     * @return The appropriate refset type based on the file name.
+     * @throws EnumConstantNotPresentException where the filename cannot be matched
+     */
+    public static RefsetType findByFilename(String filename) {
+        for (RefsetType t : RefsetType.values()) {
+            if (t.matches(filename)) {
+                return t;
+            }
+        }
+        throw new EnumConstantNotPresentException(RefsetType.class, "No refset type for " + filename + " exists");
+    }
+
+    private boolean matches(String filename) {
+        return (filename.toLowerCase().endsWith(this.fileExtension));
+    }
+
+    public MemberRefsetHandler getRefsetHandler() throws InstantiationException, IllegalAccessException {
+        if (refsetWriter == null) {
+            refsetWriter = refsetWriterClass.newInstance();
+        }
+
+        return refsetWriter;
+    }
+
+    public Concept getAuxiliaryConcept() {
+        return this.auxiliaryConcept;
+    }
+
+    public String getFileExtension() {
+        return fileExtension;
+    }
+
+    public static FilenameFilter getFileNameFilter() {
+
+        return new FilenameFilter() {
+            List<String> filenameExtensions;
+
+            public boolean accept(File dir, String name) {
+                if (filenameExtensions == null) {
+                    filenameExtensions = new ArrayList<String>();
+                    for (RefsetType refsetType : RefsetType.values()) {
+                        filenameExtensions.add(refsetType.getFileExtension());
+                    }
+                }
+
+                for (String extension : filenameExtensions) {
+                    if (name.endsWith(extension)) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+        };
+    }
+
+    public static FileHandler<I_ThinExtByRefPart> getHandlerForFile(File file) throws InstantiationException, IllegalAccessException {
+        return findByFilename(file.getName()).getRefsetHandler();
+    }
 }
---
+++ src/main/java/org/dwfa/mojo/refset/writers/ConceptDoubleRefsetHandler.java
@@ -0,0 +1,58 @@
+package org.dwfa.mojo.refset.writers;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import org.dwfa.ace.api.I_TermFactory;
+import org.dwfa.ace.api.ebr.I_ThinExtByRefPart;
+import org.dwfa.ace.api.ebr.I_ThinExtByRefPartMeasurement;
+import org.dwfa.ace.api.ebr.I_ThinExtByRefTuple;
+import org.dwfa.ace.api.ebr.I_ThinExtByRefVersioned;
+import org.dwfa.cement.RefsetAuxiliary;
+import org.dwfa.tapi.TerminologyException;
+
+public class ConceptDoubleRefsetHandler extends MemberRefsetHandler {
+    @Override
+    public String formatRefsetLine(I_TermFactory tf, I_ThinExtByRefTuple tuple, boolean sctid) throws TerminologyException, IOException {
+        I_ThinExtByRefPartMeasurement measurementPart = (I_ThinExtByRefPartMeasurement) tuple.getPart();
+
+        return super.formatRefsetLine(tf, tuple, sctid) + MemberRefsetHandler.FILE_DELIMITER
+                    + toId(tf, measurementPart.getUnitsOfMeasureId(), sctid) + MemberRefsetHandler.FILE_DELIMITER
+                    + measurementPart.getMeasurementValue();
+    }
+
+    @Override
+    public String getHeaderLine() {
+        return super.getHeaderLine() + MemberRefsetHandler.FILE_DELIMITER + "UNITS_OF_MEASURE" + MemberRefsetHandler.FILE_DELIMITER + "MEASUREMENT_VALUE";
+    }
+
+    @Override
+    protected I_ThinExtByRefPart processLine(String line) {
+        I_ThinExtByRefPartMeasurement part;
+        try {
+
+            I_ThinExtByRefVersioned versioned = getExtensionVersioned(line, RefsetAuxiliary.Concept.MEASUREMENT_EXTENSION);
+
+            part = getTermFactory().newMeasurementExtensionPart();
+            setGenericExtensionPartFields(part);
+
+            String conceptValue = getNextCurrentRowToken();
+            part.setUnitsOfMeasureId(getNid(UUID.fromString(conceptValue)));
+
+            part.setMeasurementValue(Integer.parseInt(getNextCurrentRowToken()));
+
+            versioned.addVersion(part);
+
+            if (isTransactional()) {
+                getTermFactory().addUncommitted(versioned);
+            } else {
+                getTermFactory().getDirectInterface().writeExt(versioned);
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException("Error occurred processing file " + sourceFile, e);
+        }
+
+        return part;
+    }
+}
