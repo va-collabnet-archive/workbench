63ab5398a277b5b57a325f6ec2a004bc6fce67a7
mzhang <mzhang>
2008-10-12 23:19:46 +0000

    update to support equvilance checking with snorocket

--- src/main/java/org/dwfa/ace/task/classify/BuildClassificationResultString.java
+++ src/main/java/org/dwfa/ace/task/classify/BuildClassificationResultString.java
@@ -3,6 +3,7 @@ package org.dwfa.ace.task.classify;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 import java.util.Collection;
 
 import org.dwfa.ace.api.I_GetConceptData;
@@ -16,6 +17,7 @@ import org.dwfa.bpa.process.I_Work;
 import org.dwfa.bpa.process.TaskFailedException;
 import org.dwfa.bpa.tasks.AbstractTask;
 import org.dwfa.cement.ArchitectonicAuxiliary;
+import org.dwfa.cement.SNOMED;
 
 import org.dwfa.tapi.TerminologyException;
 import org.dwfa.util.bean.BeanList;
@@ -38,6 +40,22 @@ public class BuildClassificationResultString extends AbstractTask {
 	private String stringName = ProcessKey.ClassificationResultString
 			.getAttachmentKey();
 
+	class ClassifyResult
+	{
+		int concept1;
+		int reltype;
+		int concept2;
+		public ClassifyResult(int c1,int rel, int c2) {
+			// TODO Auto-generated constructor stub
+			concept1 = c1;
+			concept2 = c2;
+			reltype = rel;
+		}
+		
+	}
+	
+	
+	
 	private void writeObject(ObjectOutputStream out) throws IOException {
 		out.writeInt(dataVersion);
 		out.writeObject(stringName);
@@ -64,6 +82,8 @@ public class BuildClassificationResultString extends AbstractTask {
 			throws TaskFailedException {
 
 		try {
+			
+			final ArrayList<ClassifyResult> CR = new ArrayList<ClassifyResult>();
 			final StringBuilder sb = new StringBuilder();
 			sb.append("<tr><th>").append("CONCEPT 1").append("</th><th>")
 					.append("RELATIONSHIPS").append("</th><th>").append(
@@ -73,12 +93,39 @@ public class BuildClassificationResultString extends AbstractTask {
 			I_SnorocketFactory rocket = (I_SnorocketFactory) process
 					.readAttachement(ProcessKey.SNOROCKET.getAttachmentKey());
 
-			rocket.getResults(new ProcessResults(worker, sb));
+			rocket.getResults(new ProcessResults(worker, sb,CR));
 
+			
+			
 			worker.getLogger().info(sb.toString());
 
 			String returnString = "<html><table align=\"center\" border=\"1\">"
 					+ sb.toString() + "</table></html>";
+			
+			//analysis result 
+			
+			I_TermFactory tf = LocalVersionedTerminology.get();
+			int isa = tf.uuidToNative(SNOMED.Concept.IS_A.getUids());
+			String equalSting = "";
+			for (ClassifyResult c: CR)
+			{
+				for (ClassifyResult d: CR)
+				{
+					if(c.concept1 == d.concept2 && c.reltype == isa && d.reltype==isa
+							&& c.concept2  == d.concept1)
+					
+					{
+						equalSting += tf.getConcept(c.concept1).toString() + "---equal--- " +
+						 				tf.getConcept(c.concept2) + "<p>";
+					}
+				}
+			}
+			
+			if (equalSting.length()>1)
+			{
+				returnString = "<html>"
+					+ equalSting + "</html>";
+			}
 			process.setProperty(stringName, returnString);
 			return Condition.CONTINUE;
 		} catch (IllegalArgumentException e) {
@@ -108,17 +155,19 @@ public class BuildClassificationResultString extends AbstractTask {
 
 		final private I_Work worker;
 		final private StringBuilder sb;
-
+		final private ArrayList<ClassifyResult> cr;
+		
 		private I_TermFactory termFactory = LocalVersionedTerminology.get();
 		public I_GetConceptData relCharacteristic;
 		public I_GetConceptData relRefinability;
 		public I_GetConceptData relStatus;
 		private int returnedRelCount = 0;
 
-		public ProcessResults(final I_Work worker, StringBuilder sb)
+		public ProcessResults(final I_Work worker, StringBuilder sb, ArrayList<ClassifyResult> cr)
 				throws Exception {
 			this.worker = worker;
 			this.sb = sb;
+			this.cr = cr;
 
 			relCharacteristic = termFactory
 					.getConcept(ArchitectonicAuxiliary.Concept.DEFINING_CHARACTERISTIC
@@ -139,7 +188,9 @@ public class BuildClassificationResultString extends AbstractTask {
 
 		public void addRelationship(int conceptId1, int roleId, int conceptId2,
 				int group) {
+			cr.add(new ClassifyResult(conceptId1,roleId,conceptId2));
 			try {
+				
 				returnedRelCount++;
 				final I_GetConceptData relSource = termFactory
 						.getConcept(conceptId1);
