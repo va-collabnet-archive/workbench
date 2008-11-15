44a63da370d3e71609819f4c4241b0673673421b
emays <emays>
2008-11-15 03:23:56 +0000

    Initial implementation

---
+++ src/main/java/org/dwfa/ace/task/commit/TestForFullySpecifiedNameBeanInfo.java
@@ -0,0 +1,24 @@
+package org.dwfa.ace.task.commit;
+
+import java.beans.BeanDescriptor;
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
+import java.beans.SimpleBeanInfo;
+
+import org.dwfa.bpa.tasks.editor.CheckboxEditor;
+import org.dwfa.bpa.tasks.editor.PropertyNameLabelEditor;
+
+public class TestForFullySpecifiedNameBeanInfo extends
+		TestForUneditableMembersetBeanInfo {
+
+	/**
+	 * @see java.beans.BeanInfo#getBeanDescriptor()
+	 */
+	public BeanDescriptor getBeanDescriptor() {
+		BeanDescriptor bd = new BeanDescriptor(TestForFullySpecifiedName.class);
+		bd
+				.setDisplayName("<html><font color='green'><center>Test For<br>Fully Specified Name");
+		return bd;
+	}
+
+}
---
+++ src/main/java/org/dwfa/ace/task/commit/TestForIsaBeanInfo.java
@@ -0,0 +1,22 @@
+package org.dwfa.ace.task.commit;
+
+import java.beans.BeanDescriptor;
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
+import java.beans.SimpleBeanInfo;
+
+import org.dwfa.bpa.tasks.editor.CheckboxEditor;
+import org.dwfa.bpa.tasks.editor.PropertyNameLabelEditor;
+
+public class TestForIsaBeanInfo extends TestForUneditableMembersetBeanInfo {
+
+	/**
+	 * @see java.beans.BeanInfo#getBeanDescriptor()
+	 */
+	public BeanDescriptor getBeanDescriptor() {
+		BeanDescriptor bd = new BeanDescriptor(TestForIsa.class);
+		bd.setDisplayName("<html><font color='green'><center>Test For<br>IS_A");
+		return bd;
+	}
+
+}
