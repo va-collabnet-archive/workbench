d9fcef26128c1c9fd5ccfdda3a1523d08b1336c3
chrissy <chrissy>
2008-11-24 04:59:59 +0000

    TCP-168: modified TestForIsa.java to check is-a's exist before they're used

--- src/main/java/org/dwfa/ace/task/commit/TestForIsa.java
+++ src/main/java/org/dwfa/ace/task/commit/TestForIsa.java
@@ -22,60 +22,70 @@ import org.dwfa.util.bean.BeanType;
 import org.dwfa.util.bean.Spec;
 
 @BeanList(specs = {
-		@Spec(directory = "tasks/ace/commit", type = BeanType.TASK_BEAN),
-		@Spec(directory = "plugins/precommit", type = BeanType.TASK_BEAN),
-		@Spec(directory = "plugins/commit", type = BeanType.TASK_BEAN) })
+        @Spec(directory = "tasks/ace/commit", type = BeanType.TASK_BEAN),
+        @Spec(directory = "plugins/precommit", type = BeanType.TASK_BEAN),
+        @Spec(directory = "plugins/commit", type = BeanType.TASK_BEAN) })
 public class TestForIsa extends AbstractConceptTest {
 
-	private static final long serialVersionUID = 1;
-	private static final int dataVersion = 1;
+    private static final long serialVersionUID = 1;
+    private static final int dataVersion = 1;
 
-	private void writeObject(ObjectOutputStream out) throws IOException {
-		out.writeInt(dataVersion);
-	}
+    private void writeObject(ObjectOutputStream out) throws IOException {
+        out.writeInt(dataVersion);
+    }
 
-	private void readObject(java.io.ObjectInputStream in) throws IOException,
-			ClassNotFoundException {
-		int objDataVersion = in.readInt();
-		if (objDataVersion == 1) {
-			//
-		} else {
-			throw new IOException("Can't handle dataversion: " + objDataVersion);
-		}
-	}
+    private void readObject(java.io.ObjectInputStream in) throws IOException,
+            ClassNotFoundException {
+        int objDataVersion = in.readInt();
+        if (objDataVersion == 1) {
+            //
+        } else {
+            throw new IOException("Can't handle dataversion: " + objDataVersion);
+        }
+    }
 
-	@Override
-	public List<AlertToDataConstraintFailure> test(I_GetConceptData concept,
-			boolean forCommit) throws TaskFailedException {
-		try {
-			ArrayList<AlertToDataConstraintFailure> alertList = new ArrayList<AlertToDataConstraintFailure>();
-			I_TermFactory termFactory = LocalVersionedTerminology.get();
-			I_ConfigAceFrame activeProfile = termFactory
-					.getActiveAceFrameConfig();
-			Set<I_Path> editingPaths = new HashSet<I_Path>(activeProfile
-					.getEditingPathSet());
-			I_GetConceptData is_a = termFactory.getConcept(SNOMED.Concept.IS_A
-					.getUids());
-			I_GetConceptData is_a_rel_aux = termFactory
-					.getConcept(ArchitectonicAuxiliary.Concept.IS_A_REL
-							.getUids());
-			Set<I_Position> positions = new HashSet<I_Position>();
-			for (I_Path path : editingPaths) {
-				positions.add(termFactory.newPosition(path, Integer.MAX_VALUE));
-			}
-			for (I_RelTuple rel : concept.getSourceRelTuples(activeProfile
-					.getAllowedStatus(), null, positions, true)) {
-				if (rel.getRelTypeId() == is_a.getConceptId()
-						|| rel.getRelTypeId() == is_a_rel_aux.getConceptId())
-					return alertList;
-			}
-			alertList.add(new AlertToDataConstraintFailure(
-					AlertToDataConstraintFailure.ALERT_TYPE.WARNING,
-					"<html>No IS_A relationship", concept));
-			return alertList;
-		} catch (Exception e) {
-			throw new TaskFailedException(e);
-		}
-	}
+    @Override
+    public List<AlertToDataConstraintFailure> test(I_GetConceptData concept,
+            boolean forCommit) throws TaskFailedException {
+        try {
+            ArrayList<AlertToDataConstraintFailure> alertList = new ArrayList<AlertToDataConstraintFailure>();
+            I_TermFactory termFactory = LocalVersionedTerminology.get();
+            I_ConfigAceFrame activeProfile = termFactory
+                    .getActiveAceFrameConfig();
+            Set<I_Path> editingPaths = new HashSet<I_Path>(activeProfile
+                    .getEditingPathSet());
+            I_GetConceptData is_a = null;
+            I_GetConceptData is_a_rel_aux = null;
+
+            // check that the SNOMED is-a exists in the current database before using it
+            if (termFactory.hasId(SNOMED.Concept.IS_A.getUids().iterator().next())) {
+                is_a = termFactory.getConcept(SNOMED.Concept.IS_A.getUids());
+            }
+
+            // check that the Architectonic is-a exists before using it
+            if (termFactory.hasId(ArchitectonicAuxiliary.Concept.IS_A_REL
+                    .getUids().iterator().next())) {
+                 is_a_rel_aux = termFactory.getConcept(ArchitectonicAuxiliary.Concept.IS_A_REL
+                         .getUids());
+            }
+
+            Set<I_Position> positions = new HashSet<I_Position>();
+            for (I_Path path : editingPaths) {
+                positions.add(termFactory.newPosition(path, Integer.MAX_VALUE));
+            }
+            for (I_RelTuple rel : concept.getSourceRelTuples(activeProfile
+                    .getAllowedStatus(), null, positions, true)) {
+                if ((is_a != null && rel.getRelTypeId() == is_a.getConceptId())
+                        || (is_a_rel_aux != null && rel.getRelTypeId() == is_a_rel_aux.getConceptId()))
+                    return alertList;
+            }
+            alertList.add(new AlertToDataConstraintFailure(
+                    AlertToDataConstraintFailure.ALERT_TYPE.WARNING,
+                    "<html>No IS_A relationship", concept));
+            return alertList;
+        } catch (Exception e) {
+            throw new TaskFailedException(e);
+        }
+    }
 
 }
