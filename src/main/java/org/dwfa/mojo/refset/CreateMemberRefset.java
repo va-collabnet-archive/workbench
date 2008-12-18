cc08c89fe11aaea2cfe11efe4a513f537d94130b
scrow <scrow>
2008-12-18 14:37:42 +0000

    Configured dev to create member refsets and to import cmsrcscmscmscmsmcmsmcsrsmcsm files

--- src/main/java/org/dwfa/mojo/refset/CreateMemberRefset.java
+++ src/main/java/org/dwfa/mojo/refset/CreateMemberRefset.java
@@ -1,17 +1,21 @@
 package org.dwfa.mojo.refset;
 
 import java.io.File;
-import java.util.ArrayList;
+import java.util.UUID;
 import java.util.List;
+import java.util.ArrayList;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.dwfa.ace.refset.MemberRefsetCalculator;
+import org.dwfa.ace.api.LocalVersionedTerminology;
+import org.dwfa.ace.api.I_TermFactory;
+import org.dwfa.ace.api.I_GetConceptData;
 import org.dwfa.mojo.ConceptDescriptor;
 
 /**
- * 
+ *
  * @author Tore Fjellheim
  *
  * @goal createMemberRefset
@@ -41,12 +45,18 @@ public class CreateMemberRefset extends AbstractMojo {
 	 * The number of items to add to the uncommitted list before committing
 	 */
 	private int commitSize = 1000;
-	
+
 	/**
 	 * @parameter
 	 * Use the direct non-transaction ACE API (defaults to false)
 	 */
 	private boolean useNonTxInterface = false;
+
+    /**
+	 * @parameter
+	 * The UUID for an alternate "Is a" relationship (defaults to null)
+	 */
+	private String altIsA = null;
 
 	/**
 	 * Output location for the change sets
@@ -55,35 +65,43 @@ public class CreateMemberRefset extends AbstractMojo {
 	 * @required
 	 */
 	private File changeSetOutputDirectory;
-	
+
+    protected I_TermFactory termFactory;
+
 	/**
 	 * Specification refsets to be processed.
 	 * @parameter
 	 */
 	ConceptDescriptor[] specRefsets = null;
-	
+
 	public void execute() throws MojoExecutionException, MojoFailureException {
-		try {
+		try {                 
+            I_GetConceptData altIsAConcept = null;
+            if (altIsA != null) {
+                termFactory = LocalVersionedTerminology.get();
+                altIsAConcept = termFactory.getConcept(new UUID[] { UUID.fromString(this.altIsA) });
+            }
 
 			MemberRefsetCalculator calc = new MemberRefsetCalculator();
-			
+
 			calc.setOutputDirectory(outputDirectory);
 			calc.setChangeSetOutputDirectory(changeSetOutputDirectory);
 			calc.setPathConcept(memberSetPathDescriptor.getVerifiedConcept());
 			calc.setValidateOnly(false);
 			calc.setCommitSize(commitSize);
 			calc.setUseNonTxInterface(useNonTxInterface);
+            calc.setAltIsA(altIsAConcept);
 
 			if (specRefsets != null) {
 		        List<Integer> allowedRefsets = new ArrayList<Integer>();
 		        for (ConceptDescriptor conceptDesc : specRefsets) {
 			        allowedRefsets.add(conceptDesc.getVerifiedConcept().getConceptId());
 		        }
-		        calc.setAllowedRefsets(allowedRefsets);			
+		        calc.setAllowedRefsets(allowedRefsets);
 			}
-			
+
 			calc.run();
-			
+
 		} catch (Exception e) {
 			throw new MojoExecutionException("member refset calculation failed with exception", e);
 		}
