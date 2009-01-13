37c1db449bfeb8ad4e8b0c0a852841d720792cda
kec <kec>
2009-01-13 20:13:21 +0000

    Added functions to lock and unlock files in subversion. Also added extensions to SubversionData to support multiple-read only mirrors.

--- src/main/java/org/dwfa/ace/api/I_HandleSubversion.java
+++ src/main/java/org/dwfa/ace/api/I_HandleSubversion.java
@@ -1,5 +1,6 @@
 package org.dwfa.ace.api;
 
+import java.io.File;
 import java.util.List;
 
 import org.tigris.subversion.javahl.PromptUserPassword3;
@@ -7,12 +8,15 @@ import org.tigris.subversion.javahl.PromptUserPassword3;
 public interface I_HandleSubversion {
 
     public void svnCheckout(SubversionData svd);
-    public void svnCommit(SubversionData svd);
-	public void svnCompleteRepoInfo(SubversionData svd);
     public void svnCleanup(SubversionData svd);
-    public void svnStatus(SubversionData svd);
+    public void svnCommit(SubversionData svd);
     public void svnPurge(SubversionData svd);
+    public void svnStatus(SubversionData svd);
     public void svnUpdate(SubversionData svd);
+    public boolean svnLock(SubversionData svd, File toLock);
+    public boolean svnUnlock(SubversionData svd, File toUnLock);
+
+    public void svnCompleteRepoInfo(SubversionData svd);
     public List<String> svnList(SubversionData svd);
 
 	public void svnCheckout(SubversionData svd, PromptUserPassword3 authenticator, boolean interactive);
@@ -21,5 +25,7 @@ public interface I_HandleSubversion {
 	public void svnPurge(SubversionData svd, PromptUserPassword3 authenticator, boolean interactive);
 	public void svnStatus(SubversionData svd, PromptUserPassword3 authenticator, boolean interactive);
 	public void svnUpdate(SubversionData svd, PromptUserPassword3 authenticator, boolean interactive);
+    public boolean svnLock(SubversionData svd, File toLock, PromptUserPassword3 authenticator, boolean interactive);
+    public boolean svnUnlock(SubversionData svd, File toUnlock, PromptUserPassword3 authenticator, boolean interactive);
 	
 }
--- src/main/java/org/dwfa/ace/api/SubversionData.java
+++ src/main/java/org/dwfa/ace/api/SubversionData.java
@@ -1,6 +1,8 @@
 package org.dwfa.ace.api;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 public class SubversionData implements Serializable {
 	/**
@@ -12,7 +14,27 @@ public class SubversionData implements Serializable {
 	private String workingCopyStr;
 	private String username;
 	private String password;
+	private Set<String> readOnlyUrlMirrors = null;
+	private String preferredReadRepository = null;
 	
+	
+	public String getPreferredReadRepository() {
+		if (preferredReadRepository == null) {
+			if (readOnlyUrlMirrors == null || readOnlyUrlMirrors.size() == 0) {
+				return repositoryUrlStr;
+			}
+			preferredReadRepository = readOnlyUrlMirrors.iterator().next();
+		}
+		return preferredReadRepository;
+	}
+
+	public void setPreferredReadRepository(String preferredReadRepository) {
+		this.preferredReadRepository = preferredReadRepository;
+	}
+
+	public SubversionData() {
+		super();
+	}
 	
 	public SubversionData(String repositoryUrlStr, String workingCopyStr) {
 		super();
@@ -20,10 +42,18 @@ public class SubversionData implements Serializable {
 		this.workingCopyStr = workingCopyStr;
 	}
 	
-	public SubversionData() {
-		super();
+	public Set<String> getReadOnlyUrlMirrors() {
+		if (readOnlyUrlMirrors == null) {
+			readOnlyUrlMirrors = new HashSet<String>();
+			readOnlyUrlMirrors.add(repositoryUrlStr);
+		}
+		return readOnlyUrlMirrors;
 	}
-	
+
+	public void addReadOnlyUrlStr(String readOnlyUrlStr) {
+		getReadOnlyUrlMirrors().add(readOnlyUrlStr);
+	}
+
 	public String getRepositoryUrlStr() {
 		return repositoryUrlStr;
 	}
