814994ad0b0cf310ee3a5e3ec59acaa42f9659ae
kec <kec>
2009-01-16 01:43:26 +0000

    Added a copy directory routine that will copy invisibles.

---
+++ dev/src/main/java/org/dwfa/maven/CopyDirectory.java
@@ -0,0 +1,62 @@
+package org.dwfa.maven;
+
+import java.io.File;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugin.logging.Log;
+import org.dwfa.util.io.FileIO;
+
+/**
+ * Goal which copies a directory to the specified location (including hidden files).
+ * 
+ * @goal copy-dir
+ * @requiresDependencyResolution compile
+ * 
+ */
+
+public class CopyDirectory extends AbstractMojo {
+    /**
+     * Location of the build directory.
+     * 
+     * @parameter
+     * @required
+     */
+    private File inputDirectory;
+    
+    /**
+     * Location of the build directory.
+     * 
+     * @parameter
+     * @required
+     */
+    private File outputDirectory;
+
+    /**
+     * Location of the build directory.
+     * 
+     * @parameter
+     * @required
+     */
+    private Boolean copyInvisibles;
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        try {
+            Log l = getLog();
+            l.info("Now executing ProcessProjectDirectories");
+
+            // calculate the SHA-1 hashcode for this mojo based on input
+            if (MojoUtil.alreadyRun(l, inputDirectory.getAbsolutePath() + outputDirectory.getAbsolutePath())) {
+                return;
+            }
+            FileIO.recursiveCopy(inputDirectory, outputDirectory, copyInvisibles);
+
+        } catch (Exception e) {
+            throw new MojoExecutionException(e.getMessage(), e);
+        }
+
+    }
+
+
+}
