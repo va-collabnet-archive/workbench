47e972152e6c5aee2c446a79c8ae04b70b57e995
kec <kec>
2009-02-02 20:57:26 +0000

    modified vodb-commit to only execute one per plugin execution id, and also to throw a proper exception when the vodb is not initialized.

--- src/main/java/org/dwfa/mojo/VodbCommit.java
+++ src/main/java/org/dwfa/mojo/VodbCommit.java
@@ -1,10 +1,15 @@
 package org.dwfa.mojo;
 
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
 import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecution;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.dwfa.ace.api.I_TermFactory;
 import org.dwfa.ace.api.LocalVersionedTerminology;
+import org.dwfa.maven.MojoUtil;
 
 /**
  * 
@@ -27,20 +32,39 @@ public class VodbCommit  extends AbstractMojo {
      */
     @SuppressWarnings("unused")
     private String outputDirectory;
+    
+    /**
+     * @parameter expression="$mojoExecution}"
+     */
+    MojoExecution execution;
      
 	public void execute() throws MojoExecutionException, MojoFailureException {
+		try {
+			if (MojoUtil.alreadyRun(getLog(), this.getClass().getCanonicalName() + execution.getExecutionId())) {
+				return;
+			}
+		} catch (NoSuchAlgorithmException e) {
+			throw new MojoExecutionException(e.getLocalizedMessage(), e);
+		} catch (IOException e) {
+			throw new MojoExecutionException(e.getLocalizedMessage(), e);
+		} 
 		I_TermFactory termFactory = LocalVersionedTerminology.get();
-        getLog().info("commiting: " + termFactory);        
-        if (termFactory.getUncommitted() != null 
-        		&& termFactory.getUncommitted().size() > 0) {
-            try {
+        getLog().info("commiting (id: " + execution.getExecutionId() + "): " + termFactory);  
+        if (termFactory != null) {
+            if (termFactory.getUncommitted() != null 
+            		&& termFactory.getUncommitted().size() > 0) {
+                try {
 
-                termFactory.commit();
-            } catch (Exception e) {
-                throw new MojoExecutionException(e.getLocalizedMessage(), e);
-            }       
+                    termFactory.commit();
+                } catch (Exception e) {
+                    throw new MojoExecutionException(e.getLocalizedMessage(), e);
+                }       
+            } else {
+            	getLog().info("termfactory.getUncommitted().size() = " + termFactory.getUncommitted().size());
+            }
         } else {
-        	getLog().info("termfactory.getUncommitted().size() = " + termFactory.getUncommitted().size());
+            Exception ex = new Exception("Attempting commit with null term factory (id: " + execution.getExecutionId() + "): " + termFactory);
+            throw new MojoExecutionException(ex.getLocalizedMessage(), ex);
         }
         
 	}
