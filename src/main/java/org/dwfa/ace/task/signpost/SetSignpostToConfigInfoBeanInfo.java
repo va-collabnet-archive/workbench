06ae0e224930b5372e20625650cc6053ced1e3e7
kec <kec>
2009-03-15 02:11:16 +0000

    artf1085: allow the user to generate configuration information to send to support.

--- src/main/java/org/dwfa/ace/task/signpost/SetSignpostToConfigInfoBeanInfo.java
+++ src/main/java/org/dwfa/ace/task/signpost/SetSignpostToConfigInfoBeanInfo.java
@@ -4,7 +4,7 @@ import java.beans.BeanDescriptor;
 import java.beans.PropertyDescriptor;
 import java.beans.SimpleBeanInfo;
 
-public class SetSignpostToDbPropertiesBeanInfo extends SimpleBeanInfo {
+public class SetSignpostToConfigInfoBeanInfo extends SimpleBeanInfo {
 
     public PropertyDescriptor[] getPropertyDescriptors() {
       return new PropertyDescriptor[] {};
@@ -13,8 +13,8 @@ public class SetSignpostToDbPropertiesBeanInfo extends SimpleBeanInfo {
      * @see java.beans.BeanInfo#getBeanDescriptor()
      */
     public BeanDescriptor getBeanDescriptor() {
-        BeanDescriptor bd = new BeanDescriptor(SetSignpostToDbProperties.class);
-        bd.setDisplayName("<html><font color='green'><center>Set Signpost<br>to DB Info");
+        BeanDescriptor bd = new BeanDescriptor(SetSignpostToConfigInfo.class);
+        bd.setDisplayName("<html><font color='green'><center>Set Signpost<br>to Config Info");
         return bd;
     }
 
