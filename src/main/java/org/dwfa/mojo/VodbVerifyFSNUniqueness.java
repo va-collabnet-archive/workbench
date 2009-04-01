8e6a24f7057799200d1cfe43c60df5aaf58cd872
chrissy <chrissy>
2009-04-01 01:53:40 +0000

    added mojo to test for FSN uniqueness

---
+++ src/main/java/org/dwfa/mojo/VodbVerifyFSNUniqueness.java
@@ -0,0 +1,122 @@
+package org.dwfa.mojo;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.UUID;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.dwfa.ace.api.I_DescriptionTuple;
+import org.dwfa.ace.api.I_DescriptionVersioned;
+import org.dwfa.ace.api.I_ProcessDescriptions;
+import org.dwfa.ace.api.I_TermFactory;
+import org.dwfa.ace.api.LocalVersionedTerminology;
+import org.dwfa.cement.ArchitectonicAuxiliary;
+
+
+/**
+ * Goal which ensures latest FSNs are unique for the given VODB.
+ * @goal vodb-verify-fsn-uniqueness
+ *
+ * @phase process-resources
+ * @requiresDependencyResolution compile
+ */
+public class VodbVerifyFSNUniqueness extends AbstractMojo {
+
+    /**
+     * The output file location.
+     * @parameter expression="${project.build.directory}/reports"
+     */
+    private File outputDirectory;
+
+    /**
+     * The output file name.
+     * @parameter
+     */
+    private String outputFileName = "release_fsn_uniqueness_report.txt";
+
+    /**
+     * Any exceptions to the uniqueness test.
+     * @parameter
+     */
+    private ArrayList<String> exceptions = new ArrayList<String>();
+
+    /**
+     * Whether to continue on, if a non-unique FSN is found.
+     * Default is to not continue - the build will fail.
+     * @parameter
+     */
+    private boolean failBuildOnException = false;
+
+    private class FindUniqueFSNs implements I_ProcessDescriptions {
+
+        I_TermFactory termFactory;
+        BufferedWriter outputWriter;
+        HashSet<String> uniqueFsns;
+
+        public FindUniqueFSNs() throws Exception {
+            outputDirectory.mkdirs();
+            outputWriter = new BufferedWriter(new BufferedWriter(
+                    new FileWriter(outputDirectory + File.separator
+                            + outputFileName)));
+            termFactory = LocalVersionedTerminology.get();
+            uniqueFsns = new HashSet<String>();
+        }
+
+        public void processDescription(I_DescriptionVersioned description) throws Exception {
+
+            I_DescriptionTuple latestDescription = description.getLastTuple();
+
+            // check if it's a FSN
+            UUID currentDescriptionTypeUuid = termFactory.getConcept(
+                    latestDescription.getTypeId()).getUids().iterator().next();
+            UUID fsnUuid = ArchitectonicAuxiliary.Concept.FULLY_SPECIFIED_DESCRIPTION_TYPE.getUids().iterator().next();
+            if (currentDescriptionTypeUuid.equals(fsnUuid)) {
+
+                String descriptionText = latestDescription.getText();
+
+                if (uniqueFsns.contains(descriptionText)) {
+                    if (exceptions.contains(descriptionText)) {
+                        // don't report as it's an exception
+                    } else {
+                        if (!failBuildOnException) {
+                            getLog().info("Duplicate FSN: " + descriptionText);
+                            outputWriter.write("Duplicate FSN: " + descriptionText);
+                            outputWriter.newLine();
+                        } else {
+                            throw new MojoFailureException("Duplicate FSN: " + descriptionText);
+                        }
+                    }
+                } else {
+                    uniqueFsns.add(descriptionText);
+                }
+            }
+
+        }
+
+        public BufferedWriter getWriter() {
+            return outputWriter;
+        }
+
+        public void setWriter(BufferedWriter outputWriter) {
+            this.outputWriter = outputWriter;
+        }
+    }
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        I_TermFactory termFactory = LocalVersionedTerminology.get();
+        try {
+            FindUniqueFSNs find = new FindUniqueFSNs();
+            termFactory.iterateDescriptions(find);
+            find.getWriter().flush();
+            find.getWriter().close();
+        } catch (Exception e) {
+            throw new MojoExecutionException(e.getLocalizedMessage(), e);
+        }
+    }
+
+}
