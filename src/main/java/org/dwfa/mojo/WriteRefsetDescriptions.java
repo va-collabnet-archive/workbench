47065d14001203918bc920f763054d318683c7f6
ssahayam <ssahayam>
2009-05-25 22:49:24 +0000

    1. Updated WriteRefsetDescriptions to use refactorings of WriteRefsetDescriptions in ace-api. TCP-1036.

--- src/main/java/org/dwfa/mojo/WriteRefsetDescriptions.java
+++ src/main/java/org/dwfa/mojo/WriteRefsetDescriptions.java
@@ -3,9 +3,12 @@ package org.dwfa.mojo;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.dwfa.ace.api.I_TermFactory;
 import org.dwfa.ace.api.LocalVersionedTerminology;
 import org.dwfa.ace.task.refset.members.CleanableProcessExtByRef;
-import org.dwfa.ace.task.refset.members.WriteRefsetDescriptionsProcessExtByRef;
+import org.dwfa.ace.task.refset.members.CleanableProcessExtByRefBuilder;
+import org.dwfa.ace.task.refset.members.WriteRefsetDescriptionsProcessExtByRefBuilder;
+import org.dwfa.ace.task.util.Logger;
 import org.dwfa.maven.MojoUtil;
 
 import java.io.File;
@@ -36,18 +39,45 @@ public class WriteRefsetDescriptions extends AbstractMojo {
      */
     private File targetDirectory;
 
+
+    private final I_TermFactory termFactory;
+
+    private final CleanableProcessExtByRefBuilder cleanableProcessExtByRefBuilder;
+
+
+    public WriteRefsetDescriptions() {
+        //default constructor for maven.
+        termFactory = LocalVersionedTerminology.get();
+        cleanableProcessExtByRefBuilder = new WriteRefsetDescriptionsProcessExtByRefBuilder();
+    }
+
+    //Used for testing.
+    WriteRefsetDescriptions(final File outputDirectory, final File targetDirectory, final I_TermFactory termFactory,
+                            final CleanableProcessExtByRefBuilder cleanableProcessExtByRefBuilder) {
+        this.outputDirectory = outputDirectory;
+        this.targetDirectory = targetDirectory;
+        this.termFactory = termFactory;
+        this.cleanableProcessExtByRefBuilder = cleanableProcessExtByRefBuilder;
+    }
+
     public void execute() throws MojoExecutionException, MojoFailureException {
 		try {
 
-			if (MojoUtil.alreadyRun(getLog(), outputDirectory.getAbsolutePath(), this.getClass(), targetDirectory)) {
+			if (MojoUtil.alreadyRun(getLog(), outputDirectory.getAbsolutePath(), getClass(), targetDirectory)) {
 				return;
 			}
 
-           CleanableProcessExtByRef exporter = new WriteRefsetDescriptionsProcessExtByRef(new MojoLogger(getLog()),
-                    outputDirectory);
-            LocalVersionedTerminology.get().iterateExtByRefs(exporter);
-            exporter.clean();
+            Logger logger = new MojoLogger(getLog());
 
+            CleanableProcessExtByRef refsetDescriptionWriter = cleanableProcessExtByRefBuilder.
+                                                                withTermFactory(termFactory).
+                                                                withLogger(logger).
+                                                                withSelectedDir(targetDirectory).
+                                                                build();
+
+            //test what happens when an exception occurs
+            termFactory.iterateExtByRefs(refsetDescriptionWriter);
+            refsetDescriptionWriter.clean();
         } catch (Exception e) {
 			throw new MojoExecutionException(e.getLocalizedMessage(), e);
 		}
