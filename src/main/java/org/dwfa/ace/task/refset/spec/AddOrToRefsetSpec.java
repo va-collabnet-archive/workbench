95a1d6c6761540be452293ab269980ced50b51b6
kec <kec>
2009-05-26 22:19:33 +0000

    Added new business process plugins to support refset specifications...

---
+++ src/main/java/org/dwfa/ace/task/refset/spec/AddOrToRefsetSpec.java
@@ -0,0 +1,61 @@
+package org.dwfa.ace.task.refset.spec;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Collection;
+
+import javax.swing.JOptionPane;
+
+import org.dwfa.bpa.process.Condition;
+import org.dwfa.bpa.process.I_EncodeBusinessProcess;
+import org.dwfa.bpa.process.I_Work;
+import org.dwfa.bpa.process.TaskFailedException;
+import org.dwfa.bpa.tasks.AbstractTask;
+import org.dwfa.util.bean.BeanList;
+import org.dwfa.util.bean.BeanType;
+import org.dwfa.util.bean.Spec;
+
+@BeanList(specs = { @Spec(directory = "tasks/refset/spec", type = BeanType.TASK_BEAN) })
+public class AddOrToRefsetSpec extends AbstractTask {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private static final int dataVersion = 1;
+
+	private void writeObject(ObjectOutputStream out) throws IOException {
+		out.writeInt(dataVersion);
+	}
+
+	private void readObject(ObjectInputStream in) throws IOException,
+			ClassNotFoundException {
+		int objDataVersion = in.readInt();
+		if (objDataVersion == dataVersion) {
+			//
+		} else {
+			throw new IOException("Can't handle dataversion: " + objDataVersion);
+		}
+	}
+
+	public void complete(I_EncodeBusinessProcess process, I_Work worker)
+			throws TaskFailedException {
+		// Nothing to do
+	}
+
+	public Condition evaluate(I_EncodeBusinessProcess process, I_Work worker)
+			throws TaskFailedException {
+		JOptionPane.showMessageDialog(null, "Adding OR");
+		return Condition.CONTINUE;
+	}
+
+	public int[] getDataContainerIds() {
+		return new int[] {};
+	}
+
+	public Collection<Condition> getConditions() {
+		return AbstractTask.CONTINUE_CONDITION;
+	}
+
+}
