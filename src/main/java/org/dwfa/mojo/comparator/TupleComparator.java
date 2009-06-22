c210b6e6ff611a269e545f01e35c3a3da1b9a81c
lswindale <lswindale>
2009-06-22 00:00:19 +0000

    Refactored the TupleComparator so that it leverages the compareTo() method in Integer class

--- src/main/java/org/dwfa/mojo/comparator/TupleComparator.java
+++ src/main/java/org/dwfa/mojo/comparator/TupleComparator.java
@@ -5,7 +5,7 @@ import org.dwfa.ace.api.I_DescriptionTuple;
 import java.util.Comparator;
 
 /**
- * This comparator sorts a list of tuples, firstly by concept id (arbitrary value,
+ * This comparator sorts a list of tuples, firstly by description id (arbitrary value,
  * but can be used to group versions) and then by description version
  */
 public class TupleComparator implements Comparator {
@@ -15,12 +15,14 @@ public class TupleComparator implements Comparator {
         I_DescriptionTuple t1 = (I_DescriptionTuple) o;
         I_DescriptionTuple t2 = (I_DescriptionTuple) o1;
 
-        if (t1.getDescId() < t2.getDescId()) {
-            return -1;
-        } else if (t1.getDescId() > t2.getDescId()) {
-            return 1;
+        int result = Integer.valueOf(t1.getDescId()).
+                compareTo(t2.getDescId());
+
+        if (result != 0) {
+            return result;
+        } else {
+            return Integer.valueOf(t1.getVersion()).
+                compareTo(t2.getVersion());
         }
-        return Integer.valueOf(t1.getVersion()).
-                compareTo(Integer.valueOf(t2.getVersion()));
-    }    
+    }
 }
