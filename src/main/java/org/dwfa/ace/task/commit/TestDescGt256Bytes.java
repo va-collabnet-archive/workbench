8095f15c8f3b0171e39c4807d2b49678d4396156
kec <kec>
2009-07-13 00:41:44 +0000

    Added test for > 255 bytes for synonyms, descriptions, and fully specified descriptions.

--- src/main/java/org/dwfa/ace/task/commit/TestDescGt256Bytes.java
+++ src/main/java/org/dwfa/ace/task/commit/TestDescGt256Bytes.java
@@ -2,18 +2,18 @@ package org.dwfa.ace.task.commit;
 
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 
-import org.apache.lucene.document.Document;
-import org.apache.lucene.search.Hits;
 import org.dwfa.ace.api.I_ConfigAceFrame;
 import org.dwfa.ace.api.I_DescriptionPart;
 import org.dwfa.ace.api.I_DescriptionTuple;
 import org.dwfa.ace.api.I_DescriptionVersioned;
 import org.dwfa.ace.api.I_GetConceptData;
+import org.dwfa.ace.api.I_IntSet;
 import org.dwfa.ace.api.I_Position;
 import org.dwfa.ace.api.I_TermFactory;
 import org.dwfa.ace.api.LocalVersionedTerminology;
@@ -27,7 +27,7 @@ import org.dwfa.util.bean.Spec;
 		@Spec(directory = "tasks/ide/commit", type = BeanType.TASK_BEAN),
 		@Spec(directory = "plugins/precommit", type = BeanType.TASK_BEAN),
 		@Spec(directory = "plugins/commit", type = BeanType.TASK_BEAN) })
-public class TestForFullySpecifiedName extends AbstractConceptTest {
+public class TestDescGt256Bytes extends AbstractConceptTest {
 
 	private static final long serialVersionUID = 1;
 	private static final int dataVersion = 1;
@@ -79,86 +79,45 @@ public class TestForFullySpecifiedName extends AbstractConceptTest {
 			throws Exception {
 		ArrayList<AlertToDataConstraintFailure> alertList = new ArrayList<AlertToDataConstraintFailure>();
 		I_TermFactory termFactory = LocalVersionedTerminology.get();
+		I_IntSet typesToCheck = termFactory.newIntSet();
 		I_GetConceptData fsn_type = getConceptSafe(termFactory,
 				ArchitectonicAuxiliary.Concept.FULLY_SPECIFIED_DESCRIPTION_TYPE
 						.getUids());
-		if (fsn_type == null)
+		I_GetConceptData pt_type = getConceptSafe(termFactory,
+				ArchitectonicAuxiliary.Concept.PREFERRED_DESCRIPTION_TYPE
+						.getUids());
+		I_GetConceptData syn_type = getConceptSafe(termFactory,
+				ArchitectonicAuxiliary.Concept.SYNONYM_DESCRIPTION_TYPE
+						.getUids());
+		if (fsn_type == null) {
 			return alertList;
-		ArrayList<Integer> actives = getActiveStatus(termFactory);
-		HashMap<String, ArrayList<I_DescriptionVersioned>> langs = new HashMap<String, ArrayList<I_DescriptionVersioned>>();
+		}
+		typesToCheck.add(fsn_type.getConceptId());
+		typesToCheck.add(pt_type.getConceptId());
+		typesToCheck.add(syn_type.getConceptId());
 		for (I_DescriptionVersioned desc : descriptions) {
 			for (I_DescriptionPart part : desc.getVersions()) {
-				if (!actives.contains(part.getStatusId()))
-					continue;
-				if (part.getTypeId() == fsn_type.getConceptId()) {
-					if (part.getText().matches(".*\\(\\?+\\).*")) {
-						alertList
-								.add(new AlertToDataConstraintFailure(
-										AlertToDataConstraintFailure.ALERT_TYPE.WARNING,
-										"<html>Unedited semantic tag", concept));
-					}
-					String lang = part.getLang();
-					if (langs.get(lang) != null) {
-						for (I_DescriptionVersioned d : langs.get(lang)) {
-							if (d.getDescId() != desc.getDescId()) {
-								alertList
-										.add(new AlertToDataConstraintFailure(
-												(forCommit ? AlertToDataConstraintFailure.ALERT_TYPE.ERROR
-														: AlertToDataConstraintFailure.ALERT_TYPE.WARNING),
-												"<html>More than one FSN for "
-														+ lang, concept));
-							}
-						}
-						langs.get(lang).add(desc);
-					} else {
-						ArrayList<I_DescriptionVersioned> dl = new ArrayList<I_DescriptionVersioned>();
-						dl.add(desc);
-						langs.put(lang, dl);
-					}
-
-					Hits hits = termFactory.doLuceneSearch("\""
-							+ part.getText().replace("(", "\\(").replace(")",
-									"\\)") + "\"");
-					// System.out.println("Found " + hits.length());
-					search: for (int i = 0; i < hits.length(); i++) {
-						// if (i == 10000)
-						// break;
-						Document doc = hits.doc(i);
-						int cnid = Integer.parseInt(doc.get("cnid"));
-						int dnid = Integer.parseInt(doc.get("dnid"));
-						if (cnid == concept.getConceptId())
-							continue;
-						I_DescriptionVersioned potential_fsn = termFactory
-								.getDescription(dnid, cnid);
-						for (I_DescriptionPart part_search : potential_fsn
-								.getVersions()) {
-							// System.out.println("Hit: "
-							// + part_search.getVersion() + "\t"
-							// + part_search.getText());
-							if (actives.contains(part_search.getStatusId())
-									&& part_search.getTypeId() == fsn_type
-											.getConceptId()
-									&& part_search.getText().equals(
-											part.getText())
-									&& part_search.getLang().equals(
-											part.getLang())) {
-								alertList
-										.add(new AlertToDataConstraintFailure(
-												AlertToDataConstraintFailure.ALERT_TYPE.WARNING,
-												"<html>FSN already used",
-												concept));
-								break search;
-							}
+				if (part.getVersion() == Integer.MAX_VALUE) {
+					if (typesToCheck.contains(part.getTypeId())) {
+						Charset utf8 = Charset.forName("UTF-8");
+						ByteBuffer bytes = utf8.encode(part.getText());
+						if (bytes.limit() > 255) {
+							I_GetConceptData typeBean = termFactory.getConcept(part.getTypeId());
+							I_DescriptionTuple typeDesc = typeBean.getDescTuple(termFactory.getActiveAceFrameConfig().getTableDescPreferenceList(), termFactory.getActiveAceFrameConfig());
+							alertList
+									.add(new AlertToDataConstraintFailure(
+											(forCommit ? AlertToDataConstraintFailure.ALERT_TYPE.ERROR
+													: AlertToDataConstraintFailure.ALERT_TYPE.WARNING),
+											"<html>" + typeDesc.getText() + ":&nbsp;&nbsp;<font color=blue>"
+											+ part.getText().substring(0, 40) + "</font>..."
+											+ "<br>exceeds the 255 byte limit by  "+ 
+											(bytes.limit() - 255) + " bytes.",
+											concept));
 						}
 					}
 				}
 			}
 		}
-		if (langs.get("en") == null)
-			alertList.add(new AlertToDataConstraintFailure(
-					(forCommit ? AlertToDataConstraintFailure.ALERT_TYPE.ERROR
-							: AlertToDataConstraintFailure.ALERT_TYPE.WARNING),
-					"<html>No FSN for en", concept));
 		return alertList;
 	}
 
