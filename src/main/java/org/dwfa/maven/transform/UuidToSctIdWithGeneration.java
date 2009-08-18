7f1254e0d777813e7cc2efbba6a2ea11eacbf9b9
edungey <edungey>
2009-08-18 02:02:51 +0000

    Inital commit for Ace2Rf2 mojo Added Ace file reader, added rf2 file writers Updated with new method UuidToSctIdWithGeneration.setupImpl(File buildDir, FilesourceDir) to allow use of UuidToSctIdWithGeneration without a Transformation mojo

--- src/main/java/org/dwfa/maven/transform/UuidToSctIdWithGeneration.java
+++ src/main/java/org/dwfa/maven/transform/UuidToSctIdWithGeneration.java
@@ -10,44 +10,44 @@ import org.dwfa.maven.transform.SctIdGenerator.TYPE;
 
 public abstract class UuidToSctIdWithGeneration extends AbstractTransform implements I_ReadAndTransform {
 
-   private File sourceDirectory = null;
-   static UuidSnomedMapHandler map;
+    private File sourceDirectory = null;
+    static UuidSnomedMapHandler map;
 
-   public void setupImpl(Transform transformer) throws IOException {
-      // Nothing to setup
-      File buildDirectory = transformer.getBuildDirectory();
-      //[INFO]  buildDirectory: /au-ct/ace/amt-release/target
-      File idGeneratedDir = new File(new File(buildDirectory, "generated-resources"), "sct-uuid-maps");
+    public void setupImpl(Transform transformer) throws IOException {
+        setupImpl(transformer.getBuildDirectory(), transformer.getSourceDirectory());
+    }
 
-      if (sourceDirectory==null) {
-          sourceDirectory = transformer.getSourceDirectory();
-      }
+    public void setupImpl(File buildDirectory, File sourceDirectoryToSet) throws IOException {
+        File idGeneratedDir = new File(new File(buildDirectory, "generated-resources"), "sct-uuid-maps");
 
-      initMap(idGeneratedDir, sourceDirectory);
-   }
+        if (sourceDirectory == null) {
+            sourceDirectory = sourceDirectoryToSet;
+        }
 
-   private static synchronized void initMap(File idGeneratedDir, File sourceDirectory) throws IOException {
- 	  if (map == null) {
-		  map = new UuidSnomedMapHandler(idGeneratedDir, sourceDirectory);
-	  }
-   }
+        initMap(idGeneratedDir, sourceDirectory);
+    }
 
-   public String transform(String input) throws Exception {
-      UUID id = UUID.fromString(input);
-      TYPE type = getType();
-      return setLastTransform(Long.toString(map.getWithGeneration(id, type)));
-   }
-   
-   public Long getMappedId(UUID id, TYPE type) {
-	   return map.getWithoutGeneration(id, type);
-   }
+    private static synchronized void initMap(File idGeneratedDir, File sourceDirectory) throws IOException {
+        if (map == null) {
+            map = new UuidSnomedMapHandler(idGeneratedDir, sourceDirectory);
+        }
+    }
 
-   public void cleanup(Transform transformer) throws Exception {
-      if (map != null) {
-         map.writeMaps();
-      }
-   }
+    public String transform(String input) throws Exception {
+        UUID id = UUID.fromString(input);
+        TYPE type = getType();
+        return setLastTransform(Long.toString(map.getWithGeneration(id, type)));
+    }
 
-   protected abstract TYPE getType();
-   
+    public Long getMappedId(UUID id, TYPE type) {
+        return map.getWithoutGeneration(id, type);
+    }
+
+    public void cleanup(Transform transformer) throws Exception {
+        if (map != null) {
+            map.writeMaps();
+        }
+    }
+
+    protected abstract TYPE getType();
 }
