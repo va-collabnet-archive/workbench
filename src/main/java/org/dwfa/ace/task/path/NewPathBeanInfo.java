b13cad1667b5fc764a047e3a2590c9ae1f209a2f
scrow <scrow>
2009-09-03 05:26:15 +0000

    removed unused import

--- src/main/java/org/dwfa/ace/task/path/NewPathBeanInfo.java
+++ src/main/java/org/dwfa/ace/task/path/NewPathBeanInfo.java
@@ -7,7 +7,6 @@ import java.beans.PropertyDescriptor;
 import java.beans.SimpleBeanInfo;
 
 import org.dwfa.bpa.tasks.editor.ConceptLabelEditor;
-import org.dwfa.bpa.tasks.editor.JTextFieldEditor;
 import org.dwfa.bpa.tasks.editor.PropertyNameLabelEditor;
 
 
