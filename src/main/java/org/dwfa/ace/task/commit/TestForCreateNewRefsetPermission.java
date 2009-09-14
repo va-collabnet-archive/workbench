8bdc186ebbefca671c6c4b633f5f69c8c134e446
chrissy <chrissy>
2009-09-14 04:23:59 +0000

    [artf1724] updated new refset permission data check - now checks both role and individual permissions when determining if a change is valid

--- src/main/java/org/dwfa/ace/task/commit/TestForCreateNewRefsetPermission.java
+++ src/main/java/org/dwfa/ace/task/commit/TestForCreateNewRefsetPermission.java
@@ -3,15 +3,22 @@ package org.dwfa.ace.task.commit;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import org.dwfa.ace.api.I_ConfigAceDb;
+import org.dwfa.ace.api.I_ConfigAceFrame;
 import org.dwfa.ace.api.I_GetConceptData;
 import org.dwfa.ace.api.I_IntSet;
 import org.dwfa.ace.api.I_Position;
+import org.dwfa.ace.api.I_RelTuple;
 import org.dwfa.ace.api.I_TermFactory;
 import org.dwfa.ace.api.LocalVersionedTerminology;
+import org.dwfa.ace.api.ebr.I_ThinExtByRefPart;
+import org.dwfa.ace.api.ebr.I_ThinExtByRefPartConcept;
+import org.dwfa.ace.api.ebr.I_ThinExtByRefTuple;
+import org.dwfa.ace.api.ebr.I_ThinExtByRefVersioned;
 import org.dwfa.bpa.process.TaskFailedException;
 import org.dwfa.cement.ArchitectonicAuxiliary;
 import org.dwfa.cement.RefsetAuxiliary;
@@ -26,6 +33,10 @@ public class TestForCreateNewRefsetPermission extends AbstractConceptTest {
 
     private static final long serialVersionUID = 1;
     private static final int dataVersion = 1;
+    private I_GetConceptData activeUser;
+    private I_TermFactory termFactory;
+    private I_ConfigAceDb configDb;
+    private I_ConfigAceFrame configFrame;
 
     private void writeObject(ObjectOutputStream out) throws IOException {
         out.writeInt(dataVersion);
@@ -45,15 +56,13 @@ public class TestForCreateNewRefsetPermission extends AbstractConceptTest {
             throws TaskFailedException {
         try {
             ArrayList<AlertToDataConstraintFailure> alertList = new ArrayList<AlertToDataConstraintFailure>();
-            I_TermFactory termFactory = LocalVersionedTerminology.get();
-            I_ConfigAceDb configDb = termFactory.getActiveAceFrameConfig().getDbConfig();
+            termFactory = LocalVersionedTerminology.get();
+            configFrame = termFactory.getActiveAceFrameConfig();
+            configDb = configFrame.getDbConfig();
+            I_GetConceptData userTopHierarchy = termFactory.getConcept(ArchitectonicAuxiliary.Concept.USER.getUids());
 
-            I_GetConceptData activeUser = configDb.getUserConcept();
-
-            if (activeUser == null) {
-                // alertList.add(new
-                // AlertToDataConstraintFailure(AlertToDataConstraintFailure.ALERT_TYPE.WARNING,
-                // "<html>Unable to find active user.", concept));
+            activeUser = configDb.getUserConcept();
+            if (activeUser == null || activeUser.equals(userTopHierarchy)) {
                 return alertList;
             }
 
@@ -64,15 +73,18 @@ public class TestForCreateNewRefsetPermission extends AbstractConceptTest {
                 return alertList;
             }
 
-            // find the refset (parents) that this user can create content under
-            Set<I_Position> allPositions = getPositions(termFactory);
-            I_IntSet activeStatuses = getActiveStatus(termFactory);
-            I_GetConceptData createNewRefsetPermissionRel =
-                    termFactory.getConcept(ArchitectonicAuxiliary.Concept.CREATE_NEW_REFSET_PERMISSION.getUids());
-            I_IntSet allowedTypes = termFactory.newIntSet();
-            allowedTypes.add(createNewRefsetPermissionRel.getConceptId());
-            Set<I_GetConceptData> permissibleRefsetParents =
-                    activeUser.getSourceRelTargets(activeStatuses, allowedTypes, allPositions, true, true);
+            // two types of permissions to check - first are permissions
+            // assigned under a user role, e.g. SC has owner permissions for
+            // CAB.
+            // second type are permissions granted to an individual. e.g. SC has
+            // create refset permission for CAB.
+
+            // add individual permissions
+            Set<I_GetConceptData> permissibleRefsetParents = new HashSet<I_GetConceptData>();
+            permissibleRefsetParents.addAll(getValidRefsetsFromIndividualUserPermissions());
+
+            // add user role permissions
+            permissibleRefsetParents.addAll(getValidRefsetsFromRolePermissions());
 
             AlertToDataConstraintFailure.ALERT_TYPE alertType;
             if (forCommit) {
@@ -98,5 +110,74 @@ public class TestForCreateNewRefsetPermission extends AbstractConceptTest {
         } catch (Exception e) {
             throw new TaskFailedException(e);
         }
+    }
+
+    private Set<I_GetConceptData> getValidRefsetsFromRolePermissions() throws Exception {
+        Set<I_GetConceptData> refsets = new HashSet<I_GetConceptData>();
+
+        Set<I_Position> allPositions = getPositions(termFactory);
+        I_IntSet activeStatuses = getActiveStatus(termFactory);
+        I_GetConceptData type = termFactory.getConcept(ArchitectonicAuxiliary.Concept.GRANTED_ROLE.getUids());
+        I_IntSet allowedTypes = termFactory.newIntSet();
+        allowedTypes.add(type.getConceptId());
+
+        List<I_RelTuple> roles = activeUser.getSourceRelTuples(activeStatuses, allowedTypes, allPositions, true, true);
+
+        I_GetConceptData createNewRefsetPermission =
+                termFactory.getConcept(ArchitectonicAuxiliary.Concept.CREATE_NEW_REFSET_PERMISSION.getUids());
+
+        for (I_RelTuple roleRel : roles) {
+            int relationshipId = roleRel.getRelId();
+            List<I_ThinExtByRefVersioned> extensions = termFactory.getAllExtensionsForComponent(relationshipId, true);
+            // TODO filter on refset type
+            I_GetConceptData extConcept = null;
+
+            for (I_ThinExtByRefVersioned currExt : extensions) {
+
+                List<I_ThinExtByRefTuple> tuples = currExt.getTuples(activeStatuses, allPositions, true, true);
+
+                if (tuples.size() > 0) {
+                    I_ThinExtByRefPart thinPart = tuples.get(0).getPart();
+
+                    if (thinPart instanceof I_ThinExtByRefPartConcept) {
+                        I_ThinExtByRefPartConcept part = (I_ThinExtByRefPartConcept) thinPart;
+                        extConcept = termFactory.getConcept(part.getC1id());
+                    }
+                }
+            }
+            if (extConcept != null) {
+
+                I_GetConceptData c2 = termFactory.getConcept(roleRel.getC2Id());
+                I_GetConceptData isAType = termFactory.getConcept(ArchitectonicAuxiliary.Concept.IS_A_REL.getUids());
+                I_IntSet isAAllowedTypes = termFactory.newIntSet();
+                isAAllowedTypes.add(isAType.getConceptId());
+
+                List<I_RelTuple> permissions =
+                        c2.getDestRelTuples(activeStatuses, isAAllowedTypes, allPositions, true, true);
+
+                for (I_RelTuple permissionRel : permissions) {
+                    if (permissionRel.getC1Id() == createNewRefsetPermission.getConceptId()) {
+                        refsets.add(extConcept);
+                    }
+                }
+            }
+
+        }
+
+        return refsets;
+    }
+
+    private Set<I_GetConceptData> getValidRefsetsFromIndividualUserPermissions() throws Exception {
+        Set<I_Position> allPositions = getPositions(termFactory);
+        I_IntSet activeStatuses = getActiveStatus(termFactory);
+        I_GetConceptData createNewRefsetPermissionRel =
+                termFactory.getConcept(ArchitectonicAuxiliary.Concept.CREATE_NEW_REFSET_PERMISSION.getUids());
+        I_IntSet allowedTypes = termFactory.newIntSet();
+        allowedTypes.add(createNewRefsetPermissionRel.getConceptId());
+
+        Set<I_GetConceptData> refsets =
+                activeUser.getSourceRelTargets(activeStatuses, allowedTypes, allPositions, true, true);
+
+        return refsets;
     }
 }
