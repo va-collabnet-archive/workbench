658a6d33f74cb19e22c9f2bd7f366de5be819495
kec <kec>
2009-10-06 18:23:35 +0000

    [artf2366] Fixed search save and restore. Also fixed issue with workflow panel inverted X & Y values.

--- src/main/java/org/dwfa/ace/task/search/I_TestSearchResults.java
+++ src/main/java/org/dwfa/ace/task/search/I_TestSearchResults.java
@@ -1,10 +1,12 @@
 package org.dwfa.ace.task.search;
 
+import java.io.Serializable;
+
 import org.dwfa.ace.api.I_AmTermComponent;
 import org.dwfa.ace.api.I_ConfigAceFrame;
 import org.dwfa.bpa.process.TaskFailedException;
 
-public interface I_TestSearchResults {
+public interface I_TestSearchResults extends Serializable {
 
     public boolean test(I_AmTermComponent component, I_ConfigAceFrame frameConfig) throws TaskFailedException;
 
