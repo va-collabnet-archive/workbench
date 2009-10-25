dd8a3ef4875b8d5114a13cc477f0c3350c8831bd
chrissy <chrissy>
2009-10-25 10:33:12 +0000

    [artf2724] updated search panel right click to include refset BPs (e.g. "Add concept-is") if the refset spec panel is open and an AND/OR is selected

--- src/main/java/org/dwfa/ace/task/refset/spec/AbstractAddRefsetSpecTaskBeanInfo.java
+++ src/main/java/org/dwfa/ace/task/refset/spec/AbstractAddRefsetSpecTaskBeanInfo.java
@@ -6,30 +6,38 @@ import java.beans.PropertyDescriptor;
 import java.beans.SimpleBeanInfo;
 
 import org.dwfa.bpa.tasks.editor.CheckboxEditor;
+import org.dwfa.bpa.tasks.editor.PropertyNameLabelEditor;
 
-public abstract class AbstractAddRefsetSpecTaskBeanInfo  extends SimpleBeanInfo {
+public abstract class AbstractAddRefsetSpecTaskBeanInfo extends SimpleBeanInfo {
 
     public PropertyDescriptor[] getPropertyDescriptors() {
-        try {  
+        try {
 
             PropertyDescriptor clauseIsTrue =
-                new PropertyDescriptor("clauseIsTrue", getBeanDescriptor().getBeanClass());
+                    new PropertyDescriptor("clauseIsTrue", getBeanDescriptor().getBeanClass());
             clauseIsTrue.setBound(true);
             clauseIsTrue.setPropertyEditorClass(CheckboxEditor.class);
             clauseIsTrue.setDisplayName("<html><font color='green'>true:");
-            clauseIsTrue.setShortDescription("If checked, the clause must be true. If not checked, the clause must be false.");
+            clauseIsTrue
+                .setShortDescription("If checked, the clause must be true. If not checked, the clause must be false.");
 
-            PropertyDescriptor rv[] =
-                { clauseIsTrue };
+            PropertyDescriptor activeConceptPropName;
+            activeConceptPropName = new PropertyDescriptor("activeConceptPropName", getBeanDescriptor().getBeanClass());
+            activeConceptPropName.setBound(true);
+            activeConceptPropName.setPropertyEditorClass(PropertyNameLabelEditor.class);
+            activeConceptPropName.setDisplayName("<html><font color='green'>active concept uuid prop name:");
+            activeConceptPropName.setShortDescription("The property to put the active concept into.");
+
+            PropertyDescriptor rv[] = { clauseIsTrue, activeConceptPropName };
             return rv;
         } catch (IntrospectionException e) {
-             throw new Error(e.toString());
+            throw new Error(e.toString());
         }
-     }        
+    }
+
     /**
      * @see java.beans.BeanInfo#getBeanDescriptor()
      */
     public abstract BeanDescriptor getBeanDescriptor();
-    
-    
+
 }
--- src/main/java/org/dwfa/ace/task/refset/spec/AddStructuralQueryToRefsetSpec.java
+++ src/main/java/org/dwfa/ace/task/refset/spec/AddStructuralQueryToRefsetSpec.java
@@ -18,52 +18,51 @@ import org.dwfa.util.bean.Spec;
 
 @BeanList(specs = { @Spec(directory = "tasks/refset/spec", type = BeanType.TASK_BEAN) })
 public class AddStructuralQueryToRefsetSpec extends AbstractAddRefsetSpecTask {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
 
-	private static final int dataVersion = 1;
+    private static final long serialVersionUID = 1L;
 
-	private void writeObject(ObjectOutputStream out) throws IOException {
-		out.writeInt(dataVersion);
-	}
+    private static final int dataVersion = 1;
 
-	private void readObject(ObjectInputStream in) throws IOException,
-			ClassNotFoundException {
-		int objDataVersion = in.readInt();
-		if (objDataVersion == dataVersion) {
-			//
-		} else {
-			throw new IOException("Can't handle dataversion: " + objDataVersion);
-		}
-	}
+    private void writeObject(ObjectOutputStream out) throws IOException {
+        out.writeInt(dataVersion);
+    }
 
-	protected int getRefsetPartTypeId() throws IOException, TerminologyException {
-		int typeId = RefsetAuxiliary.Concept.CONCEPT_CONCEPT_CONCEPT_EXTENSION
-				.localize().getNid();
-		return typeId;
-	}
+    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+        int objDataVersion = in.readInt();
+        if (objDataVersion == dataVersion) {
+            //
+        } else {
+            throw new IOException("Can't handle dataversion: " + objDataVersion);
+        }
+    }
 
-	protected I_ThinExtByRefPart createAndPopulatePart(
-			I_TermFactory tf, I_Path p, I_ConfigAceFrame configFrame) throws IOException,
-			TerminologyException {
-		I_ThinExtByRefPartConceptConceptConcept specPart = tf.newConceptConceptConceptExtensionPart();
-		if (getClauseIsTrue()) {
-			specPart.setC1id(RefsetAuxiliary.Concept.BOOLEAN_CIRCLE_ICONS_TRUE.localize().getNid());
-		} else {
-			specPart.setC1id(RefsetAuxiliary.Concept.BOOLEAN_CIRCLE_ICONS_FALSE.localize().getNid());
-		}
-		specPart.setC2id(getStructuralQueryTokenId());
-		specPart.setC3id(configFrame.getHierarchySelection().getConceptId());
-		specPart.setPathId(p.getConceptId());
-		specPart.setStatusId(ArchitectonicAuxiliary.Concept.ACTIVE.localize().getNid());
-		specPart.setVersion(Integer.MAX_VALUE);
-		return specPart;
-	}
-	
-	protected int getStructuralQueryTokenId() throws IOException, TerminologyException {
-		return RefsetAuxiliary.Concept.CONCEPT_IS.localize().getNid();
-	}
+    protected int getRefsetPartTypeId() throws IOException, TerminologyException {
+        int typeId = RefsetAuxiliary.Concept.CONCEPT_CONCEPT_CONCEPT_EXTENSION.localize().getNid();
+        return typeId;
+    }
+
+    protected I_ThinExtByRefPart createAndPopulatePart(I_TermFactory tf, I_Path p, I_ConfigAceFrame configFrame)
+            throws IOException, TerminologyException {
+        I_ThinExtByRefPartConceptConceptConcept specPart = tf.newConceptConceptConceptExtensionPart();
+        if (getClauseIsTrue()) {
+            specPart.setC1id(RefsetAuxiliary.Concept.BOOLEAN_CIRCLE_ICONS_TRUE.localize().getNid());
+        } else {
+            specPart.setC1id(RefsetAuxiliary.Concept.BOOLEAN_CIRCLE_ICONS_FALSE.localize().getNid());
+        }
+        specPart.setC2id(getStructuralQueryTokenId());
+        if (c3Concept == null) {
+            specPart.setC3id(configFrame.getHierarchySelection().getConceptId());
+        } else {
+            specPart.setC3id(c3Concept.getConceptId());
+        }
+
+        specPart.setPathId(p.getConceptId());
+        specPart.setStatusId(ArchitectonicAuxiliary.Concept.ACTIVE.localize().getNid());
+        specPart.setVersion(Integer.MAX_VALUE);
+        return specPart;
+    }
+
+    protected int getStructuralQueryTokenId() throws IOException, TerminologyException {
+        return RefsetAuxiliary.Concept.CONCEPT_IS.localize().getNid();
+    }
 }
-
