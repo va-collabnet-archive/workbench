bdc8d2640214320d10d1d10df4f703ac79d23acd
marc <marc>
2009-11-15 00:41:58 +0000

    removed unprintable character in comment that was causing build error.

--- src/test/java/org/dwfa/util/id/OidUuidFactoryTest.java
+++ src/test/java/org/dwfa/util/id/OidUuidFactoryTest.java
@@ -8,11 +8,11 @@ public class OidUuidFactoryTest extends TestCase {
 	
 	/*Tests derived from
 	 * http://www.itu.int/ITU-T/studygroups/com17/oid/X.667-E.pdf
-	EXAMPLE Ð The following is an example of the string 
+	EXAMPLE: The following is an example of the string 
 	          representation of a UUID as a URN:
 		urn:uuid:f81d4fae-7dec-11d0-a765-00a0c91e6bf6
 		
-		NOTE Ð An alternative URN format (see [6]) is available, 
+    NOTE: An alternative URN format (see [6]) is available, 
 		but is not recommended for URNs generated using UUIDs. This
 		alternative format uses the single integer value of the UUID 
 		specified in 6.3, and represents the above example as
