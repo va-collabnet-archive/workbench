caf9fee7e5e534b9e325989f6497ad36e3cea225
sneiner <sneiner>
2009-11-30 23:57:19 +0000

    added previous version of refset to export

--- src/main/java/org/dwfa/mojo/epicexport/ExportToEpicLoadFilesMojo.java
+++ src/main/java/org/dwfa/mojo/epicexport/ExportToEpicLoadFilesMojo.java
@@ -1,12 +1,9 @@
 package org.dwfa.mojo.epicexport;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Writer;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
@@ -20,13 +17,12 @@ import org.dwfa.ace.api.I_DescriptionPart;
 import org.dwfa.ace.api.I_DescriptionTuple;
 import org.dwfa.ace.api.I_DescriptionVersioned;
 import org.dwfa.ace.api.I_GetConceptData;
-import org.dwfa.ace.api.I_IntList;
+import org.dwfa.ace.api.I_IdPart;
 import org.dwfa.ace.api.I_IntSet;
 import org.dwfa.ace.api.I_Position;
 import org.dwfa.ace.api.I_ProcessConcepts;
 import org.dwfa.ace.api.I_TermFactory;
 import org.dwfa.ace.api.LocalVersionedTerminology;
-import org.dwfa.ace.api.I_ConfigAceFrame.LANGUAGE_SORT_PREF;
 import org.dwfa.ace.api.ebr.I_ThinExtByRefPart;
 import org.dwfa.ace.api.ebr.I_ThinExtByRefPartConcept;
 import org.dwfa.ace.api.ebr.I_ThinExtByRefPartConceptString;
@@ -35,17 +31,14 @@ import org.dwfa.ace.api.ebr.I_ThinExtByRefPartString;
 import org.dwfa.ace.api.ebr.I_ThinExtByRefTuple;
 import org.dwfa.ace.api.ebr.I_ThinExtByRefVersioned;
 import org.dwfa.ace.task.profile.NewDefaultProfile;
-import org.dwfa.cement.ArchitectonicAuxiliary;
 import org.dwfa.maven.MojoUtil;
 import org.dwfa.mojo.ConceptDescriptor;
 import org.dwfa.mojo.PositionDescriptor;
 import org.dwfa.mojo.epicexport.kp.EpicLoadFileFactory;
-import org.dwfa.mojo.refset.ExportSpecification;
-// import org.dwfa.mojo.refset.RefsetType;
-//import org.dwfa.mojo.vivisimo.IntSet;
-//import org.dwfa.mojo.vivisimo.GenerateVivisimoThesaurus.ExportIterator;
 import org.dwfa.tapi.TerminologyException;
-import org.dwfa.vodb.types.IntSet;
+
+
+
 
 
 /**
@@ -75,10 +68,10 @@ public class ExportToEpicLoadFilesMojo extends AbstractMojo {
 	/**
 	 * If true, do not create thesaurus entries for preferred terms with no synonyms.
 	 *
-	 * @parameter
-	 * @required
+	 * 
+	 * 
 	 */
-	private ExportSpecification[] specs;
+	// private ExportSpecification[] specs;
 
 	/**
 	 * Positions to export data.
@@ -109,6 +102,24 @@ public class ExportToEpicLoadFilesMojo extends AbstractMojo {
       * @parameter expression="${project.build.directory}"
       * @required
       */
+     
+     /**
+      * The date to start looking for changes
+      * 
+      * @parameter
+      * @required
+      */
+     private String deltaStartDate;
+     
+     /**
+      * The name of the drop
+      * 
+      * @paremeter
+      * @required
+      */
+     private String dropName;
+     
+     
      private File targetDirectory;
 
 	private HashSet<I_Position> positions;
@@ -150,26 +161,15 @@ public class ExportToEpicLoadFilesMojo extends AbstractMojo {
 				outputDirectory = outputDirectory + "/";
 			}
 			
-			/* File outputDirFile = new File(outputDirectory);
-			outputDirFile.mkdirs();
-			File dataFile = new File(outputDirectory
-					+ dataFileName);
-			outputDirFile.getParentFile().mkdirs();
-			Writer dataWriter = new BufferedWriter(new FileWriter(
-					dataFile));
-			*/
-			// getLog().info("About to iterate through concepts");
-			ExportIterator expItr = new ExportIterator();
+			ExportIterator expItr = new ExportIterator(outputDirectory, dropName, deltaStartDate);
 			
 			
 			I_GetConceptData concept = termFactory.getConcept(UUID.fromString("a7130b8c-e6c1-57d8-986a-0d88552c12e4"));
 			expItr.processConcept(concept);
 			
-			expItr.close();
+			
             // LocalVersionedTerminology.get().iterateConcepts(expItr);
-			//dataWriter.write("</thesaurus>\n");
-
-			//dataWriter.close();
+			expItr.close();
 
 		} catch (Exception e) {
 			getLog().error(e);
@@ -180,21 +180,31 @@ public class ExportToEpicLoadFilesMojo extends AbstractMojo {
      
     private class ExportIterator implements I_ProcessConcepts {
 
-		private Writer dataWriter;
-		// private IntSet preferred = new IntSet();
-		// private IntSet synonym = new IntSet();
-		private int maxWords;
-		private int minWords;
 		private String currentItem;
 		
 		private String currentMasterFile;
 		private EpicLoadFileFactory exportFactory;
 		private EpicExportManager exportManager;
-
-		public ExportIterator()
+		private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd'T'hhmmss'Z'");
+		private int startingVersion;
+		private String dropName;
+		
+		public ExportIterator(String outputDir, String drop, String start)
 		{
+			Date parsedDate = new Date();
+			
 			exportFactory = new EpicLoadFileFactory();
-			exportManager = exportFactory.getExportManager("c:/temp");
+			exportManager = exportFactory.getExportManager(outputDir);
+			this.dropName = drop;
+			try {
+				parsedDate = dateFormat.parse(start);
+			} catch (java.text.ParseException e) {
+				getLog().error("Invalid date: " + start + " - use format yyyymmssThhmmssZ+8");
+				e.printStackTrace();
+			}
+	    	
+	    	this.startingVersion = termFactory.convertToThinVersion(parsedDate.getTime());
+			
 			
 		}
 
@@ -203,21 +213,13 @@ public class ExportToEpicLoadFilesMojo extends AbstractMojo {
 		}
 		
 		public void processConcept(I_GetConceptData concept) throws Exception {
-			// getLog().info("Checking concept id " + concept.getId());
-			//if (isExportable(concept)) {
-				getLog().info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Processing concept: " + concept);
-				exportRefsetsForConcept(concept);
-				// getLog().info("**** Tested concept id " + concept.getId());
-			//}
+			exportRefsetsForConcept(concept);
 			List<I_DescriptionVersioned> descriptions = concept.getDescriptions();
 			for (Iterator<I_DescriptionVersioned> i = descriptions.iterator(); i.hasNext();) {
 				I_DescriptionVersioned desc = i.next();
-				//getLog().info(" Description: " + desc);
 				List<I_DescriptionTuple> dts = desc.getTuples();
 				for (Iterator<I_DescriptionTuple> ti = dts.iterator(); ti.hasNext();) {
 					I_DescriptionTuple descTuple = ti.next();
-					//getLog().info(" Description tuple: " + descTuple.getText());
-					//getLog().info("Description tuple concept: " + termFactory.getConcept(descTuple.getConceptId()));
 				}
 				
 			}
@@ -232,6 +234,7 @@ public class ExportToEpicLoadFilesMojo extends AbstractMojo {
 	                for (I_ThinExtByRefTuple thinExtByRefTuple : thinExtByRefVersioned.getTuples(statusValues,
 	                    positions, false, false)) {
 	                	getLog().info("getTypeid()=" + thinExtByRefVersioned.getTypeId());
+                	
 	                	export(thinExtByRefTuple, concept);
 	                }
 	        	}else {
@@ -239,93 +242,122 @@ public class ExportToEpicLoadFilesMojo extends AbstractMojo {
 	        	}
 	        }
 	        I_EpicLoadFileBuilder exportWriter = exportManager.getLoadFileBuilder(this.currentMasterFile);
-	        exportWriter.writeRecord("version");
+	        exportWriter.writeRecord(this.dropName);
 	        exportWriter.newExportRecord();
 	    }
 
 	    void export(I_ThinExtByRefTuple thinExtByRefTuple, I_GetConceptData parentConcept) throws Exception {
-	    	getLog().info("thinExtByRefTuple = " + thinExtByRefTuple);	    	
+	    	getLog().info("thinExtByRefTuple = " + thinExtByRefTuple);	 
 	        export(thinExtByRefTuple.getPart(), thinExtByRefTuple.getMemberId(), thinExtByRefTuple.getRefsetId(),
-	            thinExtByRefTuple.getComponentId(), parentConcept);
+	            thinExtByRefTuple.getComponentId(), parentConcept, getPreviousVersion(thinExtByRefTuple));
 	    }
 
-	    void export(I_ThinExtByRefPart thinExtByRefPart, Integer memberId, int refsetId, int componentId, I_GetConceptData parentConcept) throws Exception {
+	    void export(I_ThinExtByRefPart thinExtByRefPart, Integer memberId, int refsetId, int componentId, 
+	    		I_GetConceptData parentConcept, I_ThinExtByRefPart previousVersion) throws Exception {
     	
             I_GetConceptData refsetConcept = termFactory.getConcept(refsetId);
             String refsetName = refsetConcept.getInitialText();
-	       exportRefset(refsetName, refsetConcept, thinExtByRefPart, parentConcept);
+	       exportRefset(refsetName, refsetConcept, thinExtByRefPart, parentConcept, previousVersion);
+	    }
+
+	    private I_ThinExtByRefPart getPreviousVersion(I_ThinExtByRefTuple thinExtByRefTuple) throws Exception {
+	    	List<? extends I_ThinExtByRefPart> versions = thinExtByRefTuple.getVersions();
+	    	
+	    	I_ThinExtByRefPart newestOldVersion  = null;
+	    	int max = 0;
+	    	for (Iterator<? extends I_ThinExtByRefPart> i = versions.iterator(); i.hasNext(); ) {
+	    		I_ThinExtByRefPart v = i.next();
+	    		getLog().info("Found version " + v.getVersion());
+	    		getLog().info("Looking for version " + this.startingVersion); 
+	    		if (v.getVersion() <= this.startingVersion) {
+		    		if (newestOldVersion == null) {
+		    			newestOldVersion = v;
+		    			max = v.getVersion();
+		    		}
+		    			
+		    		if (v.getVersion() > max){
+		    			max = v.getVersion();
+		    			newestOldVersion = v;
+		    		}
+	    		}
+	    	}
+	    	return newestOldVersion;
+	    }
+
+	    //TODO: Exploratory code
+	    void exploreConcept(I_GetConceptData concept, I_ThinExtByRefPart thinExtByRefPart) throws Exception {
+	    	for (I_IdPart part : concept.getId().getVersions()) {
+	    		part.getVersion();
+	    		
+	    	}
 	    }
 	    
-	    private boolean isExportable(I_GetConceptData concept) throws Exception {
-			 for (ExportSpecification spec : specs) {
-				if (spec.test(concept)) {
-					return true;
-				}
-			}
-			return true;
-		}
-
-
-	    boolean testSpecification(I_GetConceptData concept) throws Exception {
-	        for (ExportSpecification spec : specs) {
-	            if (spec.test(concept)) {
-	                return true;
-	            }
-	        }
-	        
-	    	return true;
-	    }
-	    boolean testSpecification(int id) throws TerminologyException, IOException, Exception {
-	        return testSpecification(termFactory.getConcept(id));
-	    }
-
-	    
-	    public void exportRefset(String refsetName, I_GetConceptData concept, I_ThinExtByRefPart thinExtByRefPart, I_GetConceptData parentConcept) throws Exception {
+	    public void exportRefset(String refsetName, I_GetConceptData concept, 
+	    		I_ThinExtByRefPart thinExtByRefPart, I_GetConceptData parentConcept,
+	    		I_ThinExtByRefPart previousVersion) throws Exception {
 	    	getLog().info("** exportRefset: refsetName=" + refsetName);
 	    	this.setCurrentItem(null, null);
 	    	String stringValue = null;
+	    	//TODO: Re-factor into separate class, allow pattern matching, store and read from pom.xml
 	    	if(refsetName.equals("EDG Billing Item 207")) {
 	    		this.setCurrentItem(EpicExportManager.EPIC_MASTERFILE_NAME_EDG_BILLING, "207");
 	    	}
-	    	if(refsetName.equals("EDG Billing Item 2000")) {
+	    	else if(refsetName.equals("EDG Billing Item 2000")) {
 	    		this.setCurrentItem(EpicExportManager.EPIC_MASTERFILE_NAME_EDG_BILLING, "2000");
 	    	}
-	    	if(refsetName.equals("EDG Billing Item 2")) {
+	    	else if(refsetName.equals("EDG Billing Item 2")) {
 	    		this.setCurrentItem(EpicExportManager.EPIC_MASTERFILE_NAME_EDG_BILLING, "2");
 	    		stringValue = getDisplayName(parentConcept);
 	    	}
-	    	if(refsetName.equals("EDG Billing Item 40")) {
+	    	else if(refsetName.equals("EDG Billing Item 40")) {
 	    		this.setCurrentItem(EpicExportManager.EPIC_MASTERFILE_NAME_EDG_BILLING, "40");
+	    	}
+	    	else if(refsetName.equals("EDG Billing Item 11")) {
+	    		this.setCurrentItem(EpicExportManager.EPIC_MASTERFILE_NAME_EDG_BILLING, "11");
+	    	}
+	    	else if(refsetName.equals("EDG Clinical Item 11")) {
+	    		this.setCurrentItem(EpicExportManager.EPIC_MASTERFILE_NAME_EDG_CLINICAL, "11");
+	    	}
+	    	else if(refsetName.equals("EDG Clinical Item 2")) {
+	    		this.setCurrentItem(EpicExportManager.EPIC_MASTERFILE_NAME_EDG_CLINICAL, "2");
+	    	}
+	    	else if(refsetName.equals("EDG Clinical Item 200")) {
+	    		this.setCurrentItem(EpicExportManager.EPIC_MASTERFILE_NAME_EDG_CLINICAL, "200");
+	    	}
+	    	else if(refsetName.equals("EDG Clinical Item 2000")) {
+	    		this.setCurrentItem(EpicExportManager.EPIC_MASTERFILE_NAME_EDG_CLINICAL, "2000");
+	    	}
+	    	else if(refsetName.equals("EDG Clinical Item 40")) {
+	    		this.setCurrentItem(EpicExportManager.EPIC_MASTERFILE_NAME_EDG_CLINICAL, "40");
+	    	}
+	    	else if(refsetName.equals("EDG Clinical Item 7010")) {
+	    		this.setCurrentItem(EpicExportManager.EPIC_MASTERFILE_NAME_EDG_CLINICAL, "7010");
 	    	}
 	    	
 	    	if (this.currentItem != null) {
 	    		if (stringValue == null)
 	    			stringValue = getValueAsString(thinExtByRefPart);
 	    		I_EpicLoadFileBuilder exportWriter = exportManager.getLoadFileBuilder(this.currentMasterFile);
-	    		getLog().info("Exporting item " + this.currentItem + " with a value of " + stringValue);	    		
-	    		exportWriter.sendItemForExport(this.currentItem, stringValue, null);
+	    		String previousStringValue = getValueAsString(previousVersion);
+	    		getLog().info("Exporting item " + this.currentItem + " with a value of " + stringValue +
+	    				" and a previous value of " + previousStringValue);
+	    		
+	    		exportWriter.sendItemForExport(this.currentItem, stringValue, previousStringValue);
 	    	}
 	    }
 	    
 	    public String getValueAsString(I_ThinExtByRefPart thinExtByRefPart) {
 	    	String value = null;
-	    	if (I_ThinExtByRefPartString.class.isAssignableFrom(thinExtByRefPart.getClass())) {
-	    		I_ThinExtByRefPartString str = (I_ThinExtByRefPartString) thinExtByRefPart;
-	    		value = str.getStringValue();
+	    	if (thinExtByRefPart != null) {
+		    	if (I_ThinExtByRefPartString.class.isAssignableFrom(thinExtByRefPart.getClass())) {
+		    		I_ThinExtByRefPartString str = (I_ThinExtByRefPartString) thinExtByRefPart;
+		    		value = str.getStringValue();
+		    	}
+		    	if (I_ThinExtByRefPartInteger.class.isAssignableFrom(thinExtByRefPart.getClass())) {
+		    		I_ThinExtByRefPartInteger str = (I_ThinExtByRefPartInteger) thinExtByRefPart;
+		    		value = new Integer(str.getIntValue()).toString();
+		    	}
 	    	}
-	    	if (I_ThinExtByRefPartInteger.class.isAssignableFrom(thinExtByRefPart.getClass())) {
-	    		I_ThinExtByRefPartInteger str = (I_ThinExtByRefPartInteger) thinExtByRefPart;
-	    		value = new Integer(str.getIntValue()).toString();
-	    	}
-	    	if (I_ThinExtByRefPartConcept.class.isAssignableFrom(thinExtByRefPart.getClass())) {
-	    		I_ThinExtByRefPartConcept str = (I_ThinExtByRefPartConcept) thinExtByRefPart;
-	    		value = "I_ThinExtByRefPartConcept";
-	    	}
-	    	if (I_ThinExtByRefPartConceptString.class.isAssignableFrom(thinExtByRefPart.getClass())) {
-	    		I_ThinExtByRefPartConceptString str = (I_ThinExtByRefPartConceptString) thinExtByRefPart;
-	    		value = "I_ThinExtByRefPartConceptString";
-	    	}
-	    	
 	    	return value;
 
 	    }
@@ -334,21 +366,35 @@ public class ExportToEpicLoadFilesMojo extends AbstractMojo {
 	    	this.currentItem = item;
 	    }
 	    
-	    public String getDisplayName(I_GetConceptData conceptData) throws Exception {
+	    public String xxgetDisplayName(I_GetConceptData conceptData) throws Exception {
 	    	String ret = null;
 	
+	    	conceptData.getDescriptions();
+	    	
             I_ConfigAceFrame newConfig = NewDefaultProfile.newProfile("", "", "", "", "");
-            //I_GetConceptData refsetConcept = termFactory.getConcept(UUID.fromString("6fd32c1f-8096-40a1-9053-1cc204bc61e3"));
             if (newConfig != null) {
             	I_DescriptionTuple it = conceptData.getDescTuple(newConfig.getLongLabelDescPreferenceList(), newConfig);
             	if (it == null)
-            		ret = "?";
+            		ret = null;
             	else
             		ret = it.getText();
             }
 	    	return ret;
 	    }
 
+	    public String getDisplayName(I_GetConceptData conceptData) throws Exception {
+	    	String ret = null;
+	
+	    	List<I_DescriptionVersioned> descs = conceptData.getDescriptions();
+	    	for (Iterator<I_DescriptionVersioned> i = descs.iterator(); i.hasNext();) {
+	    		I_DescriptionVersioned d = i.next();
+	    		I_DescriptionTuple dt = d.getLastTuple();
+	    		I_DescriptionPart part = dt.getPart();
+	    		ret = part.getText();
+	    	}
+	    	
+	    	return ret;
+	    }
     }
 
 
