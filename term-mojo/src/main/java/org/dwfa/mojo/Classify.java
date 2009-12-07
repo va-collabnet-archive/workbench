b34e1aa99041b3d2070281fa8e553733d5e833d5
attila <attila>
2009-12-07 03:34:02 +0000

    Create initial layout. Branch old repositories to trunk

---
+++ term-mojo/src/main/java/org/dwfa/mojo/Classify.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2009 International Health Terminology Standards Development
+ * Organisation
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.dwfa.mojo;
+
+import java.io.File;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.dwfa.ace.api.I_TermFactory;
+import org.dwfa.ace.api.LocalVersionedTerminology;
+import org.dwfa.maven.MojoUtil;
+
+/**
+ * 
+ * 
+ * @goal classify
+ * 
+ * @author michaellawley
+ *
+ */
+public class Classify extends AbstractMojo {
+
+    /**
+     * The classify factory name
+     *
+     * @parameter
+     */
+    String classifyFactory = "java.lang.String";
+
+    /**
+     * Location of the build directory.
+     *
+     * @parameter expression="${project.build.directory}"
+     * @required
+     */
+    private File targetDirectory;
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        try {
+            if (MojoUtil.alreadyRun(getLog(), this.getClass()
+                .getCanonicalName()
+                + classifyFactory, this.getClass(), targetDirectory)) {
+                return;
+            }
+        } catch (NoSuchAlgorithmException e) {
+            throw new MojoExecutionException(e.getLocalizedMessage(), e);
+        } catch (IOException e) {
+            throw new MojoExecutionException(e.getLocalizedMessage(), e);
+        }
+        I_TermFactory termFactory = LocalVersionedTerminology.get();
+
+        getLog().info("classify mojo stub");
+    }
+
+}
