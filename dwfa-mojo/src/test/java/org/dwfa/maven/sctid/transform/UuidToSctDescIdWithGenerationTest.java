ff3d327eae60a58f36b3ea0978d7011b23cc57ea
attila <attila>
2009-12-07 03:58:11 +0000

    Create initial layout for Nehta branch. Branch old repositories.

---
+++ dwfa-mojo/src/test/java/org/dwfa/maven/sctid/transform/UuidToSctDescIdWithGenerationTest.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2009 International Health Terminology Standards Development
+ * Organisation
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.dwfa.maven.sctid.transform;
+
+import java.io.File;
+import java.util.Date;
+import java.util.Random;
+import java.util.UUID;
+
+import junit.framework.TestCase;
+
+import org.dwfa.maven.transform.SctIdGenerator.NAMESPACE;
+
+public class UuidToSctDescIdWithGenerationTest extends TestCase {
+    Random random = new Random(new Date().getTime());
+    UuidToSctDescIdWithGeneration uuidToSctDescIdWithGeneration;
+    Long loadTestSize = 10001l;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        uuidToSctDescIdWithGeneration = new UuidToSctDescIdWithGeneration();
+        uuidToSctDescIdWithGeneration.setupImpl(null, new File("target", "TestDb"));
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        uuidToSctDescIdWithGeneration.cleanup(null);
+    }
+
+    private NAMESPACE getRandomNamespace() {
+        return NAMESPACE.values()[random.nextInt(NAMESPACE.values().length)];
+    }
+
+    public void testConceptIdGeneration() throws Exception {
+        UUID uuid = UUID.randomUUID();
+        NAMESPACE namespace = getRandomNamespace();
+        String sctId = uuidToSctDescIdWithGeneration.transform(uuid.toString(), namespace);
+
+        assertTrue("Must return the same sctId for the same UUID",
+                sctId.equals(uuidToSctDescIdWithGeneration.transform(uuid.toString(), namespace)));
+    }
+
+    public void testLoad() throws Exception {
+        for(int i = 0; i < loadTestSize; i++){
+            String sctId = uuidToSctDescIdWithGeneration.transform(UUID.randomUUID().toString(), getRandomNamespace());
+            assertNotNull(sctId);
+        }
+    }
+}
