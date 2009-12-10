069f86283780fb81dc6716c47278044c6b8b887a
sneiner <sneiner>
2009-12-10 00:33:41 +0000

    Developing export to Epic load files

--- src/main/java/org/dwfa/mojo/epicexport/I_EpicLoadFileBuilder.java
+++ src/main/java/org/dwfa/mojo/epicexport/I_EpicLoadFileBuilder.java
@@ -25,120 +25,111 @@ import org.dwfa.mojo.epicexport.kp.EpicLoadFileFactory;
 
 public interface I_EpicLoadFileBuilder {
 
-    public EpicLoadFileFactory getExportFactory();
+	public EpicLoadFileFactory getExportFactory();
 
-    public void setExportFactory(EpicLoadFileFactory exportManager);
+	public void setExportFactory(EpicLoadFileFactory exportManager);
 
-    public I_EpicExportRecordWriter getWriter();
+	public I_EpicExportRecordWriter getWriter();
 
-    public void setWriter(I_EpicExportRecordWriter writer);
+	public void setWriter(I_EpicExportRecordWriter writer);
 
-    /**
-     * Populates an Epic item number value pair for export into loadfiles
-     * 
-     * @param refsetId - The field number from the refest which identifies what
-     *            the
-     *            element is
-     * @param value - The Object that contains the value
-     * @param previousValue - The object that contains the previous value
-     */
-    public void sendItemForExport(String item, Object value, Object previousValue);
+	/**
+	 * Populates an Epic item number value pair for export into loadfiles
+	 * 
+	 * @param refsetId - The field number from the refest which identifies what the
+	 * element is
+	 * @param value - The Object that contains the value
+	 * @param previousValue - The object that contains the previous value
+	 */
+	public void addItemForExport(String item, Object value, Object previousValue);
+	
+	/** Prepares the writer for receiving values for a record; initializes the
+	 * buffer of values.
+	 */
+	public void clearRecordContents();
+	
+	public abstract String getEpicItemNumber(int refsetNumber);
+	
+	/** 
+	 * Abstract method to determine which items export and how.
+	 * 
+	 * @param version - Passed to determine which load file to write to
+	 * @throws Exception
+	 */
+	public abstract void writeRecord(String version) throws Exception;
+	
+	/**
+	 * Writes an item to the Epic load file
+	 * 
+	 * @param @param epicItemNumber - The item number of the value to write to the load file
+	 * @param aliasItemNumber - The item number to use in the load file
+	 */
+	public void writeItem(String epicItemNumber, String aliasItemNumber);
+	
+	/**
+	 * Writes an item to the Epic load file
+	 * 
+	 * @param epicItemNumber - The item number to write to the load file
+	 */
+	public void writeItem(String epicItemNumber);
+	
+	public String getMasterfile();
 
-    /**
-     * Prepares the writer for receiving values for a record; initializes the
-     * buffer of values.
-     */
-    public void newExportRecord();
+	public void setMasterfile(String masterfile); 
+	
+	public I_GetConceptData getParentConcept();
+	
+	public void setIdConcept(I_GetConceptData parentConcept);
 
-    public abstract String getEpicItemNumber(int refsetNumber);
+	/**
+	 * If an item has no value set, will populate the the file with the value of another
+	 * item.  If the value of epicItemNumber is set, will populate with that item.
+	 * 
+	 * @param epicItemNumber - The item number to check for if populated. I
+	 * @param defaultItem - If not populated, will use this item number to populate.
+	 */
+	public void writeItemWithDefault(String epicItemNumber, String defaultItem);
 
-    /**
-     * Abstract method to determine which items export and how.
-     * 
-     * @param version - Passed to determine which load file to write to
-     * @throws Exception
-     */
-    public abstract void writeRecord(String version) throws Exception;
+	/**
+	 * If an item has changed value since the last version, will populate the load file,
+	 * otherwise will leave it out of the load file.
+	 * 
+	 * @param epicItemNumber - The item number of the value to populate
+	 * @param aliasItemNumber - The item number that will appear in the load file.
+	 */
+	public void writeItemIfChanged(String epicItemNumber, String aliasItemNumber);
+	
+	/**
+	 * If an item has changed value since the last version, will populate the load file,
+	 * otherwise will leave it out of the load file.
+	 * 
+	 * @param epicItemNumber - The item number of the value to populate
+	 */
+	public void writeItemIfChanged(String epicItemNumber);
+	
+	/**
+	 * Write an item number / literal value combination to the load file
+	 * 
+	 * @param epicItemNumber
+	 * @param literal
+	 */
+	public void writeLiteralItem(String epicItemNumber, String literal);
+	
+	/**
+	 * Determines whether an item has any changes between the current value
+	 * and previous values
+	 * 
+	 * @param epicItemNumber - The item number to check for changes
+	 * @return - true if an item has any changes
+	 */
+	public boolean itemHasChanges(String epicItemNumber);
 
-    /**
-     * Writes an item to the Epic load file
-     * 
-     * @param @param epicItemNumber - The item number of the value to write to
-     *        the load file
-     * @param aliasItemNumber - The item number to use in the load file
-     */
-    public void writeItem(String epicItemNumber, String aliasItemNumber);
-
-    /**
-     * Writes an item to the Epic load file
-     * 
-     * @param epicItemNumber - The item number to write to the load file
-     */
-    public void writeItem(String epicItemNumber);
-
-    public String getMasterfile();
-
-    public void setMasterfile(String masterfile);
-
-    public I_GetConceptData getParentConcept();
-
-    public void setParentConcept(I_GetConceptData parentConcept);
-
-    /**
-     * If an item has no value set, will populate the the file with the value of
-     * another
-     * item. If the value of epicItemNumber is set, will populate with that
-     * item.
-     * 
-     * @param epicItemNumber - The item number to check for if populated. I
-     * @param defaultItem - If not populated, will use this item number to
-     *            populate.
-     */
-    public void writeItemWithDefault(String epicItemNumber, String defaultItem);
-
-    /**
-     * If an item has changed value since the last version, will populate the
-     * load file,
-     * otherwise will leave it out of the load file.
-     * 
-     * @param epicItemNumber - The item number of the value to populate
-     * @param aliasItemNumber - The item number that will appear in the load
-     *            file.
-     */
-    public void writeItemIfChanged(String epicItemNumber, String aliasItemNumber);
-
-    /**
-     * If an item has changed value since the last version, will populate the
-     * load file,
-     * otherwise will leave it out of the load file.
-     * 
-     * @param epicItemNumber - The item number of the value to populate
-     */
-    public void writeItemIfChanged(String epicItemNumber);
-
-    /**
-     * Write an item number / literal value combination to the load file
-     * 
-     * @param epicItemNumber
-     * @param literal
-     */
-    public void writeLiteralItem(String epicItemNumber, String literal);
-
-    /**
-     * Determines whether an item has any changes between the current value
-     * and previous values
-     * 
-     * @param epicItemNumber - The item number to check for changes
-     * @return - true if an item has any changes
-     */
-    public boolean itemHasChanges(String epicItemNumber);
-
-    /**
-     * Looks for the first occurrence of the item in the exported items cache.
-     * 
-     * @param epicItemNumber
-     * @return the first item found
-     */
-    public EpicItem getFirstItem(String epicItemNumber);
+	/**
+	 * Looks for the first occurrence of the item in the exported items cache.
+	 * 
+	 * @param epicItemNumber
+	 * @return the first item found
+	 */
+	public EpicItem getFirstItem(String epicItemNumber);
 
 }
--- src/main/java/org/dwfa/mojo/epicexport/kp/EpicExportBuilderEDGClinical.java
+++ src/main/java/org/dwfa/mojo/epicexport/kp/EpicExportBuilderEDGClinical.java
@@ -14,11 +14,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.dwfa.mojo.epicexport.kp;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
 
+import org.dwfa.ace.log.AceLog;
 import org.dwfa.mojo.epicexport.AbstractEpicExportBuilder;
 import org.dwfa.mojo.epicexport.EpicExportManager;
 import org.dwfa.mojo.epicexport.EpicExportWriter;
@@ -26,103 +28,112 @@ import org.dwfa.mojo.epicexport.I_EpicExportRecordWriter;
 import org.dwfa.mojo.epicexport.I_EpicLoadFileBuilder;
 
 public class EpicExportBuilderEDGClinical extends AbstractEpicExportBuilder implements I_EpicLoadFileBuilder {
-    public static final int DISPLAY_NAME = 2;
-    public static final int ITEM_11 = 11;
-    public static final String[] INTERESTED_ITEMS = { "2", "40", "50", "100", "7010", "7000", "80", "91", "207" };
-    public static final String[] EREC_ITEMS = { "50", "7010" };
-    // public static final String[] MANDATORY_ITEMS = {"2", "40", "7000",
-    // "7010", "91", "80", "100", "207"};
-    public static final String[] MANDATORY_ITEMS = { "2", "40", "7000", "7010", "100", "207" };
+	public static final int DISPLAY_NAME = 2;
+	public static final int ITEM_11 = 11;
+	public static final String[] INTERESTED_ITEMS = {"2", "40", "50", "100", "2000", "200", "7010", "7000", "80", "91", "207"};
+	public static final String[] EREC_ITEMS = {"50", "7010"};
+	// public static final String[] MANDATORY_ITEMS = {"2", "40", "7000", "7010", "91", "80", "100", "207"};
+	public static final String[] MANDATORY_ITEMS = {"2", "40", "7000", "7010", "100", "207"};
+	
+	private String masterfile = EpicExportManager.EPIC_MASTERFILE_NAME_EDG_CLINICAL;
+	
+	public EpicExportBuilderEDGClinical(EpicLoadFileFactory exportManager, EpicExportManager em) {
+		super(exportManager, em);
+		this.setExportIfTheseItemsChanged(INTERESTED_ITEMS);
+	}
 
-    private String masterfile = EpicExportManager.EPIC_MASTERFILE_NAME_EDG_CLINICAL;
-
-    public EpicExportBuilderEDGClinical(EpicLoadFileFactory exportManager, EpicExportManager em) {
-        super(exportManager, em);
-        this.setExportIfTheseItemsChanged(INTERESTED_ITEMS);
-    }
-
-    public String getEpicItemNumber(int refsetNumber) {
-        // TODO just for now return the same number
-        return new Integer(refsetNumber).toString();
-    }
-
-    public void writeRecord(String version) throws Exception {
-
-        if (this.isChangedRecord()) {
-            if (getFirstItem("11") == null) {
-                // "NRNC" Its a new record
-                // if (! this.allItemsArePopulated(MANDATORY_ITEMS))
-                // throw new
-                // Exception("One or more mandatory items are missing");
-                I_EpicExportRecordWriter writer = getExportManager().getWriter(
-                    getWriterName(masterfile, version, "nrnc"));
-                this.setWriter(writer);
-                writer.newRecord();
-                writeLiteralItem("1", "*");
-                writeItem("2");
-                writeItem("40");
-                /*
-                 * if (this.itemIsPopulated("2000"))
-                 * writeItem("2000");
-                 * else if (this.itemIsPopulated("200"))
-                 * writeItem("200");
-                 * else
-                 * throw new Exception("Missing both items 200 and 2000");
-                 */
-                writeItemIfChanged("100");
-                writeItemIfChanged("207");
-                writeItemIfChanged("80");
-                writeItemIfChanged("91");
-                writeItemIfChanged("7010");
-                writeLiteralItem("35", this.getParentUUID());
-                writer.saveRecord();
-
-            } else {
-                if (this.onlyHasChangesIn(EREC_ITEMS)) {
-                    // "EREC" Existing record existing contact
-                    I_EpicExportRecordWriter writer = getExportManager().getWriter(
-                        getWriterName(masterfile, version, "erec"));
-                    this.setWriter(writer);
-                    writer.newRecord();
-                    writeItem("11", "1");
-                    writeItem("2");
-                    writeItem("40");
-                    writeItemIfChanged("100");
-                    writeItemIfChanged("207");
-                    writeItemIfChanged("80");
-                    writeItemIfChanged("91");
-                    writeItemIfChanged("7010");
-                    writer.saveRecord();
-                } else {
-                    // "ERNC" Existing record new contact
-                    I_EpicExportRecordWriter writer = getExportManager().getWriter(
-                        getWriterName(masterfile, version, "ernc"));
-                    this.setWriter(writer);
-                    writer.newRecord();
-                    writeItem("2");
-                    writeItem("11", "1");
-                    writeItem("40");
-                    writeItemIfChanged("100");
-                    writeItemIfChanged("207");
-                    writeItemIfChanged("80");
-                    writeItemIfChanged("91");
-                    writeItemIfChanged("7010");
-                    writer.saveRecord();
-                }
-            }
-        }
-    }
-
-    private String getWriterName(String masterfile, String version, String contact) {
-        StringBuffer ret = new StringBuffer(masterfile);
-        if (version != null) {
-            ret.append('_');
-            ret.append(version);
-        }
-        if (contact != null) {
-            ret.append('_');
-            ret.append(contact);
-        }
-        return ret.toString();
-    }
+	public String getEpicItemNumber(int refsetNumber) {
+		// TODO just for now return the same number
+		return new Integer(refsetNumber).toString();
+	}
+	
+	public void writeRecord(String version) throws Exception {
+		
+		if (this.isChangedRecord()) {
+// AceLog.getAppLog().info("Item 11 = " + getFirstItem("11"));
+			if (getFirstItem("11") == null) {
+				//"NRNC" Its a new record
+				if (! this.allItemsArePopulated(MANDATORY_ITEMS))
+					AceLog.getAppLog().warning("One or more mandatory items are missing");
+				//	throw new Exception("One or more mandatory items are missing");
+				I_EpicExportRecordWriter writer = getExportManager().getWriter(getWriterName(masterfile, version, "nrnc"));
+				this.setWriter(writer);
+				writer.newRecord();
+				writeLiteralItem("1", "*");
+				writeItem("2");
+				//writeItem("35");
+				writeLiteralItem("35", this.getParentUUID());
+				writeItem("40");
+				if (this.itemIsPopulated("2000"))
+					writeItem("2000");
+				else if (this.itemIsPopulated("200"))
+					writeItem("200");
+				/* else
+					throw new Exception("Missing both items 200 and 2000");
+					*/
+				writeItemIfChanged("100");
+				writeItemIfChanged("207");
+				writeItemIfChanged("80");
+				writeItemIfChanged("91");
+				writeItemIfChanged("7010");
+				writer.saveRecord();
+				
+			}
+			else {
+				if (this.onlyHasChangesIn(EREC_ITEMS)) {
+					// "EREC" Existing record existing contact
+					I_EpicExportRecordWriter writer = getExportManager().getWriter(getWriterName(masterfile, version, "erec"));
+					this.setWriter(writer);
+					writer.newRecord();
+					writeItem("11", "1");
+					writeItem("2");
+					writeLiteralItem("20", "L");
+					writeItem("40");
+					writeItem("35");
+					writeItemIfChanged("100");
+					if (this.itemIsPopulated("2000"))
+						writeItem("2000");
+					else if (this.itemIsPopulated("200"))
+						writeItem("200");
+					writeItemIfChanged("207");
+					writeItemIfChanged("80");
+					writeItemIfChanged("91");
+					writeItemIfChanged("7010");
+					writer.saveRecord();
+				} else {
+					// "ERNC" Existing record new contact
+					I_EpicExportRecordWriter writer = getExportManager().getWriter(getWriterName(masterfile, version, "ernc"));
+					this.setWriter(writer);
+					writer.newRecord();
+					writeItem("2");
+					writeItem("11", "1");
+					if (this.itemIsPopulated("2000"))
+						writeItem("2000");
+					else if (this.itemIsPopulated("200"))
+						writeItem("200");
+					writeItem("35");
+					writeItem("40");
+					writeItemIfChanged("100");
+					writeItemIfChanged("207");
+					writeItemIfChanged("80");
+					writeItemIfChanged("91");
+					writeItemIfChanged("7010");
+					writer.saveRecord();
+				}
+			}
+		}
+	}
+	
+	private String getWriterName(String masterfile, String version, String contact) {
+		StringBuffer ret = new StringBuffer(masterfile);
+		if (version != null) {
+			ret.append('_');
+			ret.append(version);
+		}
+		if (contact != null) {
+			ret.append('_');
+			ret.append(contact);
+		}
+		return ret.toString();
+	}
 }
