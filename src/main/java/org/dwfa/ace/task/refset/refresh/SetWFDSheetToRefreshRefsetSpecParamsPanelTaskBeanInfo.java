54eab2be8f268c727eb9e1bb14e8d4a9e08fbef0
wpreid <wpreid>
2009-12-11 18:27:33 +0000

    Added support for selecting a Reviewer to review the Refresh changes.

--- src/main/java/org/dwfa/ace/task/refset/refresh/SetWFDSheetToRefreshRefsetSpecParamsPanelTaskBeanInfo.java
+++ src/main/java/org/dwfa/ace/task/refset/refresh/SetWFDSheetToRefreshRefsetSpecParamsPanelTaskBeanInfo.java
@@ -24,14 +24,12 @@ import java.beans.SimpleBeanInfo;
 import org.dwfa.bpa.tasks.editor.PropertyNameLabelEditor;
 
 /**
- * The SetWFDSheetToRefreshRefsetSpecParamsPanelTaskBeanInfo class describes the
- * visible elements of the
- * Workflow task SetWFDSheetToRefreshRefsetSpecParamsPanelTask so that it can be
- * displayed in the
- * Process Builder.
+ * The SetWFDSheetToRefreshRefsetSpecParamsPanelTaskBeanInfo class describes the visible elements of the 
+ * Workflow task SetWFDSheetToRefreshRefsetSpecParamsPanelTask so that it can be displayed in the 
+ * Process Builder. 
  * 
- * @author Perry Reid
- * @version 1.0, November 2009
+ * @author  Perry Reid
+ * @version 1.0, November 2009 
  */
 public class SetWFDSheetToRefreshRefsetSpecParamsPanelTaskBeanInfo extends SimpleBeanInfo {
 
@@ -39,38 +37,29 @@ public class SetWFDSheetToRefreshRefsetSpecParamsPanelTaskBeanInfo extends Simpl
         super();
     }
 
-    /**
-     * Returns a list of property descriptors for this task.
-     * 
-     * @return Returns a PropertyDescriptor array containing the properties of
-     *         this task
-     * @exception Error Thrown when an exception happens during Introspection
-     */
+	/**
+	 * Returns a list of property descriptors for this task.   
+	 * @return  	Returns a PropertyDescriptor array containing the properties of this task  
+	 * @exception  	Error Thrown when an exception happens during Introspection
+	 */
     public PropertyDescriptor[] getPropertyDescriptors() {
-        try {
+        try {  
 
-            // The color "green" = denotes an [IN] property
-            // The color "blue" = denotes an [OUT] property
-            PropertyDescriptor profilePropName = new PropertyDescriptor("profilePropName",
-                getBeanDescriptor().getBeanClass());
+        	// The color "green" = denotes an [IN] property 
+        	// The color "blue"  = denotes an [OUT] property 
+            PropertyDescriptor profilePropName =
+                new PropertyDescriptor("profilePropName", getBeanDescriptor().getBeanClass());
             profilePropName.setBound(true);
             profilePropName.setPropertyEditorClass(PropertyNameLabelEditor.class);
             profilePropName.setDisplayName("<html><font color='green'>profile prop:");
             profilePropName.setShortDescription("[IN] The property that contains the working profile.");
 
-            PropertyDescriptor editorInboxPropName;
-            editorInboxPropName = new PropertyDescriptor("editorInboxPropName", getBeanDescriptor().getBeanClass());
-            editorInboxPropName.setBound(true);
-            editorInboxPropName.setPropertyEditorClass(PropertyNameLabelEditor.class);
-            editorInboxPropName.setDisplayName("<html><font color='green'>editor inbox prop name:");
-            editorInboxPropName.setShortDescription("[IN] The next person the BP will go to.");
-
-            PropertyDescriptor commentsPropName;
-            commentsPropName = new PropertyDescriptor("commentsPropName", getBeanDescriptor().getBeanClass());
-            commentsPropName.setBound(true);
-            commentsPropName.setPropertyEditorClass(PropertyNameLabelEditor.class);
-            commentsPropName.setDisplayName("<html><font color='green'>comments prop name:");
-            commentsPropName.setShortDescription("[IN] The property that contains the comments.");
+            PropertyDescriptor ownerUuidPropName;
+            ownerUuidPropName = new PropertyDescriptor("ownerUuidPropName", getBeanDescriptor().getBeanClass());
+            ownerUuidPropName.setBound(true);
+            ownerUuidPropName.setPropertyEditorClass(PropertyNameLabelEditor.class);
+            ownerUuidPropName.setDisplayName("<html><font color='green'>owner uuid prop name:");
+            ownerUuidPropName.setShortDescription("[IN] The property that contains the owner UUID.");
 
             PropertyDescriptor refsetUuidPropName;
             refsetUuidPropName = new PropertyDescriptor("refsetUuidPropName", getBeanDescriptor().getBeanClass());
@@ -86,39 +75,52 @@ public class SetWFDSheetToRefreshRefsetSpecParamsPanelTaskBeanInfo extends Simpl
             editorUuidPropName.setDisplayName("<html><font color='green'>editor UUID prop name:");
             editorUuidPropName.setShortDescription("[IN] The property that contains the editor UUID.");
 
-            PropertyDescriptor ownerUuidPropName;
-            ownerUuidPropName = new PropertyDescriptor("ownerUuidPropName", getBeanDescriptor().getBeanClass());
-            ownerUuidPropName.setBound(true);
-            ownerUuidPropName.setPropertyEditorClass(PropertyNameLabelEditor.class);
-            ownerUuidPropName.setDisplayName("<html><font color='green'>owner uuid prop name:");
-            ownerUuidPropName.setShortDescription("[IN] The property that contains the owner UUID.");
+            PropertyDescriptor editorInboxPropName;
+            editorInboxPropName =
+                    new PropertyDescriptor("editorInboxPropName", getBeanDescriptor().getBeanClass());
+            editorInboxPropName.setBound(true);
+            editorInboxPropName.setPropertyEditorClass(PropertyNameLabelEditor.class);
+            editorInboxPropName.setDisplayName("<html><font color='green'>editor inbox prop name:");
+            editorInboxPropName.setShortDescription("[IN] The next person the BP will go to.");
+
+            PropertyDescriptor reviewerUuidPropName;
+            reviewerUuidPropName = new PropertyDescriptor("reviewerUuidPropName", getBeanDescriptor().getBeanClass());
+            reviewerUuidPropName.setBound(true);
+            reviewerUuidPropName.setPropertyEditorClass(PropertyNameLabelEditor.class);
+            reviewerUuidPropName.setDisplayName("<html><font color='green'>reviewer UUID prop name:");
+            reviewerUuidPropName.setShortDescription("[IN] The property that contains the reviewer UUID.");
+
+            PropertyDescriptor commentsPropName;
+            commentsPropName = new PropertyDescriptor("commentsPropName", getBeanDescriptor().getBeanClass());
+            commentsPropName.setBound(true);
+            commentsPropName.setPropertyEditorClass(PropertyNameLabelEditor.class);
+            commentsPropName.setDisplayName("<html><font color='green'>comments prop name:");
+            commentsPropName.setShortDescription("[IN] The property that contains the comments.");
 
             PropertyDescriptor fileAttachmentsPropName;
-            fileAttachmentsPropName = new PropertyDescriptor("fileAttachmentsPropName",
-                getBeanDescriptor().getBeanClass());
+            fileAttachmentsPropName = new PropertyDescriptor("fileAttachmentsPropName", getBeanDescriptor().getBeanClass());
             fileAttachmentsPropName.setBound(true);
             fileAttachmentsPropName.setPropertyEditorClass(PropertyNameLabelEditor.class);
             fileAttachmentsPropName.setDisplayName("<html><font color='green'>file attachments prop name:");
             fileAttachmentsPropName.setShortDescription("[IN] The property that contains the file attachments.");
 
-            PropertyDescriptor rv[] = { profilePropName, editorInboxPropName, commentsPropName, refsetUuidPropName,
-                                       editorUuidPropName, ownerUuidPropName, fileAttachmentsPropName };
 
+            PropertyDescriptor rv[] =
+                    { profilePropName, ownerUuidPropName, refsetUuidPropName, editorUuidPropName, editorInboxPropName, 
+            		  reviewerUuidPropName, commentsPropName, fileAttachmentsPropName };
+            
             return rv;
         } catch (IntrospectionException e) {
-            throw new Error(e.toString());
+             throw new Error(e.toString());
         }
     }
 
-    /**
-     * Return the descriptor for this JavaBean which contains a reference to the
-     * JavaBean
-     * that implements this task as well as the display name of the task along
-     * with
-     * formating information.
-     * 
+    /** 
+     * Return the descriptor for this JavaBean which contains a reference to the JavaBean 
+	 * that implements this task as well as the display name of the task along with 
+	 * formating information.
      * @see java.beans.BeanInfo#getBeanDescriptor()
-     * @return Returns the BeanDescriptor for this task
+	 * @return	Returns the BeanDescriptor for this task      
      */
     public BeanDescriptor getBeanDescriptor() {
         BeanDescriptor bd = new BeanDescriptor(SetWFDSheetToRefreshRefsetSpecParamsPanelTask.class);
