4f3b62e01e93979ada163676825aca980365bab6
kec <kec>
2009-12-30 01:01:25 +0000

    Checkpoint after making base object implment core, part, and tuple to reduce the number of objects.

---
+++ bdb/src/main/java/org/ihtsdo/db/bdb/concept/component/refsetmember/floatConcept/MeasurementVersion.java
@@ -0,0 +1,53 @@
+package org.ihtsdo.db.bdb.concept.component.refsetmember.floatConcept;
+
+import java.io.IOException;
+
+import org.dwfa.ace.api.I_AmPart;
+import org.dwfa.ace.api.I_Path;
+import org.dwfa.ace.api.ebr.I_ThinExtByRefPart;
+import org.dwfa.ace.utypes.UniversalAceExtByRefPart;
+import org.dwfa.tapi.TerminologyException;
+import org.ihtsdo.db.bdb.concept.component.refsetmember.RefsetMemberMutablePart;
+
+import com.sleepycat.bind.tuple.TupleInput;
+
+public class MeasurementVersion extends RefsetMemberMutablePart {
+
+	public MeasurementVersion(int statusNid, int pathNid, long time) {
+		super(statusNid, pathNid, time);
+	}
+
+	public MeasurementVersion(int statusAtPositionNid) {
+		super(statusAtPositionNid);
+	}
+
+	public MeasurementVersion(TupleInput input) {
+		super(input);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public UniversalAceExtByRefPart getUniversalPart()
+			throws TerminologyException, IOException {
+		// TODO
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public I_ThinExtByRefPart makePromotionPart(I_Path promotionPath) {
+		// TODO
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public int compareTo(I_ThinExtByRefPart o) {
+		// TODO
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public MeasurementVersion getMutablePart() {
+		return this;
+	}
+
+}
