9c1d5bff3cbfca24c005cf911d42e59f02e41287
kec <kec>
2009-12-30 00:59:48 +0000

    Checkpoint after making base object implment core, part, and tuple to reduce the number of objects.

---
+++ bdb/src/main/java/org/ihtsdo/db/bdb/concept/component/refsetmember/language/LanguageVersion.java
@@ -0,0 +1,52 @@
+package org.ihtsdo.db.bdb.concept.component.refsetmember.language;
+
+import java.io.IOException;
+
+import org.dwfa.ace.api.I_Path;
+import org.dwfa.ace.api.ebr.I_ThinExtByRefPart;
+import org.dwfa.ace.utypes.UniversalAceExtByRefPart;
+import org.dwfa.tapi.TerminologyException;
+import org.ihtsdo.db.bdb.concept.component.refsetmember.RefsetMemberMutablePart;
+
+import com.sleepycat.bind.tuple.TupleInput;
+
+public class LanguageVersion extends RefsetMemberMutablePart {
+
+	public LanguageVersion(int statusNid, int pathNid, long time) {
+		super(statusNid, pathNid, time);
+	}
+
+	public LanguageVersion(int statusAtPositionNid) {
+		super(statusAtPositionNid);
+	}
+
+	public LanguageVersion(TupleInput input) {
+		super(input);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public UniversalAceExtByRefPart getUniversalPart()
+			throws TerminologyException, IOException {
+		// TODO
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public I_ThinExtByRefPart makePromotionPart(I_Path promotionPath) {
+		// TODO
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public int compareTo(I_ThinExtByRefPart o) {
+		// TODO
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public LanguageVersion getMutablePart() {
+		return this;
+	}
+
+}
