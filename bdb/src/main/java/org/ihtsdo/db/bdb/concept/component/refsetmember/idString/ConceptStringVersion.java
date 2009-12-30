6b8b6289961a1153b66b1182084dc91424c44492
kec <kec>
2009-12-30 01:00:12 +0000

    Checkpoint after making base object implment core, part, and tuple to reduce the number of objects.

---
+++ bdb/src/main/java/org/ihtsdo/db/bdb/concept/component/refsetmember/idString/ConceptStringVersion.java
@@ -0,0 +1,61 @@
+package org.ihtsdo.db.bdb.concept.component.refsetmember.idString;
+
+import java.io.IOException;
+
+import org.dwfa.ace.api.I_Path;
+import org.dwfa.ace.api.ebr.I_ThinExtByRefPart;
+import org.dwfa.ace.api.ebr.I_ThinExtByRefPartConceptString;
+import org.dwfa.ace.utypes.UniversalAceExtByRefPart;
+import org.dwfa.tapi.TerminologyException;
+import org.ihtsdo.db.bdb.concept.component.refsetmember.id.ConceptVersion;
+
+import com.sleepycat.bind.tuple.TupleInput;
+
+public class ConceptStringVersion extends ConceptVersion 
+	implements I_ThinExtByRefPartConceptString {
+
+	private String str;
+	
+	public ConceptStringVersion(int statusNid, int pathNid, long time) {
+		super(statusNid, pathNid, time);
+	}
+
+	public ConceptStringVersion(int statusAtPositionNid) {
+		super(statusAtPositionNid);
+	}
+
+	public ConceptStringVersion(TupleInput input) {
+		super(input);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public UniversalAceExtByRefPart getUniversalPart()
+			throws TerminologyException, IOException {
+		// TODO
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public I_ThinExtByRefPart makePromotionPart(I_Path promotionPath) {
+		// TODO
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public int compareTo(I_ThinExtByRefPart o) {
+		// TODO
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public String getStr() {
+		return str;
+	}
+
+	@Override
+	public void setStr(String str) {
+		this.str = str;
+	}
+
+}
