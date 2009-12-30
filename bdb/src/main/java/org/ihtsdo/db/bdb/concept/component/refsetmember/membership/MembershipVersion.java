b259780a575cf7c1fe1018e4b14a2564dc3bd54a
kec <kec>
2009-12-30 00:59:35 +0000

    Checkpoint after making base object implment core, part, and tuple to reduce the number of objects.

---
+++ bdb/src/main/java/org/ihtsdo/db/bdb/concept/component/refsetmember/membership/MembershipVersion.java
@@ -0,0 +1,52 @@
+package org.ihtsdo.db.bdb.concept.component.refsetmember.membership;
+
+import java.io.IOException;
+
+import org.dwfa.ace.api.I_Path;
+import org.dwfa.ace.api.ebr.I_ThinExtByRefPart;
+import org.dwfa.ace.utypes.UniversalAceExtByRefPart;
+import org.dwfa.tapi.TerminologyException;
+import org.ihtsdo.db.bdb.concept.component.refsetmember.RefsetMemberMutablePart;
+
+import com.sleepycat.bind.tuple.TupleInput;
+
+public class MembershipVersion extends RefsetMemberMutablePart {
+
+	
+	public MembershipVersion(int statusNid, int pathNid, long time) {
+		super(statusNid, pathNid, time);
+	}
+
+	public MembershipVersion(int statusAtPositionNid) {
+		super(statusAtPositionNid);
+	}
+
+	public MembershipVersion(TupleInput input) {
+		super(input);
+	}
+
+	@Override
+	public UniversalAceExtByRefPart getUniversalPart()
+			throws TerminologyException, IOException {
+		// TODO
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public RefsetMemberMutablePart makePromotionPart(I_Path promotionPath) {
+		// TODO
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public int compareTo(I_ThinExtByRefPart o) {
+		// TODO
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public MembershipVersion getMutablePart() {
+		return this;
+	}
+
+}
