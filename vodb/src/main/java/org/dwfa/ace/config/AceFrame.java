6589527e7e4c579310c78e5320938402dffdce0b
scrow <scrow>
2010-01-14 01:02:41 +0000

    TCP-2056. Reinstate the wizard panel - required to prompt user for input

--- vodb/src/main/java/org/dwfa/ace/config/AceFrame.java
+++ vodb/src/main/java/org/dwfa/ace/config/AceFrame.java
@@ -118,7 +118,7 @@ public class AceFrame extends ComponentFrame {
         WizardPanel wizPanel = new WizardPanel(getCdePanel());
         layers.add(wizPanel, JLayeredPane.MODAL_LAYER);
         wizPanel.setLocation(400, 0);
-        wizPanel.setVisible(false);
+        wizPanel.setVisible(true);
 
         getCdePanel().setWorkflowDetailsSheet(wizPanel.getWfDetailsPanel());
         getCdePanel().setWorkflowPanel(wizPanel.getWfPanel());
