5eeb9faca77bd26564f71e2eaa3d3ecec1e21afe
chrissy <chrissy>
2010-01-20 16:28:46 +0000

    [artf5889] added task for setting the PT from a UUID

---
+++ ace-api/src/main/java/org/dwfa/ace/task/prop/SetPTStringPropertyFromUuidBeanInfo.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2009 International Health Terminology Standards Development
+ * Organisation
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.dwfa.ace.task.prop;
+
+import java.beans.BeanDescriptor;
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
+import java.beans.SimpleBeanInfo;
+
+import org.dwfa.bpa.tasks.editor.PropertyNameLabelEditor;
+
+public class SetPTStringPropertyFromUuidBeanInfo extends SimpleBeanInfo {
+
+    public PropertyDescriptor[] getPropertyDescriptors() {
+        try {
+
+            PropertyDescriptor uuidPropName =
+                    new PropertyDescriptor("uuidPropName", getBeanDescriptor().getBeanClass());
+            uuidPropName.setBound(true);
+            uuidPropName.setPropertyEditorClass(PropertyNameLabelEditor.class);
+            uuidPropName.setDisplayName("<html><font color='green'>profile prop:");
+            uuidPropName.setShortDescription("The property that contains the profile to modify.");
+
+            PropertyDescriptor ptPropName = new PropertyDescriptor("ptPropName", getBeanDescriptor().getBeanClass());
+            ptPropName.setBound(true);
+            ptPropName.setPropertyEditorClass(PropertyNameLabelEditor.class);
+            ptPropName.setDisplayName("<html><font color='green'>pt prop:");
+            ptPropName.setShortDescription("The property to put the pt into.");
+
+            PropertyDescriptor rv[] = { uuidPropName, ptPropName };
+            return rv;
+        } catch (IntrospectionException e) {
+            throw new Error(e.toString());
+        }
+    }
+
+    /**
+     * @see java.beans.BeanInfo#getBeanDescriptor()
+     */
+    public BeanDescriptor getBeanDescriptor() {
+        BeanDescriptor bd = new BeanDescriptor(SetPTStringPropertyFromUuid.class);
+        bd.setDisplayName("<html><font color='green'><center>Set PT String<br>prop from UUID");
+        return bd;
+    }
+}
