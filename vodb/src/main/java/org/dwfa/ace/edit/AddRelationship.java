eec1c42e8927866aff2b499bf2180b1094cc267e
kec <kec>
2010-02-06 00:13:08 +0000

    Modified AddRelationship to use the API rather than hidden classes. Modified intset to implement equals.

--- vodb/src/main/java/org/dwfa/ace/edit/AddRelationship.java
+++ vodb/src/main/java/org/dwfa/ace/edit/AddRelationship.java
@@ -22,12 +22,8 @@ import java.util.UUID;
 import org.dwfa.ace.api.I_ConfigAceFrame;
 import org.dwfa.ace.api.I_ContainTermComponent;
 import org.dwfa.ace.api.I_GetConceptData;
-import org.dwfa.ace.api.I_Path;
 import org.dwfa.ace.api.Terms;
 import org.dwfa.ace.log.AceLog;
-import org.dwfa.cement.ArchitectonicAuxiliary;
-import org.dwfa.vodb.types.ThinRelPart;
-import org.dwfa.vodb.types.ThinRelVersioned;
 
 public class AddRelationship extends AddComponent {
 
@@ -39,38 +35,19 @@ public class AddRelationship extends AddComponent {
     protected void doEdit(I_ContainTermComponent termContainer, ActionEvent e, I_ConfigAceFrame config)
             throws Exception {
     	I_GetConceptData cb = (I_GetConceptData) termContainer.getTermComponent();
-        UUID newRelUid = UUID.randomUUID();
-        int idSource = Terms.get().uuidToNative(ArchitectonicAuxiliary.Concept.UNSPECIFIED_UUID.getUids());
-        int relId = Terms.get().uuidToNativeWithGeneration(newRelUid, idSource, config.getEditingPathSet(),
-            Integer.MAX_VALUE);
-        int parentId = Integer.MAX_VALUE;
-
-        if (config.getHierarchySelection() != null) {
-            parentId = config.getHierarchySelection().getConceptId();
-        } else {
-            parentId = ArchitectonicAuxiliary.Concept.ARCHITECTONIC_ROOT_CONCEPT.localize().getNid();
-        }
-        if (cb != null) {
-            ThinRelVersioned rel = new ThinRelVersioned(relId, cb.getConceptId(), parentId, 1);
-            ThinRelPart relPart = new ThinRelPart();
-            rel.addVersion(relPart);
-            int status = config.getDefaultStatus().getConceptId();
-            for (I_Path p : termContainer.getConfig().getEditingPathSet()) {
-                relPart.setVersion(Integer.MAX_VALUE);
-                relPart.setPathId(p.getConceptId());
-                relPart.setStatusId(status);
-                relPart.setTypeId(config.getDefaultRelationshipType().getConceptId());
-                relPart.setCharacteristicId(config.getDefaultRelationshipCharacteristic().getConceptId());
-                relPart.setRefinabilityId(config.getDefaultRelationshipRefinability().getConceptId());
-                relPart.setGroup(0);
-            }
-            cb.getUncommittedSourceRels().add(rel);
-            cb.getUncommittedIds().add(relId);
-            Terms.get().addUncommitted(cb);
-            termContainer.setTermComponent(cb);
-        } else {
+        if (cb == null) {
             AceLog.getAppLog().alertAndLogException(
                 new Exception("Cannot add a relationship while the component viewer is empty..."));
+        } else {
+            Terms.get().newRelationship(UUID.randomUUID(), cb, 
+            		config.getDefaultRelationshipType(),
+            		config.getHierarchySelection(),
+            		config.getDefaultRelationshipCharacteristic(),
+            		config.getDefaultRelationshipRefinability(), 
+        			config.getDefaultStatus(),
+        			0, config);
+            Terms.get().addUncommitted(cb);
+            termContainer.setTermComponent(cb);
         }
     }
 
