56a777f37fcad441e7f7a0c0d29c277d7a8ad2d4
edungey <edungey>
2010-02-24 05:53:39 +0000

    TCP-1941 Added new DatabaseExport mojo, Position and ExportSpecification.
    Re factored some ThinExtByRefPart to fix inheritance problems ie ConceptString now extends Concept
    
    Update export mojos and test.
    
    Updated IdentiferWriter and re factored Ace2Rf2

---
+++ dwfa-mojo/src/main/java/org/dwfa/dto/BaseConceptDto.java
@@ -0,0 +1,126 @@
+/**
+ * Copyright (c) 2009 International Health Terminology Standards Development
+ * Organisation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.dwfa.dto;
+
+import java.util.Date;
+import java.util.UUID;
+
+import org.dwfa.maven.transform.SctIdGenerator.NAMESPACE;
+import org.dwfa.maven.transform.SctIdGenerator.TYPE;
+
+/**
+ * Concept details for exporting.
+ */
+public class BaseConceptDto {
+    private UUID pathId;
+    private UUID status;
+    private Date dateTime;
+    private boolean isActive;
+    private NAMESPACE namespace;
+    private TYPE type;
+
+    /**
+     * Bean constructor.
+     */
+    public BaseConceptDto() {
+
+    }
+
+    /**
+     * @return the pathId
+     */
+    public UUID getPathId() {
+        return pathId;
+    }
+
+    /**
+     * @param pathId the pathId to set
+     */
+    public void setPathId(UUID pathId) {
+        this.pathId = pathId;
+    }
+
+    /**
+     * @return the status
+     */
+    public UUID getStatusId() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatusId(UUID status) {
+        this.status = status;
+    }
+
+    /**
+     * @return the dateTime
+     */
+    public Date getDateTime() {
+        return dateTime;
+    }
+
+    /**
+     * @param dateTime the dateTime to set
+     */
+    public void setDateTime(Date dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    /**
+     * @return the isActive
+     */
+    public boolean isActive() {
+        return isActive;
+    }
+
+    /**
+     * @param isActive the isActive to set
+     */
+    public void setActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    /**
+     * @return the namespace
+     */
+    public NAMESPACE getNamespace() {
+        return namespace;
+    }
+
+    /**
+     * @param namespace the namespace to set
+     */
+    public void setNamespace(NAMESPACE namespace) {
+        this.namespace = namespace;
+    }
+
+    /**
+     * @return the type
+     */
+    public TYPE getType() {
+        return type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(TYPE type) {
+        this.type = type;
+    }
+}
