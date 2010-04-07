52d67cf424b1e8c8765d3f8f9685cdb9d1a5336c
aflinton <aflinton>
2010-04-07 10:16:15 +0000

    Added a few more statics

--- wb-gui/src/main/java/org/ihtsdo/custom/statics/CustomStatics.java
+++ wb-gui/src/main/java/org/ihtsdo/custom/statics/CustomStatics.java
@@ -5,11 +5,23 @@ public class CustomStatics {
 	/** The name of the custom UI properties file*/
 	public static String CUSTOMPROPS = "customprops";
 	
+	/** The name of the custom UI properties file*/
+	public static String CUSTOMPROPSFN = "custompropsFullName";
+	
 	/** The name of the custom UI Class must implement I_ReturnMainPanel */
 	public static String CUSTOM_UI_CLASSNAME = "customUIClassname";
 	
-	/** Used to state is there is a custom class **/
+	/** Used to state if there is a custom UI class **/
 	public static String CUSTOM_UI_CLASS = "customUIClass";
 	
+
+	/** Used to get all the conceptService **/
+	public static String CUSTOM_UI_CONCEPTSERVICE = "conceptService";
+	/** Used to get all the handlers associated with the conceptService **/
+	public static String CUSTOM_UI_CONCEPTSERVICENAME = "conceptServiceName";
+	
+	
+	
+
 
 }
