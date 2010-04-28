4678c6df34d7e02729c14d4c0a26ebb78b8d8d61
edungey <edungey>
2010-04-28 04:25:59 +0000

    SCTAU-423 turn on changeset writing after loading changesets

--- ace-api/src/main/java/org/dwfa/ace/task/cs/ImportAllChangeSets.java
+++ ace-api/src/main/java/org/dwfa/ace/task/cs/ImportAllChangeSets.java
@@ -1,13 +1,13 @@
 /**
  * Copyright (c) 2009 International Health Terminology Standards Development
  * Organisation
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -37,7 +37,7 @@ import org.dwfa.util.bean.Spec;
 
 /**
  * @author kec
- * 
+ *
  */
 @BeanList(specs = { @Spec(directory = "tasks/ide/change sets", type = BeanType.TASK_BEAN) })
 public class ImportAllChangeSets extends AbstractTask {
@@ -90,9 +90,12 @@ public class ImportAllChangeSets extends AbstractTask {
             rootDirStr = (String) process.readAttachement(rootDirStr.split(ProcessAttachmentKeys.getAttachmentPrefix())[1]);
         }
 
-        LocalVersionedTerminology.get().suspendChangeSetWriters();
-        importAllChangeSets(worker.getLogger());
-        LocalVersionedTerminology.get().resumeChangeSetWriters();
+        try {
+            LocalVersionedTerminology.get().suspendChangeSetWriters();
+            importAllChangeSets(worker.getLogger());
+        } finally {
+            LocalVersionedTerminology.get().resumeChangeSetWriters();
+        }
 
         return Condition.CONTINUE;
     }
