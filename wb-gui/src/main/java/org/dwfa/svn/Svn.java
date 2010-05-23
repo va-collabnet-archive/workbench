bc0fdad5620cd3f7dcaddb336c22c8fa37bb0d3d
kec <kec>
2010-05-23 05:39:33 +0000

    Test for file exists before adding.

--- wb-gui/src/main/java/org/dwfa/svn/Svn.java
+++ wb-gui/src/main/java/org/dwfa/svn/Svn.java
@@ -266,7 +266,7 @@ public class Svn implements I_HandleSubversion {
                     	}
 
                     }
-                } else if (s.isIgnored() == false) {
+                } else if (s.isIgnored() == false && new File(s.getPath()).exists()) {
                     int depth = Depth.infinity;
                     boolean force = false;
                     boolean noIgnores = false;
