20ffa0be0baf5e46d7f81563c598c509e5719d84
aflinton <aflinton>
2010-06-15 10:31:20 +0000

    replacing CementLoadTest with latest from wb-toolkit-2.5.x branch

--- wb-bdb/src/test/java/org/ihtsdo/db/bdb/CementLoadTest.java
+++ wb-bdb/src/test/java/org/ihtsdo/db/bdb/CementLoadTest.java
@@ -8,9 +8,13 @@ import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
 
+import org.dwfa.ace.api.I_IterateIds;
 import org.dwfa.ace.api.I_RepresentIdSet;
 import org.dwfa.ace.api.IdentifierSet;
 import org.dwfa.ace.api.Terms;
@@ -75,30 +79,64 @@ public class CementLoadTest {
 			Bdb.close();
 			
 			Bdb.setup(dbTarget);
-			I_RepresentIdSet cids = Bdb.getConceptDb().getConceptIdSet();
-			System.out.println("cementLoadTest Concept count: " + cids.cardinality());
+			IdentifierSet cids = (IdentifierSet) Bdb.getConceptDb().getConceptIdSet();
+			IdentifierSet roCids = Bdb.getConceptDb().getReadOnlyConceptIdSet();
+			System.out.println("Differences: " + cids.getDifferences(roCids));
+			System.out.println("Concept count: " + cids.cardinality());
+			System.out.println("roCids count: " + roCids.cardinality());
+			Assert.assertTrue(cids.equals(roCids));
+			Assert.assertEquals(cids.cardinality(), roCids.cardinality());
+			System.out.println("Concept count: " + cids.cardinality());
 			
 			AddStringIdProcessor addProcessor  = new AddStringIdProcessor(cids);
 			Bdb.getConceptDb().iterateConceptDataInParallel(addProcessor);
-			System.out.println("cementLoadTest Unprocessed: " + addProcessor.getCids().cardinality());
-			Assert.assertEquals(0, addProcessor.getCids().cardinality());
+			System.out.println("Unprocessed: " + addProcessor.getCids().cardinality());
+			if (addProcessor.getCids().cardinality() != 0) {
+				System.out.println("Unprocessed nids[0]: " + addProcessor.getCids());
+			}
+			System.out.println("Unprocessed conCids: " + addProcessor.getConCids().size());
+			if (addProcessor.getCids().cardinality() != 0) {
+				System.out.println("Unprocessed conCids[0]: " + addProcessor.getConCids().size());
+			}
+			//Assert.assertEquals(0, addProcessor.getCids().cardinality());
 			Bdb.commit();
 			
 			CheckStringIdProcessor csidp = new CheckStringIdProcessor(cids);
 			Bdb.getConceptDb().iterateConceptDataInParallel(csidp);
-			System.out.println("cementLoadTest Unmatched: " + csidp.getCids().cardinality());
-			Assert.assertEquals(0, csidp.getCids().cardinality());
+			System.out.println("Unmatched: " + csidp.getCids().cardinality());
+			if (csidp.getCids().cardinality() != 0) {
+				System.out.println("Unprocessed nids[1]: " + csidp.getCids());
+			}
+			System.out.println("Unmatched conCids: " + csidp.getConCids().size());
+			if (addProcessor.getCids().cardinality() != 0) {
+				System.out.println("Unprocessed conCids[1]: " + csidp.getConCids().size());
+			}
+			//Assert.assertEquals(0, csidp.getCids().cardinality());
 			Bdb.close();
 			
 			Bdb.setup(dbTarget);
 			csidp = new CheckStringIdProcessor(cids);
 			Bdb.getConceptDb().iterateConceptDataInParallel(csidp);
-			Bdb.close();
 			System.out.println("Unmatched: " + csidp.getCids().cardinality());
+			if (csidp.getCids().cardinality() != 0) {
+				System.out.println("Unprocessed nids[2]: " + csidp.getCids());
+			}
+			System.out.println("Unmatched conCids: " + csidp.getConCids().size());
+			if (addProcessor.getCids().cardinality() != 0) {
+				System.out.println("Unprocessed conCids[2]: " + csidp.getConCids().size());
+			}
 			Assert.assertEquals(0, csidp.getCids().cardinality());
 		} catch (Exception e) {
 			e.printStackTrace();
 			Assert.fail(e.toString());
+		} finally {
+			try {
+				Bdb.close();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} catch (ExecutionException e) {
+				e.printStackTrace();
+			}
 		}
 
 	}
@@ -106,19 +144,30 @@ public class CementLoadTest {
 	private static class AddStringIdProcessor implements I_ProcessConceptData {
 
 		I_RepresentIdSet cids;
+		ConcurrentHashMap<Integer, Integer> conCids;
 		
+		public ConcurrentHashMap<Integer, Integer> getConCids() {
+			return conCids;
+		}
+
 		public I_RepresentIdSet getCids() {
 			return cids;
 		}
 
-		public AddStringIdProcessor(I_RepresentIdSet cids) {
+		public AddStringIdProcessor(I_RepresentIdSet cids) throws IOException {
 			super();
 			this.cids = new IdentifierSet((IdentifierSet) cids);
+			conCids = new ConcurrentHashMap<Integer, Integer>(cids.cardinality());
+			I_IterateIds cidItr = cids.iterator();
+			while (cidItr.next()) {
+				conCids.put(cidItr.nid(), cidItr.nid());
+			}
 		}
 
 		@Override
 		public void processConceptData(Concept concept) throws Exception {
 			cids.setNotMember(concept.getNid());
+			conCids.remove(concept.getNid());
 			String uuidOid = "2.25." + concept.getConceptAttributes().getPrimUuid();
 			int statusNid = Bdb.uuidsToNid(ArchitectonicAuxiliary.Concept.CURRENT.getUids());
 			int pathNid = Bdb.uuidsToNid(ArchitectonicAuxiliary.Concept.ARCHITECTONIC_BRANCH.getUids());
@@ -140,14 +189,24 @@ public class CementLoadTest {
 	
 	private static class CheckStringIdProcessor implements I_ProcessConceptData {
 		I_RepresentIdSet cids;
+		ConcurrentHashMap<Integer, Integer> conCids;
+		
+		public ConcurrentHashMap<Integer, Integer> getConCids() {
+			return conCids;
+		}
 		
 		public I_RepresentIdSet getCids() {
 			return cids;
 		}
 
-		public CheckStringIdProcessor(I_RepresentIdSet cids) {
+		public CheckStringIdProcessor(I_RepresentIdSet cids) throws IOException {
 			super();
 			this.cids = new IdentifierSet((IdentifierSet) cids);
+			conCids = new ConcurrentHashMap<Integer, Integer>(cids.cardinality());
+			I_IterateIds cidItr = cids.iterator();
+			while (cidItr.next()) {
+				conCids.put(cidItr.nid(), cidItr.nid());
+			}
 		}
 
 		@Override
@@ -158,6 +217,7 @@ public class CementLoadTest {
 				for (IdentifierVersion id: ids) {
 					if (id.getDenotation().equals(uuidOid)) {
 						cids.setNotMember(concept.getNid());						
+						conCids.remove(concept.getNid());
 					}
 				}
 			} else {
