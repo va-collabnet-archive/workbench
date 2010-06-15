9afd344c7dedef3f5d14353b992b73cb328b5eef
kec <kec>
2010-06-15 02:39:43 +0000

    Integrated Adam's fix to missing identifiers in change sets.

--- wb-bdb/src/main/java/org/ihtsdo/cs/econcept/EConceptChangeSetComputer.java
+++ wb-bdb/src/main/java/org/ihtsdo/cs/econcept/EConceptChangeSetComputer.java
@@ -300,30 +300,37 @@ public class EConceptChangeSetComputer implements I_ComputeEConceptForChangeSet 
         ec.revisions.add(ev);
     }
 
-    @SuppressWarnings("unchecked")
-    private void setupFirstVersion(EComponent ec, ConceptComponent<?, ?>.Version v) throws IOException {
-        ec.primordialUuid = v.getPrimUuid();
-        ec.setPathUuid(Bdb.getPrimUuidForConcept(v.getPathId()));
-        ec.setStatusUuid(Bdb.getPrimUuidForConcept(v.getStatusId()));
-        ec.setTime(v.getTime());
-        if (v.getAdditionalIdentifierParts() != null) {
-            for (IdentifierVersion idv : v.getAdditionalIdentifierParts()) {
-                EIdentifier eIdv = null;
-                if (idv.getSapNid() >= minSapNid && idv.getSapNid() <= maxSapNid && v.getTime() != Long.MIN_VALUE) {
-                    if (IdentifierVersionLong.class.isAssignableFrom(idv.getClass())) {
-                        eIdv = new EIdentifierLong();
-                    } else if (IdentifierVersionString.class.isAssignableFrom(idv.getClass())) {
-                        eIdv = new EIdentifierString();
-                    } else if (IdentifierVersionUuid.class.isAssignableFrom(idv.getClass())) {
-                        eIdv = new EIdentifierUuid();
-                    }
-                    eIdv.setDenotation(idv.getDenotation());
-                    eIdv.setAuthorityUuid(Bdb.getPrimUuidForConcept(idv.getAuthorityNid()));
-                    eIdv.setPathUuid(Bdb.getPrimUuidForConcept(idv.getPathId()));
-                    eIdv.setStatusUuid(Bdb.getPrimUuidForConcept(idv.getStatusId()));
-                    eIdv.setTime(idv.getTime());
-                }
-            }
-        }
-    }
+	@SuppressWarnings("unchecked")
+	private void setupFirstVersion(EComponent ec,
+			ConceptComponent<?, ?>.Version v) throws IOException {
+		ec.primordialUuid = v.getPrimUuid();
+		ec.setPathUuid(Bdb.getPrimUuidForConcept(v.getPathId()));
+		ec.setStatusUuid(Bdb.getPrimUuidForConcept(v.getStatusId()));
+		ec.setTime(v.getTime());
+		if (v.getAdditionalIdentifierParts() != null) {
+			List<EIdentifier> additionalIdComponents = new ArrayList<EIdentifier>(v.getAdditionalIdentifierParts().size());
+			for (IdentifierVersion idv : v.getAdditionalIdentifierParts()) {
+				EIdentifier eIdv = null;
+				if (idv.getSapNid() >= minSapNid && idv.getSapNid() <= maxSapNid && v.getTime() != Long.MIN_VALUE) {
+					if (IdentifierVersionLong.class.isAssignableFrom(idv.getClass())) {
+						eIdv = new EIdentifierLong();
+					} else if (IdentifierVersionString.class.isAssignableFrom(idv.getClass())) {
+						eIdv = new EIdentifierString();
+					} else if (IdentifierVersionUuid.class.isAssignableFrom(idv.getClass())) {
+						eIdv = new EIdentifierUuid();
+					}
+					eIdv.setDenotation(idv.getDenotation());
+					eIdv.setAuthorityUuid(Bdb.getPrimUuidForConcept(idv.getAuthorityNid()));
+					eIdv.setPathUuid(Bdb.getPrimUuidForConcept(idv.getPathId()));
+					eIdv.setStatusUuid(Bdb.getPrimUuidForConcept(idv.getStatusId()));
+					eIdv.setTime(idv.getTime());
+					additionalIdComponents.add(eIdv);
+				}
+			}
+			if (additionalIdComponents.size() > 0) {
+				ec.setAdditionalIdComponents(additionalIdComponents);
+			}
+		}
+	}
+
 }
