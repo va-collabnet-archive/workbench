f2d29630a78576f0451acf5fbba217ecfe3f5a2d
aflinton <aflinton>
2010-08-03 10:46:46 +0000

    debugging BDBTF newRelationship

--- wb-bdb/src/main/java/org/ihtsdo/db/bdb/BdbTermFactory.java
+++ wb-bdb/src/main/java/org/ihtsdo/db/bdb/BdbTermFactory.java
@@ -1179,6 +1179,8 @@ public class BdbTermFactory implements I_TermFactory, I_ImplementTermFactory, I_
             }
         }
         c.getSourceRels().add(r);
+        AceLog.getAppLog().info("BDBTF newRelationship r ="+r );
+        AceLog.getAppLog().info("BDBTF newRelationship c ="+c.toLongString() );
         return r;
     }
 
