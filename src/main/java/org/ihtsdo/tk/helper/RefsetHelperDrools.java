f26d59fba87af0537d3c9408f81e6c65e2c773dc
alopez <alopez>
2010-09-23 22:23:01 +0000

    Added a refsetMemberhelper to drools helpers

---
+++ src/main/java/org/ihtsdo/tk/helper/RefsetHelperDrools.java
@@ -0,0 +1,12 @@
+package org.ihtsdo.tk.helper;
+
+public class RefsetHelperDrools {
+
+	public RefsetHelperDrools() {
+	}
+	
+	public boolean isMemberOf(String conceptUUID, String refsetUUID) {
+		return true;
+	}
+
+}
