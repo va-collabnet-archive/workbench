135cd1651db18c71bfded34d717c1cf4433c75e4
alopez <alopez>
2010-10-07 19:07:11 +0000

    Added properties to helper rels template

--- tk-drools/src/main/java/org/ihtsdo/tk/helper/templates/RelationshipTemplate.java
+++ tk-drools/src/main/java/org/ihtsdo/tk/helper/templates/RelationshipTemplate.java
@@ -2,8 +2,36 @@ package org.ihtsdo.tk.helper.templates;
 
 public class RelationshipTemplate extends AbstractTemplate {
 	
+	private String sourceUuid;
+	private String typeUuid;
+	private String targetUuid;
+
 	public RelationshipTemplate() {
 		setType(TemplateType.RELATIONSHIP);
+	}
+
+	public String getSourceUuid() {
+		return sourceUuid;
+	}
+
+	public void setSourceUuid(String sourceUuid) {
+		this.sourceUuid = sourceUuid;
+	}
+
+	public String getTypeUuid() {
+		return typeUuid;
+	}
+
+	public void setTypeUuid(String typeUuid) {
+		this.typeUuid = typeUuid;
+	}
+
+	public String getTargetUuid() {
+		return targetUuid;
+	}
+
+	public void setTargetUuid(String targetUuid) {
+		this.targetUuid = targetUuid;
 	}
 
 }
