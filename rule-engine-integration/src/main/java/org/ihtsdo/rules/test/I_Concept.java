9bedbb2abaaa05b00334e8ba4043564d42380a3d
alopez <alopez>
2010-10-25 20:15:25 +0000

    Removed old db fixture

---
+++ rule-engine-integration/src/main/java/org/ihtsdo/rules/test/I_Concept.java
@@ -0,0 +1,10 @@
+package org.ihtsdo.rules.test;
+
+public interface I_Concept {
+
+	public Integer getId();
+	public void setId(Integer id);
+	
+	public String getName();
+	public void setName(String name);
+}
