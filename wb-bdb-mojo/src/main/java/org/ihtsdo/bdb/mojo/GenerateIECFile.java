f4958caad7aafb400341aeb11f9e6f8bfacea8b8
chrissy <chrissy>
2010-11-25 02:28:33 +0000

    merged IEC changes from wb-toolkit_trek branch

--- wb-bdb-mojo/src/main/java/org/ihtsdo/bdb/mojo/GenerateIECFile.java
+++ wb-bdb-mojo/src/main/java/org/ihtsdo/bdb/mojo/GenerateIECFile.java
@@ -17,7 +17,8 @@ import org.ihtsdo.time.TimeUtil;
 
 
 /**
- * Goal which generates an incremental e-concept (iec) file.
+ * Goal which generates an incremental e-concept file, in a change set 
+ * format (.eccs), or in incremental e-concept format (.iec).
  * 
  * @goal generate-iec-file
  * 
@@ -27,7 +28,7 @@ import org.ihtsdo.time.TimeUtil;
 public class GenerateIECFile extends AbstractMojo  {
 
     /**
-     * Parent of the new path.
+     * ConceptSpec for the view paths to base the export on.
      * 
      * @parameter
      * @required
@@ -35,7 +36,8 @@ public class GenerateIECFile extends AbstractMojo  {
     private ConceptSpec[] pathsToExport;
 
     /**
-     * Start date for inclusion in the change set.
+     * Start date for inclusion in the change set, in
+     * yyyy-MM-dd-HH.mm.ss format.
      * 
      * @parameter
      * @required
@@ -43,7 +45,8 @@ public class GenerateIECFile extends AbstractMojo  {
     private String startDate;
     
     /**
-     * End date for inclusion in the change set.
+     * End date for inclusion in the change set, in
+     * yyyy-MM-dd-HH.mm.ss format.
      * 
      * @parameter
      * @required
@@ -51,7 +54,7 @@ public class GenerateIECFile extends AbstractMojo  {
     private String endDate;
 
     /**
-     * End date for inclusion in the change set.
+     * Policy for generation of the change set.
      * 
      * @parameter default-value="INCREMENTAL"
      * @required
@@ -60,13 +63,26 @@ public class GenerateIECFile extends AbstractMojo  {
     private String policy;
     
     /**
-     * End date for inclusion in the change set.
+     * File name for the resulting generated change set.
      * 
-     * @parameter default-value="export.iec"
+     * @parameter default-value="exportFile"
      * @required
      * 
      */
     private String changeSetFile;
+    
+    /**
+     * Format of the generated file.
+     * Allowed values are: <code>eccs</code> (e-concept change set) and <code>iec</code> (incremental e-concept).
+     * <p>
+     * The format will be appended to the end of the provided file name. In the default case, the resulting
+     * file name will be: <code>exportFile.eccs</code>
+     * 
+     * @parameter default-value="eccs"
+     * @required
+     * 
+     */
+    private String format;
     
     /**
      * output directory.
@@ -77,7 +93,7 @@ public class GenerateIECFile extends AbstractMojo  {
     private File output;
 
     /**
-     * Generated resources directory.
+     * Directory of the berkeley database to export from.
      * 
      * @parameter expression="${project.build.directory}/generated-resources/berkeley-db"
      * @required
@@ -95,8 +111,12 @@ public class GenerateIECFile extends AbstractMojo  {
                 }
             }
             ChangeSetPolicy changeSetPolicy = ChangeSetPolicy.valueOf(policy);
+            if (!changeSetFile.endsWith(format)) {
+                changeSetFile = changeSetFile + "." + format;
+            }
+            boolean timeStampEnabled = format.toLowerCase().equals("eccs");
             EConceptChangeSetWriter writer = new EConceptChangeSetWriter(new File(output, changeSetFile), 
-                new File(output, changeSetFile + ".tmp"), changeSetPolicy.convert(), false);
+                new File(output, changeSetFile + ".tmp"), changeSetPolicy.convert(), timeStampEnabled);
             String key = UUID.randomUUID().toString();
             ChangeSetWriterHandler.addWriter(key, writer);
             IntSet sapsToWrite = Bdb.getSapDb().getSpecifiedSapNids(pathIds, TimeUtil.getFileDateFormat().parse(startDate).getTime(), 
