e275df3cd5b53d80fcc96e9e6a766139179d5ce3
afurber <afurber>
2010-12-02 01:02:09 +0000

    added a concept spec for fully specified name

--- tk-concept-component/src/main/java/org/ihtsdo/tk/example/binding/Snomed.java
+++ tk-concept-component/src/main/java/org/ihtsdo/tk/example/binding/Snomed.java
@@ -20,6 +20,9 @@ public class Snomed {
 	public static ConceptSpec CLINICAL_FINDING = 
 		new ConceptSpec("Clinical finding (finding)", 
 						UUID.fromString("bd83b1dd-5a82-34fa-bb52-06f666420a1c"));
-
+	
+	public static ConceptSpec FULLY_SPECIFIED_DESCRIPTION_TYPE = 
+		new ConceptSpec("fully specified name (description type)", 
+						UUID.fromString("5e1fe940-8faf-11db-b606-0800200c9a66"));
 
 }
