39fde6c5858e4026e66ab2af86a90607ed737f48
chrissy <chrissy>
2010-12-21 01:56:58 +0000

    reverted change to taxonomy desc popup

--- wb-gui/src/main/java/org/dwfa/ace/tree/TreeMouseListener.java
+++ wb-gui/src/main/java/org/dwfa/ace/tree/TreeMouseListener.java
@@ -172,8 +172,8 @@ public class TreeMouseListener extends MouseAdapter {
                             boolean excludeContains = true;
                             if (refsetSpecHelper.isDescriptionComputeType()) {
                                 // show AND, OR, !AND, !OR
-                                // do not show desc clauses (as taxonomy view represents concepts, not descriptions)
-                                excludeDesc = true;
+                                // show desc clauses
+                                excludeDesc = false;
                             } else if (refsetSpecHelper.isRelationshipComputeType()) {
                                 // show AND, OR, !AND, !OR
                                 // show rel clauses
