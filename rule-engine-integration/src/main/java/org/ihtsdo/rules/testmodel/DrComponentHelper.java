1da07d9ff4143a5225fdb959892ffdc93d3a22c8
alopez <alopez>
2010-12-28 22:03:17 +0000

    Added basic implementation for constraint normal form

--- rule-engine-integration/src/main/java/org/ihtsdo/rules/testmodel/DrComponentHelper.java
+++ rule-engine-integration/src/main/java/org/ihtsdo/rules/testmodel/DrComponentHelper.java
@@ -13,6 +13,7 @@ import org.dwfa.ace.api.LocalVersionedTerminology;
 import org.dwfa.ace.api.Terms;
 import org.dwfa.cement.ArchitectonicAuxiliary;
 import org.dwfa.tapi.TerminologyException;
+import org.ihtsdo.rules.RulesLibrary.INFERRED_VIEW_ORIGIN;
 import org.ihtsdo.testmodel.DrConcept;
 import org.ihtsdo.testmodel.DrDefiningRolesSet;
 import org.ihtsdo.testmodel.DrDescription;
@@ -28,7 +29,8 @@ public class DrComponentHelper {
 
 	public static I_IntSet allRels;
 
-	public static DrConcept getDrConcept(ConceptVersionBI conceptBi, String factContextName) {
+	public static DrConcept getDrConcept(ConceptVersionBI conceptBi, String factContextName, 
+			INFERRED_VIEW_ORIGIN inferredOrigin) {
 		I_TermFactory tf = Terms.get();
 		DrConcept concept = new DrConcept();
 
@@ -80,27 +82,53 @@ public class DrComponentHelper {
 					allRels.add(loopConcept.getNid());
 				}
 			}
-			
-			for (RelationshipVersionBI relTuple : oldStyleConcept.getSourceRelTuples(config.getAllowedStatus(), 
-					allRels, 
-					config.getViewPositionSetReadOnly(), config.getPrecedence(), 
-					config.getConflictResolutionStrategy(), config.getClassifierConcept().getNid(), 
-					RelAssertionType.INFERRED)) {
-				DrRelationship loopRel = new DrRelationship();
-				loopRel.setModifierUuid("someUuid");
-				loopRel.setAuthorUuid(tf.nidToUuid(relTuple.getAuthorNid()).toString());
-				loopRel.setSourceUuid(tf.nidToUuid(relTuple.getOriginNid()).toString());
-				loopRel.setTargetUuid(tf.nidToUuid(relTuple.getDestinationNid()).toString());
-				loopRel.setCharacteristicUuid(tf.nidToUuid(relTuple.getCharacteristicNid()).toString());
-				loopRel.setPathUuid(tf.nidToUuid(relTuple.getPathNid()).toString());
-				loopRel.setPrimordialUuid(relTuple.getPrimUuid().toString());
-				loopRel.setRelGroup(relTuple.getGroup());
-				loopRel.setStatusUuid(tf.nidToUuid(relTuple.getStatusNid()).toString());
-				loopRel.setTime(relTuple.getTime());
-				loopRel.setTypeUuid(tf.nidToUuid(relTuple.getTypeNid()).toString());
-				loopRel.setFactContextName(factContextName);
-				concept.getOutgoingRelationships().add(loopRel);
-				inferredRolesSet.getRelationships().add(loopRel);
+
+			if (inferredOrigin == INFERRED_VIEW_ORIGIN.CLASSIFIER) {
+				for (RelationshipVersionBI relTuple : oldStyleConcept.getSourceRelTuples(config.getAllowedStatus(), 
+						allRels, 
+						config.getViewPositionSetReadOnly(), config.getPrecedence(), 
+						config.getConflictResolutionStrategy(), config.getClassifierConcept().getNid(), 
+						RelAssertionType.INFERRED)) {
+					DrRelationship loopRel = new DrRelationship();
+					loopRel.setModifierUuid("someUuid");
+					loopRel.setAuthorUuid(tf.nidToUuid(relTuple.getAuthorNid()).toString());
+					loopRel.setSourceUuid(tf.nidToUuid(relTuple.getOriginNid()).toString());
+					loopRel.setTargetUuid(tf.nidToUuid(relTuple.getDestinationNid()).toString());
+					loopRel.setCharacteristicUuid(tf.nidToUuid(relTuple.getCharacteristicNid()).toString());
+					loopRel.setPathUuid(tf.nidToUuid(relTuple.getPathNid()).toString());
+					loopRel.setPrimordialUuid(relTuple.getPrimUuid().toString());
+					loopRel.setRelGroup(relTuple.getGroup());
+					loopRel.setStatusUuid(tf.nidToUuid(relTuple.getStatusNid()).toString());
+					loopRel.setTime(relTuple.getTime());
+					loopRel.setTypeUuid(tf.nidToUuid(relTuple.getTypeNid()).toString());
+					loopRel.setFactContextName(factContextName);
+					concept.getOutgoingRelationships().add(loopRel);
+					inferredRolesSet.getRelationships().add(loopRel);
+				}
+			} else if (inferredOrigin == INFERRED_VIEW_ORIGIN.CONSTRAINTED_NORMAL_FORM) {
+				// TODO implement CONSTRAINTED_NORMAL_FORM calculation
+				// Temporary implementation using classifier inferred view from database
+				for (RelationshipVersionBI relTuple : oldStyleConcept.getSourceRelTuples(config.getAllowedStatus(), 
+						allRels, 
+						config.getViewPositionSetReadOnly(), config.getPrecedence(), 
+						config.getConflictResolutionStrategy(), config.getClassifierConcept().getNid(), 
+						RelAssertionType.INFERRED)) {
+					DrRelationship loopRel = new DrRelationship();
+					loopRel.setModifierUuid("someUuid");
+					loopRel.setAuthorUuid(tf.nidToUuid(relTuple.getAuthorNid()).toString());
+					loopRel.setSourceUuid(tf.nidToUuid(relTuple.getOriginNid()).toString());
+					loopRel.setTargetUuid(tf.nidToUuid(relTuple.getDestinationNid()).toString());
+					loopRel.setCharacteristicUuid(tf.nidToUuid(relTuple.getCharacteristicNid()).toString());
+					loopRel.setPathUuid(tf.nidToUuid(relTuple.getPathNid()).toString());
+					loopRel.setPrimordialUuid(relTuple.getPrimUuid().toString());
+					loopRel.setRelGroup(relTuple.getGroup());
+					loopRel.setStatusUuid(tf.nidToUuid(relTuple.getStatusNid()).toString());
+					loopRel.setTime(relTuple.getTime());
+					loopRel.setTypeUuid(tf.nidToUuid(relTuple.getTypeNid()).toString());
+					loopRel.setFactContextName(factContextName);
+					concept.getOutgoingRelationships().add(loopRel);
+					inferredRolesSet.getRelationships().add(loopRel);
+				}
 			}
 
 			for (RelationshipVersionBI relTuple : oldStyleConcept.getSourceRelTuples(config.getAllowedStatus(), 
