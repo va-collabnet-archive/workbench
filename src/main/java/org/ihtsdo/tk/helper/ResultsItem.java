935481a4927e9ee0c24158f464a5c443f98ca94c
alopez <alopez>
2010-12-28 22:24:37 +0000

    Added severity enum

--- src/main/java/org/ihtsdo/tk/helper/ResultsItem.java
+++ src/main/java/org/ihtsdo/tk/helper/ResultsItem.java
@@ -9,6 +9,24 @@ public class ResultsItem {
 	private String severity;
 	private String ruleUuid;
 	
+	public enum Severity {
+		
+		ERROR(UUID.fromString("f9545a20-12cf-11e0-ac64-0800200c9a66")),
+		WARNING(UUID.fromString("f9545a21-12cf-11e0-ac64-0800200c9a66")),
+		NOTIFICATION(UUID.fromString("f9545a22-12cf-11e0-ac64-0800200c9a66"));
+		
+		private final UUID severityUuid;
+		
+		Severity(UUID severityUuid) {
+			this.severityUuid = severityUuid;
+		}
+
+		public UUID getSeverityUuid() {
+			return severityUuid;
+		}
+		
+	}
+	
 	public ResultsItem() {
 		super();
 	}
