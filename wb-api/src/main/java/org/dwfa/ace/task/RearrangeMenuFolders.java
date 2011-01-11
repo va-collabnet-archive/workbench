715c97b5a46d41167166d28d6c0875fd9c8d1ab4
alopez <alopez>
2011-01-11 21:09:36 +0000

    Add menu folders copy task

---
+++ wb-api/src/main/java/org/dwfa/ace/task/RearrangeMenuFolders.java
@@ -0,0 +1,143 @@
+/**
+ * Copyright (c) 2009 International Health Terminology Standards Development
+ * Organisation
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.dwfa.ace.task;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.util.Collection;
+
+import javax.swing.JList;
+
+import org.dwfa.ace.api.I_ConfigAceFrame;
+import org.dwfa.ace.api.I_ModelTerminologyList;
+import org.dwfa.bpa.process.Condition;
+import org.dwfa.bpa.process.I_EncodeBusinessProcess;
+import org.dwfa.bpa.process.I_Work;
+import org.dwfa.bpa.process.TaskFailedException;
+import org.dwfa.bpa.tasks.AbstractTask;
+import org.dwfa.util.bean.BeanList;
+import org.dwfa.util.bean.BeanType;
+import org.dwfa.util.bean.Spec;
+
+@BeanList(specs = { @Spec(directory = "tasks/ide/listview", type = BeanType.TASK_BEAN) })
+public class RearrangeMenuFolders extends AbstractTask {
+
+	private static final long serialVersionUID = 1;
+
+	private static final int dataVersion = 1;
+
+	private void writeObject(ObjectOutputStream out) throws IOException {
+		out.writeInt(dataVersion);
+	}
+
+	private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException {
+		int objDataVersion = in.readInt();
+		if (objDataVersion == 1) {
+			// nothing to read...
+		} else {
+			throw new IOException("Can't handle dataversion: " + objDataVersion);
+		}
+
+	}
+
+	/**
+	 * @see org.dwfa.bpa.process.I_DefineTask#evaluate(org.dwfa.bpa.process.I_EncodeBusinessProcess,
+	 *      org.dwfa.bpa.process.I_Work)
+	 */
+	public Condition evaluate(I_EncodeBusinessProcess process, I_Work worker) throws TaskFailedException {
+		File dir1 = new File("plugins/menu/20_Tasks");
+		File dir2 = new File("plugins/menu/21_Tasks2");
+		try {
+			copyDirectory(dir1, dir2);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return Condition.CONTINUE;
+	}
+
+	/**
+	 * @see org.dwfa.bpa.process.I_DefineTask#complete(org.dwfa.bpa.process.I_EncodeBusinessProcess,
+	 *      org.dwfa.bpa.process.I_Work)
+	 */
+	public void complete(I_EncodeBusinessProcess process, I_Work worker) throws TaskFailedException {
+		// Nothing to do
+
+	}
+
+	/**
+	 * @see org.dwfa.bpa.process.I_DefineTask#getConditions()
+	 */
+	public Collection<Condition> getConditions() {
+		return CONTINUE_CONDITION;
+	}
+
+	/**
+	 * @see org.dwfa.bpa.process.I_DefineTask#getDataContainerIds()
+	 */
+	public int[] getDataContainerIds() {
+		return new int[] {};
+	}
+
+	public void copyDirectory(File sourceLocation , File targetLocation)
+	throws IOException {
+
+		if (sourceLocation.isDirectory()) {
+			if (!targetLocation.exists()) {
+				targetLocation.mkdir();
+			}
+
+			String[] children = sourceLocation.list();
+			for (int i=0; i<children.length; i++) {
+				copyDirectory(new File(sourceLocation, children[i]),
+						new File(targetLocation, children[i]));
+			}
+		} else {
+
+			InputStream in = new FileInputStream(sourceLocation);
+			OutputStream out = new FileOutputStream(targetLocation);
+
+			// Copy the bits from instream to outstream
+			byte[] buf = new byte[1024];
+			int len;
+			while ((len = in.read(buf)) > 0) {
+				out.write(buf, 0, len);
+			}
+			in.close();
+			out.close();
+		}
+	}
+
+	public boolean deleteDirectory(File path) {
+		if( path.exists() ) {
+			File[] files = path.listFiles();
+			for(int i=0; i<files.length; i++) {
+				if(files[i].isDirectory()) {
+					deleteDirectory(files[i]);
+				}
+				else {
+					files[i].delete();
+				}
+			}
+		}
+		return( path.delete() );
+	}
+}
