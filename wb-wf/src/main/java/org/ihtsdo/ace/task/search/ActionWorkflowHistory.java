efe5111c3a358544d0a22b7503e13e33957d4d10
kec <kec>
2011-01-17 21:26:32 +0000

    Improved error handling.

--- wb-wf/src/main/java/org/ihtsdo/ace/task/search/ActionWorkflowHistory.java
+++ wb-wf/src/main/java/org/ihtsdo/ace/task/search/ActionWorkflowHistory.java
@@ -8,21 +8,20 @@ import java.util.UUID;
 import org.dwfa.ace.api.I_ConfigAceFrame;
 import org.dwfa.ace.api.I_GetConceptData;
 import org.dwfa.ace.api.Terms;
+import org.dwfa.ace.log.AceLog;
 import org.dwfa.bpa.process.TaskFailedException;
 import org.dwfa.util.bean.BeanList;
 import org.dwfa.util.bean.BeanType;
 import org.dwfa.util.bean.Spec;
 import org.ihtsdo.workflow.WorkflowHistoryJavaBean;
 
-@BeanList(specs = { @Spec(directory = "tasks/ide/search", type = BeanType.TASK_BEAN),
-                   @Spec(directory = "search/workflow", type = BeanType.TASK_BEAN) })
-
+@BeanList(specs = {
+    @Spec(directory = "tasks/ide/search", type = BeanType.TASK_BEAN),
+    @Spec(directory = "search/workflow", type = BeanType.TASK_BEAN)})
 public class ActionWorkflowHistory extends AbstractWorkflowHistorySearchTest {
 
     private static final long serialVersionUID = 1;
-
     private static final int dataVersion = 1;
-
     /**
      * Property name for the Action being searched.
      */
@@ -37,14 +36,16 @@ public class ActionWorkflowHistory extends AbstractWorkflowHistorySearchTest {
         int objDataVersion = in.readInt();
         if (objDataVersion == 1) {
             this.testAction = (I_GetConceptData) in.readObject();
-            if (this.testAction == null)
-            {
-            	try {
-    				this.testAction = Terms.get().getActiveAceFrameConfig().getWorkflowActions().iterator().next();
-            	} catch (Exception e) {
-            		e.printStackTrace();
-            	}
-        	}
+            if (this.testAction == null) {
+                try {
+                    java.util.Iterator<? extends I_GetConceptData> wfitr = Terms.get().getActiveAceFrameConfig().getWorkflowActions().iterator();
+                    if (wfitr.hasNext()) {
+                        this.testAction = Terms.get().getActiveAceFrameConfig().getWorkflowActions().iterator().next();
+                    }
+                } catch (Exception e) {
+                    AceLog.getAppLog().alertAndLogException(e);
+                }
+            }
         } else {
             throw new IOException("Can't handle dataversion: " + objDataVersion);
         }
@@ -52,37 +53,38 @@ public class ActionWorkflowHistory extends AbstractWorkflowHistorySearchTest {
 
     @Override
     public boolean test(WorkflowHistoryJavaBean bean, I_ConfigAceFrame frameConfig) throws TaskFailedException {
-		UUID testUUID = testAction.getPrimUuid();
-		
-		if (testUUID == null || !bean.getAction().equals(testUUID))
-			return false;
-		else
-			return true;
+        UUID testUUID = testAction.getPrimUuid();
+
+        if (testUUID == null || !bean.getAction().equals(testUUID)) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
-	@Override
-	public boolean test(Set<WorkflowHistoryJavaBean> wfHistory) throws TaskFailedException 
-	{
-		UUID testUUID = getCurrentTestUUID();
-	    
-    	if (testUUID == null)
-    		throw new TaskFailedException("Failed to complete test.  UUID null.");
-    	
-    	//If any item in the list passes the filter, return true.
-    	for (WorkflowHistoryJavaBean wfHistoryItem : wfHistory) {
-			if (wfHistoryItem.getAction().equals(testUUID)) {
-				return true;
-			}
-		}
+    @Override
+    public boolean test(Set<WorkflowHistoryJavaBean> wfHistory) throws TaskFailedException {
+        UUID testUUID = getCurrentTestUUID();
 
-    	return false;
-	}
-	
+        if (testUUID == null) {
+            throw new TaskFailedException("Failed to complete test.  UUID null.");
+        }
+
+        //If any item in the list passes the filter, return true.
+        for (WorkflowHistoryJavaBean wfHistoryItem : wfHistory) {
+            if (wfHistoryItem.getAction().equals(testUUID)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     private UUID getCurrentTestUUID() throws TaskFailedException {
-		return testAction.getPrimUuid();
+        return testAction.getPrimUuid();
     }
-	
-	public I_GetConceptData getTestAction() {
+
+    public I_GetConceptData getTestAction() {
         return testAction;
     }
 
