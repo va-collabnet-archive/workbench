2da141dda47d6f2275d32ebc19e056a28f308047
kec <kec>
2011-01-17 21:22:43 +0000

    Improved error handling.

--- wb-wf/src/main/java/org/ihtsdo/ace/task/search/ActionLastWorkflowHistory.java
+++ wb-wf/src/main/java/org/ihtsdo/ace/task/search/ActionLastWorkflowHistory.java
@@ -4,48 +4,50 @@ import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.util.Set;
 import java.util.UUID;
+import javax.swing.text.html.HTMLDocument.Iterator;
 
 import org.dwfa.ace.api.I_ConfigAceFrame;
 import org.dwfa.ace.api.I_GetConceptData;
 import org.dwfa.ace.api.Terms;
+import org.dwfa.ace.log.AceLog;
 import org.dwfa.bpa.process.TaskFailedException;
 import org.dwfa.util.bean.BeanList;
 import org.dwfa.util.bean.BeanType;
 import org.dwfa.util.bean.Spec;
 import org.ihtsdo.workflow.WorkflowHistoryJavaBean;
 
-@BeanList(specs = { @Spec(directory = "tasks/ide/search", type = BeanType.TASK_BEAN),
-					@Spec(directory = "search/workflow", type = BeanType.TASK_BEAN) })
-
+@BeanList(specs = {
+    @Spec(directory = "tasks/ide/search", type = BeanType.TASK_BEAN),
+    @Spec(directory = "search/workflow", type = BeanType.TASK_BEAN)})
 public class ActionLastWorkflowHistory extends AbstractWorkflowHistorySearchTest {
- 
+
     private static final long serialVersionUID = 1;
-
     private static final int dataVersion = 1;
-
     /**
      * Property name for the Action being searched.
      */
-     private I_GetConceptData testAction = null;
+    private I_GetConceptData testAction = null;
 
     private void writeObject(ObjectOutputStream out) throws IOException {
         out.writeInt(dataVersion);
         out.writeObject(this.testAction);
 
     }
- 
+
     private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException {
         int objDataVersion = in.readInt();
         if (objDataVersion == 1) {
             this.testAction = (I_GetConceptData) in.readObject();
-			if (this.testAction == null)
-			{
-				try {
-					this.testAction = Terms.get().getActiveAceFrameConfig().getWorkflowActions().iterator().next();
-	            } catch (Exception e) {
-	            	e.printStackTrace();
-	            }
-			}
+            if (this.testAction == null) {
+                try {
+                    java.util.Iterator<? extends I_GetConceptData> wfitr = Terms.get().getActiveAceFrameConfig().getWorkflowActions().iterator();
+                    if (wfitr.hasNext()) {
+                        this.testAction = Terms.get().getActiveAceFrameConfig().getWorkflowActions().iterator().next();
+                    }
+                } catch (Exception e) {
+                    AceLog.getAppLog().alertAndLogException(e);
+                }
+            }
         } else {
             throw new IOException("Can't handle dataversion: " + objDataVersion);
         }
@@ -53,40 +55,42 @@ public class ActionLastWorkflowHistory extends AbstractWorkflowHistorySearchTest
 
     @Override
     public boolean test(WorkflowHistoryJavaBean bean, I_ConfigAceFrame frameConfig) throws TaskFailedException {
-		return false;
-	}
+        return false;
+    }
 
-	@Override
-	public boolean test(Set<WorkflowHistoryJavaBean> wfHistory) throws TaskFailedException 
-	{	
-		UUID testUUID = getCurrentTestUUID();
-	    
-    	if (testUUID == null)
-    		throw new TaskFailedException("Failed to complete test.  UUID null.");
-    	
-    	if (getCurrent(wfHistory).getAction().equals(testUUID)) 
-			return true;
-		
-    	return false;
-	}
-	
-	public WorkflowHistoryJavaBean getCurrent (Set<WorkflowHistoryJavaBean> wfHistory) {
+    @Override
+    public boolean test(Set<WorkflowHistoryJavaBean> wfHistory) throws TaskFailedException {
+        UUID testUUID = getCurrentTestUUID();
 
-		WorkflowHistoryJavaBean current = null;
-		
-		for (WorkflowHistoryJavaBean wfHistoryItem : wfHistory) {
-			if (current == null || wfHistoryItem.getTimeStamp() > current.getTimeStamp()) 
-				current = wfHistoryItem;
-		}
-		
-		return current;
-	}
+        if (testUUID == null) {
+            throw new TaskFailedException("Failed to complete test.  UUID null.");
+        }
+
+        if (getCurrent(wfHistory).getAction().equals(testUUID)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public WorkflowHistoryJavaBean getCurrent(Set<WorkflowHistoryJavaBean> wfHistory) {
+
+        WorkflowHistoryJavaBean current = null;
+
+        for (WorkflowHistoryJavaBean wfHistoryItem : wfHistory) {
+            if (current == null || wfHistoryItem.getTimeStamp() > current.getTimeStamp()) {
+                current = wfHistoryItem;
+            }
+        }
+
+        return current;
+    }
 
     private UUID getCurrentTestUUID() throws TaskFailedException {
-		return testAction.getPrimUuid();
+        return testAction.getPrimUuid();
     }
-	
-	public I_GetConceptData getTestAction() {
+
+    public I_GetConceptData getTestAction() {
         return testAction;
     }
 
