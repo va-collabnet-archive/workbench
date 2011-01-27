47ace8f59db91f22278854fe32782d48d3a6007b
jefron <jefron>
2011-01-27 16:20:19 +0000

    Utilize integration mechanism for using resource files in populating refsets in database project

--- src/main/java/org/ihtsdo/workflow/refset/mojo/init/InitializeSemanticAreaHierarchyMojo.java
+++ src/main/java/org/ihtsdo/workflow/refset/mojo/init/InitializeSemanticAreaHierarchyMojo.java
@@ -1,18 +1,12 @@
 package org.ihtsdo.workflow.refset.mojo.init;
 
 import java.io.File;
-import java.security.NoSuchAlgorithmException;
-import java.util.Collection;
 import java.util.Scanner;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.dwfa.ace.api.I_TermFactory;
 import org.dwfa.ace.api.Terms;
-import org.dwfa.ace.api.ebr.I_ExtendByRef;
-import org.ihtsdo.mojo.maven.MojoUtil;
-import org.ihtsdo.workflow.refset.semHier.SemanticAreaHierarchyRefset;
 import org.ihtsdo.workflow.refset.semHier.SemanticAreaHierarchyRefsetWriter;
 
 /**
@@ -25,22 +19,6 @@ import org.ihtsdo.workflow.refset.semHier.SemanticAreaHierarchyRefsetWriter;
 public class InitializeSemanticAreaHierarchyMojo extends AbstractMojo {
 
     /**
-     * Location of the build directory.
-     * 
-     * @parameter expression="${project.build.directory}"
-     * @required
-     */
-    private File targetDirectory;
-    
-    /**
-     * Location of the build directory.
-     * 
-     * @parameter expression="${project.build.sourceDirectory}"
-     * @required
-     */
-    private File baseDirectory;
-
-    /**
      * The name of the database to create. All sql inserts will be against this
      * database.
      * 
@@ -49,21 +27,14 @@ public class InitializeSemanticAreaHierarchyMojo extends AbstractMojo {
      */
     private String filePath;
 
-    private String basePath = "/../resources/";
-
     public void execute() throws MojoExecutionException, MojoFailureException 
     {
         System.setProperty("java.awt.headless", "true");
         try {
 
-            String resourceFilePath = baseDirectory.getAbsoluteFile() + basePath + filePath;
- 
-            SemanticAreaHierarchyRefset refset = new SemanticAreaHierarchyRefset();
-            I_TermFactory tf = Terms.get();
-
             SemanticAreaHierarchyRefsetWriter writer = new SemanticAreaHierarchyRefsetWriter();
 
-            Scanner scanner = new Scanner(new File(resourceFilePath));
+            Scanner scanner = new Scanner(new File(filePath));
 
             while (scanner.hasNextLine())
             {
@@ -84,13 +55,4 @@ public class InitializeSemanticAreaHierarchyMojo extends AbstractMojo {
 		}
 
 	}
-
-    public File getTargetDirectory() {
-        return targetDirectory;
-	}
-
-    public void setTargetDirectory(File targetDirectory) {
-        this.targetDirectory = targetDirectory;
-	}
-    
 }
