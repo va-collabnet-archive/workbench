de16545e8d77f2f2604bc9f59ef56b5f62f556d1
kec <kec>
2011-01-27 22:31:49 +0000

    Added wizard panel for arena.

--- src/main/java/org/ihtsdo/workflow/refset/mojo/init/InitializeEditorCategoryMojo.java
+++ src/main/java/org/ihtsdo/workflow/refset/mojo/init/InitializeEditorCategoryMojo.java
@@ -17,7 +17,7 @@ import org.ihtsdo.workflow.refset.utilities.WorkflowHelper;
 
 /**
  * @author Jesse Efron
- * 
+ *
  * @goal initialize-editor-category-refset
  * @requiresDependencyResolution compile
  */
@@ -26,7 +26,7 @@ public class InitializeEditorCategoryMojo extends AbstractMojo {
 
     /**
      * Location of the build directory.
-     * 
+     *
      * @parameter expression="${project.build.directory}"
      * @required
      */
@@ -35,17 +35,18 @@ public class InitializeEditorCategoryMojo extends AbstractMojo {
     /**
      * The name of the database to create. All sql inserts will be against this
      * database.
-     * 
+     *
      * @parameter
      * @required
      */
     private String filePath;
-    
-    public void execute() throws MojoExecutionException, MojoFailureException 
+
+   @Override
+    public void execute() throws MojoExecutionException, MojoFailureException
     {
         System.setProperty("java.awt.headless", "true");
         String line = null;
-        
+
         try {
             EditorCategoryRefsetWriter writer = new EditorCategoryRefsetWriter();
 
@@ -55,20 +56,20 @@ public class InitializeEditorCategoryMojo extends AbstractMojo {
             {
             	line = scanner.nextLine();
             	String[] columns = line.split("\t");
-            
+
             	writer.setEditor(getEditor(columns[0]));
             	writer.setSemanticArea(columns[1]);
             	writer.setCategory(WorkflowHelper.lookupEditorCategory(columns[2]));
 
             	writer.addMember();
             }
-            	
+
             Terms.get().addUncommitted(writer.getRefsetConcept());
         } catch (Exception e) {
         	AceLog.getAppLog().log(Level.WARNING, line, e);
 		}
 	}
-    
+
     public File getTargetDirectory() {
         return targetDirectory;
 	}
@@ -76,7 +77,7 @@ public class InitializeEditorCategoryMojo extends AbstractMojo {
     public void setTargetDirectory(File targetDirectory) {
         this.targetDirectory = targetDirectory;
 	}
-    
+
     private I_GetConceptData getEditor(String editor) throws TerminologyException, IOException {
     	return WorkflowHelper.lookupModeler(editor);
     }
