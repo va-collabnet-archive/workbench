92ce63180455203636e25be02b44087ed8f9fd20
alopez <alopez>
2011-01-28 20:44:09 +0000

    Creating new trunk project from prototype branch

---
+++ qa-runner/src/main/java/com/termmed/qa/runner/App.java
@@ -0,0 +1,13 @@
+package com.termmed.qa.runner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
