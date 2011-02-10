c39a276512c88eaaec6690e7e8de4b4c4e6411a8
jefron <jefron>
2011-02-10 18:17:23 +0000

    1) Use varibles to define column position 2) Optional outputing of error if row can't be added (default = true)

--- wb-mojo/src/main/java/org/ihtsdo/workflow/refset/mojo/init/InitializeSemanticAreaHierarchyMojo.java
+++ wb-mojo/src/main/java/org/ihtsdo/workflow/refset/mojo/init/InitializeSemanticAreaHierarchyMojo.java
@@ -2,11 +2,13 @@ package org.ihtsdo.workflow.refset.mojo.init;
 
 import java.io.File;
 import java.util.Scanner;
+import java.util.logging.Level;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.dwfa.ace.api.Terms;
+import org.dwfa.ace.log.AceLog;
 import org.ihtsdo.workflow.refset.semHier.SemanticAreaHierarchyRefsetWriter;
 
 /**
@@ -26,6 +28,20 @@ public class InitializeSemanticAreaHierarchyMojo extends AbstractMojo {
      * @required
      */
     private String filePath;
+    
+    /**
+     * Whether to alert user of a bad row that can't be imported into the database
+     * 
+     * @parameter
+     * default-value=true
+     * @required
+     */
+    private boolean reportErrors;
+
+    private static final int childSemanticAreaPosition = 0;							// 0
+    private static final int parentSemanticAreaPosition = childSemanticAreaPosition + 1;		// 1
+
+    private static final int numberOfColumns = parentSemanticAreaPosition + 1;			// 2
 
     public void execute() throws MojoExecutionException, MojoFailureException 
     {
@@ -41,13 +57,15 @@ public class InitializeSemanticAreaHierarchyMojo extends AbstractMojo {
             	String line = scanner.nextLine();
             	String[] columns = line.split("\t");
             
-            	if (columns.length == 2)
-            	{
-	            	writer.setChildSemanticArea(columns[0]);
-	            	writer.setParentSemanticArea(columns[1]);
-	
-	            	writer.addMember();
-            	}
+        		if (columns.length == numberOfColumns)
+        		{
+        			writer.setChildSemanticArea(columns[childSemanticAreaPosition]);
+        			writer.setParentSemanticArea(columns[parentSemanticAreaPosition]);
+
+        			writer.addMember();
+        		} else if (reportErrors) {
+                	AceLog.getAppLog().log(Level.WARNING, line, new Exception("Unable to import this row into semantic area hierarchy refset"));
+    			}
             }
 
             // Single RefCompId, so commit at end
