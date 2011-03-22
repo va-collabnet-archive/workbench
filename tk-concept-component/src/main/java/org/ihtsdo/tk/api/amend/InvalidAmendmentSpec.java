b13846a4baa092de03ebc8de64d967707f920aea
kec <kec>
2011-03-22 00:30:09 +0000

    Added back and deprecated InvalidAmendmentSpec as it was being referenced in Drools.

---
+++ tk-concept-component/src/main/java/org/ihtsdo/tk/api/amend/InvalidAmendmentSpec.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2011 International Health Terminology Standards Development Organisation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ihtsdo.tk.api.amend;
+
+import org.ihtsdo.tk.api.blueprint.InvalidCAB;
+
+/**
+ *
+ * @author kec
+ * @deprecated use InvalidCAB
+ */
+
+@Deprecated
+public class InvalidAmendmentSpec extends InvalidCAB {
+
+    public InvalidAmendmentSpec() {
+    }
+
+    public InvalidAmendmentSpec(String string) {
+        super(string);
+    }
+
+    public InvalidAmendmentSpec(String string, Throwable thrwbl) {
+        super(string, thrwbl);
+    }
+
+    public InvalidAmendmentSpec(Throwable thrwbl) {
+        super(thrwbl);
+    }
+
+}
