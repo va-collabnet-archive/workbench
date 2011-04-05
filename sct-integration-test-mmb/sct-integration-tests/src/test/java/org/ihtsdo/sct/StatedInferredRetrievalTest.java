53ffb5ec9caae42ddab8358f320ae34a0b58d99c
marc <marc>
2011-04-05 20:51:59 +0000

    added test case.

--- sct-integration-test-mmb/sct-integration-tests/src/test/java/org/ihtsdo/sct/StatedInferredRetrievalTest.java
+++ sct-integration-test-mmb/sct-integration-tests/src/test/java/org/ihtsdo/sct/StatedInferredRetrievalTest.java
@@ -94,4 +94,90 @@ public class StatedInferredRetrievalTest {
         Assert.assertTrue(v.getAuthorNid() == statedVc.getClassifierNid());
         System.out.println("inferred then stated version: " + v);
     }
+
+    @Test
+    public void testCornealGluingIsaOperativeProcedure() throws IOException, ContraditionException {
+        RelationshipChronicleBI rel =
+                (RelationshipChronicleBI) Ts.get().getComponent(UUID.fromString("12d3d337-f810-5dc5-bc63-ac54459d9c18"));
+
+        ViewCoordinate primordialVc = Ts.get().getMetadataVC();
+        System.out.println("found rel: " + rel);
+
+        RelationshipVersionBI v = rel.getVersion(primordialVc);
+        System.out.println("primordial version: " + v);
+        Assert.assertNull(v);
+
+        PathBI snomedPath = Ts.get().getPath(rel.getPrimordialVersion().getPathNid());
+        PositionBI latestOnSnomedPath = new Position(Long.MAX_VALUE, snomedPath);
+        ViewCoordinate statedVc = new ViewCoordinate(primordialVc);
+        statedVc.setRelAssertionType(RelAssertionType.STATED);
+
+        statedVc.setPositionSet(new PositionSet(latestOnSnomedPath));
+
+        v = rel.getVersion(statedVc);
+        Assert.assertNotNull(v);
+        Assert.assertTrue(v.getAuthorNid() != statedVc.getClassifierNid());
+        System.out.println("stated version: " + v);
+
+        ViewCoordinate inferredVc = new ViewCoordinate(statedVc);
+        inferredVc.setRelAssertionType(RelAssertionType.INFERRED);
+
+        v = rel.getVersion(inferredVc);
+        Assert.assertNull(v);
+        // Assert.assertTrue(v.getAuthorNid() == statedVc.getClassifierNid());
+        // System.out.println("inferred version: " + v);
+        System.out.println("inferred version: null");
+
+
+        ViewCoordinate inferredThenStatedVc = new ViewCoordinate(statedVc);
+        inferredThenStatedVc.setRelAssertionType(RelAssertionType.INFERRED_THEN_STATED);
+
+        v = rel.getVersion(inferredThenStatedVc);
+        Assert.assertNotNull(v);
+        Assert.assertTrue(v.getAuthorNid() != statedVc.getClassifierNid());
+        System.out.println("inferred then stated version: " + v);
+    }
+
+        @Test
+    public void testCornealGluingIsaGluingOfCornea() throws IOException, ContraditionException {
+        RelationshipChronicleBI rel =
+                (RelationshipChronicleBI) Ts.get().getComponent(UUID.fromString("64469c96-871c-56f9-90d9-6207185715cb"));
+
+        ViewCoordinate primordialVc = Ts.get().getMetadataVC();
+        System.out.println("found rel: " + rel);
+
+        RelationshipVersionBI v = rel.getVersion(primordialVc);
+        System.out.println("primordial version: " + v);
+        Assert.assertNull(v);
+
+        PathBI snomedPath = Ts.get().getPath(rel.getPrimordialVersion().getPathNid());
+        PositionBI latestOnSnomedPath = new Position(Long.MAX_VALUE, snomedPath);
+        ViewCoordinate statedVc = new ViewCoordinate(primordialVc);
+        statedVc.setRelAssertionType(RelAssertionType.STATED);
+
+        statedVc.setPositionSet(new PositionSet(latestOnSnomedPath));
+
+        v = rel.getVersion(statedVc);
+        Assert.assertNull(v);
+        // Assert.assertTrue(v.getAuthorNid() != statedVc.getClassifierNid());
+        // System.out.println("stated version: " + v);
+        System.out.println("stated version: null");
+
+        ViewCoordinate inferredVc = new ViewCoordinate(statedVc);
+        inferredVc.setRelAssertionType(RelAssertionType.INFERRED);
+
+        v = rel.getVersion(inferredVc);
+        Assert.assertNotNull(v);
+        Assert.assertTrue(v.getAuthorNid() == statedVc.getClassifierNid());
+        System.out.println("inferred version: " + v);
+
+
+        ViewCoordinate inferredThenStatedVc = new ViewCoordinate(statedVc);
+        inferredThenStatedVc.setRelAssertionType(RelAssertionType.INFERRED_THEN_STATED);
+
+        v = rel.getVersion(inferredThenStatedVc);
+        Assert.assertNotNull(v);
+        Assert.assertTrue(v.getAuthorNid() == statedVc.getClassifierNid());
+        System.out.println("inferred then stated version: " + v);
+    }
 }
