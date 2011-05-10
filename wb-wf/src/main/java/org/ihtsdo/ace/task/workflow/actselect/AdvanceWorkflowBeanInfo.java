f529548a5dc5951aab2e30ca48477afaf754a5a1
jefron <jefron>
2011-05-10 04:29:17 +0000

    Not sure why, but while the AdvanceWorkflow refactor broke the related BP as expected, recreating it would not remove Failure on BP execution.  Will refactor & fix shortly

--- wb-wf/src/main/java/org/ihtsdo/ace/task/workflow/actselect/AdvanceWorkflowBeanInfo.java
+++ wb-wf/src/main/java/org/ihtsdo/ace/task/workflow/actselect/AdvanceWorkflowBeanInfo.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.ihtsdo.ace.task.workflow.select;
+package org.ihtsdo.ace.task.workflow.actselect;
 
 import java.beans.BeanDescriptor;
 import java.beans.PropertyDescriptor;
