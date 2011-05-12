4c2fd2f338f96160bb5ef7b557aa7583073886a5
alopez <alopez>
2011-05-12 19:32:24 +0000

    Improved features to support hierarchy uniqueness detection for descriptions

--- rule-engine-integration/src/main/java/org/ihtsdo/rules/testmodel/TerminologyHelperDroolsWorkbench.java
+++ rule-engine-integration/src/main/java/org/ihtsdo/rules/testmodel/TerminologyHelperDroolsWorkbench.java
@@ -134,7 +134,7 @@ public class TerminologyHelperDroolsWorkbench extends TerminologyHelperDrools {
 			if (docs.length > 0) {
 				I_GetConceptData originalConcept = Terms.get().getConcept(UUID.fromString(conceptUuid));
 				I_DescriptionTuple originalFsn = null;
-				
+
 				for (I_DescriptionTuple loopDescription : originalConcept.getDescriptionTuples(config.getAllowedStatus(), 
 						config.getDescTypes(), config.getViewPositionSetReadOnly(), 
 						config.getPrecedence(), config.getConflictResolutionStrategy())) {
@@ -148,218 +148,216 @@ public class TerminologyHelperDroolsWorkbench extends TerminologyHelperDrools {
 						Document doc = results.searcher.doc(i);
 						int cnid = Integer.parseInt(doc.get("cnid"));
 						int dnid = Integer.parseInt(doc.get("dnid"));
-						
-						I_GetConceptData potentialMatchConcept = Terms.get().getConcept(Integer.parseInt(doc.get("cnid")));
-						
-						I_DescriptionTuple potentialMatchFsn = null;
-						
-						for (I_DescriptionTuple loopDescription : potentialMatchConcept.getDescriptionTuples(config.getAllowedStatus(), 
-								config.getDescTypes(), config.getViewPositionSetReadOnly(), 
-								config.getPrecedence(), config.getConflictResolutionStrategy())) {
-							if (loopDescription.getTypeNid() == fsnTypeNid && loopDescription.getLang().toLowerCase().startsWith("en")) {
-								potentialMatchFsn = loopDescription;
-								potentialMatchSemtag = potentialMatchFsn.getText().substring(potentialMatchFsn.getText().lastIndexOf("(")).trim();
-							}
-						}
-						
-						I_DescriptionVersioned potential_match = Terms.get().getDescription(dnid, cnid);
-						if (potential_match != null) {
-
-							DescriptionVersionBI description = (DescriptionVersionBI) 
-							Ts.get().getComponentVersion(Terms.get().getActiveAceFrameConfig().getViewCoordinate(), dnid);
-
-							if (description.getText() == originalDescText && 
-									originalSemTag.equals(potentialMatchSemtag)) {
-								result = true;
-							}
-						}
-					}catch(Exception e){
-						//Do Nothing
-					}
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (ParseException e) {
-			e.printStackTrace();
-		} catch (TerminologyException e) {
-			e.printStackTrace();
-		}
-		return result;
-	}
-
-	@Override
-	public boolean isFsnTextNotUnique(String fsn, String conceptUuid) throws Exception{
-		boolean result = false;
-		I_TermFactory tf = Terms.get();
-		try {
-			int fsnTypeNid = tf.uuidToNative(ArchitectonicAuxiliary.Concept.FULLY_SPECIFIED_DESCRIPTION_TYPE.getUids());
-			fsn = "+\"" + QueryParser.escape(fsn) + "\"";
-			SearchResult results = tf.doLuceneSearch(fsn);
-			TopDocs topDocs = results.topDocs;
-			ScoreDoc[] docs = topDocs.scoreDocs;
-			for (int i = 0 ; i < docs.length  ; i++) {
-				try{
-					Document doc = results.searcher.doc(i);
-					int cnid = Integer.parseInt(doc.get("cnid"));
-					int dnid = Integer.parseInt(doc.get("dnid"));
-					I_DescriptionVersioned potential_fsn = Terms.get().getDescription(dnid, cnid);
-					if (potential_fsn != null) {
 
 						DescriptionVersionBI description = (DescriptionVersionBI) 
 						Ts.get().getComponentVersion(Terms.get().getActiveAceFrameConfig().getViewCoordinate(), dnid);
+						
+						if (description != null && description.getText().trim().equals(originalDescText.trim())) { 
 
-						if (description.getTypeNid() == fsnTypeNid
-								&& description.getText().equals(fsn)
-								&& description.getLang().equals(potential_fsn.getLang())) {
-							result = true;
-						}
+							I_GetConceptData potentialMatchConcept = Terms.get().getConcept(Integer.parseInt(doc.get("cnid")));
+
+							I_DescriptionTuple potentialMatchFsn = null;
+
+							for (I_DescriptionTuple loopDescription : potentialMatchConcept.getDescriptionTuples(config.getAllowedStatus(), 
+									config.getDescTypes(), config.getViewPositionSetReadOnly(), 
+									config.getPrecedence(), config.getConflictResolutionStrategy())) {
+								if (loopDescription.getTypeNid() == fsnTypeNid && loopDescription.getLang().toLowerCase().startsWith("en")) {
+									potentialMatchFsn = loopDescription;
+									potentialMatchSemtag = potentialMatchFsn.getText().substring(potentialMatchFsn.getText().lastIndexOf("(")).trim();
+								}
+							}
+
+							if (originalSemTag.equals(potentialMatchSemtag)) {
+								result = true;
+							}
 					}
 				}catch(Exception e){
 					//Do Nothing
 				}
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (ParseException e) {
-			e.printStackTrace();
-		} catch (TerminologyException e) {
-			e.printStackTrace();
 		}
-		return result;
+	} catch (IOException e) {
+		e.printStackTrace();
+	} catch (ParseException e) {
+		e.printStackTrace();
+	} catch (TerminologyException e) {
+		e.printStackTrace();
 	}
+	return result;
+}
 
-	@Override
-	public boolean isActive(String conceptUUID) {
-		boolean result = false;
+@Override
+public boolean isFsnTextNotUnique(String fsn, String conceptUuid) throws Exception{
+	boolean result = false;
+	I_TermFactory tf = Terms.get();
+	try {
+		int fsnTypeNid = tf.uuidToNative(ArchitectonicAuxiliary.Concept.FULLY_SPECIFIED_DESCRIPTION_TYPE.getUids());
+		fsn = "+\"" + QueryParser.escape(fsn) + "\"";
+		SearchResult results = tf.doLuceneSearch(fsn);
+		TopDocs topDocs = results.topDocs;
+		ScoreDoc[] docs = topDocs.scoreDocs;
+		for (int i = 0 ; i < docs.length  ; i++) {
+			try{
+				Document doc = results.searcher.doc(i);
+				int cnid = Integer.parseInt(doc.get("cnid"));
+				int dnid = Integer.parseInt(doc.get("dnid"));
+				I_DescriptionVersioned potential_fsn = Terms.get().getDescription(dnid, cnid);
+				if (potential_fsn != null) {
 
-		try {
-			ConceptVersionBI concept = Ts.get().getConceptVersion(Terms.get().getActiveAceFrameConfig().getViewCoordinate(), UUID.fromString(conceptUUID));
-			int status = concept.getConAttrs().getVersion(Terms.get().getActiveAceFrameConfig().getViewCoordinate()).getStatusNid();
-			if (status == ArchitectonicAuxiliary.Concept.ACTIVE.localize().getNid() ||
-					status == ArchitectonicAuxiliary.Concept.CURRENT.localize().getNid()) {
-				result = true;
-			}
+					DescriptionVersionBI description = (DescriptionVersionBI) 
+					Ts.get().getComponentVersion(Terms.get().getActiveAceFrameConfig().getViewCoordinate(), dnid);
 
-		} catch (TerminologyException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (ContraditionException e) {
-			e.printStackTrace();
-		}
-
-		return result;
-	}
-
-	@Override
-	public boolean isValidSemtag(String semtag){
-		return getValidSemtags().contains(semtag);
-	}
-
-	public static Set<I_GetConceptData> getDescendants(Set<I_GetConceptData> descendants, I_GetConceptData concept) {
-		try {
-			I_TermFactory termFactory = Terms.get();
-			I_ConfigAceFrame config = termFactory.getActiveAceFrameConfig();
-			I_IntSet allowedDestRelTypes =  termFactory.newIntSet();
-			allowedDestRelTypes.add(termFactory.uuidToNative(ArchitectonicAuxiliary.Concept.IS_A_REL.getUids()));
-			Set<I_GetConceptData> childrenSet = new HashSet<I_GetConceptData>();
-			childrenSet.addAll(concept.getDestRelOrigins(config.getAllowedStatus(), allowedDestRelTypes, 
-					config.getViewPositionSetReadOnly(), config.getPrecedence(), config.getConflictResolutionStrategy()));
-			descendants.addAll(childrenSet);
-			for (I_GetConceptData loopConcept : childrenSet) {
-				descendants = getDescendants(descendants, loopConcept);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (TerminologyException e) {
-			e.printStackTrace();
-		}
-		return descendants;
-	}
-
-	@Override
-	public boolean isParentOfStatedChildren(String conceptUuid){
-		boolean result = false;
-		//TODO implement for Stated when the definitive structure is defined
-		if (conceptUuid != null) {
-			try {
-				result = getDescendants(new HashSet<I_GetConceptData>(), Terms.get().getConcept(UUID.fromString(conceptUuid))).size() > 0;
-			} catch (TerminologyException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		return result;
-	}
-
-	@Override
-	public List<String> getListOfDomainsUuids(String conceptUuid) {
-		if (domains == null) {
-			domains = new ArrayList<String>();
-			I_TermFactory tf = Terms.get();
-			try {
-				I_ConfigAceFrame config = tf.getActiveAceFrameConfig();
-				Set<I_GetConceptData> allDomains = getDescendants(new HashSet<I_GetConceptData>(), 
-						Terms.get().getConcept(RefsetAuxiliary.Concept.MRCM_DOMAINS.getUids()));
-				for (I_GetConceptData domain : allDomains) {
-					if (isMemberOf(conceptUuid, domain.getPrimUuid().toString())) {
-						domains.add(domain.getPrimUuid().toString());
+					if (description.getTypeNid() == fsnTypeNid
+							&& description.getText().equals(fsn)
+							&& description.getLang().equals(potential_fsn.getLang())) {
+						result = true;
 					}
 				}
-			} catch (TerminologyException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
+			}catch(Exception e){
+				//Do Nothing
 			}
-			return domains;
-		} else {
-			return domains;
+		}
+	} catch (IOException e) {
+		e.printStackTrace();
+	} catch (ParseException e) {
+		e.printStackTrace();
+	} catch (TerminologyException e) {
+		e.printStackTrace();
+	}
+	return result;
+}
+
+@Override
+public boolean isActive(String conceptUUID) {
+	boolean result = false;
+
+	try {
+		ConceptVersionBI concept = Ts.get().getConceptVersion(Terms.get().getActiveAceFrameConfig().getViewCoordinate(), UUID.fromString(conceptUUID));
+		int status = concept.getConAttrs().getVersion(Terms.get().getActiveAceFrameConfig().getViewCoordinate()).getStatusNid();
+		if (status == ArchitectonicAuxiliary.Concept.ACTIVE.localize().getNid() ||
+				status == ArchitectonicAuxiliary.Concept.CURRENT.localize().getNid()) {
+			result = true;
+		}
+
+	} catch (TerminologyException e) {
+		e.printStackTrace();
+	} catch (IOException e) {
+		e.printStackTrace();
+	} catch (ContraditionException e) {
+		e.printStackTrace();
+	}
+
+	return result;
+}
+
+@Override
+public boolean isValidSemtag(String semtag){
+	return getValidSemtags().contains(semtag);
+}
+
+public static Set<I_GetConceptData> getDescendants(Set<I_GetConceptData> descendants, I_GetConceptData concept) {
+	try {
+		I_TermFactory termFactory = Terms.get();
+		I_ConfigAceFrame config = termFactory.getActiveAceFrameConfig();
+		I_IntSet allowedDestRelTypes =  termFactory.newIntSet();
+		allowedDestRelTypes.add(termFactory.uuidToNative(ArchitectonicAuxiliary.Concept.IS_A_REL.getUids()));
+		Set<I_GetConceptData> childrenSet = new HashSet<I_GetConceptData>();
+		childrenSet.addAll(concept.getDestRelOrigins(config.getAllowedStatus(), allowedDestRelTypes, 
+				config.getViewPositionSetReadOnly(), config.getPrecedence(), config.getConflictResolutionStrategy()));
+		descendants.addAll(childrenSet);
+		for (I_GetConceptData loopConcept : childrenSet) {
+			descendants = getDescendants(descendants, loopConcept);
+		}
+	} catch (IOException e) {
+		e.printStackTrace();
+	} catch (TerminologyException e) {
+		e.printStackTrace();
+	}
+	return descendants;
+}
+
+@Override
+public boolean isParentOfStatedChildren(String conceptUuid){
+	boolean result = false;
+	//TODO implement for Stated when the definitive structure is defined
+	if (conceptUuid != null) {
+		try {
+			result = getDescendants(new HashSet<I_GetConceptData>(), Terms.get().getConcept(UUID.fromString(conceptUuid))).size() > 0;
+		} catch (TerminologyException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
+	return result;
+}
 
-	@Override
-	public boolean isTargetOfReferToLink(String conceptUuid) {
-		//TODO implement when refer to import is defined
-		return false;
-	}
-
-	@Override
-	public boolean isExtensionConcept(String conceptUuid) {
-		//TODO implement when extensions representation is defined
-		return false;
-	}
-
-	@Override
-	public boolean isSemanticTagEqualsInAllTerms(String conceptUuid) {
-		boolean result = false;
+@Override
+public List<String> getListOfDomainsUuids(String conceptUuid) {
+	if (domains == null) {
+		domains = new ArrayList<String>();
+		I_TermFactory tf = Terms.get();
 		try {
-			I_TermFactory tf = Terms.get();
 			I_ConfigAceFrame config = tf.getActiveAceFrameConfig();
-
-			I_GetConceptData focusConcept = tf.getConcept(UUID.fromString(conceptUuid));
-			List<String> currentSemtags = new ArrayList<String>();
-			for (I_DescriptionTuple tuple : focusConcept.getDescriptionTuples(config.getAllowedStatus(),
-					config.getDescTypes(), config.getViewPositionSetReadOnly(), config.getPrecedence(),
-					config.getConflictResolutionStrategy())) {
-				if (tuple.getTypeNid() == ArchitectonicAuxiliary.Concept.FULLY_SPECIFIED_DESCRIPTION_TYPE.localize().getNid() 
-						&& tuple.getLang().equals("en")) {
-					if (tuple.getText().lastIndexOf("(") > -1 && tuple.getText().lastIndexOf(")") > -1) {
-						currentSemtags.add(tuple.getText().substring(tuple.getText().lastIndexOf("(")+1,tuple.getText().lastIndexOf(")")));
-					}
+			Set<I_GetConceptData> allDomains = getDescendants(new HashSet<I_GetConceptData>(), 
+					Terms.get().getConcept(RefsetAuxiliary.Concept.MRCM_DOMAINS.getUids()));
+			for (I_GetConceptData domain : allDomains) {
+				if (isMemberOf(conceptUuid, domain.getPrimUuid().toString())) {
+					domains.add(domain.getPrimUuid().toString());
 				}
-			}
-
-			if (getValidSemtags().containsAll(currentSemtags)) {
-				result = true;
 			}
 		} catch (TerminologyException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return result;
+		return domains;
+	} else {
+		return domains;
 	}
+}
+
+@Override
+public boolean isTargetOfReferToLink(String conceptUuid) {
+	//TODO implement when refer to import is defined
+	return false;
+}
+
+@Override
+public boolean isExtensionConcept(String conceptUuid) {
+	//TODO implement when extensions representation is defined
+	return false;
+}
+
+@Override
+public boolean isSemanticTagEqualsInAllTerms(String conceptUuid) {
+	boolean result = false;
+	try {
+		I_TermFactory tf = Terms.get();
+		I_ConfigAceFrame config = tf.getActiveAceFrameConfig();
+
+		I_GetConceptData focusConcept = tf.getConcept(UUID.fromString(conceptUuid));
+		List<String> currentSemtags = new ArrayList<String>();
+		for (I_DescriptionTuple tuple : focusConcept.getDescriptionTuples(config.getAllowedStatus(),
+				config.getDescTypes(), config.getViewPositionSetReadOnly(), config.getPrecedence(),
+				config.getConflictResolutionStrategy())) {
+			if (tuple.getTypeNid() == ArchitectonicAuxiliary.Concept.FULLY_SPECIFIED_DESCRIPTION_TYPE.localize().getNid() 
+					&& tuple.getLang().equals("en")) {
+				if (tuple.getText().lastIndexOf("(") > -1 && tuple.getText().lastIndexOf(")") > -1) {
+					currentSemtags.add(tuple.getText().substring(tuple.getText().lastIndexOf("(")+1,tuple.getText().lastIndexOf(")")));
+				}
+			}
+		}
+
+		if (getValidSemtags().containsAll(currentSemtags)) {
+			result = true;
+		}
+	} catch (TerminologyException e) {
+		e.printStackTrace();
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
+	return result;
+}
 
 }
