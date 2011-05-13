fec45d5fe514382b8929e611cf433f4bfda6d6bb
alopez <alopez>
2011-05-13 19:46:08 +0000

    removed language combo box from index dictionary

--- document-management-ndb/src/main/java/org/ihtsdo/document/DictionaryIndexPanel.java
+++ document-management-ndb/src/main/java/org/ihtsdo/document/DictionaryIndexPanel.java
@@ -52,30 +52,30 @@ public class DictionaryIndexPanel extends JPanel  implements ActionListener {
 
 	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = 1L;
-	
+
 	/** The html pane. */
 	JEditorPane htmlPane;
-	
+
 	/** The browser pane. */
 	JScrollPane browserPane;
-	
+
 	/** The index file button. */
 	JButton indexFileButton;
-	
+
 	/** The index wb button. */
 	JButton indexWBButton;
 
 	private JComboBox cmbLangs;
-	
+
 	private JLabel helpLabel;
 
-	
+
 	/**
 	 * Instantiates a new dictionary index panel.
 	 */
 	public DictionaryIndexPanel() {
 		this.setLayout(new BorderLayout());
-		
+
 		String results = "<html><body><font style='font-family:arial,sans-serif'>" +
 		"Select an option to index the dictionary..." +
 		"</font></body></html>";
@@ -96,14 +96,14 @@ public class DictionaryIndexPanel extends JPanel  implements ActionListener {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}catch (Exception e){
-//				e.printStackTrace();
+				//				e.printStackTrace();
 			}
 		}
 
 		indexFileButton = new JButton("Index dictionary from text file");
 		indexFileButton.setActionCommand("indexFromFile");
 		indexFileButton.addActionListener(this);
-		
+
 		helpLabel = new JLabel();
 		helpLabel.setIcon(IconUtilities.helpIcon);
 		helpLabel.setText("");
@@ -119,114 +119,100 @@ public class DictionaryIndexPanel extends JPanel  implements ActionListener {
 				};
 			}
 		});
-		
-//		indexWBButton = new JButton("Index dictionary from current workbench database");
-//		indexWBButton.setActionCommand("indexFromWBDB");
-//		indexWBButton.addActionListener(this);
-		
+
+		//		indexWBButton = new JButton("Index dictionary from current workbench database");
+		//		indexWBButton.setActionCommand("indexFromWBDB");
+		//		indexWBButton.addActionListener(this);
+
 		Container centerContainer = new Container();
 		centerContainer.setLayout(new BorderLayout());
 		centerContainer.add(browserPane, BorderLayout.CENTER);
 		this.add(centerContainer, BorderLayout.CENTER);
-		
+
 		Container bottomContainer = new Container();
 		bottomContainer.setLayout(new BoxLayout(bottomContainer, BoxLayout.X_AXIS));
-		bottomContainer.add(cmbLangs);
+		//bottomContainer.add(cmbLangs);
 		bottomContainer.add(indexFileButton);
 		bottomContainer.add(helpLabel);
-		
-//		bottomContainer.add(indexWBButton);
-		
+
+		//		bottomContainer.add(indexWBButton);
+
 		this.add(bottomContainer, BorderLayout.PAGE_END);
-		
+
 	}
-	
-	 /**
- 	 * Creates the and show gui.
- 	 */
- 	private static void createAndShowGUI() {
-	        //Create and set up the window.
-	        JFrame frame = new JFrame("DictionaryIndexPanel");
-	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-	        //Create and set up the content pane.
-	        DictionaryIndexPanel newContentPane = new DictionaryIndexPanel();
-	        newContentPane.setOpaque(true); //content panes must be opaque
-	        frame.setContentPane(newContentPane);
+	/**
+	 * Creates the and show gui.
+	 */
+	private static void createAndShowGUI() {
+		//Create and set up the window.
+		JFrame frame = new JFrame("DictionaryIndexPanel");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-	        //Display the window.
-	        frame.pack();
-	        frame.setVisible(true);
-	    }
+		//Create and set up the content pane.
+		DictionaryIndexPanel newContentPane = new DictionaryIndexPanel();
+		newContentPane.setOpaque(true); //content panes must be opaque
+		frame.setContentPane(newContentPane);
 
-	    /**
-    	 * The main method.
-    	 * 
-    	 * @param args the arguments
-    	 */
-    	public static void main(String[] args) {
-	        //Schedule a job for the event-dispatching thread:
-	        //creating and showing this application's GUI.
-	        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-	            public void run() {
-	                createAndShowGUI();
-	            }
-	        });
-	    }
-	    
-	    /* (non-Javadoc)
-    	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
-    	 */
-    	public void actionPerformed(ActionEvent e) {
-    		if ("indexFromFile".equals(e.getActionCommand())) {
-    			String results = "<html><body><font style='font-family:arial,sans-serif'>" +
-    			"Indexing from file..." +
-    			"</font></body></html>";
-    			htmlPane.setText(results);
-    			htmlPane.setCaretPosition(0);
-    			htmlPane.revalidate();
-    			I_GetConceptData concLang= (I_GetConceptData)cmbLangs.getSelectedItem();
-    			if (concLang!=null){
-    				String lCode=null;
-    				try {
-    					lCode = ArchitectonicAuxiliary.getLanguageCode(concLang.getUids());
-    				} catch (NoSuchElementException e1) {
-    					// TODO Auto-generated catch block
-    					e1.printStackTrace();
-    				} catch (IOException e1) {
-    					// TODO Auto-generated catch block
-    					e1.printStackTrace();
-    				}
-    				if (lCode!=null){
-    					JFileChooser fileopen = new JFileChooser();
-    					int ret = fileopen.showDialog(this, "Open file");
-    					File dictionaryTextFile = null;
-    					if (ret == JFileChooser.APPROVE_OPTION) {
-    						dictionaryTextFile = fileopen.getSelectedFile();
-    					}
-    					if(dictionaryTextFile != null){
-    						results = DocumentManager.indexDictionaryFromTextFile(true,dictionaryTextFile);
-    					}else{
-    						results = "No file was selected...";
-    					}
-    					htmlPane.setText(results);
-    					htmlPane.setCaretPosition(0);
-    					htmlPane.revalidate();
-    				}
-    			}
-    		}
-	    	
-	    	if ("indexFromWBDB".equals(e.getActionCommand())) {
-	    		String results = "<html><body><font style='font-family:arial,sans-serif'>" +
-	    		"Indexing from WorkBench database..." +
-	    		"</font></body></html>";
-	    		htmlPane.setText(results);
-	    		htmlPane.setCaretPosition(0);
-	    		htmlPane.revalidate();
-	    		results = DocumentManager.indexDictionaryFromWorkbenchDatabase(true);
-	    		htmlPane.setText(results);
-	    		htmlPane.setCaretPosition(0);
-	    		htmlPane.revalidate();
-	    	}
-	    }
+		//Display the window.
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	/**
+	 * The main method.
+	 * 
+	 * @param args the arguments
+	 */
+	public static void main(String[] args) {
+		//Schedule a job for the event-dispatching thread:
+		//creating and showing this application's GUI.
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndShowGUI();
+			}
+		});
+	}
+
+	/* (non-Javadoc)
+	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 */
+	public void actionPerformed(ActionEvent e) {
+		if ("indexFromFile".equals(e.getActionCommand())) {
+			String results = "<html><body><font style='font-family:arial,sans-serif'>" +
+			"Indexing from file..." +
+			"</font></body></html>";
+			htmlPane.setText(results);
+			htmlPane.setCaretPosition(0);
+			htmlPane.revalidate();
+			I_GetConceptData concLang= (I_GetConceptData)cmbLangs.getSelectedItem();
+			JFileChooser fileopen = new JFileChooser();
+			int ret = fileopen.showDialog(this, "Open file");
+			File dictionaryTextFile = null;
+			if (ret == JFileChooser.APPROVE_OPTION) {
+				dictionaryTextFile = fileopen.getSelectedFile();
+			}
+			if(dictionaryTextFile != null){
+				results = DocumentManager.indexDictionaryFromTextFile(true,dictionaryTextFile);
+			}else{
+				results = "No file was selected...";
+			}
+			htmlPane.setText(results);
+			htmlPane.setCaretPosition(0);
+			htmlPane.revalidate();
+		}
+
+		if ("indexFromWBDB".equals(e.getActionCommand())) {
+			String results = "<html><body><font style='font-family:arial,sans-serif'>" +
+			"Indexing from WorkBench database..." +
+			"</font></body></html>";
+			htmlPane.setText(results);
+			htmlPane.setCaretPosition(0);
+			htmlPane.revalidate();
+			results = DocumentManager.indexDictionaryFromWorkbenchDatabase(true);
+			htmlPane.setText(results);
+			htmlPane.setCaretPosition(0);
+			htmlPane.revalidate();
+		}
+	}
 }
