63e3c9933dfbac75bb1eb4cb123c6f2017fc737c
kec <kec>
2011-05-16 22:25:28 +0000

    Removed unnecessary import statement.

--- wb-gui/src/main/java/org/ihtsdo/arena/conceptview/ComponentVersionDragPanel.java
+++ wb-gui/src/main/java/org/ihtsdo/arena/conceptview/ComponentVersionDragPanel.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.EnumSet;
 import java.util.List;
-import java.util.Locale;
 import java.util.concurrent.ExecutionException;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
