f74fbb259e6a9492ba78cfaf1b8018492256404f
jefron <jefron>
2011-05-27 19:56:45 +0000

    Proper identification for WfHx Refset Contradictions (STATE & WFID Only)

--- wb-bdb/src/main/java/org/ihtsdo/concept/RefsetAttributeComparer.java
+++ wb-bdb/src/main/java/org/ihtsdo/concept/RefsetAttributeComparer.java
@@ -1,9 +1,20 @@
 package org.ihtsdo.concept;
 
 
+import java.io.IOException;
+import java.util.UUID;
+import java.util.logging.Level;
+
+import org.dwfa.ace.api.Terms;
+import org.dwfa.ace.api.ebr.I_ExtendByRef;
 import org.dwfa.ace.api.ebr.I_ExtendByRefVersion;
+import org.dwfa.ace.log.AceLog;
+import org.dwfa.cement.RefsetAuxiliary;
+import org.dwfa.tapi.TerminologyException;
 import org.ihtsdo.tk.api.ComponentVersionBI;
 import org.ihtsdo.tk.contradiction.ComponentType;
+import org.ihtsdo.workflow.WorkflowHistoryJavaBean;
+import org.ihtsdo.workflow.refset.utilities.WorkflowHelper;
 
 public class RefsetAttributeComparer extends AttributeComparer {
 
@@ -14,9 +25,15 @@ public class RefsetAttributeComparer extends AttributeComparer {
 	private int lcaStatusNid = 0;
 	private int hashCode = 0;
 
-	public RefsetAttributeComparer() {
+	private int wfHxRefsetId;
+
+	private UUID workflowId = UUID.randomUUID();
+	private UUID workflowState = UUID.randomUUID();
+	
+	public RefsetAttributeComparer() throws TerminologyException, IOException {
 		super();
 		componentType = ComponentType.REFSET;
+		wfHxRefsetId = Terms.get().getConcept(RefsetAuxiliary.Concept.WORKFLOW_HISTORY.getPrimoridalUid()).getConceptNid();
 	}
 
 	@Override
@@ -32,6 +49,14 @@ public class RefsetAttributeComparer extends AttributeComparer {
 			return false;
 		}
 
+		if (refsetId == wfHxRefsetId) {
+			WorkflowHistoryJavaBean bean =  WorkflowHelper.populateWorkflowHistoryJavaBean((I_ExtendByRef) refsetVersion);
+
+			if ((bean.getState() != workflowState) || 
+				(bean.getWorkflowId() != workflowId)) {
+				return false;
+			}
+		}
 		return true;
 	}
 
@@ -48,6 +73,15 @@ public class RefsetAttributeComparer extends AttributeComparer {
 		
 		comparerInitialized = true;
 		
-	}
+		if (refsetId == wfHxRefsetId) {
+			try {
+				WorkflowHistoryJavaBean bean =  WorkflowHelper.populateWorkflowHistoryJavaBean((I_ExtendByRef) refsetVersion);
 
+				workflowState = bean.getState();
+				workflowId = bean.getWorkflowId();
+			} catch (Exception e) {
+	            AceLog.getAppLog().log(Level.WARNING, "Failure to read WfHx Java Bean from Refset Version");
+			}
+		}
+	}
 }
