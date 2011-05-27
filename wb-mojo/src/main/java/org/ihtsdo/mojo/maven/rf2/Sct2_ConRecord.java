d330f199d2aa39f2b3c9556ba90a5ec43b01e66c
marc <marc>
2011-05-27 06:45:50 +0000

    SCT RF2 to ARF conversion

---
+++ wb-mojo/src/main/java/org/ihtsdo/mojo/maven/rf2/Sct2_ConRecord.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2011 International Health Terminology Standards Development Organisation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ihtsdo.mojo.maven.rf2;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.maven.plugin.MojoFailureException;
+import org.dwfa.tapi.TerminologyException;
+
+class Sct2_ConRecord implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private static final String LINE_TERMINATOR = "\r\n";
+    private static final String TAB_CHARACTER = "\t";
+    // RECORD FIELDS
+    long conSnoIdL; //  id
+    String effDateStr; // effectiveTime
+    boolean isActive; // CONCEPTSTATUS
+    String pathStr; // Module ID
+    boolean isPrimitiveB; // ISPRIMITIVE
+
+    public Sct2_ConRecord(long conIdL, String dateStr, boolean active, String path, boolean isPrim) {
+        this.conSnoIdL = conIdL;
+        this.effDateStr = dateStr;
+        this.isActive = active;
+
+        this.pathStr = path;
+        this.isPrimitiveB = isPrim;
+    }
+
+    static Sct2_ConRecord[] parseConcepts(Rf2File f) throws MojoFailureException {
+        try {
+            int count = Rf2File.countFileLines(f);
+            Sct2_ConRecord[] a = new Sct2_ConRecord[count];
+
+            // DATA COLUMNS
+            int ID = 0;// id
+            int EFFECTIVE_TIME = 2; // effectiveTime
+            int ACTIVE = 1; // active
+            int MODULE_ID = 3; // moduleId
+            int DEFINITION_STATUS_ID = 4; // definitionStatusId
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    new FileInputStream(f.file), "UTF-8"));
+
+            int idx = 0;
+            br.readLine(); // Header row
+            while (br.ready()) {
+                String[] line = br.readLine().split(TAB_CHARACTER);
+
+                a[idx] = new Sct2_ConRecord(Long.parseLong(line[ID]),
+                        Rf2x.convertEffectiveTimeToDate(line[EFFECTIVE_TIME]),
+                        Rf2x.convertStringToBoolean(line[ACTIVE]),
+                        Rf2x.convertIdToUuidStr(line[MODULE_ID]),
+                        Rf2x.convertDefinitionStatusToIsPrimitive(line[DEFINITION_STATUS_ID]));
+                idx++;
+            }
+
+            return a;
+        } catch (IOException ex) {
+            Logger.getLogger(Sct2_ConRecord.class.getName()).log(Level.SEVERE, null, ex);
+            throw new MojoFailureException("error parsing rf2 concepts", ex);
+        }
+    }
+
+    // Create string to show some input fields for exception reporting
+    @Override
+    public String toString() {
+        return conSnoIdL + TAB_CHARACTER + isActive + TAB_CHARACTER + isPrimitiveB;
+    }
+
+    public void writeArf(BufferedWriter writer) throws IOException, TerminologyException {
+        // Concept UUID
+        writer.append(Rf2x.convertIdToUuidStr(conSnoIdL) + TAB_CHARACTER);
+
+        // Status UUID
+        writer.append(Rf2x.convertActiveToStatusUuid(isActive) + TAB_CHARACTER);
+
+        // Primitive string 0 (false == defined) or 1 (true == primitive)
+        if (isPrimitiveB) {
+            writer.append("1" + TAB_CHARACTER);
+        } else {
+            writer.append("0" + TAB_CHARACTER);
+        }
+
+        // Effective Date yyyy-MM-dd HH:mm:ss
+        writer.append(effDateStr + TAB_CHARACTER);
+
+        // Path UUID
+        writer.append(pathStr + LINE_TERMINATOR);
+    }
+}
