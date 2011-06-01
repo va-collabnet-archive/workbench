244066b0bcb98ed27720aa1e96afc3a27038d461
marc <marc>
2011-06-01 22:42:18 +0000

    Added conditional to handle RF2 historic relationships

--- src/main/java/org/ihtsdo/mojo/maven/rf2/Sct2_RelRecord.java
+++ src/main/java/org/ihtsdo/mojo/maven/rf2/Sct2_RelRecord.java
@@ -138,7 +138,7 @@ class Sct2_RelRecord implements Serializable {
         // Characteristic Type UUID
         if (characteristicL >= 0) {
             writer.append(Rf2x.convertIdToUuidStr(characteristicL) + TAB_CHARACTER);
-        } else {
+        } else { // -1 == historical relationship
             writer.append("1d054ca3-2b32-3004-b7af-2701276059d5" + TAB_CHARACTER);
         }
 
