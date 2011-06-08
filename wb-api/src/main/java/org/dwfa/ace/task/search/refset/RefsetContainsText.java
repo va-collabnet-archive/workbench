2552ba8181b97ad9610b760bb4b52d80b46c8068
kec <kec>
2011-06-08 23:02:50 +0000

    [artf222241] made refset member query case insensitive.

--- wb-api/src/main/java/org/dwfa/ace/task/search/refset/RefsetContainsText.java
+++ wb-api/src/main/java/org/dwfa/ace/task/search/refset/RefsetContainsText.java
@@ -12,17 +12,16 @@ import org.dwfa.util.bean.BeanList;
 import org.dwfa.util.bean.BeanType;
 import org.dwfa.util.bean.Spec;
 
-@BeanList(specs = { @Spec(directory = "tasks/ide/search", type = BeanType.TASK_BEAN),
-        @Spec(directory = "search", type = BeanType.TASK_BEAN) })
+@BeanList(specs = {
+    @Spec(directory = "tasks/ide/search", type = BeanType.TASK_BEAN),
+    @Spec(directory = "search", type = BeanType.TASK_BEAN)})
 public class RefsetContainsText extends AbstractSearchTest {
 
     private static final long serialVersionUID = 1;
-
     private static final int dataVersion = 1;
-    
     private String text = "text";
 
-	private void writeObject(ObjectOutputStream out) throws IOException {
+    private void writeObject(ObjectOutputStream out) throws IOException {
         out.writeInt(dataVersion);
         out.writeUTF(text);
     }
@@ -30,31 +29,28 @@ public class RefsetContainsText extends AbstractSearchTest {
     private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException {
         int objDataVersion = in.readInt();
         if (objDataVersion == 1) {
-        	text = in.readUTF();
+            text = in.readUTF();
         } else {
             throw new IOException("Can't handle dataversion: " + objDataVersion);
         }
     }
 
     @Override
-	public boolean test(I_AmTermComponent component,
-			I_ConfigAceFrame frameConfig) throws TaskFailedException {
-		if (I_ExtendByRefVersion.class.isAssignableFrom(component.getClass())) {
-			I_ExtendByRefVersion extV = (I_ExtendByRefVersion) component;
-			String extStr = extV.toString().toLowerCase();
-			return extStr.contains(text);
-		}
-		return false;
-	}
-
+    public boolean test(I_AmTermComponent component,
+            I_ConfigAceFrame frameConfig) throws TaskFailedException {
+        if (I_ExtendByRefVersion.class.isAssignableFrom(component.getClass())) {
+            I_ExtendByRefVersion extV = (I_ExtendByRefVersion) component;
+            String extStr = extV.toString().toLowerCase();
+            return extStr.contains(text.toLowerCase());
+        }
+        return false;
+    }
 
     public String getText() {
-		return text;
-	}
+        return text;
+    }
 
-	public void setText(String text) {
-		this.text = text;
-	}
-
-
+    public void setText(String text) {
+        this.text = text;
+    }
 }
