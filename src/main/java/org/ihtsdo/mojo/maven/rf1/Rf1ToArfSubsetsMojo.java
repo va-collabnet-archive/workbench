9e7d9bd09e5e5b93cb9bda47e160eb7c45a18ad6
vparekh <vparekh>
2011-06-13 21:33:52 +0000

    Supporting uuid creation for language refset...

--- src/main/java/org/ihtsdo/mojo/maven/rf1/Rf1ToArfSubsetsMojo.java
+++ src/main/java/org/ihtsdo/mojo/maven/rf1/Rf1ToArfSubsetsMojo.java
@@ -401,9 +401,11 @@ public class Rf1ToArfSubsetsMojo extends AbstractMojo implements Serializable {
         }else if(sid.getRefsetFsName().equals("Non-human Subset")){
         	uuid = Type5UuidFactory.get("447564002" + Long.toString(m.memberId)); //public final static String NON_HUMAN_REFSET_ID = "447564002";
         }else if(sid.getRefsetFsName().equals("GB English Dialect Subset")){
-	    	uuid = Type5UuidFactory.get("900000000000508004" + m.memberId); //public final static String GB_LANG_REFSET_ID = "900000000000508004";
+        	getLog().info("==GB Lang Refset====" + sid.getRefsetFsName());
+	    	uuid = Type5UuidFactory.get("900000000000508004" + Long.toString(m.memberId)); //public final static String GB_LANG_REFSET_ID = "900000000000508004";
 	    }else if(sid.getRefsetFsName().equals("US English Dialect Subset")){
-	    	uuid = Type5UuidFactory.get("900000000000509007" + m.memberId); //public final static String US_LANG_REFSET_ID = "900000000000509007";
+	    	getLog().info("==US Lang Refset====" + sid.getRefsetFsName());
+	    	uuid = Type5UuidFactory.get("900000000000509007" + Long.toString(m.memberId)); //public final static String US_LANG_REFSET_ID = "900000000000509007";
 	    }else if(sid.getRefsetFsName().equals("US Specific Proprietary Drug Descriptions Subset")){
         	uuid = Type5UuidFactory.get("US Specific Proprietary Drug Descriptions Subset" + m.memberId); 
         }else if(sid.getRefsetFsName().equals("US Specific Proprietary Drug Concepts Subset")){
