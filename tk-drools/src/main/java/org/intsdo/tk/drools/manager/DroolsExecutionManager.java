6712df19039cff0006bca4d14100247a2ef0599e
kec <kec>
2011-06-18 21:06:50 +0000

    Removed properties specifying Janino for drools.

--- tk-drools/src/main/java/org/intsdo/tk/drools/manager/DroolsExecutionManager.java
+++ tk-drools/src/main/java/org/intsdo/tk/drools/manager/DroolsExecutionManager.java
@@ -61,241 +61,240 @@ import org.ihtsdo.tk.drools.SatisfiesConstraintEvaluatorDefinition;
  */
 public class DroolsExecutionManager {
 
-	private KnowledgeBase kbase;
-	private final Set<File> kbFiles;
-	private final File drlPkgFile;
-	private final KnowledgeBaseConfiguration kBaseConfig;
+    private KnowledgeBase kbase;
+    private final Set<File> kbFiles;
+    private final File drlPkgFile;
+    private final KnowledgeBaseConfiguration kBaseConfig;
 
-	public static enum ExtraEvaluators {
+    public static enum ExtraEvaluators {
 
-		IS_KIND_OF, SAFISFIES_CONSTRAINT,
-		IS_MEMBER_OF, IS_PARENT_MEMBER_OF,
-		IS_MISSING_DESC_FOR, IS_GB_MEMBER_TYPE_OF,
-		IS_US_MEMBER_TYPE_OF, IS_SYNONYM_MEMBER_TYPE_OF;
-	}
-	private boolean failed = false;
-	Collection<KnowledgePackage> kpkgs = null;
-	private static ConcurrentHashMap<String, Semaphore> packageLocks = new ConcurrentHashMap<String, Semaphore>();
+        IS_KIND_OF, SAFISFIES_CONSTRAINT,
+        IS_MEMBER_OF, IS_PARENT_MEMBER_OF,
+        IS_MISSING_DESC_FOR, IS_GB_MEMBER_TYPE_OF,
+        IS_US_MEMBER_TYPE_OF, IS_SYNONYM_MEMBER_TYPE_OF;
+    }
+    private boolean failed = false;
+    Collection<KnowledgePackage> kpkgs = null;
+    private static ConcurrentHashMap<String, Semaphore> packageLocks = new ConcurrentHashMap<String, Semaphore>();
 
-	public DroolsExecutionManager(Set<File> kbFiles, String kbKey) throws IOException {
-		this(EnumSet.allOf(ExtraEvaluators.class),
-				kbFiles, kbKey);
-	}
+    public DroolsExecutionManager(Set<File> kbFiles, String kbKey) throws IOException {
+        this(EnumSet.allOf(ExtraEvaluators.class),
+                kbFiles, kbKey);
+    }
 
-	public DroolsExecutionManager(EnumSet<ExtraEvaluators> extraEvaluators,
-			Set<File> kbFiles, String kbKey) throws IOException {
+    public DroolsExecutionManager(EnumSet<ExtraEvaluators> extraEvaluators,
+            Set<File> kbFiles, String kbKey) throws IOException {
 
-		boolean buildKb = false;
-		File ruleDirectory = new File("rules");
-		ruleDirectory.mkdirs();
-		this.kbFiles = kbFiles;
-		kbKey = sanatizeKey(kbKey);
-		drlPkgFile = new File(ruleDirectory, kbKey + ".kpkgs");
+        boolean buildKb = false;
+        File ruleDirectory = new File("rules");
+        ruleDirectory.mkdirs();
+        this.kbFiles = kbFiles;
+        kbKey = sanatizeKey(kbKey);
+        drlPkgFile = new File(ruleDirectory, kbKey + ".kpkgs");
 
-		Semaphore s = new Semaphore(1);
-		Semaphore s2 = packageLocks.putIfAbsent(drlPkgFile.getCanonicalPath(), s);
-		if (s2 != null) {
-			s = s2;
-		}
+        Semaphore s = new Semaphore(1);
+        Semaphore s2 = packageLocks.putIfAbsent(drlPkgFile.getCanonicalPath(), s);
+        if (s2 != null) {
+            s = s2;
+        }
 
-		for (File f : this.kbFiles) {
-			if (!drlPkgFile.exists() || drlPkgFile.lastModified() < f.lastModified()) {
-				s.acquireUninterruptibly();
-				if (!drlPkgFile.exists() || drlPkgFile.lastModified() < f.lastModified()) {
-					buildKb = true;
-					break;
-				} else {
-					s.release();
-				}
-			}
-		}
+        for (File f : this.kbFiles) {
+            if (!drlPkgFile.exists() || drlPkgFile.lastModified() < f.lastModified()) {
+                s.acquireUninterruptibly();
+                if (!drlPkgFile.exists() || drlPkgFile.lastModified() < f.lastModified()) {
+                    buildKb = true;
+                    break;
+                } else {
+                    s.release();
+                }
+            }
+        }
 
-		if (buildKb) {
-			loadKnowledgePackages(kbFiles, extraEvaluators, drlPkgFile);
-			s.release();
-		} else {
-			ObjectInputStream in = new ObjectInputStream(new FileInputStream(drlPkgFile));
-			try {
-				kpkgs = (Collection<KnowledgePackage>) in.readObject();
-			} catch (ClassNotFoundException e) {
-				throw new IOException(e);
-			} finally {
-				in.close();
-			}
-		}
+        if (buildKb) {
+            loadKnowledgePackages(kbFiles, extraEvaluators, drlPkgFile);
+            s.release();
+        } else {
+            ObjectInputStream in = new ObjectInputStream(new FileInputStream(drlPkgFile));
+            try {
+                kpkgs = (Collection<KnowledgePackage>) in.readObject();
+            } catch (ClassNotFoundException e) {
+                throw new IOException(e);
+            } finally {
+                in.close();
+            }
+        }
 
-		kBaseConfig =
-			KnowledgeBaseFactory.newKnowledgeBaseConfiguration();
+        kBaseConfig =
+                KnowledgeBaseFactory.newKnowledgeBaseConfiguration();
 
-		Class<? extends ConsequenceExceptionHandler> exHandlerClass =
-			DroolsExceptionHandler.class;
+        Class<? extends ConsequenceExceptionHandler> exHandlerClass =
+                DroolsExceptionHandler.class;
 
-		ConsequenceExceptionHandlerOption cehOption =
-			ConsequenceExceptionHandlerOption.get(exHandlerClass);
+        ConsequenceExceptionHandlerOption cehOption =
+                ConsequenceExceptionHandlerOption.get(exHandlerClass);
 
-		kBaseConfig.setOption(cehOption);
-		kBaseConfig.setProperty("drools.dialect.java.compiler", "JANINO");
+        kBaseConfig.setOption(cehOption);
+//        kBaseConfig.setProperty("drools.dialect.java.compiler", "JANINO");
 
-		kbase = KnowledgeBaseFactory.newKnowledgeBase(kBaseConfig);
-		if (kpkgs != null) {
-			kbase.addKnowledgePackages(kpkgs);
-		}
-	}
+        kbase = KnowledgeBaseFactory.newKnowledgeBase(kBaseConfig);
+        if (kpkgs != null) {
+            kbase.addKnowledgePackages(kpkgs);
+        }
+    }
 
-	private static String sanatizeKey(String kbKey) {
-		if (kbKey.contains(":")) {
-			kbKey = kbKey.replace(':', '.');
-		}
-		if (kbKey.contains("/")) {
-			kbKey = kbKey.replace('/', '.');
-		}
-		if (kbKey.contains("*")) {
-			kbKey = kbKey.replace('*', '.');
-		}
-		if (kbKey.contains("\\")) {
-			kbKey = kbKey.replace('\\', '.');
-		}
-		return kbKey;
-	}
+    private static String sanatizeKey(String kbKey) {
+        if (kbKey.contains(":")) {
+            kbKey = kbKey.replace(':', '.');
+        }
+        if (kbKey.contains("/")) {
+            kbKey = kbKey.replace('/', '.');
+        }
+        if (kbKey.contains("*")) {
+            kbKey = kbKey.replace('*', '.');
+        }
+        if (kbKey.contains("\\")) {
+            kbKey = kbKey.replace('\\', '.');
+        }
+        return kbKey;
+    }
 
-	public final void loadKnowledgePackages(Set<File> kbFiles, EnumSet<ExtraEvaluators> extraEvaluators, File drlPkgFile) throws RuntimeException, IOException {
-		System.out.println("Writing: " + drlPkgFile.getCanonicalPath());
-		HashMap<Resource, ResourceType> resources = new HashMap<Resource, ResourceType>();
-		for (File f : kbFiles) {
-			resources.put(ResourceFactory.newFileResource(f), ResourceType.DRL);
-		}
-		Properties props = new Properties();
-	    props.setProperty("drools.dialect.java.compiler", "JANINO");
-		KnowledgeBuilderConfiguration builderConfig =
-			KnowledgeBuilderFactory.newKnowledgeBuilderConfiguration(props, null);
+    public final void loadKnowledgePackages(Set<File> kbFiles, EnumSet<ExtraEvaluators> extraEvaluators, File drlPkgFile) throws RuntimeException, IOException {
+        System.out.println("Writing: " + drlPkgFile.getCanonicalPath());
+        HashMap<Resource, ResourceType> resources = new HashMap<Resource, ResourceType>();
+        for (File f : kbFiles) {
+            resources.put(ResourceFactory.newFileResource(f), ResourceType.DRL);
+        }
+        Properties props = new Properties();
+        //props.setProperty("drools.dialect.java.compiler", "JANINO");
+        KnowledgeBuilderConfiguration builderConfig =
+                KnowledgeBuilderFactory.newKnowledgeBuilderConfiguration(props, null);
 
-		if (extraEvaluators.contains(ExtraEvaluators.IS_KIND_OF)) {
-			builderConfig.setOption(EvaluatorOption.get(
-					IsKindOfEvaluatorDefinition.IS_KIND_OF.getOperatorString(),
-					new IsKindOfEvaluatorDefinition()));
-		}
-		if (extraEvaluators.contains(ExtraEvaluators.SAFISFIES_CONSTRAINT)) {
-			builderConfig.setOption(EvaluatorOption.get(
-					SatisfiesConstraintEvaluatorDefinition.SATISFIES_CONSTRAINT.getOperatorString(),
-					new SatisfiesConstraintEvaluatorDefinition()));
-		}
-		if (extraEvaluators.contains(ExtraEvaluators.IS_MEMBER_OF)) {
-			builderConfig.setOption(EvaluatorOption.get(
-					IsMemberOfEvaluatorDefinition.IS_MEMBER_OF.getOperatorString(),
-					new IsMemberOfEvaluatorDefinition()));
-		}
-		if (extraEvaluators.contains(ExtraEvaluators.IS_PARENT_MEMBER_OF)) {
-			builderConfig.setOption(EvaluatorOption.get(
-					IsParentMemberOfEvaluatorDefinition.IS_PARENT_MEMBER_OF.getOperatorString(),
-					new IsParentMemberOfEvaluatorDefinition()));
-		}
-		if (extraEvaluators.contains(ExtraEvaluators.IS_MISSING_DESC_FOR)) {
-			builderConfig.setOption(EvaluatorOption.get(
-					IsMissingDescForDialectEvaluatorDefinition.IS_MISSING_DESC_FOR.getOperatorString(),
-					new IsMissingDescForDialectEvaluatorDefinition()));
-		}
+        if (extraEvaluators.contains(ExtraEvaluators.IS_KIND_OF)) {
+            builderConfig.setOption(EvaluatorOption.get(
+                    IsKindOfEvaluatorDefinition.IS_KIND_OF.getOperatorString(),
+                    new IsKindOfEvaluatorDefinition()));
+        }
+        if (extraEvaluators.contains(ExtraEvaluators.SAFISFIES_CONSTRAINT)) {
+            builderConfig.setOption(EvaluatorOption.get(
+                    SatisfiesConstraintEvaluatorDefinition.SATISFIES_CONSTRAINT.getOperatorString(),
+                    new SatisfiesConstraintEvaluatorDefinition()));
+        }
+        if (extraEvaluators.contains(ExtraEvaluators.IS_MEMBER_OF)) {
+            builderConfig.setOption(EvaluatorOption.get(
+                    IsMemberOfEvaluatorDefinition.IS_MEMBER_OF.getOperatorString(),
+                    new IsMemberOfEvaluatorDefinition()));
+        }
+        if (extraEvaluators.contains(ExtraEvaluators.IS_PARENT_MEMBER_OF)) {
+            builderConfig.setOption(EvaluatorOption.get(
+                    IsParentMemberOfEvaluatorDefinition.IS_PARENT_MEMBER_OF.getOperatorString(),
+                    new IsParentMemberOfEvaluatorDefinition()));
+        }
+        if (extraEvaluators.contains(ExtraEvaluators.IS_MISSING_DESC_FOR)) {
+            builderConfig.setOption(EvaluatorOption.get(
+                    IsMissingDescForDialectEvaluatorDefinition.IS_MISSING_DESC_FOR.getOperatorString(),
+                    new IsMissingDescForDialectEvaluatorDefinition()));
+        }
 
-		if (extraEvaluators.contains(ExtraEvaluators.IS_GB_MEMBER_TYPE_OF)) {
-			builderConfig.setOption(EvaluatorOption.get(
-					IsGbMemberTypeOfEvaluatorDefinition.IS_GB_MEMBER_TYPE_OF.getOperatorString(),
-					new IsGbMemberTypeOfEvaluatorDefinition()));
-		}
+        if (extraEvaluators.contains(ExtraEvaluators.IS_GB_MEMBER_TYPE_OF)) {
+            builderConfig.setOption(EvaluatorOption.get(
+                    IsGbMemberTypeOfEvaluatorDefinition.IS_GB_MEMBER_TYPE_OF.getOperatorString(),
+                    new IsGbMemberTypeOfEvaluatorDefinition()));
+        }
 
-		if (extraEvaluators.contains(ExtraEvaluators.IS_US_MEMBER_TYPE_OF)) {
-			builderConfig.setOption(EvaluatorOption.get(
-					IsUsMemberTypeOfEvaluatorDefinition.IS_US_MEMBER_TYPE_OF.getOperatorString(),
-					new IsUsMemberTypeOfEvaluatorDefinition()));
-		}
+        if (extraEvaluators.contains(ExtraEvaluators.IS_US_MEMBER_TYPE_OF)) {
+            builderConfig.setOption(EvaluatorOption.get(
+                    IsUsMemberTypeOfEvaluatorDefinition.IS_US_MEMBER_TYPE_OF.getOperatorString(),
+                    new IsUsMemberTypeOfEvaluatorDefinition()));
+        }
 
-		if (extraEvaluators.contains(ExtraEvaluators.IS_SYNONYM_MEMBER_TYPE_OF)) {
-			builderConfig.setOption(EvaluatorOption.get(
-					IsSynonymMemberTypeOfEvaluatorDefinition.IS_SYNONYM_MEMBER_TYPE_OF.getOperatorString(),
-					new IsSynonymMemberTypeOfEvaluatorDefinition()));
-		}
+        if (extraEvaluators.contains(ExtraEvaluators.IS_SYNONYM_MEMBER_TYPE_OF)) {
+            builderConfig.setOption(EvaluatorOption.get(
+                    IsSynonymMemberTypeOfEvaluatorDefinition.IS_SYNONYM_MEMBER_TYPE_OF.getOperatorString(),
+                    new IsSynonymMemberTypeOfEvaluatorDefinition()));
+        }
 
+//        if (kBaseConfig != null) {
+//            kBaseConfig.setProperty("drools.dialect.java.compiler", "JANINO");
+//        }
+        kbase = KnowledgeBaseFactory.newKnowledgeBase(kBaseConfig);
+        KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder(kbase, builderConfig);
+        for (Resource resource : resources.keySet()) {
+            kbuilder.add(resource, resources.get(resource));
+        }
+        if (kbuilder.hasErrors()) {
+            throw new RuntimeException(kbuilder.getErrors().toString());
+        }
 
-		if (kBaseConfig != null) {
-			kBaseConfig.setProperty("drools.dialect.java.compiler", "JANINO");
-		}
-		kbase = KnowledgeBaseFactory.newKnowledgeBase(kBaseConfig);
-		KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder(kbase, builderConfig);
-		for (Resource resource : resources.keySet()) {
-			kbuilder.add(resource, resources.get(resource));
-		}
-		if (kbuilder.hasErrors()) {
-			throw new RuntimeException(kbuilder.getErrors().toString());
-		}
+        kpkgs = kbuilder.getKnowledgePackages();
+        ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(drlPkgFile));
+        out.writeObject(kpkgs);
+        out.close();
+    }
+    private static ConcurrentHashMap<String, DroolsExecutionManager> managerMap =
+            new ConcurrentHashMap<String, DroolsExecutionManager>();
 
-		kpkgs = kbuilder.getKnowledgePackages();
-		ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(drlPkgFile));
-		out.writeObject(kpkgs);
-		out.close();
-	}
-	private static ConcurrentHashMap<String, DroolsExecutionManager> managerMap =
-		new ConcurrentHashMap<String, DroolsExecutionManager>();
+    public static void setup(String kbKey, Set<File> kbFiles) throws IOException {
+        kbKey = sanatizeKey(kbKey);
+        if (!managerMap.containsKey(kbKey)) {
+            managerMap.put(kbKey, new DroolsExecutionManager(kbFiles, kbKey));
+        }
 
-	public static void setup(String kbKey, Set<File> kbFiles) throws IOException {
-		kbKey = sanatizeKey(kbKey);
-		if (!managerMap.containsKey(kbKey)) {
-			managerMap.put(kbKey, new DroolsExecutionManager(kbFiles, kbKey));
-		}
+    }
 
-	}
+    public static boolean fireAllRules(String kbKey, Set<File> kbFiles,
+            Map<String, Object> globals, Collection<Object> facts, boolean useLogger)
+            throws IOException, DroolsException {
+        kbKey = sanatizeKey(kbKey);
+        KnowledgeRuntimeLogger logger = null;
+        DroolsExecutionManager mgr = managerMap.get(kbKey);
+        try {
+            if (mgr == null) {
+                mgr = new DroolsExecutionManager(kbFiles, kbKey);
+                managerMap.put(kbKey, mgr);
+            }
+            if (mgr.failed) {
+                return false;
+            }
 
-	public static boolean fireAllRules(String kbKey, Set<File> kbFiles,
-			Map<String, Object> globals, Collection<Object> facts, boolean useLogger)
-	throws IOException, DroolsException {
-		kbKey = sanatizeKey(kbKey);
-		KnowledgeRuntimeLogger logger = null;
-		DroolsExecutionManager mgr = managerMap.get(kbKey);
-		try {
-			if (mgr == null) {
-				mgr = new DroolsExecutionManager(kbFiles, kbKey);
-				managerMap.put(kbKey, mgr);
-			}
-			if (mgr.failed) {
-				return false;
-			}
+            StatefulKnowledgeSession ksession = mgr.kbase.newStatefulKnowledgeSession();
+            try {
+                if (useLogger) {
+                    logger = KnowledgeRuntimeLoggerFactory.newConsoleLogger(ksession);
+                }
+                for (Map.Entry<String, Object> e : globals.entrySet()) {
+                    ksession.setGlobal(e.getKey(), e.getValue());
+                }
+                for (Object fact : facts) {
+                    ksession.insert(fact);
+                }
+                ksession.fireAllRules();
+            } finally {
+                ksession.dispose();
+            }
+        } catch (Throwable ex) {
+            if (mgr != null) {
+                mgr.failed = true;
+            }
+            if (ex instanceof IOException) {
+                IOException ioe = (IOException) ex;
+                throw ioe;
+            }
+            if (ex instanceof DroolsException) {
+                DroolsException de = (DroolsException) ex;
+                throw de;
+            }
+            throw new IOException(ex);
+        } finally {
+            if (logger != null) {
+                logger.close();
+            }
+        }
+        return true;
+    }
 
-			StatefulKnowledgeSession ksession = mgr.kbase.newStatefulKnowledgeSession();
-			try {
-				if (useLogger) {
-					logger = KnowledgeRuntimeLoggerFactory.newConsoleLogger(ksession);
-				}
-				for (Map.Entry<String, Object> e : globals.entrySet()) {
-					ksession.setGlobal(e.getKey(), e.getValue());
-				}
-				for (Object fact : facts) {
-					ksession.insert(fact);
-				}
-				ksession.fireAllRules();
-			} finally {
-				ksession.dispose();
-			}
-		} catch (Throwable ex) {
-			if (mgr != null) {
-				mgr.failed = true;
-			}
-			if (ex instanceof IOException) {
-				IOException ioe = (IOException) ex;
-				throw ioe;
-			}
-			if (ex instanceof DroolsException) {
-				DroolsException de = (DroolsException) ex;
-				throw de;
-			}
-			throw new IOException(ex);
-		} finally {
-			if (logger != null) {
-				logger.close();
-			}
-		}
-		return true;
-	}
-
-	public static void resetFailures() {
-		for (DroolsExecutionManager mgr : managerMap.values()) {
-			mgr.failed = false;
-		}
-	}
+    public static void resetFailures() {
+        for (DroolsExecutionManager mgr : managerMap.values()) {
+            mgr.failed = false;
+        }
+    }
 }
