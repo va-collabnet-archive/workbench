bc6d50ee954dc955fc9acb41fc07f0554e470194
vmanukyan <vmanukyan>
2011-08-05 18:32:47 +0000

    reject translation without comment bug fixed.

--- translation-module-ndb/src/main/java/org/ihtsdo/translation/CommentPopUpDialog.java
+++ translation-module-ndb/src/main/java/org/ihtsdo/translation/CommentPopUpDialog.java
@@ -5,6 +5,7 @@
 package org.ihtsdo.translation;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
@@ -12,6 +13,8 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -28,6 +31,7 @@ import javax.swing.border.EmptyBorder;
 
 import org.dwfa.ace.api.I_GetConceptData;
 import org.ihtsdo.project.refset.CommentsRefset;
+import org.ihtsdo.project.util.IconUtilities;
 
 /**
  * @author Guillermo Reynoso
@@ -40,11 +44,19 @@ public class CommentPopUpDialog extends JDialog {
 
 	public CommentPopUpDialog(String title, I_GetConceptData commentType) {
 		this.commentType = commentType;
-		response = new HashMap<I_GetConceptData, String>();
 		initComponents();
 		initCustomComponents();
 		label1.setText(title.trim());
 		pack();
+		errorLabel.setVisible(false);
+		errorLabel.setIcon(IconUtilities.deleteIcon);
+		errorLabel.setForeground(Color.RED);
+		errorLabel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				errorLabel.setVisible(false);
+			}
+		});
 		setVisible(true);
 	}
 
@@ -62,13 +74,15 @@ public class CommentPopUpDialog extends JDialog {
 	}
 
 	private void okButtonActionPerformed(ActionEvent e) {
+		if(textArea1.getText().trim().equals("")){
+			errorLabel.setVisible(true);
+			errorLabel.setText("Comment is required for rejection.");
+			return;
+		}
 		if (textArea1.getText() != null && rejectionReasonCombo.getSelectedItem() instanceof I_GetConceptData) {
+			response = new HashMap<I_GetConceptData, String>();
 			response.put((I_GetConceptData) rejectionReasonCombo.getSelectedItem(), textArea1.getText().trim());
 		}
-		dispose();
-	}
-
-	private void cancelButtonActionPerformed(ActionEvent e) {
 		dispose();
 	}
 
@@ -84,67 +98,83 @@ public class CommentPopUpDialog extends JDialog {
 		scrollPane1 = new JScrollPane();
 		textArea1 = new JTextArea();
 		buttonBar = new JPanel();
+		errorLabel = new JLabel();
 		okButton = new JButton();
-		cancelButton = new JButton();
 
-		// ======== this ========
+		//======== this ========
 		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 		setModal(true);
 		Container contentPane = getContentPane();
 		contentPane.setLayout(new BorderLayout());
 
-		// ======== dialogPane ========
+		//======== dialogPane ========
 		{
 			dialogPane.setBorder(new EmptyBorder(12, 12, 12, 12));
 			dialogPane.setLayout(new BorderLayout());
 
-			// ======== contentPanel ========
+			//======== contentPanel ========
 			{
 				contentPanel.setLayout(new GridBagLayout());
-				((GridBagLayout) contentPanel.getLayout()).columnWidths = new int[] { 316, 0 };
-				((GridBagLayout) contentPanel.getLayout()).rowHeights = new int[] { 0, 0, 150, 0 };
-				((GridBagLayout) contentPanel.getLayout()).columnWeights = new double[] { 1.0, 1.0E-4 };
-				((GridBagLayout) contentPanel.getLayout()).rowWeights = new double[] { 0.0, 0.0, 1.0, 1.0E-4 };
+				((GridBagLayout)contentPanel.getLayout()).columnWidths = new int[] {316, 0};
+				((GridBagLayout)contentPanel.getLayout()).rowHeights = new int[] {0, 0, 150, 0};
+				((GridBagLayout)contentPanel.getLayout()).columnWeights = new double[] {1.0, 1.0E-4};
+				((GridBagLayout)contentPanel.getLayout()).rowWeights = new double[] {0.0, 0.0, 1.0, 1.0E-4};
 
-				// ======== panel1 ========
+				//======== panel1 ========
 				{
 					panel1.setLayout(new GridBagLayout());
-					((GridBagLayout) panel1.getLayout()).columnWidths = new int[] { 0, 0, 0 };
-					((GridBagLayout) panel1.getLayout()).rowHeights = new int[] { 0, 0 };
-					((GridBagLayout) panel1.getLayout()).columnWeights = new double[] { 0.0, 1.0, 1.0E-4 };
-					((GridBagLayout) panel1.getLayout()).rowWeights = new double[] { 0.0, 1.0E-4 };
+					((GridBagLayout)panel1.getLayout()).columnWidths = new int[] {0, 0, 0};
+					((GridBagLayout)panel1.getLayout()).rowHeights = new int[] {0, 0};
+					((GridBagLayout)panel1.getLayout()).columnWeights = new double[] {0.0, 1.0, 1.0E-4};
+					((GridBagLayout)panel1.getLayout()).rowWeights = new double[] {0.0, 1.0E-4};
 
-					// ---- label2 ----
-					label2.setText("Subtype:");
-					panel1.add(label2, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 5), 0, 0));
-					panel1.add(rejectionReasonCombo, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+					//---- label2 ----
+					label2.setText("Sub Type:");
+					panel1.add(label2, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
+						GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+						new Insets(0, 0, 0, 5), 0, 0));
+					panel1.add(rejectionReasonCombo, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0,
+						GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+						new Insets(0, 0, 0, 0), 0, 0));
 				}
-				contentPanel.add(panel1, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 10, 0), 0, 0));
+				contentPanel.add(panel1, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
+					GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+					new Insets(0, 0, 10, 0), 0, 0));
 
-				// ---- label1 ----
+				//---- label1 ----
 				label1.setText("Enter text:");
 				label1.setFont(new Font("Arial", Font.PLAIN, 14));
-				contentPanel.add(label1, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 10, 0), 0, 0));
+				contentPanel.add(label1, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0,
+					GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+					new Insets(0, 0, 10, 0), 0, 0));
 
-				// ======== scrollPane1 ========
+				//======== scrollPane1 ========
 				{
 
-					// ---- textArea1 ----
+					//---- textArea1 ----
 					textArea1.setLineWrap(true);
 					scrollPane1.setViewportView(textArea1);
 				}
-				contentPanel.add(scrollPane1, new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+				contentPanel.add(scrollPane1, new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0,
+					GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+					new Insets(0, 0, 0, 0), 0, 0));
 			}
 			dialogPane.add(contentPanel, BorderLayout.CENTER);
 
-			// ======== buttonBar ========
+			//======== buttonBar ========
 			{
 				buttonBar.setBorder(new EmptyBorder(12, 0, 0, 0));
 				buttonBar.setLayout(new GridBagLayout());
-				((GridBagLayout) buttonBar.getLayout()).columnWidths = new int[] { 0, 85, 80 };
-				((GridBagLayout) buttonBar.getLayout()).columnWeights = new double[] { 1.0, 0.0, 0.0 };
+				((GridBagLayout)buttonBar.getLayout()).columnWidths = new int[] {0, 80};
+				((GridBagLayout)buttonBar.getLayout()).columnWeights = new double[] {1.0, 0.0};
 
-				// ---- okButton ----
+				//---- errorLabel ----
+				errorLabel.setVisible(false);
+				buttonBar.add(errorLabel, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
+					GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+					new Insets(0, 0, 0, 5), 0, 0));
+
+				//---- okButton ----
 				okButton.setText("OK");
 				okButton.addActionListener(new ActionListener() {
 					@Override
@@ -152,24 +182,15 @@ public class CommentPopUpDialog extends JDialog {
 						okButtonActionPerformed(e);
 					}
 				});
-				buttonBar.add(okButton, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 5), 0, 0));
-
-				// ---- cancelButton ----
-				cancelButton.setText("Cancel");
-				cancelButton.addActionListener(new ActionListener() {
-					@Override
-					public void actionPerformed(ActionEvent e) {
-						cancelButtonActionPerformed(e);
-					}
-				});
-				buttonBar.add(cancelButton, new GridBagConstraints(2, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+				buttonBar.add(okButton, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0,
+					GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+					new Insets(0, 0, 0, 0), 0, 0));
 			}
 			dialogPane.add(buttonBar, BorderLayout.SOUTH);
 		}
 		contentPane.add(dialogPane, BorderLayout.CENTER);
 		pack();
 		setLocationRelativeTo(getOwner());
-		// JFormDesigner - End of component initialization
 		// //GEN-END:initComponents
 	}
 
@@ -184,8 +205,7 @@ public class CommentPopUpDialog extends JDialog {
 	private JScrollPane scrollPane1;
 	private JTextArea textArea1;
 	private JPanel buttonBar;
+	private JLabel errorLabel;
 	private JButton okButton;
-	private JButton cancelButton;
-
 	// JFormDesigner - End of variables declaration //GEN-END:variables
 }
