9752b52f685ec21a65f0b7bfb0a5a4a10ba9fb6c
afurber <afurber>
2011-08-22 17:37:45 +0000

    corrected to use 'historical' instead of 'stated'

--- wb-gui/src/main/java/org/ihtsdo/arena/context/action/AddFromSpecAction.java
+++ wb-gui/src/main/java/org/ihtsdo/arena/context/action/AddFromSpecAction.java
@@ -25,74 +25,72 @@ import org.ihtsdo.tk.spec.RelSpec;
 
 public class AddFromSpecAction extends AbstractAction {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
+    ConceptVersionBI concept;
+    SpecFact<?> spec;
 
-	ConceptVersionBI concept;
-	SpecFact<?> spec;
+    public AddFromSpecAction(String actionName,
+            ConceptFact concept, SpecFact<?> spec) throws IOException {
+        super(actionName);
+        this.concept = concept.getConcept();
+        this.spec = spec;
 
-	public AddFromSpecAction(String actionName, 
-			ConceptFact concept, SpecFact<?> spec) throws IOException {
-		super(actionName);
-		this.concept = concept.getConcept();
-		this.spec = spec;
-		
-	}
+    }
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		try {
-			if (RelSpecFact.class.isAssignableFrom(spec.getClass())) {
-				addRel();
-			} else if (DescSpecFact.class.isAssignableFrom(spec.getClass())) {
-				addDesc();
-			} else {
-				throw new Exception("Can't handle type: " + spec);
-			}
-		} catch (Exception ex) {
-			AceLog.getAppLog().alertAndLogException(ex);
-		}
-	}
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        try {
+            if (RelSpecFact.class.isAssignableFrom(spec.getClass())) {
+                addRel();
+            } else if (DescSpecFact.class.isAssignableFrom(spec.getClass())) {
+                addDesc();
+            } else {
+                throw new Exception("Can't handle type: " + spec);
+            }
+        } catch (Exception ex) {
+            AceLog.getAppLog().alertAndLogException(ex);
+        }
+    }
 
-	private void addDesc() throws TerminologyException, IOException {
-		DescriptionSpec descSpec = ((DescSpecFact) spec).getDescSpec();
-		I_ConfigAceFrame config = Terms.get().getActiveAceFrameConfig();
-		Terms.get().newDescription(UUID.randomUUID(),  Terms.get().getConcept(concept.getNid()), 
-				descSpec.getLangText(), 
-				descSpec.getDescText(), 
-				Terms.get().getConcept(descSpec.getDescTypeSpec().get(concept.getViewCoordinate()).getNid()), 
-				config, SnomedMetadataRfx.getSTATUS_CURRENT_NID());
-		Terms.get().addUncommitted(Terms.get().getConcept(concept.getNid()));
-	}
-	
-	private void addRel() {
-		RelSpec relSpec = ((RelSpecFact) spec).getRelSpec();
-		try {
-			I_ConfigAceFrame config = Terms.get().getActiveAceFrameConfig();
-			
-			Iterator<PathBI> pathItr = config.getEditingPathSet().iterator();
-			I_GetConceptData originConcept = Terms.get().getConcept(concept.getNid());
-			I_RelVersioned newRel = Terms.get().newRelationshipNoCheck(UUID.randomUUID(), 
-					originConcept, 
-					relSpec.getRelTypeSpec().get(concept.getViewCoordinate()).getNid(), 
-					relSpec.getDestinationSpec().get(concept.getViewCoordinate()).getNid(),
-					SnomedMetadataRfx.getREL_CH_STATED_RELATIONSHIP_NID(), 
-					SnomedMetadataRfx.getREL_OPTIONAL_REFINABILITY_NID(), 
-					0, 
-					SnomedMetadataRfx.getSTATUS_CURRENT_NID(), 
-					config.getDbConfig().getUserConcept().getNid(),
-					pathItr.next().getConceptNid(), 
-		            Long.MAX_VALUE);
-			
-			while (pathItr.hasNext()) {
-				newRel.makeAnalog(newRel.getStatusNid(), newRel.getAuthorNid(), pathItr.next().getConceptNid(), Long.MAX_VALUE);
-			}
-			Terms.get().addUncommitted(originConcept);
+    private void addDesc() throws TerminologyException, IOException {
+        DescriptionSpec descSpec = ((DescSpecFact) spec).getDescSpec();
+        I_ConfigAceFrame config = Terms.get().getActiveAceFrameConfig();
+        Terms.get().newDescription(UUID.randomUUID(), Terms.get().getConcept(concept.getNid()),
+                descSpec.getLangText(),
+                descSpec.getDescText(),
+                Terms.get().getConcept(descSpec.getDescTypeSpec().get(concept.getViewCoordinate()).getNid()),
+                config, SnomedMetadataRfx.getSTATUS_CURRENT_NID());
+        Terms.get().addUncommitted(Terms.get().getConcept(concept.getNid()));
+    }
 
-		} catch (TerminologyException e1) {
-			AceLog.getAppLog().alertAndLogException(e1);
-		} catch (IOException e1) {
-			AceLog.getAppLog().alertAndLogException(e1);
-		}
-	}
+    private void addRel() {
+        RelSpec relSpec = ((RelSpecFact) spec).getRelSpec();
+        try {
+            I_ConfigAceFrame config = Terms.get().getActiveAceFrameConfig();
 
+            Iterator<PathBI> pathItr = config.getEditingPathSet().iterator();
+            I_GetConceptData originConcept = Terms.get().getConcept(concept.getNid());
+            I_RelVersioned newRel = Terms.get().newRelationshipNoCheck(UUID.randomUUID(),
+                    originConcept,
+                    relSpec.getRelTypeSpec().get(concept.getViewCoordinate()).getNid(),
+                    relSpec.getDestinationSpec().get(concept.getViewCoordinate()).getNid(),
+                    SnomedMetadataRfx.getREL_HISTORY_HISTORIC_NID(),
+                    SnomedMetadataRfx.getREL_OPTIONAL_REFINABILITY_NID(),
+                    0,
+                    SnomedMetadataRfx.getSTATUS_CURRENT_NID(),
+                    config.getDbConfig().getUserConcept().getNid(),
+                    pathItr.next().getConceptNid(),
+                    Long.MAX_VALUE);
+
+            while (pathItr.hasNext()) {
+                newRel.makeAnalog(newRel.getStatusNid(), newRel.getAuthorNid(), pathItr.next().getConceptNid(), Long.MAX_VALUE);
+            }
+            Terms.get().addUncommitted(originConcept);
+
+        } catch (TerminologyException e1) {
+            AceLog.getAppLog().alertAndLogException(e1);
+        } catch (IOException e1) {
+            AceLog.getAppLog().alertAndLogException(e1);
+        }
+    }
 }
