ee12979df08c772f1d5dc83e22696b47785e3bfa
vparekh <vparekh>
2011-09-07 20:31:33 +0000

    SctId Generation Process Mojo...

---
+++ src/main/java/org/ihtsdo/rf2/identifier/dao/Key.java
@@ -0,0 +1,10 @@
+package org.ihtsdo.rf2.identifier.dao;
+
+import java.util.ArrayList;
+
+public class Key {
+	
+	public ArrayList<String> keyOrdinals;
+	public int effectiveTimeOrdinal;
+
+}
---
+++ src/main/java/org/ihtsdo/rf2/identifier/dao/RF2IdentifierFile.java
@@ -0,0 +1,11 @@
+package org.ihtsdo.rf2.identifier.dao;
+
+import org.ihtsdo.rf2.identifier.dao.Key;
+
+public class RF2IdentifierFile {
+	
+	public String fileName;
+	public String sctIdFileName;
+	public Key key;
+	
+}
