b581e2f16976d337ea2c2c3b6ea0a7199a8283cc
vmanukyan <vmanukyan>
2011-09-07 22:32:51 +0000

    Select all qa cases, And number of qa cases per page enhanced to 500 and 1000.

--- rule-engine-integration/src/main/java/org/ihtsdo/qa/store/gui/QACasesBrowser.java
+++ rule-engine-integration/src/main/java/org/ihtsdo/qa/store/gui/QACasesBrowser.java
@@ -6,6 +6,7 @@
 package org.ihtsdo.qa.store.gui;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -16,6 +17,7 @@ import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -27,7 +29,9 @@ import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
+import javax.swing.AbstractButton;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -36,11 +40,15 @@ import javax.swing.JSeparator;
 import javax.swing.JTabbedPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.UIManager;
 import javax.swing.border.EmptyBorder;
 import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
 import javax.swing.table.AbstractTableModel;
+import javax.swing.table.JTableHeader;
+import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
 
 import org.dwfa.ace.api.I_GetConceptData;
 import org.dwfa.ace.api.Terms;
@@ -123,6 +131,11 @@ public class QACasesBrowser extends JPanel {
 		caseTable.setTransferHandler(th);
 		caseTable.setDragEnabled(false);
 
+		TableColumn tc = caseTable.getColumnModel().getColumn(1);
+		tc.setCellEditor(caseTable.getDefaultEditor(Boolean.class));
+		tc.setCellRenderer(caseTable.getDefaultRenderer(Boolean.class));
+		tc.setHeaderRenderer(new CheckBoxHeader(new MyItemListener()));
+
 		TableColumn conceptUuidCol = caseTable.getColumnModel().getColumn(tableModel.CONCEPT_UUID);
 		TableColumn conceptSctidCol = caseTable.getColumnModel().getColumn(tableModel.CONCEPT_SCTID);
 		TableColumn rowCheckBoxCol = caseTable.getColumnModel().getColumn(tableModel.ROW_CHECKBOX);
@@ -190,6 +203,8 @@ public class QACasesBrowser extends JPanel {
 		comboBox6.addItem("50");
 		comboBox6.addItem("75");
 		comboBox6.addItem("100");
+		comboBox6.addItem("500");
+		comboBox6.addItem("1000");
 	}
 
 	private void setupAssignedToFilterCombo() {
@@ -202,7 +217,7 @@ public class QACasesBrowser extends JPanel {
 	}
 
 	public void setupPanel(QAStoreBI store) {
-			setupBatchDispoStatusCombo();
+		setupBatchDispoStatusCombo();
 		if (rule == null || !this.rule.getRuleUuid().equals(resultsPanel.getRule().getRuleUuid())) {
 			if (this.rule != null) {
 				this.rule = resultsPanel.getRule();
@@ -214,7 +229,7 @@ public class QACasesBrowser extends JPanel {
 			panel4.setVisible(false);
 
 			clearTable1();
-
+			((CheckBoxHeader)caseTable.getColumnModel().getColumn(1).getHeaderRenderer()).setSelected(false);
 			QADatabase qaDatabase = getQaDatabase(coordinate.getDatabaseUuid());
 			label7.setText(qaDatabase.getName());
 
@@ -966,6 +981,93 @@ public class QACasesBrowser extends JPanel {
 			return false;
 		}
 
+	}
+
+	class CheckBoxHeader extends JCheckBox implements TableCellRenderer, MouseListener {
+		protected CheckBoxHeader rendererComponent;
+		protected int column;
+		protected boolean mousePressed = false;
+
+		public CheckBoxHeader(ItemListener itemListener) {
+			rendererComponent = this;
+			rendererComponent.addItemListener(itemListener);
+		}
+
+		public CheckBoxHeader getComp(){
+			return this.rendererComponent;
+		}
+		
+		public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+			if (table != null) {
+				JTableHeader header = table.getTableHeader();
+				if (header != null) {
+					rendererComponent.setForeground(header.getForeground());
+					rendererComponent.setBackground(header.getBackground());
+					rendererComponent.setFont(header.getFont());
+					header.addMouseListener(rendererComponent);
+				}
+			}
+			setColumn(column);
+			rendererComponent.setText("Check All");
+			setBorder(UIManager.getBorder("TableHeader.cellBorder"));
+			return rendererComponent;
+		}
+
+		protected void setColumn(int column) {
+			this.column = column;
+		}
+
+		public int getColumn() {
+			return column;
+		}
+
+		protected void handleClickEvent(MouseEvent e) {
+			if (mousePressed) {
+				mousePressed = false;
+				JTableHeader header = (JTableHeader) (e.getSource());
+				JTable tableView = header.getTable();
+				TableColumnModel columnModel = tableView.getColumnModel();
+				int viewColumn = columnModel.getColumnIndexAtX(e.getX());
+				int column = tableView.convertColumnIndexToModel(viewColumn);
+
+				if (viewColumn == this.column && e.getClickCount() == 1 && column != -1) {
+					doClick();
+				}
+			}
+		}
+
+		public void mouseClicked(MouseEvent e) {
+			handleClickEvent(e);
+			((JTableHeader) e.getSource()).repaint();
+		}
+
+		public void mousePressed(MouseEvent e) {
+			mousePressed = true;
+		}
+
+		public void mouseReleased(MouseEvent e) {
+		}
+
+		public void mouseEntered(MouseEvent e) {
+		}
+
+		public void mouseExited(MouseEvent e) {
+		}
+	}
+
+	class MyItemListener implements ItemListener {
+		public void itemStateChanged(ItemEvent e) {
+			Object source = e.getSource();
+			if (source instanceof AbstractButton == false)
+				return;
+			boolean checked = e.getStateChange() == ItemEvent.SELECTED;
+			batchAssigneTo.setEnabled(checked);
+			batchAssigneTo.setEnabled(checked);
+			bathcSaveButton.setEnabled(checked);
+			for (int x = 0, y = caseTable.getRowCount(); x < y; x++) {
+				caseTable.setValueAt(new Boolean(checked), x, 1);
+			}
+		}
 	}
 
 }
