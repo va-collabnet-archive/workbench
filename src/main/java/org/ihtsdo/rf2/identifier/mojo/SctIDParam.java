43b7cb11809a26f4dc5617c679dd4a9a9d3321e2
vparekh <vparekh>
2011-10-18 16:54:36 +0000

    Removed HardCoding in order to create SCTID...

---
+++ src/main/java/org/ihtsdo/rf2/identifier/mojo/SctIDParam.java
@@ -0,0 +1,14 @@
+package org.ihtsdo.rf2.identifier.mojo;
+
+import java.util.ArrayList;
+
+public class SctIDParam {
+	
+	//public ArrayList<String> sctidParameters;
+	public String namespaceId;
+	public String partitionId;
+	public String executionId;
+	public String moduleId;
+	public String releaseId;
+	public String componentType;
+}
