00a5a1b293aba06cce477c7d146be1ecbf228e01
jefron <jefron>
2011-10-22 00:47:20 +0000

    Add ability to retrieve workflow history for a single workflow id

--- wb-bdb/src/main/java/org/ihtsdo/workflow/WorkflowHistoryRefsetSearcher.java
+++ wb-bdb/src/main/java/org/ihtsdo/workflow/WorkflowHistoryRefsetSearcher.java
@@ -2,18 +2,19 @@ package org.ihtsdo.workflow;
 
 import java.io.IOException;
 import java.util.Collection;
+import java.util.TreeSet;
 import java.util.UUID;
 import java.util.logging.Level;
 
 import org.dwfa.ace.api.I_GetConceptData;
 import org.dwfa.ace.api.Terms;
-import org.dwfa.ace.api.ebr.I_ExtendByRef;
-import org.dwfa.ace.api.ebr.I_ExtendByRefPartStr;
 import org.dwfa.ace.log.AceLog;
 import org.dwfa.cement.ArchitectonicAuxiliary;
 import org.dwfa.tapi.TerminologyException;
+import org.ihtsdo.concept.Concept;
 import org.ihtsdo.tk.api.refex.RefexVersionBI;
 import org.ihtsdo.tk.api.refex.type_str.RefexStrVersionBI;
+import org.ihtsdo.workflow.refset.utilities.WfComparator;
 import org.ihtsdo.workflow.refset.utilities.WorkflowHelper;
 import org.ihtsdo.workflow.refset.utilities.WorkflowRefsetSearcher;
 
@@ -68,5 +69,27 @@ public class WorkflowHistoryRefsetSearcher extends WorkflowRefsetSearcher {
 		
 		return null;
 	}
-}
 
+	public TreeSet<WorkflowHistoryJavaBean> getAllHistoryForWorkflowId(Concept concept, UUID wfId) {
+		TreeSet<WorkflowHistoryJavaBean> retSet = new TreeSet<WorkflowHistoryJavaBean>(WfComparator.getInstance().createWfHxEarliestFirstTimeComparer());
+
+		try {
+			Collection<? extends RefexVersionBI<?>> members = concept.getCurrentAnnotationMembers(Terms.get().getActiveAceFrameConfig().getViewCoordinate(), refsetNid);
+			for (RefexVersionBI<?> m : members ) {
+				
+				RefexStrVersionBI member = (RefexStrVersionBI) m;
+				WorkflowHistoryJavaBean bean = WorkflowHelper.populateWorkflowHistoryJavaBean(member.getNid(), concept.getPrimUuid(), 
+																							  member.getStr1(), new Long(member.getTime()));
+		
+				if (bean.getWorkflowId().equals(wfId)) {
+					retSet.add(bean);
+				}
+				
+			}
+		} catch (Exception e) {
+			AceLog.getAppLog().log(Level.WARNING, "Unable to access Workflow History Refset members with error: " + e.getMessage());
+		}
+		
+		return retSet;
+	}
+}
