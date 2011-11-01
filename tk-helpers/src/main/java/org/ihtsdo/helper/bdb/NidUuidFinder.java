13536d73ac51496d44d680b5aef8e16e0408ec74
vparekh <vparekh>
2011-11-01 23:48:55 +0000

    To test integrity of NidUuid Map for the refset members.

---
+++ tk-helpers/src/main/java/org/ihtsdo/helper/bdb/NidUuidFinder.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright 2011 International Health Terminology Standards Development Organisation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+
+package org.ihtsdo.helper.bdb;
+
+//~--- non-JDK imports --------------------------------------------------------
+
+import org.dwfa.ace.api.Terms;
+import org.dwfa.ace.api.ebr.I_ExtendByRef;
+import org.dwfa.ace.api.ebr.I_ExtendByRefPartCid;
+import org.ihtsdo.tk.Ts;
+import org.ihtsdo.tk.api.ComponentChroncileBI;
+import org.ihtsdo.tk.api.ConceptFetcherBI;
+import org.ihtsdo.tk.api.NidBitSetBI;
+import org.ihtsdo.tk.api.ProcessUnfetchedConceptDataBI;
+import org.ihtsdo.tk.api.concept.ConceptChronicleBI;
+import org.ihtsdo.tk.api.description.DescriptionChronicleBI;
+import org.ihtsdo.tk.api.media.MediaChronicleBI;
+import org.ihtsdo.tk.api.refex.RefexChronicleBI;
+import org.ihtsdo.tk.api.relationship.RelationshipChronicleBI;
+
+//~--- JDK imports ------------------------------------------------------------
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ *
+ * @author kec
+ */
+public class NidUuidFinder implements ProcessUnfetchedConceptDataBI {
+   private AtomicInteger       count        = new AtomicInteger(0);
+   private AtomicInteger       dots         = new AtomicInteger(0);
+   ConcurrentSkipListSet<Integer> allPrimNids = new ConcurrentSkipListSet<Integer>();
+   ConcurrentSkipListSet<Integer> incorrectNids     = new ConcurrentSkipListSet<Integer>();
+   File                           missingNidFile     = new File("incorrectNid.oos");
+   
+   private final NidBitSetBI   nidset;
+
+   //~--- constant enums ------------------------------------------------------
+
+   private enum PASS { PASS_ONE, PASS_TWO }
+
+   //~--- constructors --------------------------------------------------------
+   public NidUuidFinder() throws IOException, ClassNotFoundException {
+      nidset = Ts.get().getAllConceptNids();
+   }
+   
+   @Override
+   public boolean continueWork() {
+      return true;
+   }
+
+   private void processConcept(ConceptChronicleBI concept) throws IOException {
+      try{
+	     List<? extends I_ExtendByRef> extensions = Terms.get().getAllExtensionsForComponent(concept.getNid(), true);
+	     for (I_ExtendByRef extension : extensions) {
+	    	I_ExtendByRefPartCid part = (I_ExtendByRefPartCid) extension.getMutableParts().get(0);
+	     	try{
+	  	    	UUID c1Uuid  = Terms.get().nidToUuid(part.getC1id());
+		  	}catch(Exception e){
+		  	    incorrectNids.add(part.getC1id());
+		  	}
+	     }
+	     }catch(Exception e){
+	    	//don't do anything , just ignore
+	     }
+   }
+
+   @Override
+   public void processUnfetchedConceptData(int cNid, ConceptFetcherBI fetcher) throws Exception {
+      count.incrementAndGet();
+
+      if (count.get() % 1000 == 0) {
+         System.out.print(".");
+         System.out.flush();
+         dots.incrementAndGet();
+
+         if (dots.get() > 80) {
+            dots.set(0);
+            System.out.println();
+            System.out.print(count.get() + ": ");
+         }
+      }
+
+      processConcept(fetcher.fetch());
+   }
+
+   public void writeIncorrectNidFile() throws IOException {
+      count.set(0);
+      FileOutputStream     fosNid = new FileOutputStream(missingNidFile);
+      BufferedOutputStream bosNid = new BufferedOutputStream(fosNid);
+      ObjectOutputStream   oosNid = new ObjectOutputStream(bosNid);
+
+      try {
+         oosNid.writeObject(incorrectNids);
+      } finally {
+    	  oosNid.close();
+      }
+   }
+
+   //~--- get methods ---------------------------------------------------------
+
+   @Override
+   public NidBitSetBI getNidSet() throws IOException {
+      return nidset;
+   }
+   
+   
+   public ConcurrentSkipListSet<Integer> getincorrectNids() {
+	  return incorrectNids;
+   } 
+   
+}
