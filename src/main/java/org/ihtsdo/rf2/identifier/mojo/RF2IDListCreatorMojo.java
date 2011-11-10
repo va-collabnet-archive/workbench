72e3534de7222f33e51aa7a9bb2d65e5139fb47c
arodriguez <arodriguez>
2011-11-10 03:25:14 +0000

    Changed called factory in RF2IdListCreatorMojo clas

--- src/main/java/org/ihtsdo/rf2/identifier/mojo/RF2IDListCreatorMojo.java
+++ src/main/java/org/ihtsdo/rf2/identifier/mojo/RF2IDListCreatorMojo.java
@@ -1,33 +1,13 @@
 package org.ihtsdo.rf2.identifier.mojo;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.LineNumberReader;
-import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
-import java.util.UUID;
 
-import org.apache.log4j.Logger;
-import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
-
-import org.ihtsdo.rf2.core.factory.RF2ConceptFactory;
-import org.ihtsdo.rf2.identifier.factory.RF2IdGeneratorFactory;
+import org.ihtsdo.rf2.identifier.factory.RF2IdListGeneratorFactory;
 import org.ihtsdo.rf2.mojo.ReleaseConfigMojo;
-
 import org.ihtsdo.rf2.util.Config;
-import org.ihtsdo.rf2.util.ExportUtil;
 import org.ihtsdo.rf2.util.JAXBUtil;
-import org.ihtsdo.rf2.util.WriteUtil;
 
 
 
@@ -144,7 +124,7 @@ public class RF2IDListCreatorMojo extends ReleaseConfigMojo {
 		// Initialize meta hierarchy
 		//ExportUtil.init();
 
-		RF2IdGeneratorFactory factory = new RF2IdGeneratorFactory(config);
+		RF2IdListGeneratorFactory factory = new RF2IdListGeneratorFactory(config);
 		factory.export();		
 		
 		getLog().info("Running the RF2 File ID Creation with the following ");
