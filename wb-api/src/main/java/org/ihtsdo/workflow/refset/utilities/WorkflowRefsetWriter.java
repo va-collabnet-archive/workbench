a73cc9f2e7c80345293bdad1f6208c9c74b8a144
afurber <afurber>
2011-12-09 21:13:41 +0000

    Converted addMember() to use the new api and added additional check of testing if the refex was an annotation or not, then adding as uncommitted either the concept or the refset based on the result of the test. Previously just the refset concept was being added as uncommitted.

--- wb-api/src/main/java/org/ihtsdo/workflow/refset/utilities/WorkflowRefsetWriter.java
+++ wb-api/src/main/java/org/ihtsdo/workflow/refset/utilities/WorkflowRefsetWriter.java
@@ -3,6 +3,7 @@ package org.ihtsdo.workflow.refset.utilities;
 import java.io.IOException;
 import java.util.UUID;
 import java.util.logging.Level;
+import org.dwfa.ace.api.I_ConfigAceFrame;
 
 import org.dwfa.ace.api.I_HelpRefsets;
 import org.dwfa.ace.api.RefsetPropertyMap;
@@ -13,70 +14,87 @@ import org.dwfa.ace.log.AceLog;
 import org.dwfa.tapi.I_ConceptualizeUniversally;
 import org.dwfa.tapi.TerminologyException;
 import org.ihtsdo.etypes.EConcept.REFSET_TYPES;
+import org.ihtsdo.tk.Ts;
+import org.ihtsdo.tk.api.ComponentChroncileBI;
+import org.ihtsdo.tk.api.TerminologyConstructorBI;
+import org.ihtsdo.tk.api.blueprint.RefexCAB;
+import org.ihtsdo.tk.api.concept.ConceptChronicleBI;
+import org.ihtsdo.tk.api.coordinate.EditCoordinate;
+import org.ihtsdo.tk.api.coordinate.ViewCoordinate;
+import org.ihtsdo.tk.api.refex.type_cnid_str.RefexCnidStrAnalogBI;
+import org.ihtsdo.tk.api.refex.type_cnid_str.RefexCnidStrVersionBI;
+import org.ihtsdo.tk.dto.concept.component.refset.TK_REFSET_TYPE;
 import org.ihtsdo.workflow.refset.WorkflowRefset;
 
-/* 
+/*
  * @author Jesse Efron
- * 
+ *
  */
 public abstract class WorkflowRefsetWriter extends WorkflowRefset {
-	protected I_HelpRefsets helper = null;
-	
-	public abstract String fieldsToRefsetString() throws IOException;
-	
-	protected WorkflowRefsetWriter(I_ConceptualizeUniversally refsetConcept)
-			throws TerminologyException, IOException {
- 		super(refsetConcept);
- 		
- 		if (Terms.get() != null) {
- 			helper = Terms.get().getRefsetHelper(Terms.get().getActiveAceFrameConfig());
- 		}
-	}
-	
-	public I_ExtendByRef addMember() {
-		I_ExtendByRef ref = null;
-		
-		try {
-			RefsetPropertyMap propMap = new RefsetPropertyMap();
-			
-			if (fields.valuesExist()) {
-				propMap.put(REFSET_PROPERTY.STRING_VALUE, fieldsToRefsetString());
 
-				ref = helper.makeWfMetadataMemberAndSetup(refsetNid, fields.getReferencedComponentNid(), REFSET_TYPES.STR, propMap, UUID.randomUUID());
-			
-				if (ref != null) {
-					Terms.get().addUncommittedNoChecks(ref);
-				}
-			}
-		} catch (Exception io) {
-        	AceLog.getAppLog().log(Level.WARNING, "Failed to Add Member with error: " + io.getMessage());
-		}
+    protected I_HelpRefsets helper = null;
 
-		fields.cleanValues();
-		return ref;
-	}
+    public abstract String fieldsToRefsetString() throws IOException;
 
-	public I_ExtendByRef retireMember()  {
-		I_ExtendByRef ref = null;
+    protected WorkflowRefsetWriter(I_ConceptualizeUniversally refsetConcept)
+            throws TerminologyException, IOException {
+        super(refsetConcept);
 
-		try {
-			RefsetPropertyMap propMap = new RefsetPropertyMap();
+        if (Terms.get() != null) {
+            helper = Terms.get().getRefsetHelper(Terms.get().getActiveAceFrameConfig());
+        }
+    }
 
-			if (fields.valuesExist()) {
-				propMap.put(REFSET_PROPERTY.STRING_VALUE, fieldsToRefsetString());
-				ref = helper.getRefsetExtension(refsetNid, fields.getReferencedComponentNid(), propMap);
+    public I_ExtendByRef addMember() {
+        I_ExtendByRef ref = null;
 
-				if (ref != null) {
-					helper.retireRefsetStrExtension(refsetNid, fields.getReferencedComponentNid(), propMap);
-					
-					Terms.get().addUncommittedNoChecks(ref);
-				}
-			}
-		} catch (Exception io) {
-			AceLog.getAppLog().log(Level.WARNING, "Failed to retire member with error: " + io.getMessage());
-		}
-		
-		fields.cleanValues();
-		return ref;
-	}
+        try {
+            ConceptChronicleBI concept = Ts.get().getConcept(fields.getReferencedComponentUid());
+            ConceptChronicleBI refset = Ts.get().getConcept(refsetNid);
+            I_ConfigAceFrame config = Terms.get().getActiveAceFrameConfig();
+            ViewCoordinate vc = config.getViewCoordinate();
+            EditCoordinate ec = config.getEditCoordinate();
+            TerminologyConstructorBI builder = Ts.get().getTerminologyConstructor(ec, vc);
+            RefexCAB refexBp = new RefexCAB(TK_REFSET_TYPE.CID_STR,
+                    fields.getReferencedComponentNid(),
+                    refsetNid);
+            refexBp.put(RefexCAB.RefexProperty.CNID1, Ts.get().getNidForUuids(fields.getReferencedComponentUid()));
+            RefexCnidStrAnalogBI rv = (RefexCnidStrAnalogBI) builder.construct(refexBp);
+            rv.setStr1(fieldsToRefsetString());
+            if (refset.isAnnotationStyleRefex()) {
+                Ts.get().addUncommitted(concept);
+            } else {
+                Ts.get().addUncommitted(refset);
+            }
+        } catch (Exception io) {
+            AceLog.getAppLog().log(Level.WARNING, "Failed to Add Member with error: " + io.getMessage());
+        }
+
+        fields.cleanValues();
+        return ref;
+    }
+
+    public I_ExtendByRef retireMember() {
+        I_ExtendByRef ref = null;
+
+        try {
+            RefsetPropertyMap propMap = new RefsetPropertyMap();
+
+            if (fields.valuesExist()) {
+                propMap.put(REFSET_PROPERTY.STRING_VALUE, fieldsToRefsetString());
+                ref = helper.getRefsetExtension(refsetNid, fields.getReferencedComponentNid(), propMap);
+
+                if (ref != null) {
+                    helper.retireRefsetStrExtension(refsetNid, fields.getReferencedComponentNid(), propMap);
+
+                    Terms.get().addUncommittedNoChecks(ref);
+                }
+            }
+        } catch (Exception io) {
+            AceLog.getAppLog().log(Level.WARNING, "Failed to retire member with error: " + io.getMessage());
+        }
+
+        fields.cleanValues();
+        return ref;
+    }
 }
