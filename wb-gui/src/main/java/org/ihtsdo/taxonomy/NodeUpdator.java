56183a6f20fd1e46f4e5a69587a885caa64c0822
kec <kec>
2011-12-19 03:38:31 +0000

    Modifications to LastChange to make taxonomy update more efficient after refset compute.

--- wb-gui/src/main/java/org/ihtsdo/taxonomy/NodeUpdator.java
+++ wb-gui/src/main/java/org/ihtsdo/taxonomy/NodeUpdator.java
@@ -14,9 +14,11 @@ import org.ihtsdo.taxonomy.model.NodePath;
 import org.ihtsdo.taxonomy.model.TaxonomyModel;
 import org.ihtsdo.taxonomy.nodes.TaxonomyNode;
 import org.ihtsdo.tk.Ts;
+import org.ihtsdo.tk.api.*;
 import org.ihtsdo.tk.api.ContradictionException;
 import org.ihtsdo.tk.api.TerminologyStoreDI;
 import org.ihtsdo.tk.api.concept.ConceptVersionBI;
+import org.ihtsdo.tk.api.coordinate.ViewCoordinate;
 import org.ihtsdo.tk.api.relationship.RelationshipVersionBI;
 
 //~--- JDK imports ------------------------------------------------------------
@@ -32,8 +34,7 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.swing.SwingWorker;
-import org.ihtsdo.tk.api.*;
-import org.ihtsdo.tk.api.coordinate.ViewCoordinate;
+import org.dwfa.ace.api.I_IterateIds;
 
 /**
  *
@@ -44,15 +45,15 @@ public class NodeUpdator extends SwingWorker<Object, PublishRecord> implements P
       new ConcurrentHashMap<Integer, ConcurrentSkipListSet<UpdateNodeBI>>();
    private final HashSet<Integer>     changedConcepts                   = new HashSet<Integer>();
    private final HashSet<Integer>     referencedConceptsOfChangedRefexs = new HashSet<Integer>();
+   private AtomicInteger              updateNodeId                      = new AtomicInteger();
    private final IdentifierSet        conceptsToRetrieve;
+   private CountDownLatch             latch;
    private final TaxonomyModel        model;
    private final HashSet<Integer>     noTaxonomyChange;
    private final TaxonomyNodeRenderer renderer;
-   private long                               sequence;
-   private final ViewCoordinate vc;
+   private long                       sequence;
    private final TerminologyStoreDI   ts;
-   private CountDownLatch latch;
-   private AtomicInteger updateNodeId = new AtomicInteger();
+   private final ViewCoordinate       vc;
 
    //~--- constructors --------------------------------------------------------
 
@@ -66,20 +67,18 @@ public class NodeUpdator extends SwingWorker<Object, PublishRecord> implements P
       this.sequence           = sequence;
       this.conceptsToRetrieve = (IdentifierSet) ts.getEmptyNidSet();
       this.renderer           = renderer;
-      this.vc = renderer.getViewCoordinate();
+      this.vc                 = renderer.getViewCoordinate();
 
       for (Integer nid : changedComponents) {
          int cNid = ts.getConceptNidForNid(nid);
 
          this.changedConcepts.add(cNid);
-         this.conceptsToRetrieve.setMember(cNid);
       }
 
       for (Integer nid : referencedComponentsOfChangedRefexs) {
          int cNid = ts.getConceptNidForNid(nid);
 
          this.referencedConceptsOfChangedRefexs.add(cNid);
-         this.conceptsToRetrieve.setMember(cNid);
       }
 
       noTaxonomyChange = new HashSet(changedConcepts);
@@ -103,28 +102,15 @@ public class NodeUpdator extends SwingWorker<Object, PublishRecord> implements P
       destinationsOfChangedRels.removeAll(originsAndDestinationsChanged);
 
       for (int cNid : noTaxonomyChange) {
-         if (!nodesToChange.containsKey(cNid)) {
-            nodesToChange.put(cNid, new ConcurrentSkipListSet<UpdateNodeBI>());
-         }
-
          for (Long nodeId : model.getNodeStore().getNodeIdsForConcept(cNid)) {
             TaxonomyNode currentNode = model.getNodeStore().nodeMap.get(nodeId);
 
             if (currentNode != null) {
-               nodesToChange.get(cNid).add(new NoTaxonomyChangeNodeUpdate(currentNode));
-            }
-         }
-      }
+               if (!nodesToChange.containsKey(cNid)) {
+                  nodesToChange.put(cNid, new ConcurrentSkipListSet<UpdateNodeBI>());
+               }
 
-      for (int cNid : noTaxonomyChange) {
-         if (!nodesToChange.containsKey(cNid)) {
-            nodesToChange.put(cNid, new ConcurrentSkipListSet<UpdateNodeBI>());
-         }
-
-         for (Long nodeId : model.getNodeStore().getNodeIdsForConcept(cNid)) {
-            TaxonomyNode currentNode = model.getNodeStore().nodeMap.get(nodeId);
-
-            if (currentNode != null) {
+               this.conceptsToRetrieve.setMember(cNid);
                nodesToChange.get(cNid).add(new NoTaxonomyChangeNodeUpdate(currentNode));
             }
          }
@@ -136,6 +122,8 @@ public class NodeUpdator extends SwingWorker<Object, PublishRecord> implements P
 
             if (currentNode != null) {
                ParentChangeNodeUpdate pcnu = new ParentChangeNodeUpdate();
+
+               this.conceptsToRetrieve.setMember(cNid);
 
                if (!nodesToChange.containsKey(cNid)) {
                   nodesToChange.put(cNid, new ConcurrentSkipListSet<UpdateNodeBI>());
@@ -151,6 +139,8 @@ public class NodeUpdator extends SwingWorker<Object, PublishRecord> implements P
             TaxonomyNode currentNode = model.getNodeStore().nodeMap.get(nodeId);
 
             if (currentNode != null) {
+               this.conceptsToRetrieve.setMember(cNid);
+
                int[]                          possibleChildren     = ts.getPossibleChildren(cNid,
                                                                         renderer.getViewCoordinate());
                ConcurrentSkipListSet<Integer> possibleChildrenCSLS = new ConcurrentSkipListSet<Integer>();
@@ -177,6 +167,8 @@ public class NodeUpdator extends SwingWorker<Object, PublishRecord> implements P
             TaxonomyNode currentNode = model.getNodeStore().nodeMap.get(nodeId);
 
             if (currentNode != null) {
+               this.conceptsToRetrieve.setMember(cNid);
+
                int[]                          possibleChildren     = ts.getPossibleChildren(cNid,
                                                                         renderer.getViewCoordinate());
                ConcurrentSkipListSet<Integer> possibleChildrenCSLS = new ConcurrentSkipListSet<Integer>();
@@ -197,16 +189,30 @@ public class NodeUpdator extends SwingWorker<Object, PublishRecord> implements P
             }
          }
       }
-      
-       latch = new CountDownLatch(conceptsToRetrieve.size());
+
+      latch = new CountDownLatch(conceptsToRetrieve.size());
    }
 
    //~--- methods -------------------------------------------------------------
 
    @Override
+   public boolean continueWork() {
+      return true;
+   }
+
+   @Override
    protected Object doInBackground() throws Exception {
-      Ts.get().iterateConceptDataInParallel(this);
-      latch.await();
+       if (conceptsToRetrieve.size() > 100) {
+        Ts.get().iterateConceptDataInParallel(this);
+       } else {
+          I_IterateIds cnidItr = conceptsToRetrieve.iterator();
+          TerminologySnapshotDI tSnap = ts.getSnapshot(vc);
+          while (cnidItr.next()) {
+              processConcept(tSnap.getConceptForNid(cnidItr.nid()));
+          }
+       }
+       latch.await();
+
       return true;
    }
 
@@ -252,28 +258,30 @@ public class NodeUpdator extends SwingWorker<Object, PublishRecord> implements P
       }
    }
 
-    @Override
-    public void processUnfetchedConceptData(int cNid, ConceptFetcherBI fetcher) throws Exception {
-        if (conceptsToRetrieve.isMember(cNid)) {
-            ConceptVersionBI concept = fetcher.fetch(vc);
-            if (nodesToChange.containsKey(cNid)) {
-                for (UpdateNodeBI un: nodesToChange.get(cNid)) {
-                    un.update(concept);
-                }
-            }
-            latch.countDown();
+   @Override
+   public void processUnfetchedConceptData(int cNid, ConceptFetcherBI fetcher) throws Exception {
+      if (conceptsToRetrieve.isMember(cNid)) {
+            processConcept(fetcher.fetch(vc));
+      }
+   }
+
+    private void processConcept(ConceptVersionBI concept) throws Exception {
+        int cNid = concept.getNid();
+        if (nodesToChange.containsKey(cNid)) {
+           for (UpdateNodeBI un : nodesToChange.get(cNid)) {
+              un.update(concept);
+           }
         }
+
+        latch.countDown();
     }
 
-    @Override
-    public NidBitSetBI getNidSet() throws IOException {
-        return conceptsToRetrieve;
-    }
+   //~--- get methods ---------------------------------------------------------
 
-    @Override
-    public boolean continueWork() {
-        return true;
-    }
+   @Override
+   public NidBitSetBI getNidSet() throws IOException {
+      return conceptsToRetrieve;
+   }
 
    //~--- inner interfaces ----------------------------------------------------
 
@@ -281,13 +289,6 @@ public class NodeUpdator extends SwingWorker<Object, PublishRecord> implements P
       public void update(ConceptVersionBI cv);
    }
 
-   private abstract class UpdateNode implements Comparable<UpdateNode> {
-       private int updateNodeId = NodeUpdator.this.updateNodeId.getAndIncrement();
-        @Override
-        public int compareTo(UpdateNode o) {
-            return this.updateNodeId - o.updateNodeId;
-        }
-    }
 
    //~--- inner classes -------------------------------------------------------
 
@@ -378,6 +379,18 @@ public class NodeUpdator extends SwingWorker<Object, PublishRecord> implements P
       @Override
       public void update(ConceptVersionBI cv) {
          throw new UnsupportedOperationException("Not supported yet.");
+      }
+   }
+
+
+   private abstract class UpdateNode implements Comparable<UpdateNode> {
+      private int updateNodeId = NodeUpdator.this.updateNodeId.getAndIncrement();
+
+      //~--- methods ----------------------------------------------------------
+
+      @Override
+      public int compareTo(UpdateNode o) {
+         return this.updateNodeId - o.updateNodeId;
       }
    }
 }
