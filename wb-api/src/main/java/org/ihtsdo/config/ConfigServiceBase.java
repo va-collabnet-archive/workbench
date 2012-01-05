d5aaa31a0bb4178d544b3476c2699ba5f14f150c
aflinton <aflinton>
2012-01-05 13:30:53 +0000

    adding Object getConfigObject(String lookupVal)

--- wb-api/src/main/java/org/ihtsdo/config/ConfigServiceBase.java
+++ wb-api/src/main/java/org/ihtsdo/config/ConfigServiceBase.java
@@ -1,5 +1,7 @@
 package org.ihtsdo.config;
 
+
+
 public interface ConfigServiceBase {
     
     /**
@@ -7,5 +9,13 @@ public interface ConfigServiceBase {
      * @return A string message with any errors. If null or empty then OK
      */
     String init();
+    
+    /**
+     * Given a string key value this should return an object.
+     * @param lookupVal The key value
+     * @return AN object from the config service
+     */
+    Object getConfigObject(String lookupVal);    
+    
 
 }
