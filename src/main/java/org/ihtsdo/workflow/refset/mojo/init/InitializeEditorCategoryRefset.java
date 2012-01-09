169fe5b8d16adb8665b21ffa7541fc2746de676f
jefron <jefron>
2012-01-09 17:18:53 +0000

    Remove Hard-Coding of workflow values as they will be in the database at when required

--- src/main/java/org/ihtsdo/workflow/refset/mojo/init/InitializeEditorCategoryRefset.java
+++ src/main/java/org/ihtsdo/workflow/refset/mojo/init/InitializeEditorCategoryRefset.java
@@ -3,17 +3,15 @@ package org.ihtsdo.workflow.refset.mojo.init;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 import java.util.logging.Level;
 
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
 import org.dwfa.ace.api.I_GetConceptData;
 import org.dwfa.ace.api.Terms;
 import org.dwfa.ace.log.AceLog;
 import org.dwfa.cement.ArchitectonicAuxiliary;
-import org.dwfa.tapi.TerminologyException;
 import org.ihtsdo.workflow.refset.edcat.EditorCategoryRefsetWriter;
 import org.ihtsdo.workflow.refset.utilities.WorkflowHelper;
 
@@ -47,8 +45,24 @@ public class InitializeEditorCategoryRefset implements I_InitializeWorkflowRefse
    @Override
    public boolean initializeRefset(String resourceFilePath)  {
 		String line = null;
+		Map<String, UUID> users = new HashMap<String, UUID>();
+		Map<String, UUID> categories = new HashMap<String, UUID>();
         
-        try {
+		try {
+			I_GetConceptData parentEditorConcept = Terms.get().getConcept(ArchitectonicAuxiliary.Concept.USER.getPrimoridalUid());
+			for (I_GetConceptData con : WorkflowHelper.getChildren(parentEditorConcept)) {
+				users.put(WorkflowHelper.getPrefTerm(con).toLowerCase(), con.getPrimUuid());
+			}
+
+			I_GetConceptData parentCategoryConcept = Terms.get().getConcept(ArchitectonicAuxiliary.Concept.WORKFLOW_ROLES.getPrimoridalUid());
+			for (I_GetConceptData con : WorkflowHelper.getChildren(parentCategoryConcept)) {
+				categories.put(WorkflowHelper.getPrefTerm(con).toLowerCase(), con.getPrimUuid());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		try {
     		File f = new File(resourceFilePath + File.separatorChar + fileName);
 	    	BufferedReader inputFile = new BufferedReader(new FileReader(f));    	
 
@@ -63,12 +77,9 @@ public class InitializeEditorCategoryRefset implements I_InitializeWorkflowRefse
 
         		if (columns.length == numberOfColumns)
         		{
-        			int ss = columns.length;
-        			String s = columns[editorPosition];
-        			UUID mod = lookupModeler(columns[editorPosition]);
-        			writer.setEditor(mod);
+        			writer.setEditor(users.get(columns[editorPosition].toLowerCase()));
 	            	writer.setSemanticArea(columns[semanticAreaPosition]);
-	            	writer.setCategory(lookupRole(columns[categoryPosition]));
+	            	writer.setCategory(categories.get(columns[categoryPosition].toLowerCase()));
 
 	            	writer.addMember();
         		} else {
@@ -82,86 +93,4 @@ public class InitializeEditorCategoryRefset implements I_InitializeWorkflowRefse
         	return false;
     	}
 	}
-
-   private I_GetConceptData getRole(String role) throws TerminologyException, IOException {
-   		return Terms.get().getConcept(lookupRole(role));
-   }
-
-   private UUID lookupRole(String role) throws IOException, TerminologyException {
-	   	if (role.equalsIgnoreCase("Clinical editor role A")) {
-	   		return ArchitectonicAuxiliary.Concept.WORKFLOW_ROLE_A.getPrimoridalUid();
-	   	} else if (role.equalsIgnoreCase("Clinical editor role B")) {
-	   		return ArchitectonicAuxiliary.Concept.WORKFLOW_ROLE_B.getPrimoridalUid();
-	   	} else if (role.equalsIgnoreCase("Clinical editor role C")) {
-	   		return ArchitectonicAuxiliary.Concept.WORKFLOW_ROLE_C.getPrimoridalUid();
-	   	} else if (role.equalsIgnoreCase("Clinical editor role D")) {
-	   		return ArchitectonicAuxiliary.Concept.WORKFLOW_ROLE_D.getPrimoridalUid();
-	   	} else if (role.equalsIgnoreCase("Clinical editor role All")) {
-	   		return ArchitectonicAuxiliary.Concept.WORKFLOW_ROLE_ALL.getPrimoridalUid();
-	   	}
-	   	
-	   	return null;
-   	}
-   	
-    private UUID lookupModeler(String modeler) throws IOException, TerminologyException {
-    	if (modeler.equalsIgnoreCase("IHTSDO")) {
-    		return ArchitectonicAuxiliary.Concept.IHTSDO.getPrimoridalUid();
-    	} else if (modeler.equalsIgnoreCase("spackman")) {
-			return ArchitectonicAuxiliary.Concept.KENT_SPACKMAN.getPrimoridalUid();
-		} else if (modeler.equalsIgnoreCase("mvanber")) {
-			return ArchitectonicAuxiliary.Concept.MONIQUE_VAN_BERKUM.getPrimoridalUid();
-		} else if (modeler.equalsIgnoreCase("khaake")) {
-			return ArchitectonicAuxiliary.Concept.KIRSTEN_HAAKE.getPrimoridalUid();
-		} else if (modeler.equalsIgnoreCase("jmirza")) {
-			return ArchitectonicAuxiliary.Concept.JALEH_MIZRA.getPrimoridalUid();
-		} else if (modeler.equalsIgnoreCase("llivesa")) {
-			return ArchitectonicAuxiliary.Concept.PENNY_LIVESAY.getPrimoridalUid();
-		} else if (modeler.equalsIgnoreCase("mgerard")) {
-			return ArchitectonicAuxiliary.Concept.MARY_GERARD.getPrimoridalUid();
-		} else if (modeler.equalsIgnoreCase("msmith")) {
-			return ArchitectonicAuxiliary.Concept.MIKE_SMITH.getPrimoridalUid();
-		} else if (modeler.equalsIgnoreCase("rturnbu")) {
-			return ArchitectonicAuxiliary.Concept.ROBERT_TURNBULL.getPrimoridalUid();
-		} else if (modeler.equalsIgnoreCase("phought")) {
-			return ArchitectonicAuxiliary.Concept.PATRICIA_HOUGHTON.getPrimoridalUid();
-		} else if (modeler.equalsIgnoreCase("pbrottm")) {
-			return ArchitectonicAuxiliary.Concept.PHILLIP_BROTTMAN.getPrimoridalUid();
-		} else if (modeler.equalsIgnoreCase("greynos")) {
-			return ArchitectonicAuxiliary.Concept.GUILLERMO_REYNOSO.getPrimoridalUid();
-		} else if (modeler.equalsIgnoreCase("alopez")) {
-			return ArchitectonicAuxiliary.Concept.ALEJANDRO_LOPEZ.getPrimoridalUid();
-		} else if (modeler.equalsIgnoreCase("emme")) {
-			return ArchitectonicAuxiliary.Concept.EMMA_MELHUISH.getPrimoridalUid();
-		} else if (modeler.equalsIgnoreCase("dkonice")) {
-			return ArchitectonicAuxiliary.Concept.DEBORAH_KONICEK.getPrimoridalUid();
-		} else if (modeler.equalsIgnoreCase("skeni")) {
-			return ArchitectonicAuxiliary.Concept.SARITA_KENI.getPrimoridalUid();
-		} else if (modeler.equalsIgnoreCase("jogo")) {
-			return ArchitectonicAuxiliary.Concept.JO_GOULDING.getPrimoridalUid();
-		} else if (modeler.equalsIgnoreCase("sspence")) {
-			return ArchitectonicAuxiliary.Concept.SAMANTHA_SPENCER.getPrimoridalUid();
-		} else if (modeler.equalsIgnoreCase("clundbe")) {
-			return ArchitectonicAuxiliary.Concept.CYNDIE_LUNDBERG.getPrimoridalUid();
-		} else if (modeler.equalsIgnoreCase("rmoldwi")) {
-			return ArchitectonicAuxiliary.Concept.RICHARD_MOLDWIN.getPrimoridalUid();
-		} else if (modeler.equalsIgnoreCase("nalbarr")) {
-			return ArchitectonicAuxiliary.Concept.NARCISO_ALBARRACIN.getPrimoridalUid();
-		} else if (modeler.equalsIgnoreCase("vparekh")) {
-			return ArchitectonicAuxiliary.Concept.VARSHA_PAREKH.getPrimoridalUid();
-		} else if (modeler.equalsIgnoreCase("cspisla")) {
-			return ArchitectonicAuxiliary.Concept.CHRISTINE_SPISLA.getPrimoridalUid();
-		} else if (modeler.equalsIgnoreCase("dmcginn")) {
-			return ArchitectonicAuxiliary.Concept.DORIS_MCGINNESS.getPrimoridalUid();
-		} else if (modeler.equalsIgnoreCase("asyed")) {
-			return ArchitectonicAuxiliary.Concept.ASIF_SYED.getPrimoridalUid();
-		} else if (modeler.equalsIgnoreCase("cvalles")) {
-			return ArchitectonicAuxiliary.Concept.CECILIA_VALLESE.getPrimoridalUid();
-		} else if (modeler.equalsIgnoreCase("alejandro")) {
-			return ArchitectonicAuxiliary.Concept.ALEJANDRO_RODRIGUEZ.getPrimoridalUid();
-		} else if (modeler.equalsIgnoreCase("NHS")) {
-			return ArchitectonicAuxiliary.Concept.NHS.getPrimoridalUid();
-		}
-		
-		return null;
-    }
 }
