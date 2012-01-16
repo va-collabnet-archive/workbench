59e4991cbadaf0eb0da320b86da3b17d5c8a948d
vmanukyan <vmanukyan>
2012-01-16 20:00:37 +0000

    previous promotion status, and previous promotion status time fixed.

--- project-management-ndb/src/main/java/org/ihtsdo/project/refset/PromotionAndAssignmentRefset.java
+++ project-management-ndb/src/main/java/org/ihtsdo/project/refset/PromotionAndAssignmentRefset.java
@@ -19,15 +19,10 @@ import org.ihtsdo.etypes.EConcept;
 import org.ihtsdo.tk.Ts;
 import org.ihtsdo.tk.api.PathBI;
 import org.ihtsdo.tk.api.TerminologyBuilderBI;
-import org.ihtsdo.tk.api.blueprint.RefexCAB;
-import org.ihtsdo.tk.api.blueprint.RefexCAB.RefexProperty;
 import org.ihtsdo.tk.api.refex.RefexChronicleBI;
 import org.ihtsdo.tk.api.refex.RefexVersionBI;
-import org.ihtsdo.tk.api.refex.type_cnid_cnid.RefexCnidCnidAnalogBI;
 import org.ihtsdo.tk.api.refex.type_cnid_cnid.RefexCnidCnidVersionBI;
 import org.ihtsdo.tk.binding.snomed.SnomedMetadataRf2;
-import org.ihtsdo.tk.binding.snomed.SnomedMetadataRfx;
-import org.ihtsdo.tk.dto.concept.component.refset.TK_REFSET_TYPE;
 
 public class PromotionAndAssignmentRefset extends PromotionRefset {
 
@@ -36,7 +31,7 @@ public class PromotionAndAssignmentRefset extends PromotionRefset {
 
 	public PromotionAndAssignmentRefset(I_GetConceptData refsetConcept) throws Exception {
 		super(refsetConcept);
-		//TODO: validate if refsetConcept is promotion refset?
+		// TODO: validate if refsetConcept is promotion refset?
 		this.refsetConcept = refsetConcept;
 		this.refsetName = refsetConcept.toString();
 		this.refsetId = refsetConcept.getConceptNid();
@@ -106,13 +101,21 @@ public class PromotionAndAssignmentRefset extends PromotionRefset {
 	public I_GetConceptData getPreviousPromotionStatus(int componentId, I_ConfigAceFrame config) throws IOException, TerminologyException {
 		I_GetConceptData component = termFactory.getConcept(componentId);
 		Collection<? extends RefexChronicleBI<?>> members = component.getAnnotations();
+		Long lastStatusTime = getLastStatusTime(componentId, config);
 		for (RefexChronicleBI<?> promotionMember : members) {
 			if (promotionMember.getCollectionNid() == this.refsetId) {
-				try {
-					RefexCnidCnidVersionBI promotionExtensionPart = (RefexCnidCnidVersionBI) promotionMember.getVersion(config.getViewCoordinate());
+				RefexCnidCnidVersionBI promotionExtensionPart = null;
+				Collection<? extends RefexVersionBI> versions = promotionMember.getVersions();
+
+				long previousToLastVersion = Long.MIN_VALUE;
+				for (RefexVersionBI loopPart : versions) {
+					if (loopPart.getTime() >= previousToLastVersion && loopPart.getTime() < lastStatusTime) {
+						previousToLastVersion = loopPart.getTime();
+						promotionExtensionPart = (RefexCnidCnidVersionBI) loopPart;
+					}
+				}
+				if (promotionExtensionPart != null) {
 					return termFactory.getConcept(promotionExtensionPart.getCnid1());
-				} catch (Exception e) {
-					AceLog.getAppLog().alertAndLogException(e);
 				}
 			}
 		}
@@ -125,10 +128,8 @@ public class PromotionAndAssignmentRefset extends PromotionRefset {
 		Long lastStatusTime = getLastStatusTime(componentId, config);
 		for (RefexChronicleBI<?> promotionMember : members) {
 			if (promotionMember.getCollectionNid() == this.refsetId) {
-				Collection<? extends RefexVersionBI> loopParts = promotionMember.getVersions(config.getViewCoordinate());
-				long lastVersion = Long.MIN_VALUE;
+				Collection<? extends RefexVersionBI> loopParts = promotionMember.getVersions();
 				RefexCnidCnidVersionBI promotionExtensionPart = null;
-				Collection<? extends RefexVersionBI> versions = promotionMember.getVersions(config.getViewCoordinate());
 
 				long previousToLastVersion = Long.MIN_VALUE;
 				for (RefexVersionBI loopPart : loopParts) {
@@ -147,8 +148,7 @@ public class PromotionAndAssignmentRefset extends PromotionRefset {
 
 	public void setPromotionStatus(int componentId, int statusConceptId) throws Exception {
 		I_ConfigAceFrame config = termFactory.getActiveAceFrameConfig();
-		TerminologyBuilderBI tc = Ts.get().getTerminologyBuilder(config.getEditCoordinate(),
-				config.getViewCoordinate());
+		TerminologyBuilderBI tc = Ts.get().getTerminologyBuilder(config.getEditCoordinate(), config.getViewCoordinate());
 
 		I_GetConceptData component = termFactory.getConcept(componentId);
 		boolean statusAlreadyPresent = false;
@@ -164,69 +164,62 @@ public class PromotionAndAssignmentRefset extends PromotionRefset {
 					promotionStatusExtensionPart = (I_ExtendByRefPartCidCid) loopPart;
 				}
 			}
-			if (promotionStatusExtensionPart!=null 
-					&& (promotionStatusExtensionPart.getStatusNid()!=activeValueNid
-							|| promotionStatusExtensionPart.getC1id() != statusConceptId)){
+			if (promotionStatusExtensionPart != null && (promotionStatusExtensionPart.getStatusNid() != activeValueNid || promotionStatusExtensionPart.getC1id() != statusConceptId)) {
 
 				for (PathBI editPath : config.getEditingPathSet()) {
-					I_ExtendByRefPartCidCid newPromotionStatusPart =(I_ExtendByRefPartCidCid) 
-					promotionStatusExtensionPart.makeAnalog(
-							SnomedMetadataRf2.ACTIVE_VALUE_RF2.getLenient().getNid(),
-							config.getDbConfig().getUserConcept().getNid(),
-							editPath.getConceptNid(), 
-							Long.MAX_VALUE);
+					I_ExtendByRefPartCidCid newPromotionStatusPart = (I_ExtendByRefPartCidCid) promotionStatusExtensionPart.makeAnalog(SnomedMetadataRf2.ACTIVE_VALUE_RF2.getLenient().getNid(), config
+							.getDbConfig().getUserConcept().getNid(), editPath.getConceptNid(), Long.MAX_VALUE);
 					newPromotionStatusPart.setC1id(statusConceptId);
 					oldExtension.addVersion(newPromotionStatusPart);
 				}
-				//termFactory.addUncommittedNoChecks(refsetConcept);
+				// termFactory.addUncommittedNoChecks(refsetConcept);
 				termFactory.addUncommittedNoChecks(component);
-				//				termFactory.commit();
+				// termFactory.commit();
 			}
-			
+
 			// new proposal by keith and aimee
-//			RefexCnidCnidVersionBI oldStatusCnid = (RefexCnidCnidVersionBI) oldStatus;
-//			RefexCAB refexBp = oldStatusCnid.makeBlueprint(config.getViewCoordinate());
-//			refexBp.put(RefexProperty.CNID1, statusConceptId);
-//			tc.construct(refexBp);
-			
-//			RefexCnidCnidVersionBI oldStatusCnid = (RefexCnidCnidVersionBI) oldStatus;
-//			if (oldStatusCnid.getCnid1() != statusConceptId) {
-//				for (PathBI editPath : config.getEditingPathSet()) {
-//					RefexCnidCnidAnalogBI newVersion = 
-//						(RefexCnidCnidAnalogBI) oldStatusCnid.makeAnalog(activeValueNid,
-//								config.getDbConfig().getUserConcept().getNid(),
-//								editPath.getConceptNid(), 
-//								Long.MAX_VALUE);
-//					newVersion.setCnid1(statusConceptId);
-//					oldStatus.getChronicle().getVersions().add(newVersion);
-//				}
-//				termFactory.addUncommittedNoChecks(component);
-//			}
+			// RefexCnidCnidVersionBI oldStatusCnid = (RefexCnidCnidVersionBI)
+			// oldStatus;
+			// RefexCAB refexBp =
+			// oldStatusCnid.makeBlueprint(config.getViewCoordinate());
+			// refexBp.put(RefexProperty.CNID1, statusConceptId);
+			// tc.construct(refexBp);
+
+			// RefexCnidCnidVersionBI oldStatusCnid = (RefexCnidCnidVersionBI)
+			// oldStatus;
+			// if (oldStatusCnid.getCnid1() != statusConceptId) {
+			// for (PathBI editPath : config.getEditingPathSet()) {
+			// RefexCnidCnidAnalogBI newVersion =
+			// (RefexCnidCnidAnalogBI) oldStatusCnid.makeAnalog(activeValueNid,
+			// config.getDbConfig().getUserConcept().getNid(),
+			// editPath.getConceptNid(),
+			// Long.MAX_VALUE);
+			// newVersion.setCnid1(statusConceptId);
+			// oldStatus.getChronicle().getVersions().add(newVersion);
+			// }
+			// termFactory.addUncommittedNoChecks(component);
+			// }
 		} else {
 			I_GetConceptData newMemberConcept = termFactory.getConcept(componentId);
 			I_HelpRefsets refsetHelper = termFactory.getRefsetHelper(config);
 
 			RefsetPropertyMap propMap = new RefsetPropertyMap();
-			propMap.put(RefsetPropertyMap.REFSET_PROPERTY.CID_ONE, 
-					statusConceptId);
-			propMap.put(RefsetPropertyMap.REFSET_PROPERTY.CID_TWO, 
-					defaultUserNid);
+			propMap.put(RefsetPropertyMap.REFSET_PROPERTY.CID_ONE, statusConceptId);
+			propMap.put(RefsetPropertyMap.REFSET_PROPERTY.CID_TWO, defaultUserNid);
 
-			refsetHelper.newRefsetExtension(this.refsetId, 
-					componentId, EConcept.REFSET_TYPES.CID_CID, 
-					propMap, config);
+			refsetHelper.newRefsetExtension(this.refsetId, componentId, EConcept.REFSET_TYPES.CID_CID, propMap, config);
 
-			//termFactory.addUncommittedNoChecks(refsetConcept);
+			// termFactory.addUncommittedNoChecks(refsetConcept);
 			termFactory.addUncommittedNoChecks(newMemberConcept);
-			
-//			RefexCAB newSpec = new RefexCAB(
-//					TK_REFSET_TYPE.CID_CID,
-//					componentId,
-//					refsetId);
-//			newSpec.put(RefexProperty.CNID1, statusConceptId);
-//			newSpec.put(RefexProperty.CNID2, defaultUserNid);
-//			RefexChronicleBI<?> newRefex = tc.construct(newSpec);
-//			termFactory.addUncommittedNoChecks(component);
+
+			// RefexCAB newSpec = new RefexCAB(
+			// TK_REFSET_TYPE.CID_CID,
+			// componentId,
+			// refsetId);
+			// newSpec.put(RefexProperty.CNID1, statusConceptId);
+			// newSpec.put(RefexProperty.CNID2, defaultUserNid);
+			// RefexChronicleBI<?> newRefex = tc.construct(newSpec);
+			// termFactory.addUncommittedNoChecks(component);
 		}
 
 		return;
@@ -234,8 +227,7 @@ public class PromotionAndAssignmentRefset extends PromotionRefset {
 
 	public void setDestination(int componentId, int destinationUserConceptId) throws Exception {
 		I_ConfigAceFrame config = termFactory.getActiveAceFrameConfig();
-		TerminologyBuilderBI tc = Ts.get().getTerminologyBuilder(config.getEditCoordinate(),
-				config.getViewCoordinate());
+		TerminologyBuilderBI tc = Ts.get().getTerminologyBuilder(config.getEditCoordinate(), config.getViewCoordinate());
 
 		I_GetConceptData component = termFactory.getConcept(componentId);
 		boolean statusAlreadyPresent = false;
@@ -252,70 +244,59 @@ public class PromotionAndAssignmentRefset extends PromotionRefset {
 					promotionStatusExtensionPart = (I_ExtendByRefPartCidCid) loopPart;
 				}
 			}
-			if (promotionStatusExtensionPart!=null 
-					&& (promotionStatusExtensionPart.getStatusNid()!=activeValueNid
-							|| promotionStatusExtensionPart.getC2id() != destinationUserConceptId)){
+			if (promotionStatusExtensionPart != null && (promotionStatusExtensionPart.getStatusNid() != activeValueNid || promotionStatusExtensionPart.getC2id() != destinationUserConceptId)) {
 				for (PathBI editPath : config.getEditingPathSet()) {
-					I_ExtendByRefPartCidCid newPromotionStatusPart =(I_ExtendByRefPartCidCid) 
-					promotionStatusExtensionPart.makeAnalog(
-							SnomedMetadataRf2.ACTIVE_VALUE_RF2.getLenient().getNid(),
-							config.getDbConfig().getUserConcept().getNid(),
-							editPath.getConceptNid(), 
-							Long.MAX_VALUE);
+					I_ExtendByRefPartCidCid newPromotionStatusPart = (I_ExtendByRefPartCidCid) promotionStatusExtensionPart.makeAnalog(SnomedMetadataRf2.ACTIVE_VALUE_RF2.getLenient().getNid(), config
+							.getDbConfig().getUserConcept().getNid(), editPath.getConceptNid(), Long.MAX_VALUE);
 					newPromotionStatusPart.setC2id(destinationUserConceptId);
 					oldExtension.addVersion(newPromotionStatusPart);
 				}
-				//termFactory.addUncommittedNoChecks(refsetConcept);
+				// termFactory.addUncommittedNoChecks(refsetConcept);
 				termFactory.addUncommittedNoChecks(component);
-				//				termFactory.commit();
+				// termFactory.commit();
 			}
-			//			RefexCnidCnidVersionBI oldStatusCnid = (RefexCnidCnidVersionBI) oldStatus;
-			//			if (oldStatusCnid.getCnid2() != destinationUserConceptId) {
-			//				for (PathBI editPath : config.getEditingPathSet()) {
-			//					RefexCnidCnidAnalogBI newVersion = 
-			//						(RefexCnidCnidAnalogBI) oldStatusCnid.makeAnalog(activeValueNid,
-			//								config.getDbConfig().getUserConcept().getNid(),
-			//								editPath.getConceptNid(), 
-			//								Long.MAX_VALUE);
-			//					newVersion.setCnid2(destinationUserConceptId);
-			//					oldStatus.getChronicle().getVersions().add(newVersion);
-			//				}
-			//				termFactory.addUncommittedNoChecks(component);
-			//			}
+			// RefexCnidCnidVersionBI oldStatusCnid = (RefexCnidCnidVersionBI)
+			// oldStatus;
+			// if (oldStatusCnid.getCnid2() != destinationUserConceptId) {
+			// for (PathBI editPath : config.getEditingPathSet()) {
+			// RefexCnidCnidAnalogBI newVersion =
+			// (RefexCnidCnidAnalogBI) oldStatusCnid.makeAnalog(activeValueNid,
+			// config.getDbConfig().getUserConcept().getNid(),
+			// editPath.getConceptNid(),
+			// Long.MAX_VALUE);
+			// newVersion.setCnid2(destinationUserConceptId);
+			// oldStatus.getChronicle().getVersions().add(newVersion);
+			// }
+			// termFactory.addUncommittedNoChecks(component);
+			// }
 		} else {
 			I_GetConceptData newMemberConcept = termFactory.getConcept(componentId);
 			I_HelpRefsets refsetHelper = termFactory.getRefsetHelper(config);
 
 			RefsetPropertyMap propMap = new RefsetPropertyMap();
-			propMap.put(RefsetPropertyMap.REFSET_PROPERTY.CID_ONE, 
-					defaultStatusNid);
-			propMap.put(RefsetPropertyMap.REFSET_PROPERTY.CID_TWO, 
-					destinationUserConceptId);
+			propMap.put(RefsetPropertyMap.REFSET_PROPERTY.CID_ONE, defaultStatusNid);
+			propMap.put(RefsetPropertyMap.REFSET_PROPERTY.CID_TWO, destinationUserConceptId);
 
-			refsetHelper.newRefsetExtension(this.refsetId, 
-					componentId, EConcept.REFSET_TYPES.CID_CID, 
-					propMap, config);
+			refsetHelper.newRefsetExtension(this.refsetId, componentId, EConcept.REFSET_TYPES.CID_CID, propMap, config);
 
-			//termFactory.addUncommittedNoChecks(refsetConcept);
+			// termFactory.addUncommittedNoChecks(refsetConcept);
 			termFactory.addUncommittedNoChecks(newMemberConcept);
-//			RefexCAB newSpec = new RefexCAB(
-//					TK_REFSET_TYPE.CID_CID,
-//					componentId,
-//					refsetId);
-//			newSpec.put(RefexProperty.CNID1, defaultStatusNid);
-//			newSpec.put(RefexProperty.CNID2, destinationUserConceptId);
-//			RefexChronicleBI<?> newRefex = tc.construct(newSpec);
-//			termFactory.addUncommittedNoChecks(component);
+			// RefexCAB newSpec = new RefexCAB(
+			// TK_REFSET_TYPE.CID_CID,
+			// componentId,
+			// refsetId);
+			// newSpec.put(RefexProperty.CNID1, defaultStatusNid);
+			// newSpec.put(RefexProperty.CNID2, destinationUserConceptId);
+			// RefexChronicleBI<?> newRefex = tc.construct(newSpec);
+			// termFactory.addUncommittedNoChecks(component);
 		}
 
 		return;
 	}
-	
-	public void setDestinationAndPromotionStatus(int componentId, int destinationUserConceptId,
-			int statusConceptId) throws Exception {
+
+	public void setDestinationAndPromotionStatus(int componentId, int destinationUserConceptId, int statusConceptId) throws Exception {
 		I_ConfigAceFrame config = termFactory.getActiveAceFrameConfig();
-		TerminologyBuilderBI tc = Ts.get().getTerminologyBuilder(config.getEditCoordinate(),
-				config.getViewCoordinate());
+		TerminologyBuilderBI tc = Ts.get().getTerminologyBuilder(config.getEditCoordinate(), config.getViewCoordinate());
 
 		I_GetConceptData component = termFactory.getConcept(componentId);
 		boolean statusAlreadyPresent = false;
@@ -332,66 +313,56 @@ public class PromotionAndAssignmentRefset extends PromotionRefset {
 					promotionStatusExtensionPart = (I_ExtendByRefPartCidCid) loopPart;
 				}
 			}
-			if (promotionStatusExtensionPart!=null 
-					&& (promotionStatusExtensionPart.getStatusNid()!=activeValueNid
-							|| promotionStatusExtensionPart.getC2id() != destinationUserConceptId ||
-							promotionStatusExtensionPart.getC1id() != statusConceptId)){
+			if (promotionStatusExtensionPart != null
+					&& (promotionStatusExtensionPart.getStatusNid() != activeValueNid || promotionStatusExtensionPart.getC2id() != destinationUserConceptId || promotionStatusExtensionPart.getC1id() != statusConceptId)) {
 				for (PathBI editPath : config.getEditingPathSet()) {
-					I_ExtendByRefPartCidCid newPromotionStatusPart =(I_ExtendByRefPartCidCid) 
-					promotionStatusExtensionPart.makeAnalog(
-							SnomedMetadataRf2.ACTIVE_VALUE_RF2.getLenient().getNid(),
-							config.getDbConfig().getUserConcept().getNid(),
-							editPath.getConceptNid(), 
-							Long.MAX_VALUE);
+					I_ExtendByRefPartCidCid newPromotionStatusPart = (I_ExtendByRefPartCidCid) promotionStatusExtensionPart.makeAnalog(SnomedMetadataRf2.ACTIVE_VALUE_RF2.getLenient().getNid(), config
+							.getDbConfig().getUserConcept().getNid(), editPath.getConceptNid(), Long.MAX_VALUE);
 					newPromotionStatusPart.setC1id(statusConceptId);
 					newPromotionStatusPart.setC2id(destinationUserConceptId);
 					oldExtension.addVersion(newPromotionStatusPart);
 				}
-				//termFactory.addUncommittedNoChecks(refsetConcept);
+				// termFactory.addUncommittedNoChecks(refsetConcept);
 				termFactory.addUncommittedNoChecks(component);
-				//				termFactory.commit();
+				// termFactory.commit();
 			}
-			//			RefexCnidCnidVersionBI oldStatusCnid = (RefexCnidCnidVersionBI) oldStatus;
-			//			if (oldStatusCnid.getCnid2() != destinationUserConceptId) {
-			//				for (PathBI editPath : config.getEditingPathSet()) {
-			//					RefexCnidCnidAnalogBI newVersion = 
-			//						(RefexCnidCnidAnalogBI) oldStatusCnid.makeAnalog(activeValueNid,
-			//								config.getDbConfig().getUserConcept().getNid(),
-			//								editPath.getConceptNid(), 
-			//								Long.MAX_VALUE);
-			//					newVersion.setCnid2(destinationUserConceptId);
-			//					oldStatus.getChronicle().getVersions().add(newVersion);
-			//				}
-			//				termFactory.addUncommittedNoChecks(component);
-			//			}
+			// RefexCnidCnidVersionBI oldStatusCnid = (RefexCnidCnidVersionBI)
+			// oldStatus;
+			// if (oldStatusCnid.getCnid2() != destinationUserConceptId) {
+			// for (PathBI editPath : config.getEditingPathSet()) {
+			// RefexCnidCnidAnalogBI newVersion =
+			// (RefexCnidCnidAnalogBI) oldStatusCnid.makeAnalog(activeValueNid,
+			// config.getDbConfig().getUserConcept().getNid(),
+			// editPath.getConceptNid(),
+			// Long.MAX_VALUE);
+			// newVersion.setCnid2(destinationUserConceptId);
+			// oldStatus.getChronicle().getVersions().add(newVersion);
+			// }
+			// termFactory.addUncommittedNoChecks(component);
+			// }
 		} else {
 			I_GetConceptData newMemberConcept = termFactory.getConcept(componentId);
 			I_HelpRefsets refsetHelper = termFactory.getRefsetHelper(config);
 
 			RefsetPropertyMap propMap = new RefsetPropertyMap();
-			propMap.put(RefsetPropertyMap.REFSET_PROPERTY.CID_ONE, 
-					statusConceptId);
-			propMap.put(RefsetPropertyMap.REFSET_PROPERTY.CID_TWO, 
-					destinationUserConceptId);
+			propMap.put(RefsetPropertyMap.REFSET_PROPERTY.CID_ONE, statusConceptId);
+			propMap.put(RefsetPropertyMap.REFSET_PROPERTY.CID_TWO, destinationUserConceptId);
 
-			refsetHelper.newRefsetExtension(this.refsetId, 
-					componentId, EConcept.REFSET_TYPES.CID_CID, 
-					propMap, config);
+			refsetHelper.newRefsetExtension(this.refsetId, componentId, EConcept.REFSET_TYPES.CID_CID, propMap, config);
 
-			//termFactory.addUncommittedNoChecks(refsetConcept);
+			// termFactory.addUncommittedNoChecks(refsetConcept);
 			termFactory.addUncommittedNoChecks(newMemberConcept);
-//			RefexCAB newSpec = new RefexCAB(
-//					TK_REFSET_TYPE.CID_CID,
-//					componentId,
-//					refsetId);
-//			newSpec.put(RefexProperty.CNID1, defaultStatusNid);
-//			newSpec.put(RefexProperty.CNID2, destinationUserConceptId);
-//			RefexChronicleBI<?> newRefex = tc.construct(newSpec);
-//			termFactory.addUncommittedNoChecks(component);
+			// RefexCAB newSpec = new RefexCAB(
+			// TK_REFSET_TYPE.CID_CID,
+			// componentId,
+			// refsetId);
+			// newSpec.put(RefexProperty.CNID1, defaultStatusNid);
+			// newSpec.put(RefexProperty.CNID2, destinationUserConceptId);
+			// RefexChronicleBI<?> newRefex = tc.construct(newSpec);
+			// termFactory.addUncommittedNoChecks(component);
 		}
 
 		return;
 	}
-	
-	
+
 }
