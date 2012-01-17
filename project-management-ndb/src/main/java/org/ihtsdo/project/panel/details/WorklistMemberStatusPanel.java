aa71a228dca79c99e3bc479d682dae4943b9a8d7
vmanukyan <vmanukyan>
2012-01-17 19:25:33 +0000

    Wl statuses (null) problem solved

--- project-management-ndb/src/main/java/org/ihtsdo/project/panel/details/WorklistMemberStatusPanel.java
+++ project-management-ndb/src/main/java/org/ihtsdo/project/panel/details/WorklistMemberStatusPanel.java
@@ -4,42 +4,43 @@
 
 package org.ihtsdo.project.panel.details;
 
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
-import javax.swing.*;
 
 import javax.swing.DefaultListModel;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTable;
 import javax.swing.SwingWorker;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
-import javax.swing.table.DefaultTableModel;
 
 import org.dwfa.ace.api.I_ConfigAceFrame;
 import org.dwfa.ace.api.I_GetConceptData;
-import org.dwfa.ace.api.Terms;
 import org.dwfa.tapi.TerminologyException;
 import org.ihtsdo.project.TerminologyProjectDAO;
-import org.ihtsdo.project.model.TranslationProject;
 import org.ihtsdo.project.model.WorkList;
 import org.ihtsdo.project.model.WorkListMember;
-import org.ihtsdo.project.panel.details.WorklistMemberStatusPanel.WorklistMembersStatusWorker;
-import org.ihtsdo.project.workflow.model.WfUser;
+import org.ihtsdo.project.workflow.api.WorkflowSearcher;
+import org.ihtsdo.project.workflow.filters.WfSearchFilterBI;
 
 /**
  * @author Guillermo Reynoso
  */
 public class WorklistMemberStatusPanel extends JPanel {
+	private static final long serialVersionUID = 4054703594663442841L;
 	private WorkList workList;
 	private I_ConfigAceFrame config;
 	private DefaultListModel list5Model;
@@ -135,58 +136,70 @@ public class WorklistMemberStatusPanel extends JPanel {
 		panel1 = new JPanel();
 		button1 = new JButton();
 
-		// ======== this ========
+		//======== this ========
 		setLayout(new GridBagLayout());
-		((GridBagLayout) getLayout()).columnWidths = new int[] { 0, 0 };
-		((GridBagLayout) getLayout()).rowHeights = new int[] { 0, 0, 0 };
-		((GridBagLayout) getLayout()).columnWeights = new double[] { 1.0, 1.0E-4 };
-		((GridBagLayout) getLayout()).rowWeights = new double[] { 1.0, 0.0, 1.0E-4 };
+		((GridBagLayout)getLayout()).columnWidths = new int[] {0, 0};
+		((GridBagLayout)getLayout()).rowHeights = new int[] {0, 0, 0};
+		((GridBagLayout)getLayout()).columnWeights = new double[] {1.0, 1.0E-4};
+		((GridBagLayout)getLayout()).rowWeights = new double[] {1.0, 0.0, 1.0E-4};
 
-		// ======== panel20 ========
+		//======== panel20 ========
 		{
 			panel20.setLayout(new GridBagLayout());
-			((GridBagLayout) panel20.getLayout()).columnWidths = new int[] { 251, 77, 286, 0 };
-			((GridBagLayout) panel20.getLayout()).rowHeights = new int[] { 0, 0, 0, 0 };
-			((GridBagLayout) panel20.getLayout()).columnWeights = new double[] { 1.0, 0.0, 1.0, 1.0E-4 };
-			((GridBagLayout) panel20.getLayout()).rowWeights = new double[] { 0.0, 0.0, 1.0, 1.0E-4 };
+			((GridBagLayout)panel20.getLayout()).columnWidths = new int[] {251, 77, 286, 0};
+			((GridBagLayout)panel20.getLayout()).rowHeights = new int[] {0, 0, 0, 0};
+			((GridBagLayout)panel20.getLayout()).columnWeights = new double[] {1.0, 0.0, 1.0, 1.0E-4};
+			((GridBagLayout)panel20.getLayout()).rowWeights = new double[] {0.0, 0.0, 1.0, 1.0E-4};
 
-			// ======== panel24 ========
+			//======== panel24 ========
 			{
 				panel24.setLayout(new GridBagLayout());
-				((GridBagLayout) panel24.getLayout()).columnWidths = new int[] { 0, 0 };
-				((GridBagLayout) panel24.getLayout()).rowHeights = new int[] { 0, 0 };
-				((GridBagLayout) panel24.getLayout()).columnWeights = new double[] { 1.0, 1.0E-4 };
-				((GridBagLayout) panel24.getLayout()).rowWeights = new double[] { 0.0, 1.0E-4 };
-				panel24.add(label19, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+				((GridBagLayout)panel24.getLayout()).columnWidths = new int[] {0, 0};
+				((GridBagLayout)panel24.getLayout()).rowHeights = new int[] {0, 0};
+				((GridBagLayout)panel24.getLayout()).columnWeights = new double[] {1.0, 1.0E-4};
+				((GridBagLayout)panel24.getLayout()).rowWeights = new double[] {0.0, 1.0E-4};
+				panel24.add(label19, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
+					GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+					new Insets(0, 0, 0, 0), 0, 0));
 			}
-			panel20.add(panel24, new GridBagConstraints(0, 0, 3, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 5, 0), 0, 0));
+			panel20.add(panel24, new GridBagConstraints(0, 0, 3, 1, 0.0, 0.0,
+				GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+				new Insets(0, 0, 5, 0), 0, 0));
 
-			// ---- label13 ----
+			//---- label13 ----
 			label13.setText("Current statuses of WorkList members");
-			panel20.add(label13, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 5, 5), 0, 0));
+			panel20.add(label13, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0,
+				GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+				new Insets(0, 0, 5, 5), 0, 0));
 
-			// ======== panel5 ========
+			//======== panel5 ========
 			{
 				panel5.setLayout(new GridBagLayout());
-				((GridBagLayout) panel5.getLayout()).columnWidths = new int[] { 255, 10, 0 };
-				((GridBagLayout) panel5.getLayout()).rowHeights = new int[] { 0, 0 };
-				((GridBagLayout) panel5.getLayout()).columnWeights = new double[] { 1.0, 1.0, 1.0E-4 };
-				((GridBagLayout) panel5.getLayout()).rowWeights = new double[] { 0.0, 1.0E-4 };
+				((GridBagLayout)panel5.getLayout()).columnWidths = new int[] {255, 10, 0};
+				((GridBagLayout)panel5.getLayout()).rowHeights = new int[] {0, 0};
+				((GridBagLayout)panel5.getLayout()).columnWeights = new double[] {1.0, 1.0, 1.0E-4};
+				((GridBagLayout)panel5.getLayout()).rowWeights = new double[] {0.0, 1.0E-4};
 
-				// ---- label14 ----
+				//---- label14 ----
 				label14.setText("Members with selected status");
-				panel5.add(label14, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 5), 0, 0));
+				panel5.add(label14, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
+					GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
+					new Insets(0, 0, 0, 5), 0, 0));
 
-				// ---- label15 ----
+				//---- label15 ----
 				label15.setText("-");
-				panel5.add(label15, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.VERTICAL, new Insets(0, 0, 0, 0), 0, 0));
+				panel5.add(label15, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0,
+					GridBagConstraints.CENTER, GridBagConstraints.VERTICAL,
+					new Insets(0, 0, 0, 0), 0, 0));
 			}
-			panel20.add(panel5, new GridBagConstraints(2, 1, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 5, 0), 0, 0));
+			panel20.add(panel5, new GridBagConstraints(2, 1, 1, 1, 0.0, 0.0,
+				GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+				new Insets(0, 0, 5, 0), 0, 0));
 
-			// ======== scrollPane5 ========
+			//======== scrollPane5 ========
 			{
 
-				// ---- list5 ----
+				//---- list5 ----
 				list5.addListSelectionListener(new ListSelectionListener() {
 					@Override
 					public void valueChanged(ListSelectionEvent e) {
@@ -195,35 +208,43 @@ public class WorklistMemberStatusPanel extends JPanel {
 				});
 				scrollPane5.setViewportView(list5);
 			}
-			panel20.add(scrollPane5, new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 5), 0, 0));
+			panel20.add(scrollPane5, new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0,
+				GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+				new Insets(0, 0, 0, 5), 0, 0));
 
-			// ======== panel21 ========
+			//======== panel21 ========
 			{
 				panel21.setLayout(new GridBagLayout());
-				((GridBagLayout) panel21.getLayout()).columnWidths = new int[] { 0, 0 };
-				((GridBagLayout) panel21.getLayout()).rowHeights = new int[] { 0, 0 };
-				((GridBagLayout) panel21.getLayout()).columnWeights = new double[] { 1.0, 1.0E-4 };
-				((GridBagLayout) panel21.getLayout()).rowWeights = new double[] { 1.0, 1.0E-4 };
+				((GridBagLayout)panel21.getLayout()).columnWidths = new int[] {0, 0};
+				((GridBagLayout)panel21.getLayout()).rowHeights = new int[] {0, 0};
+				((GridBagLayout)panel21.getLayout()).columnWeights = new double[] {1.0, 1.0E-4};
+				((GridBagLayout)panel21.getLayout()).rowWeights = new double[] {1.0, 1.0E-4};
 
-				// ======== scrollPane1 ========
+				//======== scrollPane1 ========
 				{
 					scrollPane1.setViewportView(list1);
 				}
-				panel21.add(scrollPane1, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+				panel21.add(scrollPane1, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
+					GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+					new Insets(0, 0, 0, 0), 0, 0));
 			}
-			panel20.add(panel21, new GridBagConstraints(2, 2, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+			panel20.add(panel21, new GridBagConstraints(2, 2, 1, 1, 0.0, 0.0,
+				GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+				new Insets(0, 0, 0, 0), 0, 0));
 		}
-		add(panel20, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 5, 0), 0, 0));
+		add(panel20, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
+			GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+			new Insets(0, 0, 5, 0), 0, 0));
 
-		// ======== panel1 ========
+		//======== panel1 ========
 		{
 			panel1.setLayout(new GridBagLayout());
-			((GridBagLayout) panel1.getLayout()).columnWidths = new int[] { 0, 0, 0, 0 };
-			((GridBagLayout) panel1.getLayout()).rowHeights = new int[] { 0, 0 };
-			((GridBagLayout) panel1.getLayout()).columnWeights = new double[] { 0.0, 0.0, 0.0, 1.0E-4 };
-			((GridBagLayout) panel1.getLayout()).rowWeights = new double[] { 0.0, 1.0E-4 };
+			((GridBagLayout)panel1.getLayout()).columnWidths = new int[] {0, 0, 0, 0};
+			((GridBagLayout)panel1.getLayout()).rowHeights = new int[] {0, 0};
+			((GridBagLayout)panel1.getLayout()).columnWeights = new double[] {0.0, 0.0, 0.0, 1.0E-4};
+			((GridBagLayout)panel1.getLayout()).rowWeights = new double[] {0.0, 1.0E-4};
 
-			// ---- button1 ----
+			//---- button1 ----
 			button1.setText("Refresh");
 			button1.setFont(new Font("Lucida Grande", Font.PLAIN, 11));
 			button1.addActionListener(new ActionListener() {
@@ -232,10 +253,13 @@ public class WorklistMemberStatusPanel extends JPanel {
 					button1ActionPerformed(e);
 				}
 			});
-			panel1.add(button1, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 5), 0, 0));
+			panel1.add(button1, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
+				GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+				new Insets(0, 0, 0, 5), 0, 0));
 		}
-		add(panel1, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
-		// JFormDesigner - End of component initialization
+		add(panel1, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0,
+			GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+			new Insets(0, 0, 0, 0), 0, 0));
 		// //GEN-END:initComponents
 	}
 
@@ -255,7 +279,6 @@ public class WorklistMemberStatusPanel extends JPanel {
 	private JList list1;
 	private JPanel panel1;
 	private JButton button1;
-
 	// JFormDesigner - End of variables declaration //GEN-END:variables
 	class WorklistMembersStatusWorker extends SwingWorker<String, String> {
 
@@ -273,7 +296,6 @@ public class WorklistMemberStatusPanel extends JPanel {
 						return f1.toString().compareTo(f2.toString());
 					}
 				});
-
 				statuses = TerminologyProjectDAO.getWorkListMemberStatuses(workList, config);
 				for (I_GetConceptData status : statuses.keySet()) {
 					publish(status.toString() + " (" + statuses.get(status) + ")");
@@ -288,7 +310,7 @@ public class WorklistMemberStatusPanel extends JPanel {
 		@Override
 		protected void process(List<String> chunks) {
 			for (String objects : chunks) {
-				list5Model.addElement(objects.toString() + " (" + statuses.get(objects) + ")");
+				list5Model.addElement(objects.toString());
 			}
 		}
 
