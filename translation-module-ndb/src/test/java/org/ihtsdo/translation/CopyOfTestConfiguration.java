cad8a50ee30218b5ff2809ab4326af62622897fe
vmanukyan <vmanukyan>
2012-01-24 22:29:06 +0000

    report workers

--- translation-module-ndb/src/test/java/org/ihtsdo/translation/CopyOfTestConfiguration.java
+++ translation-module-ndb/src/test/java/org/ihtsdo/translation/CopyOfTestConfiguration.java
@@ -24,8 +24,6 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.UUID;
 
