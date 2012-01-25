69db13aef8d3510c2bc5dd6d2eae577742486956
alopez <alopez>
2012-01-25 19:13:52 +0000

    Added rels based permissions creation to GenerateUsers

--- wb-bdb-mojo/src/main/java/org/ihtsdo/bdb/mojo/GenerateUsers.java
+++ wb-bdb-mojo/src/main/java/org/ihtsdo/bdb/mojo/GenerateUsers.java
@@ -16,22 +16,53 @@
 package org.ihtsdo.bdb.mojo;
 
 import java.awt.Color;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.UUID;
 import java.util.logging.Level;
+
 import javax.swing.JOptionPane;
+
 import net.jini.config.Configuration;
 import net.jini.config.ConfigurationException;
 import net.jini.config.ConfigurationProvider;
 import net.jini.core.entry.Entry;
+
+import org.apache.lucene.document.Document;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.dwfa.ace.api.*;
+import org.dwfa.ace.api.I_ConfigAceDb;
+import org.dwfa.ace.api.I_ConfigAceFrame;
+import org.dwfa.ace.api.I_DescriptionVersioned;
+import org.dwfa.ace.api.I_GetConceptData;
+import org.dwfa.ace.api.I_HostConceptPlugins;
+import org.dwfa.ace.api.I_ImplementTermFactory;
+import org.dwfa.ace.api.I_IntList;
+import org.dwfa.ace.api.I_IntSet;
+import org.dwfa.ace.api.I_PluginToConceptPanel;
+import org.dwfa.ace.api.I_RelVersioned;
+import org.dwfa.ace.api.I_TermFactory;
+import org.dwfa.ace.api.Terms;
 import org.dwfa.ace.api.cs.ChangeSetPolicy;
 import org.dwfa.ace.api.cs.ChangeSetWriterThreading;
 import org.dwfa.ace.log.AceLog;
 import org.dwfa.ace.task.commit.AlertToDataConstraintFailure;
+import org.dwfa.bpa.process.TaskFailedException;
 import org.dwfa.cement.ArchitectonicAuxiliary;
 import org.dwfa.jini.ElectronicAddress;
 import org.dwfa.queue.QueueServer;
@@ -40,16 +71,23 @@ import org.dwfa.util.id.Type5UuidFactory;
 import org.dwfa.util.io.FileIO;
 import org.ihtsdo.db.bdb.Bdb;
 import org.ihtsdo.db.bdb.computer.ReferenceConcepts;
+import org.ihtsdo.lucene.SearchResult;
 import org.ihtsdo.tk.Ts;
 import org.ihtsdo.tk.api.PathBI;
 import org.ihtsdo.tk.api.PositionBI;
 import org.ihtsdo.tk.api.Precedence;
 import org.ihtsdo.tk.api.RelAssertionType;
+import org.ihtsdo.tk.api.blueprint.RelCAB;
 import org.ihtsdo.tk.api.changeset.ChangeSetGenerationPolicy;
 import org.ihtsdo.tk.api.changeset.ChangeSetGeneratorBI;
 import org.ihtsdo.tk.api.concept.ConceptVersionBI;
 import org.ihtsdo.tk.api.coordinate.ViewCoordinate;
-import org.ihtsdo.tk.binding.snomed.*;
+import org.ihtsdo.tk.binding.snomed.Snomed;
+import org.ihtsdo.tk.binding.snomed.SnomedMetadataRf1;
+import org.ihtsdo.tk.binding.snomed.SnomedMetadataRf2;
+import org.ihtsdo.tk.binding.snomed.Taxonomies;
+import org.ihtsdo.tk.binding.snomed.TermAux;
+import org.ihtsdo.tk.dto.concept.component.relationship.TkRelType;
 import org.ihtsdo.tk.spec.ConceptSpec;
 import org.ihtsdo.workflow.refset.edcat.EditorCategoryRefsetSearcher;
 import org.ihtsdo.workflow.refset.edcat.EditorCategoryRefsetWriter;
@@ -85,6 +123,11 @@ public class GenerateUsers extends AbstractMojo {
      * expression="${project.build.directory}/users/userPermissionRefset.txt"
      */
     private File wfPermissionsFile;
+    /**
+	 *
+	 * @parameter expression="${project.build.directory}/users/userPermissionRels.txt"
+	 */
+	private File relPermissionsFile;
     /**
      *
      * @parameter expression="${project.build.directory}/users/userConfig.txt"
@@ -221,6 +264,24 @@ public class GenerateUsers extends AbstractMojo {
 
                 wfLine = wfReader.readLine();
             }
+            
+            try {
+				FileReader     fr = new FileReader(relPermissionsFile);
+				BufferedReader br = new BufferedReader(fr);
+
+				br.readLine();
+
+				String relPermissionLine = br.readLine();
+
+				while (relPermissionLine != null) {
+					String[] parts = relPermissionLine.split("\t");
+
+					addRelPermission(parts[0], parts[1], parts[2], parts[3], parts[4]);
+					relPermissionLine = br.readLine();
+				}
+			} catch (Exception ex) {
+				throw new TaskFailedException(ex);
+			}
 
             Terms.get().commit();
 
@@ -234,6 +295,46 @@ public class GenerateUsers extends AbstractMojo {
         }
 
     }
+    
+    private void addRelPermission(String userName, String typeUid, String typeName,
+			String targetUid, String targetName) throws Exception {
+		I_TermFactory tf = Terms.get();
+		I_ConfigAceFrame config = tf.getActiveAceFrameConfig();
+
+		I_GetConceptData user = null;
+		SearchResult results = tf.doLuceneSearch(userName);
+		for (int i = 0 ; i <results.topDocs.scoreDocs.length  ; i++) {
+			Document doc = results.searcher.doc(results.topDocs.scoreDocs[i].doc);
+			int cnid = Integer.parseInt(doc.get("cnid"));
+			int dnid = Integer.parseInt(doc.get("dnid"));
+			//System.out.println(doc);
+			I_DescriptionVersioned<?> foundDescription = tf.getDescription(dnid);
+			if (foundDescription.getTuples(
+					config.getConflictResolutionStrategy()).iterator().next().getText().equals(userName)) {
+				user = tf.getConcept(cnid);
+				break;
+			}
+		}
+		if (user == null) {
+			throw new Exception("User unknown");
+		} else {
+			RelCAB relCab = new RelCAB(user.getPrimUuid(),UUID.fromString(typeUid),
+					UUID.fromString(targetUid),0,TkRelType.STATED_ROLE);
+			Ts.get().getTerminologyBuilder(config.getEditCoordinate(), 
+					config.getViewCoordinate()).constructIfNotCurrent(relCab);
+			
+			
+//			old way
+//			tf.newRelationship(UUID.randomUUID(), user, 
+//					tf.getConcept(UUID.fromString(typeUid)), 
+//					tf.getConcept(UUID.fromString(targetUid)), 
+//					tf.getConcept(ArchitectonicAuxiliary.Concept.STATED_RELATIONSHIP.getUids()), 
+//					tf.getConcept(ArchitectonicAuxiliary.Concept.OPTIONAL_REFINABILITY.getUids()),
+//					tf.getConcept(ArchitectonicAuxiliary.Concept.CURRENT.getUids()), 
+//					0, config);
+			tf.addUncommittedNoChecks(user);
+		}
+	}
 
     private boolean setupUser(String fullname, String username, String password, String userUuid,
             String adminUsername, String adminPassword)
