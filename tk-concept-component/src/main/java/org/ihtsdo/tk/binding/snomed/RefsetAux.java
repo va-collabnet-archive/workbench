30a79b4c5304daef40d9d1983d3741d014170f37
afurber <afurber>
2012-02-10 19:38:00 +0000

    changed arena rules to only show gmdn review statuses on concepts which are members of the gmdn review refset.

--- tk-concept-component/src/main/java/org/ihtsdo/tk/binding/snomed/RefsetAux.java
+++ tk-concept-component/src/main/java/org/ihtsdo/tk/binding/snomed/RefsetAux.java
@@ -9,4 +9,7 @@ public class RefsetAux {
     public static ConceptSpec NL_REFEX =
             new ConceptSpec("Dutch [International Organization for Standardization 639-1 code nl] language reference set (foundation metadata concept)",
             UUID.fromString("592fe43f-f07e-568b-90e1-96de4c33b2a8"));
+    public static ConceptSpec GMDN_REFEX =
+            new ConceptSpec("GMDN review status reference set (foundation metadata concept)",
+            UUID.fromString("c5994e33-21d3-327c-ab25-06d2356d2d68"));
 }
