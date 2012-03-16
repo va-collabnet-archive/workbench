0b3edc271f2f2a93a859bf84781e5459eb65ebbf
marc <marc>
2012-03-16 23:45:44 +0000

    added button to added classifier report to list

--- wb-gui/src/main/java/org/dwfa/ace/classifier/DiffPathPanel.java
+++ wb-gui/src/main/java/org/dwfa/ace/classifier/DiffPathPanel.java
@@ -1,45 +1,45 @@
 /**
- * Copyright (c) 2009 International Health Terminology Standards Development
- * Organisation
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
+ * Copyright (c) 2009 International Health Terminology Standards Development Organisation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package org.dwfa.ace.classifier;
 
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.*;
 import javax.swing.table.TableColumn;
-import org.dwfa.ace.api.I_ConfigAceFrame;
-import org.dwfa.ace.api.I_TermFactory;
-import org.dwfa.ace.api.Terms;
+import org.dwfa.ace.ACE;
+import org.dwfa.ace.api.*;
 import org.dwfa.ace.dnd.TerminologyTransferHandler;
+import org.dwfa.ace.log.AceLog;
 import org.dwfa.ace.table.JTableWithDragImage;
 import org.dwfa.ace.task.classify.SnoQuery;
 import org.dwfa.ace.task.classify.SnoRel;
 import org.dwfa.tapi.TerminologyException;
 
 public class DiffPathPanel extends JPanel {
+
     private static final long serialVersionUID = 1L;
     // ** WORKBENCH PARTICULARS **
     private I_TermFactory tf;
     I_ConfigAceFrame config;
-
     String typeFont = "<font face='Dialog' size='3' color='blue'>";
     String valueFont = "<font face='Dialog' size='3' color='green'>";
 
@@ -68,6 +68,11 @@ public class DiffPathPanel extends JPanel {
         // ADDED ISAs
         this.add(new JLabel("Added ISAs:"), c);
         if (SnoQuery.getIsaAdded() != null && SnoQuery.getIsaAdded().size() > 0) {
+            c.gridx = 1;
+            JButton jbtn = new JButton(new ImageIcon(ACE.class.getResource("/16x16/plain/notebook_add.png")));
+            jbtn.addActionListener(new AddIsaAddedToList());
+            jbtn.setToolTipText("add concepts from \'Added ISAs:\' to list view");
+            this.add(jbtn, c);
 
             table = updateTable(SnoQuery.getIsaAdded());
             table.setDragEnabled(false);
@@ -82,6 +87,7 @@ public class DiffPathPanel extends JPanel {
             c.fill = GridBagConstraints.BOTH;
             c.weightx = 1;
             c.weighty = 1;
+            c.gridx = 0;
             c.gridy += 1;
             table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
             JScrollPane jscrollpane = new JScrollPane(table);
@@ -100,6 +106,12 @@ public class DiffPathPanel extends JPanel {
         c.gridy += 1;
         this.add(new JLabel("Retired ISAs:"), c); // Dropped
         if (SnoQuery.getIsaDropped() != null && SnoQuery.getIsaDropped().size() > 0) {
+            c.gridx = 1;
+            JButton jbtn = new JButton(new ImageIcon(ACE.class.getResource("/16x16/plain/notebook_add.png")));
+            jbtn.addActionListener(new AddIsaDroppedToList());
+            jbtn.setToolTipText("add concepts from \'Retired ISAs:\' to list view");
+            this.add(jbtn, c);
+
             table = updateTable(SnoQuery.getIsaDropped());
             table.setDragEnabled(false);
             table.setTransferHandler(new TerminologyTransferHandler(table));
@@ -113,6 +125,7 @@ public class DiffPathPanel extends JPanel {
             c.fill = GridBagConstraints.BOTH;
             c.weightx = 1;
             c.weighty = 1;
+            c.gridx = 0;
             c.gridy += 1;
             table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
             JScrollPane jscrollpane = new JScrollPane(table);
@@ -130,6 +143,11 @@ public class DiffPathPanel extends JPanel {
         c.gridy += 1;
         this.add(new JLabel("Added Roles:"), c);
         if (SnoQuery.getRoleAdded() != null && SnoQuery.getRoleAdded().size() > 0) {
+            c.gridx = 1;
+            JButton jbtn = new JButton(new ImageIcon(ACE.class.getResource("/16x16/plain/notebook_add.png")));
+            jbtn.addActionListener(new AddRoleAddedToList());
+            jbtn.setToolTipText("add concepts from \'Added Roles:\' to list view");
+            this.add(jbtn, c);
             table = updateTable(SnoQuery.getRoleAdded());
             table.setDragEnabled(false);
             table.setTransferHandler(new TerminologyTransferHandler(table));
@@ -143,6 +161,7 @@ public class DiffPathPanel extends JPanel {
             c.fill = GridBagConstraints.BOTH;
             c.weightx = 1;
             c.weighty = 1;
+            c.gridx = 0;
             c.gridy += 1;
             table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
             JScrollPane jscrollpane = new JScrollPane(table);
@@ -161,6 +180,13 @@ public class DiffPathPanel extends JPanel {
         c.gridy += 1;
         this.add(new JLabel("Retired Roles:"), c);
         if (SnoQuery.getRoleDropped() != null && SnoQuery.getRoleDropped().size() > 0) {
+            c.gridx = 1;
+            JButton jbtn = new JButton(new ImageIcon(ACE.class.getResource("/16x16/plain/notebook_add.png")));
+            jbtn.addActionListener(new AddRoleDroppedToList());
+            jbtn.setToolTipText("add concepts from \'Retired Roles:\' to list view");
+            this.add(jbtn, c);
+
+            c.gridx = 0;
             c.gridy += 1;
             c.weightx = 0.5;
             c.weighty = 0.5;
@@ -224,6 +250,122 @@ public class DiffPathPanel extends JPanel {
         return table;
     }
 
+    public class AddIsaAddedToList implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try {
+                JList conceptList = config.getBatchConceptList();
+                I_ModelTerminologyList conceptListModel = (I_ModelTerminologyList) conceptList.getModel();
+
+                ArrayList<SnoRel> added = SnoQuery.getIsaAdded();
+                Collections.sort(added);
+
+                for (int i = 0; i < added.size(); i++) {
+                    if (i < added.size() - 1) {
+                        if (added.get(i).c1Id != added.get(i + 1).c1Id) {
+                            I_GetConceptData cb = Terms.get().getConcept(added.get(i).c1Id);
+                            conceptListModel.addElement(cb);
+                        }
+                    } else {
+                        I_GetConceptData cb = Terms.get().getConcept(added.get(i).c1Id);
+                        conceptListModel.addElement(cb);
+                    }
+                }
+
+            } catch (Exception ex) {
+                AceLog.getAppLog().alertAndLogException(ex);
+            }
+        }
+    }
+
+    public class AddIsaDroppedToList implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try {
+                JList conceptList = config.getBatchConceptList();
+                I_ModelTerminologyList conceptListModel = (I_ModelTerminologyList) conceptList.getModel();
+
+                ArrayList<SnoRel> added = SnoQuery.getIsaDropped();
+                Collections.sort(added);
+
+                for (int i = 0; i < added.size(); i++) {
+                    if (i < added.size() - 1) {
+                        if (added.get(i).c1Id != added.get(i + 1).c1Id) {
+                            I_GetConceptData cb = Terms.get().getConcept(added.get(i).c1Id);
+                            conceptListModel.addElement(cb);
+                        }
+                    } else {
+                        I_GetConceptData cb = Terms.get().getConcept(added.get(i).c1Id);
+                        conceptListModel.addElement(cb);
+                    }
+                }
+
+            } catch (Exception ex) {
+                AceLog.getAppLog().alertAndLogException(ex);
+            }
+        }
+    }
+
+    public class AddRoleAddedToList implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try {
+                JList conceptList = config.getBatchConceptList();
+                I_ModelTerminologyList conceptListModel = (I_ModelTerminologyList) conceptList.getModel();
+
+                ArrayList<SnoRel> added = SnoQuery.getRoleAdded();
+                Collections.sort(added);
+
+                for (int i = 0; i < added.size(); i++) {
+                    if (i < added.size() - 1) {
+                        if (added.get(i).c1Id != added.get(i + 1).c1Id) {
+                            I_GetConceptData cb = Terms.get().getConcept(added.get(i).c1Id);
+                            conceptListModel.addElement(cb);
+                        }
+                    } else {
+                        I_GetConceptData cb = Terms.get().getConcept(added.get(i).c1Id);
+                        conceptListModel.addElement(cb);
+                    }
+                }
+
+            } catch (Exception ex) {
+                AceLog.getAppLog().alertAndLogException(ex);
+            }
+        }
+    }
+
+    public class AddRoleDroppedToList implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try {
+                JList conceptList = config.getBatchConceptList();
+                I_ModelTerminologyList conceptListModel = (I_ModelTerminologyList) conceptList.getModel();
+
+                ArrayList<SnoRel> added = SnoQuery.getRoleDropped();
+                Collections.sort(added);
+
+                for (int i = 0; i < added.size(); i++) {
+                    if (i < added.size() - 1) {
+                        if (added.get(i).c1Id != added.get(i + 1).c1Id) {
+                            I_GetConceptData cb = Terms.get().getConcept(added.get(i).c1Id);
+                            conceptListModel.addElement(cb);
+                        }
+                    } else {
+                        I_GetConceptData cb = Terms.get().getConcept(added.get(i).c1Id);
+                        conceptListModel.addElement(cb);
+                    }
+                }
+
+            } catch (Exception ex) {
+                AceLog.getAppLog().alertAndLogException(ex);
+            }
+        }
+    }
+
     private Object[][] getTableData(ArrayList<SnoRel> srl) throws TerminologyException, IOException {
         int totalCol = 1;
         int totalRows = srl.size();
@@ -236,5 +378,4 @@ public class DiffPathPanel extends JPanel {
 
         return tableStrings;
     }
-
 }
