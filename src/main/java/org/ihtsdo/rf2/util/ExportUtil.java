7a0a1adfd8feeb82b85e89d46c785b2000fc4ea3
arodriguez <arodriguez>
2012-03-25 17:55:48 +0000

    Debug sentences inserted

--- src/main/java/org/ihtsdo/rf2/util/ExportUtil.java
+++ src/main/java/org/ihtsdo/rf2/util/ExportUtil.java
@@ -1264,11 +1264,19 @@ public class ExportUtil {
 		try {
 			IdAssignmentImpl idGen = getIdGeneratorClient( config);
 			snomedId = idGen.getSNOMEDID(uuid);
+
+			if( uuid.toString().equals("1018284d-0267-4fd2-b3e0-88997de7d29a")){
+				System.out.println("=====concept 450598001 getSnomedId===" + snomedId + "****");
+			}
 			if(snomedId!=null && !snomedId.equals("") ){
 				return snomedId;
 			}
 
 			snomedId = idGen.createSNOMEDID(uuid, parentSnomedId);
+
+			if( uuid.toString().equals("1018284d-0267-4fd2-b3e0-88997de7d29a")){
+				System.out.println("=====concept 450598001 parent:" + parentSnomedId + " createSNOMEDID===" + snomedId + "****");
+			}
 			logger.info("===SnomedId Created: " + snomedId);
 		} catch (Exception cE) {
 			logger.error("Message : SnomedId creation error for UUID :" + uuid, cE);
