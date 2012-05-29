2b1370be53b61feff80add26d0a91d6b3b268780
tireland <tireland>
2012-05-29 14:50:34 +0000

    Fixed bug in which subset exporteer was generating concept type sctid (10) for refsets (13).

--- wb-mojo/src/main/java/org/ihtsdo/mojo/mojo/refset/SubsetExport.java
+++ wb-mojo/src/main/java/org/ihtsdo/mojo/mojo/refset/SubsetExport.java
@@ -142,6 +142,17 @@ public class SubsetExport extends AbstractMojo implements I_ProcessConcepts {
      * @parameter
      */
     String FILE_DELIMITER = "\t";
+    
+    /**
+     * Used to group released subset member files into catalogues e.g. Pathology.
+     * 
+     * @required
+     * @parameter
+     */
+    
+    String subsetCatalogue ="";
+    
+    
 
     private I_TermFactory tf = Terms.get();
 
@@ -318,9 +329,11 @@ public class SubsetExport extends AbstractMojo implements I_ProcessConcepts {
         }
 
         BufferedWriter subsetIndexFileWriter = writerMap.get("INDEX");
+        //TODO create a refset grouping catalogue e.g. all pathology refsets ?
+       // would requier a new parameter called refsetCatalogue 
         if (subsetIndexFileWriter == null) {
             subsetIndexFileWriter =
-                    new BufferedWriter(new FileWriter(new File(subsetOutputDirectory, "sct_subsets_" + countryCode
+                    new BufferedWriter(new FileWriter(new File(subsetOutputDirectory, "der1_Subsets_"+ subsetCatalogue + countryCode
                         + "_" + releaseVersion + ".txt")));
             subsetIndexFileWriter.write("SUBSETID" + FILE_DELIMITER + "SUBSETORIGINALID" + FILE_DELIMITER
                 + "SUBSETVERSION" + FILE_DELIMITER + "SUBSETNAME" + FILE_DELIMITER + "SUBSETTYPE" + FILE_DELIMITER
@@ -337,7 +350,7 @@ public class SubsetExport extends AbstractMojo implements I_ProcessConcepts {
 
             refsetName = refsetName.replace("/", "-");
             refsetName = refsetName.replace("'", "_");
-            
+            refsetName = refsetName.replace(" ", "");
             // TODO strip out spaces e.g. "my refset" becomes "myrefset"
             
             String refsetStatus=""; 
@@ -349,7 +362,7 @@ public class SubsetExport extends AbstractMojo implements I_ProcessConcepts {
             File subsetFile = getExportFile(refsetId);
             if (subsetFile == null) {
                 subsetFile =
-                        new File(subsetOutputDirectory, refsetStatus + "der1_SubsetMembers_" + refsetName + "_"+ countryCode+namespace
+                        new File(subsetOutputDirectory, currentSpec.status + "der1_SubsetMembers_" + refsetName + "_"+ countryCode+namespace
                            + "_" + releaseVersion + ".txt");
             } else {
                 subsetFile = new File(subsetOutputDirectory, subsetFile.getName());
@@ -439,10 +452,14 @@ public class SubsetExport extends AbstractMojo implements I_ProcessConcepts {
                     currentLine = storedSubsetMemberFileReader.readLine();
                 }
                 storedSubsetMemberFileReader.close();
-                if (previousMemberUuids.equals(memberUuids)) {
-                    setSubsetId(storedSubsetId);
-                    return "" + storedSubsetVersion;
-                } else {
+                
+                //according to Chris Morris we should always increment when exporting
+                // regardless of whether the content has changed. 
+                
+//                if (previousMemberUuids.equals(memberUuids)) {
+//                    setSubsetId(storedSubsetId);
+//                    return "" + storedSubsetVersion;
+//                } else {
                     int subsetVersion = storedSubsetVersion + 1;
                     long subsetId =
                             Long.parseLong(refsetType.getRefsetHandler().generateNewSctId(refsetId, subsetVersion,
@@ -450,16 +467,27 @@ public class SubsetExport extends AbstractMojo implements I_ProcessConcepts {
                     setSubsetId(subsetId);
                     updateStoredSubsetMemberFile(storedSubsetMemberFile, subsetId, subsetVersion);
                     return "" + subsetVersion;
-                }
+             //   }
             } else {
                 // the subset hasn't be released before. Use the SNOMED ID
                 // attached to the refset concept as the subset
                 // ID. Generate the member file for use in the next release.
+            	// If no SNOMED ID is associated with the refset create a new one. 
                 try {
-
-                    long subsetId =
+                	long subsetId;
+                	String snomedId = refsetType.getRefsetHandler().getSnomedIntegerId(tf, refsetId);
+                	  if (snomedId != null) {
+                		  
+                	  
+                	 subsetId =
                             Long.parseLong(refsetType.getRefsetHandler().toId(tf, refsetId, true, namespace, project));
-
+                	  }
+                	  else {
+                		  subsetId =
+                                  Long.parseLong(refsetType.getRefsetHandler().generateNewSctId(refsetId, 1,
+                                      namespace, project));
+                		  
+					}
                     setSubsetId(subsetId);
                     updateStoredSubsetMemberFile(storedSubsetMemberFile, subsetId, currentSpec.subsetVersion);
                     return "" + currentSpec.subsetVersion;
