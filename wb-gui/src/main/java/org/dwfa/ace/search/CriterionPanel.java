5fd1cb1742c71c7b9b2136e68360fc8ee11bee23
vmanukyan <vmanukyan>
2012-06-28 20:24:47 +0000

    Search panel ui fixed

--- wb-gui/src/main/java/org/dwfa/ace/search/CriterionPanel.java
+++ wb-gui/src/main/java/org/dwfa/ace/search/CriterionPanel.java
@@ -16,9 +16,11 @@
  */
 package org.dwfa.ace.search;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -54,247 +56,249 @@ import org.dwfa.bpa.gui.TaskPanel.EditorGlue;
 
 public class CriterionPanel extends JPanel {
 
-    public I_TestSearchResults bean;
+	public I_TestSearchResults bean;
 
-    public class CriterionListener implements ActionListener {
+	public class CriterionListener implements ActionListener {
 
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            try {
-                editorPanel.removeAll();
-                editorPanel.setLayout(new GridBagLayout());
-                GridBagConstraints gbc = new GridBagConstraints();
-                gbc.anchor = GridBagConstraints.WEST;
-                gbc.fill = GridBagConstraints.HORIZONTAL;
-                gbc.gridx = 0;
-                gbc.gridy = 0;
-                gbc.gridwidth = 1;
-                gbc.gridheight = 1;
-                gbc.weightx = 1.0;
-                gbc.weighty = 0;
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			try {
+				editorPanel.removeAll();
+				editorPanel.setLayout(new GridBagLayout());
+				((GridBagLayout) editorPanel.getLayout()).columnWidths = new int[] { 0, 0, 0, 0, 0 };
+				((GridBagLayout) editorPanel.getLayout()).rowHeights = new int[] { 25, 0, 0, 0, 0 };
+				((GridBagLayout) editorPanel.getLayout()).columnWeights = new double[] { 0.0, 1.0, 0.0, 1.0};
+				((GridBagLayout) editorPanel.getLayout()).rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 1.0E-4 };
+				GridBagConstraints gbc = new GridBagConstraints();
+				gbc.anchor = GridBagConstraints.CENTER;
+				gbc.fill = GridBagConstraints.BOTH;
+				gbc.gridx = 0;
+				gbc.gridy = 0;
+				gbc.gridwidth = 1;
+				gbc.gridheight = 1;
+				gbc.weightx = 0.0;
+				gbc.weighty = 0.0;
+				gbc.ipadx = 0;
+				gbc.ipady = 0;
+				gbc.insets = new Insets(0, 0, 5, 5);
 
-                String menuItem = (String) criterionCombo.getSelectedItem();
-                BeanInfo searchInfo = menuInfoMap.get(menuItem);
-                bean = menuBeanMap.get(menuItem);
+				String menuItem = (String) criterionCombo.getSelectedItem();
+				BeanInfo searchInfo = menuInfoMap.get(menuItem);
+				bean = menuBeanMap.get(menuItem);
+				int count = 1;
+				for (PropertyDescriptor pd : searchInfo.getPropertyDescriptors()) {
+					JLabel editorLabel = new JLabel(getName(pd.getDisplayName()));
+					editorLabel.setForeground(new Color(9, 100, 10));
+					editorLabel.setToolTipText(pd.getShortDescription());
+					editorPanel.add(editorLabel, gbc);
+					gbc.gridx++;
+					PropertyEditor editor = pd.createPropertyEditor(bean);
+					if (AbstractButton.class.isAssignableFrom(editor.getCustomEditor().getClass())) {
+						//gbc.weightx = 0.0;
+					}
 
-                for (PropertyDescriptor pd : searchInfo.getPropertyDescriptors()) {
-                    gbc.weightx = 0.0;
-                    JLabel editorLabel = new JLabel(pd.getDisplayName());
-                    editorLabel.setBorder(BorderFactory.createEmptyBorder(0, 5, 2, 5));
-                    editorLabel.setToolTipText(pd.getShortDescription());
-                    editorPanel.add(editorLabel, gbc);
-                    gbc.weightx = 1.0;
-                    gbc.gridx++;
-                    PropertyEditor editor = pd.createPropertyEditor(bean);
-                    if (AbstractButton.class.isAssignableFrom(editor.getCustomEditor().getClass())) {
-                        gbc.weightx = 0.0;
-                    }
+					Method readMethod = pd.getReadMethod();
+					Object value = readMethod.invoke(bean, (Object[]) null);
+					editor.setValue(value);
 
-                    Method readMethod = pd.getReadMethod();
-                    Object value = readMethod.invoke(bean, (Object[]) null);
-                    editor.setValue(value);
+					editor.addPropertyChangeListener(new EditorGlue(editor, pd.getWriteMethod(), bean));
+					JComponent editorComponent = (JComponent) editor.getCustomEditor();
+					editorComponent.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
+					editorPanel.add(editorComponent, gbc);
 
-                    editor.addPropertyChangeListener(new EditorGlue(editor, pd.getWriteMethod(), bean));
-                    JComponent editorComponent = (JComponent) editor.getCustomEditor();
-                    editorComponent.setBorder(BorderFactory.createEmptyBorder(2, 5, 0, 8));
-                    editorPanel.add(editorComponent, gbc);
+					gbc.gridx++;
+					if (count % 2 == 0) {
+						gbc.gridy++;
+						gbc.gridx = 0;
+					}
+					count++;
+				}
+				editorPanel.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 10));
+				editorPanel.revalidate();
 
-                    gbc.gridx++;
-                }
-                editorPanel.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 10));
-                editorPanel.invalidate();
-                editorPanel.validate();
-                editorPanel.doLayout();
+				CriterionPanel.this.revalidate();
+			} catch (Exception ex) {
+				AceLog.getAppLog().alertAndLogException(ex);
+			}
+		}
 
-                CriterionPanel.this.invalidate();
-                CriterionPanel.this.validate();
-                CriterionPanel.this.doLayout();
-            } catch (Exception ex) {
-                AceLog.getAppLog().alertAndLogException(ex);
-            }
-        }
+	}
+	
+	private String getName(String tagHeader) {
+		return tagHeader.replaceAll("\\<[^>]*>", "");
+	}
 
-    }
-
-    /**
+	/**
      * 
      */
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    List<I_TestSearchResults> criterionOptions;
+	List<I_TestSearchResults> criterionOptions;
 
-    Map<I_TestSearchResults, BeanInfo> criterionMap = new HashMap<I_TestSearchResults, BeanInfo>();
+	Map<I_TestSearchResults, BeanInfo> criterionMap = new HashMap<I_TestSearchResults, BeanInfo>();
 
-    protected Map<String, BeanInfo> menuInfoMap = new HashMap<String, BeanInfo>();
+	protected Map<String, BeanInfo> menuInfoMap = new HashMap<String, BeanInfo>();
 
-    Map<String, I_TestSearchResults> menuBeanMap = new HashMap<String, I_TestSearchResults>();
+	Map<String, I_TestSearchResults> menuBeanMap = new HashMap<String, I_TestSearchResults>();
 
-    protected List<String> comboItems = new ArrayList<String>();
+	protected List<String> comboItems = new ArrayList<String>();
 
-    protected JPanel editorPanel = new JPanel();
+	protected JPanel editorPanel = new JPanel();
 
-    protected JComboBox criterionCombo;
+	protected JComboBox criterionCombo;
 
-    public CriterionPanel(I_MakeCriterionPanel searchPanel, I_TestSearchResults beanToSet)
-            throws ClassNotFoundException, InstantiationException, IllegalAccessException {
-        this(searchPanel, beanToSet, new ArrayList<I_TestSearchResults>());
+	public CriterionPanel(I_MakeCriterionPanel searchPanel, I_TestSearchResults beanToSet) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+		this(searchPanel, beanToSet, new ArrayList<I_TestSearchResults>());
 
-    }
+	}
 
-    public CriterionPanel(boolean dumbVal) {
-    	super(new GridBagLayout());
-    }
-    
-    public CriterionPanel(I_MakeCriterionPanel searchPanel, I_TestSearchResults beanToSet,
-            List<I_TestSearchResults> criterionOptions) throws ClassNotFoundException, InstantiationException,
-            IllegalAccessException {
-        super(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.anchor = GridBagConstraints.WEST;
-        gbc.fill = GridBagConstraints.NONE;
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.gridwidth = 1;
-        gbc.gridheight = 1;
-        gbc.weightx = 0;
-        gbc.weighty = 0;
+	public CriterionPanel(boolean dumbVal) {
+		super(new GridBagLayout());
+	}
 
-        JButton addButton = new JButton(new ImageIcon(ACE.class.getResource("/16x16/plain/add2.png")));
-        addButton.setIconTextGap(0);
-        addButton.addActionListener(new AddCriterion(searchPanel));
-        add(addButton, gbc);
-        addButton.setToolTipText("add a new AND search clause to end of query");
-        gbc.gridx++;
-        JButton removeButton = new JButton(new ImageIcon(ACE.class.getResource("/16x16/plain/delete2.png")));
-        removeButton.addActionListener(new RemoveCriterion(searchPanel, this));
-        removeButton.setIconTextGap(0);
-        add(removeButton, gbc);
-        removeButton.setToolTipText("remove this search clause");
+	public CriterionPanel(I_MakeCriterionPanel searchPanel, I_TestSearchResults beanToSet, List<I_TestSearchResults> criterionOptions) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+		super(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.anchor = GridBagConstraints.WEST;
+		gbc.fill = GridBagConstraints.NONE;
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.gridwidth = 1;
+		gbc.gridheight = 1;
+		gbc.weightx = 0;
+		gbc.weighty = 0;
 
-        gbc.gridx++;
+		JButton addButton = new JButton(new ImageIcon(ACE.class.getResource("/16x16/plain/add2.png")));
+		addButton.setIconTextGap(0);
+		addButton.addActionListener(new AddCriterion(searchPanel));
+		add(addButton, gbc);
+		addButton.setToolTipText("add a new AND search clause to end of query");
+		gbc.gridx++;
+		JButton removeButton = new JButton(new ImageIcon(ACE.class.getResource("/16x16/plain/delete2.png")));
+		removeButton.addActionListener(new RemoveCriterion(searchPanel, this));
+		removeButton.setIconTextGap(0);
+		add(removeButton, gbc);
+		removeButton.setToolTipText("remove this search clause");
 
-        setupCriterionOptions(criterionOptions);
+		gbc.gridx++;
 
-        criterionCombo = new JComboBox(comboItems.toArray()) {
-            /**
+		setupCriterionOptions(criterionOptions);
+
+		criterionCombo = new JComboBox(comboItems.toArray()) {
+			/**
 			 * 
 			 */
-            private static final long serialVersionUID = 1L;
+			private static final long serialVersionUID = 1L;
 
-            @Override
-            public void setSize(Dimension d) {
-                d.width = getMinimumSize().width;
-                super.setSize(d);
-            }
+			@Override
+			public void setSize(Dimension d) {
+				d.width = getMinimumSize().width;
+				super.setSize(d);
+			}
 
-            @Override
-            public void setSize(int width, int height) {
-                super.setSize(getMinimumSize().width, height);
-            }
+			@Override
+			public void setSize(int width, int height) {
+				super.setSize(getMinimumSize().width, height);
+			}
 
-            @Override
-            public void setBounds(int x, int y, int width, int height) {
-                super.setBounds(x, y, getMinimumSize().width, height);
-            }
+			@Override
+			public void setBounds(int x, int y, int width, int height) {
+				super.setBounds(x, y, getMinimumSize().width, height);
+			}
 
-            @Override
-            public void setBounds(Rectangle r) {
-                r.width = getMinimumSize().width;
-                super.setBounds(r);
-            }
+			@Override
+			public void setBounds(Rectangle r) {
+				r.width = getMinimumSize().width;
+				super.setBounds(r);
+			}
 
-            @Override
-            public Dimension getPreferredSize() {
-                Dimension d = super.getPreferredSize();
-                d.width = getMinimumSize().width;
-                return d;
-            }
+			@Override
+			public Dimension getPreferredSize() {
+				Dimension d = super.getPreferredSize();
+				d.width = getMinimumSize().width;
+				return d;
+			}
 
-        };
-        criterionCombo.setMinimumSize(new Dimension(175, 20));
-        gbc.fill = GridBagConstraints.BOTH;
-        add(criterionCombo, gbc);
-        criterionCombo.addActionListener(new CriterionListener());
-        gbc.fill = GridBagConstraints.NONE;
-        if (beanToSet != null) {
-            for (String comboItem : comboItems) {
-                I_TestSearchResults menuBean = menuBeanMap.get(comboItem);
-                if (menuBean.getClass().equals(beanToSet.getClass())) {
-                    menuBeanMap.put(comboItem, beanToSet);
-                    BeanInfo info = criterionMap.remove(menuBean);
-                    criterionMap.put(beanToSet, info);
+		};
+		criterionCombo.setMinimumSize(new Dimension(175, 20));
+		gbc.fill = GridBagConstraints.BOTH;
+		add(criterionCombo, gbc);
+		criterionCombo.addActionListener(new CriterionListener());
+		gbc.fill = GridBagConstraints.NONE;
+		if (beanToSet != null) {
+			for (String comboItem : comboItems) {
+				I_TestSearchResults menuBean = menuBeanMap.get(comboItem);
+				if (menuBean.getClass().equals(beanToSet.getClass())) {
+					menuBeanMap.put(comboItem, beanToSet);
+					BeanInfo info = criterionMap.remove(menuBean);
+					criterionMap.put(beanToSet, info);
 
-                    int index = criterionOptions.indexOf(menuBean);
-                    criterionOptions.set(index, beanToSet);
+					int index = criterionOptions.indexOf(menuBean);
+					criterionOptions.set(index, beanToSet);
 
-                    criterionCombo.setSelectedItem(comboItem);
-                    break;
-                }
-            }
-        } else {
-            criterionCombo.setSelectedIndex(0);
-        }
+					criterionCombo.setSelectedItem(comboItem);
+					break;
+				}
+			}
+		} else {
+			criterionCombo.setSelectedIndex(0);
+		}
 
-        gbc.weightx = 1;
-        gbc.fill = GridBagConstraints.BOTH;
-        gbc.gridx++;
-        add(editorPanel, gbc);
-    }
+		gbc.weightx = 1;
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.gridx++;
+		add(editorPanel, gbc);
+	}
 
-    public CriterionPanel(I_MakeCriterionPanel searchPanel) throws ClassNotFoundException, InstantiationException,
-            IllegalAccessException {
-        this(searchPanel, null);
-    }
+	public CriterionPanel(I_MakeCriterionPanel searchPanel) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+		this(searchPanel, null);
+	}
 
-    @SuppressWarnings("unchecked")
-    private void setupCriterionOptions(List<I_TestSearchResults> criterionOptions) throws ClassNotFoundException,
-            InstantiationException, IllegalAccessException {
-        File searchPluginFolder = new File("search");
-        this.criterionOptions = new ArrayList<I_TestSearchResults>();
-        if (criterionOptions == null || criterionOptions.isEmpty()) {
-            File[] searchPlugins = searchPluginFolder.listFiles(new FilenameFilter() {
-                @Override
-                public boolean accept(File dir, String name) {
-                    return name.endsWith(".task");
-                }
-            });
-            if (searchPlugins != null) {
-                for (File plugin : searchPlugins) {
-                    try {
-                        ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(new FileInputStream(
-                            plugin)));
-                        Object pluginObj = ois.readObject();
-                        ois.close();
-                        if (I_TestSearchResults.class.isAssignableFrom(pluginObj.getClass())) {
-                            criterionOptions.add((I_TestSearchResults) pluginObj);
-                        }
-                    } catch (IOException ex) {
-                        AceLog.getAppLog().alertAndLog(Level.WARNING, "Processing: " + plugin.getAbsolutePath(), ex);
-                    } catch (ClassNotFoundException ex) {
-                        AceLog.getAppLog().alertAndLog(Level.WARNING, "Processing: " + plugin.getAbsolutePath(), ex);
-                    }
-                }
-            } else {
-                AceLog.getAppLog().alertAndLogException(this,
-                    new Exception("No search plugins in folder: " + searchPluginFolder.getAbsolutePath()));
-            }
-        }
-        for (I_TestSearchResults optionBean : criterionOptions) {
-            try {
-                String searchInfoClassName = optionBean.getClass().getName() + "SearchInfo";
-                Class<BeanInfo> searchInfoClass = (Class<BeanInfo>) optionBean.getClass().getClassLoader().loadClass(
-                    searchInfoClassName);
-                BeanInfo searchInfo = searchInfoClass.newInstance();
-                comboItems.add(searchInfo.getBeanDescriptor().getDisplayName());
-                menuInfoMap.put(searchInfo.getBeanDescriptor().getDisplayName(), searchInfo);
-                menuBeanMap.put(searchInfo.getBeanDescriptor().getDisplayName(), optionBean);
-            } catch (Exception ex) {
-                AceLog.getAppLog().alertAndLogException(ex);
-            }
-        }
-    }
+	@SuppressWarnings("unchecked")
+	private void setupCriterionOptions(List<I_TestSearchResults> criterionOptions) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+		File searchPluginFolder = new File("search");
+		this.criterionOptions = new ArrayList<I_TestSearchResults>();
+		if (criterionOptions == null || criterionOptions.isEmpty()) {
+			File[] searchPlugins = searchPluginFolder.listFiles(new FilenameFilter() {
+				@Override
+				public boolean accept(File dir, String name) {
+					return name.endsWith(".task");
+				}
+			});
+			if (searchPlugins != null) {
+				for (File plugin : searchPlugins) {
+					try {
+						ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(new FileInputStream(plugin)));
+						Object pluginObj = ois.readObject();
+						ois.close();
+						if (I_TestSearchResults.class.isAssignableFrom(pluginObj.getClass())) {
+							criterionOptions.add((I_TestSearchResults) pluginObj);
+						}
+					} catch (IOException ex) {
+						AceLog.getAppLog().alertAndLog(Level.WARNING, "Processing: " + plugin.getAbsolutePath(), ex);
+					} catch (ClassNotFoundException ex) {
+						AceLog.getAppLog().alertAndLog(Level.WARNING, "Processing: " + plugin.getAbsolutePath(), ex);
+					}
+				}
+			} else {
+				AceLog.getAppLog().alertAndLogException(this, new Exception("No search plugins in folder: " + searchPluginFolder.getAbsolutePath()));
+			}
+		}
+		for (I_TestSearchResults optionBean : criterionOptions) {
+			try {
+				String searchInfoClassName = optionBean.getClass().getName() + "SearchInfo";
+				Class<BeanInfo> searchInfoClass = (Class<BeanInfo>) optionBean.getClass().getClassLoader().loadClass(searchInfoClassName);
+				BeanInfo searchInfo = searchInfoClass.newInstance();
+				comboItems.add(searchInfo.getBeanDescriptor().getDisplayName());
+				menuInfoMap.put(searchInfo.getBeanDescriptor().getDisplayName(), searchInfo);
+				menuBeanMap.put(searchInfo.getBeanDescriptor().getDisplayName(), optionBean);
+			} catch (Exception ex) {
+				AceLog.getAppLog().alertAndLogException(ex);
+			}
+		}
+	}
 
-    public I_TestSearchResults getBean() {
-        return bean;
-    }
+	public I_TestSearchResults getBean() {
+		return bean;
+	}
 }
