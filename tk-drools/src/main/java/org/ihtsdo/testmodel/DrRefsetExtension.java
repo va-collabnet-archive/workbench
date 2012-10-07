faee7b95629a63e79c267d290fb1361841074893
alopez <alopez>
2012-10-07 22:38:03 +0000

    Fixing null pointer exception in toString

--- tk-drools/src/main/java/org/ihtsdo/testmodel/DrRefsetExtension.java
+++ tk-drools/src/main/java/org/ihtsdo/testmodel/DrRefsetExtension.java
@@ -7,190 +7,191 @@ import java.util.UUID;
 import org.ihtsdo.tk.Ts;
 import org.ihtsdo.tk.api.concept.ConceptChronicleBI;
 
-public class DrRefsetExtension extends DrComponent{
-	private String primordialUuid;
+public class DrRefsetExtension extends DrComponent {
 
-	private String refsetUuid;
-	private String componentUuid;
+    private String primordialUuid;
+    private String refsetUuid;
+    private String componentUuid;
+    private String c1Uuid;
+    private String c2Uuid;
+    private String c3Uuid;
+    private String strValue;
+    private boolean booleanValue;
+    private float floatValue;
+    private int intValue;
+    private long longValue;
+    private RefsetType refsetType;
+    private List<DrIdentifier> identifiers;
 
-	private String c1Uuid;
-	private String c2Uuid;
-	private String c3Uuid;
-	private String strValue;
-	private boolean booleanValue;
-	private float floatValue;
-	private int intValue;
-	private long longValue;
-	private RefsetType refsetType;
+    //Inferred properties
+    // none yet
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer("");
+        try {
+            sb.append(" String Value: " + strValue + "(" + primordialUuid + "),");
+            sb.append(" Boolean Value: " + booleanValue + ",");
+            sb.append(" Float Value: " + floatValue + ",");
+            sb.append(" Int Value: " + intValue + ",");
+            sb.append(" Long Value: " + longValue + ",");
+            sb.append(" Refset Type: " + refsetType + ",");
 
-	private List<DrIdentifier> identifiers;
-	
-	//Inferred properties
-	// none yet
+            try {
+                ConceptChronicleBI refset = Ts.get().getConcept(UUID.fromString(refsetUuid));
+                sb.append(" Refset: " + refset + " (" + refsetUuid + "),");
+            } catch (IllegalArgumentException ex) {
+            }
 
-	@Override
-	public String toString() {
-		StringBuffer sb = new StringBuffer("");
-		try {
-			sb.append(" String Value: " + strValue + "(" + primordialUuid +"),");
-			sb.append(" Boolean Value: " + booleanValue + ",");
-			sb.append(" Float Value: " + floatValue + ",");
-			sb.append(" Int Value: " + intValue + ",");
-			sb.append(" Long Value: " + longValue + ",");
-			sb.append(" Refset Type: " + refsetType + ",");
-			
-			try {
-				ConceptChronicleBI refset = Ts.get().getConcept(UUID.fromString(refsetUuid));
-				sb.append(" Refset: " + refset + " (" + refsetUuid + "),");
-			} catch (IllegalArgumentException ex) {
-			}
+            sb.append(" C1: ");
+            if (c1Uuid != null) {
+                try {
+                    ConceptChronicleBI c1 = Ts.get().getConcept(UUID.fromString(c1Uuid));
+                    sb.append(c1 + " (" + c1Uuid + "),");
+                } catch (IllegalArgumentException ex) {
+                }
+            }
 
-			try {
-				ConceptChronicleBI c1 = Ts.get().getConcept(UUID.fromString(c1Uuid));
-				sb.append(" C1: " + c1 + " (" + c1Uuid + "),");
-			} catch (IllegalArgumentException ex) {
-			}
-			
-			try {
-				ConceptChronicleBI c2 = Ts.get().getConcept(UUID.fromString(c2Uuid));
-				sb.append(" C2: " + c2 + " (" + c2Uuid + "),");
-			} catch (IllegalArgumentException ex) {
-			}
-			
-			try {
-				ConceptChronicleBI c3 = Ts.get().getConcept(UUID.fromString(c3Uuid));
-				sb.append(" C3: " + c3 + " (" + c3Uuid + "),");
-			} catch (IllegalArgumentException ex) {
-			}
-			
-			try {
-				ConceptChronicleBI c1 = Ts.get().getConcept(UUID.fromString(c1Uuid));
-				sb.append(" C1: " + c1 + " (" + c1Uuid + "),");
-			} catch (IllegalArgumentException ex) {
-			}
-			sb.append(" DRCOMPONENT FIELDS: {" + super.toString() + "}, ");
-			sb.append("\nIdentifiers: [");
-			if (identifiers != null) {
-				for (DrIdentifier identifier : identifiers) {
-					int i = 0;
-					sb.append(identifier.toString() + (i == identifiers.size() - 1 ? "" : ","));
-					i++;
-				}
-			}
-			sb.append("]");
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return sb.toString();
-	}
-	
-	public DrRefsetExtension() {
-		identifiers = new ArrayList<DrIdentifier>();
-	}
+            sb.append(" C2: ");
+            if (c2Uuid != null) {
+                try {
+                    ConceptChronicleBI c1 = Ts.get().getConcept(UUID.fromString(c2Uuid));
+                    sb.append(c1 + " (" + c2Uuid + "),");
+                } catch (IllegalArgumentException ex) {
+                }
+            }
+            
+            sb.append(" C3: ");
+            if (c3Uuid != null) {
+                try {
+                    ConceptChronicleBI c1 = Ts.get().getConcept(UUID.fromString(c3Uuid));
+                    sb.append(c1 + " (" + c3Uuid + "),");
+                } catch (IllegalArgumentException ex) {
+                }
+            }
+            
 
-	public String getPrimordialUuid() {
-		return primordialUuid;
-	}
+            sb.append(" DRCOMPONENT FIELDS: {" + super.toString() + "}, ");
+            sb.append("\nIdentifiers: [");
+            if (identifiers != null) {
+                for (DrIdentifier identifier : identifiers) {
+                    int i = 0;
+                    sb.append(identifier.toString() + (i == identifiers.size() - 1 ? "" : ","));
+                    i++;
+                }
+            }
+            sb.append("]");
 
-	public void setPrimordialUuid(String primordialUuid) {
-		this.primordialUuid = primordialUuid;
-	}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
+    }
 
-	public String getC1Uuid() {
-		return c1Uuid;
-	}
+    public DrRefsetExtension() {
+        identifiers = new ArrayList<DrIdentifier>();
+    }
 
-	public void setC1Uuid(String uuid) {
-		c1Uuid = uuid;
-	}
+    public String getPrimordialUuid() {
+        return primordialUuid;
+    }
 
-	public String getC2Uuid() {
-		return c2Uuid;
-	}
+    public void setPrimordialUuid(String primordialUuid) {
+        this.primordialUuid = primordialUuid;
+    }
 
-	public void setC2Uuid(String uuid) {
-		c2Uuid = uuid;
-	}
+    public String getC1Uuid() {
+        return c1Uuid;
+    }
 
-	public String getC3Uuid() {
-		return c3Uuid;
-	}
+    public void setC1Uuid(String uuid) {
+        c1Uuid = uuid;
+    }
 
-	public void setC3Uuid(String uuid) {
-		c3Uuid = uuid;
-	}
+    public String getC2Uuid() {
+        return c2Uuid;
+    }
 
-	public String getStrValue() {
-		return strValue;
-	}
+    public void setC2Uuid(String uuid) {
+        c2Uuid = uuid;
+    }
 
-	public void setStrValue(String strValue) {
-		this.strValue = strValue;
-	}
+    public String getC3Uuid() {
+        return c3Uuid;
+    }
 
-	public boolean isBooleanValue() {
-		return booleanValue;
-	}
+    public void setC3Uuid(String uuid) {
+        c3Uuid = uuid;
+    }
 
-	public void setBooleanValue(boolean booleanValue) {
-		this.booleanValue = booleanValue;
-	}
+    public String getStrValue() {
+        return strValue;
+    }
 
-	public float getFloatValue() {
-		return floatValue;
-	}
+    public void setStrValue(String strValue) {
+        this.strValue = strValue;
+    }
 
-	public void setFloatValue(float floatValue) {
-		this.floatValue = floatValue;
-	}
+    public boolean isBooleanValue() {
+        return booleanValue;
+    }
 
-	public int getIntValue() {
-		return intValue;
-	}
+    public void setBooleanValue(boolean booleanValue) {
+        this.booleanValue = booleanValue;
+    }
 
-	public void setIntValue(int intValue) {
-		this.intValue = intValue;
-	}
+    public float getFloatValue() {
+        return floatValue;
+    }
 
-	public long getLongValue() {
-		return longValue;
-	}
+    public void setFloatValue(float floatValue) {
+        this.floatValue = floatValue;
+    }
 
-	public void setLongValue(long longValue) {
-		this.longValue = longValue;
-	}
+    public int getIntValue() {
+        return intValue;
+    }
 
-	public List<DrIdentifier> getIdentifiers() {
-		return identifiers;
-	}
+    public void setIntValue(int intValue) {
+        this.intValue = intValue;
+    }
 
-	public void setIdentifiers(List<DrIdentifier> identifiers) {
-		this.identifiers = identifiers;
-	}
+    public long getLongValue() {
+        return longValue;
+    }
 
-	public RefsetType getRefsetType() {
-		return refsetType;
-	}
+    public void setLongValue(long longValue) {
+        this.longValue = longValue;
+    }
 
-	public void setRefsetType(RefsetType refsetType) {
-		this.refsetType = refsetType;
-	}
+    public List<DrIdentifier> getIdentifiers() {
+        return identifiers;
+    }
 
-	public String getRefsetUuid() {
-		return refsetUuid;
-	}
+    public void setIdentifiers(List<DrIdentifier> identifiers) {
+        this.identifiers = identifiers;
+    }
 
-	public void setRefsetUuid(String refsetUuid) {
-		this.refsetUuid = refsetUuid;
-	}
+    public RefsetType getRefsetType() {
+        return refsetType;
+    }
 
-	public String getComponentUuid() {
-		return componentUuid;
-	}
+    public void setRefsetType(RefsetType refsetType) {
+        this.refsetType = refsetType;
+    }
 
-	public void setComponentUuid(String componentUuid) {
-		this.componentUuid = componentUuid;
-	}
+    public String getRefsetUuid() {
+        return refsetUuid;
+    }
 
+    public void setRefsetUuid(String refsetUuid) {
+        this.refsetUuid = refsetUuid;
+    }
+
+    public String getComponentUuid() {
+        return componentUuid;
+    }
+
+    public void setComponentUuid(String componentUuid) {
+        this.componentUuid = componentUuid;
+    }
 }
