0836ed0884603dc687c011cdb04ad8b923900037
marc <marc>
2012-10-18 10:36:45 +0000

    updated create union set for relationship to refset parent to be correctly constructed.

--- wb-gui/src/main/java/org/ihtsdo/batch/BatchActionTaskLogicUnionCreateUI.java
+++ wb-gui/src/main/java/org/ihtsdo/batch/BatchActionTaskLogicUnionCreateUI.java
@@ -280,11 +280,19 @@ public class BatchActionTaskLogicUnionCreateUI extends javax.swing.JPanel
                     preferred_description_type, config);
 
             // ADD RELATIONSHIP
+            I_GetConceptData roleType = tf.getConcept(
+                    TermAux.IS_A.getLenient().getNid());
             I_GetConceptData parent = tf.getConcept(
                     DescriptionLogic.UNION_SETS_REFSET.getLenient().getNid());
-            I_RelVersioned rel = tf.newRelationship(parentRelUuid, newConcept, config);
-            rel.setTypeNid(TermAux.IS_A.getLenient().getNid());
-            rel.setC2Id(parent.getConceptNid());
+            I_RelVersioned rel = tf.newRelationship(parentRelUuid, // newRelUid
+                    newConcept, // concept
+                    roleType, // relType
+                    parent, // relDestination
+                    config.getDefaultRelationshipCharacteristic(), // relCharacteristic
+                    config.getDefaultStatus(), // relRefinability
+                    config.getDefaultStatus(), // relStatus
+                    0, // relGroup
+                    config);
 
             tf.addUncommitted(newConcept);
 
@@ -294,9 +302,11 @@ public class BatchActionTaskLogicUnionCreateUI extends javax.swing.JPanel
     }
 
     // Create or update union concept
-    private int createOrUpdateConceptSet(UUID unionSetConceptUuid, I_GetConceptData parent,
+    private int createOrUpdateConceptSet(UUID unionSetConceptUuid,
+            I_GetConceptData parent,
             ViewCoordinate vc)
-            throws IOException, InvalidCAB, ContradictionException, TerminologyException {
+            throws IOException, InvalidCAB, ContradictionException,
+            TerminologyException {
         int nid;
         // TEST FOR EXISTING UNION_SETS_REFSET SET
         // SCENARIO: EXISTS ... then make current
@@ -304,7 +314,8 @@ public class BatchActionTaskLogicUnionCreateUI extends javax.swing.JPanel
             ConceptVersionBI unionSetCB;
             unionSetCB = BatchActionTask.ts.getConceptVersion(vc, unionSetConceptUuid);
             // :SNOOWL: review added null, null)
-            ConceptAttributeAB cCab = new ConceptAttributeAB(unionSetConceptUuid, false, null, null);
+            ConceptAttributeAB cCab = new ConceptAttributeAB(unionSetConceptUuid,
+                    false, null, null);
             cCab.setCurrent();
             BatchActionTask.tsSnapshot.construct(cCab);
 
@@ -312,18 +323,19 @@ public class BatchActionTaskLogicUnionCreateUI extends javax.swing.JPanel
             UUID roleTypeUuid = Snomed.IS_A.getLenient().getPrimUuid();
             UUID destUuid;
             destUuid = parent.getPrimUuid();
-            RelationshipCAB rCab = new RelationshipCAB(unionSetConceptUuid, roleTypeUuid, destUuid, 0,
+            RelationshipCAB rCab = new RelationshipCAB(unionSetConceptUuid,
+                    roleTypeUuid, destUuid, 0,
                     TkRelationshipType.STATED_HIERARCHY);
             rCab.setCurrent();
             BatchActionTask.tsSnapshot.construct(rCab);
 
             // SET UNION_SETS_REFSET SET RELATIONSHIP TO CURRENT
             destUuid = DescriptionLogic.UNION_SETS_REFSET.getUuids()[0];
-            RelationshipCAB rCab2 = new RelationshipCAB(unionSetConceptUuid, roleTypeUuid, destUuid, 0,
+            RelationshipCAB rCab2 = new RelationshipCAB(unionSetConceptUuid,
+                    roleTypeUuid, destUuid, 0,
                     TkRelationshipType.STATED_HIERARCHY);
             rCab2.setCurrent();
             BatchActionTask.tsSnapshot.construct(rCab2);
-
 
             Ts.get().addUncommitted(unionSetCB);
 
@@ -357,10 +369,12 @@ public class BatchActionTaskLogicUnionCreateUI extends javax.swing.JPanel
 
             // CREATE CONCEPT
             UUID isa = Snomed.IS_A.getLenient().getPrimUuid();
-            ConceptCB newUnionSetCB = new ConceptCB(fsnStr, ptStr, LANG_CODE.EN, isa,
+            ConceptCB newUnionSetCB = new ConceptCB(fsnStr, ptStr, LANG_CODE.EN,
+                    isa,
                     parentUuids);
             newUnionSetCB.setComponentUuid(unionSetConceptUuid);
-            TerminologyBuilderBI tc = Ts.get().getTerminologyBuilder(config.getEditCoordinate(),
+            TerminologyBuilderBI tc = Ts.get().getTerminologyBuilder(config.
+                    getEditCoordinate(),
                     config.getViewCoordinate());
             ConceptChronicleBI newUnionSetConcept = tc.constructIfNotCurrent(newUnionSetCB);
 
@@ -396,7 +410,8 @@ public class BatchActionTaskLogicUnionCreateUI extends javax.swing.JPanel
     }
 
     // Handles US & GB Language Refsets
-    private void createBlueprintRefex(ConceptVersionBI cvbi) throws ContradictionException {
+    private void createBlueprintRefex(ConceptVersionBI cvbi)
+            throws ContradictionException {
         try {
             TerminologyBuilderBI tc = Ts.get().getTerminologyBuilder(
                     config.getEditCoordinate(), config.getViewCoordinate());
