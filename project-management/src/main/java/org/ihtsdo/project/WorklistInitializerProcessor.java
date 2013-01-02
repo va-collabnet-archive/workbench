e448c5a56da961031b99696a7dbacf757364af89
vmanukyan <vmanukyan>
2013-01-02 19:13:26 +0000

    handling null updater

--- project-management/src/main/java/org/ihtsdo/project/WorklistInitializerProcessor.java
+++ project-management/src/main/java/org/ihtsdo/project/WorklistInitializerProcessor.java
@@ -61,200 +61,207 @@ import org.ihtsdo.tk.spec.ValidationException;
  */
 public class WorklistInitializerProcessor implements ProcessUnfetchedConceptDataBI {
 
-    /**
-     * The members nid set.
-     */
-    NidBitSetBI membersNidSet;
-    /**
-     * The vc.
-     */
-    ViewCoordinate vc;
-    /**
-     * The prom ref.
-     */
-    PromotionAndAssignmentRefset promRef;
-    /**
-     * The active nid.
-     */
-    int activeNid;
-    /**
-     * The active uuid.
-     */
-    UUID activeUuid;
-    /**
-     * The inactive nid.
-     */
-    int inactiveNid;
-    /**
-     * The inactive uuid.
-     */
-    UUID inactiveUuid;
-    /**
-     * The updater.
-     */
-    ActivityUpdater updater;
-    /**
-     * The tc.
-     */
-    TerminologyBuilderBI tc;
-    /**
-     * The work list nid.
-     */
-    int workListNid;
-    /**
-     * The interpreter.
-     */
-    WorkflowInterpreter interpreter;
-    /**
-     * The user.
-     */
-    WfUser user;
-    /**
-     * The work list.
-     */
-    WorkList workList;
-    /**
-     * The user nid.
-     */
-    int userNid;
-    /**
-     * The assigned nid.
-     */
-    int assignedNid;
-    /**
-     * The ts.
-     */
-    TerminologyStoreDI ts;
+	/**
+	 * The members nid set.
+	 */
+	NidBitSetBI membersNidSet;
+	/**
+	 * The vc.
+	 */
+	ViewCoordinate vc;
+	/**
+	 * The prom ref.
+	 */
+	PromotionAndAssignmentRefset promRef;
+	/**
+	 * The active nid.
+	 */
+	int activeNid;
+	/**
+	 * The active uuid.
+	 */
+	UUID activeUuid;
+	/**
+	 * The inactive nid.
+	 */
+	int inactiveNid;
+	/**
+	 * The inactive uuid.
+	 */
+	UUID inactiveUuid;
+	/**
+	 * The updater.
+	 */
+	ActivityUpdater updater;
+	/**
+	 * The tc.
+	 */
+	TerminologyBuilderBI tc;
+	/**
+	 * The work list nid.
+	 */
+	int workListNid;
+	/**
+	 * The interpreter.
+	 */
+	WorkflowInterpreter interpreter;
+	/**
+	 * The user.
+	 */
+	WfUser user;
+	/**
+	 * The work list.
+	 */
+	WorkList workList;
+	/**
+	 * The user nid.
+	 */
+	int userNid;
+	/**
+	 * The assigned nid.
+	 */
+	int assignedNid;
+	/**
+	 * The ts.
+	 */
+	TerminologyStoreDI ts;
 
-    /**
-     * Instantiates a new worklist initializer processor.
-     *
-     * @param partition the partition
-     * @param workList the work list
-     * @param config the config
-     * @param updater the updater
-     */
-    public WorklistInitializerProcessor(Partition partition, WorkList workList,
-            I_ConfigAceFrame config, ActivityUpdater updater) {
-        super();
-        try {
-            vc = config.getViewCoordinate();
-            tc = Ts.get().getTerminologyBuilder(config.getEditCoordinate(),
-                    config.getViewCoordinate());
-            this.updater = updater;
-            this.workList = workList;
-            updater.startActivity();
-            workListNid = workList.getId();
-            promRef = workList.getPromotionRefset(config);
-            ts = Ts.get();
-            interpreter = WorkflowInterpreter.createWorkflowInterpreter(workList.getWorkflowDefinition());
+	/**
+	 * Instantiates a new worklist initializer processor.
+	 * 
+	 * @param partition
+	 *            the partition
+	 * @param workList
+	 *            the work list
+	 * @param config
+	 *            the config
+	 * @param updater
+	 *            the updater
+	 */
+	public WorklistInitializerProcessor(Partition partition, WorkList workList, I_ConfigAceFrame config, ActivityUpdater updater) {
+		super();
+		try {
+			vc = config.getViewCoordinate();
+			tc = Ts.get().getTerminologyBuilder(config.getEditCoordinate(), config.getViewCoordinate());
+			this.updater = updater;
+			this.workList = workList;
+			if (updater != null) {
+				updater.startActivity();
+			}
+			workListNid = workList.getId();
+			promRef = workList.getPromotionRefset(config);
+			ts = Ts.get();
+			interpreter = WorkflowInterpreter.createWorkflowInterpreter(workList.getWorkflowDefinition());
 
-            membersNidSet = ts.getEmptyNidSet();
+			membersNidSet = ts.getEmptyNidSet();
 
-            updater.setTaskMessage("Processing partitionMembers");
-            ConceptChronicleBI partitionChronicle = (ConceptChronicleBI) partition.getConcept();
-            Collection<? extends RefexVersionBI<?>> partitionMembersList = partitionChronicle.getRefsetMembersActive(vc);
-            for (RefexVersionBI<?> loopMember : partitionMembersList) {
-                membersNidSet.setMember(loopMember.getReferencedComponentNid());
-            }
+			if (updater != null) {
+				updater.setTaskMessage("Processing partitionMembers");
+			}
+			ConceptChronicleBI partitionChronicle = (ConceptChronicleBI) partition.getConcept();
+			Collection<? extends RefexVersionBI<?>> partitionMembersList = partitionChronicle.getRefsetMembersActive(vc);
+			for (RefexVersionBI<?> loopMember : partitionMembersList) {
+				membersNidSet.setMember(loopMember.getReferencedComponentNid());
+			}
 
-            activeNid = SnomedMetadataRf1.CURRENT_RF1.getLenient().getNid();
-            inactiveNid = SnomedMetadataRf1.RETIRED_INACTIVE_STATUS_RF1.getLenient().getNid();
-            activeUuid = SnomedMetadataRf1.CURRENT_RF1.getLenient().getPrimUuid();
-            inactiveUuid = SnomedMetadataRf1.RETIRED_INACTIVE_STATUS_RF1.getLenient().getPrimUuid();
-            assignedNid = Terms.get().uuidToNative(ArchitectonicAuxiliary.Concept.WORKLIST_ITEM_ASSIGNED_STATUS.getUids());
-            updater.setTaskMessage("Initializing WorkList");
-            updater.startCount(membersNidSet.cardinality());
+			activeNid = SnomedMetadataRf1.CURRENT_RF1.getLenient().getNid();
+			inactiveNid = SnomedMetadataRf1.RETIRED_INACTIVE_STATUS_RF1.getLenient().getNid();
+			activeUuid = SnomedMetadataRf1.CURRENT_RF1.getLenient().getPrimUuid();
+			inactiveUuid = SnomedMetadataRf1.RETIRED_INACTIVE_STATUS_RF1.getLenient().getPrimUuid();
+			assignedNid = Terms.get().uuidToNative(ArchitectonicAuxiliary.Concept.WORKLIST_ITEM_ASSIGNED_STATUS.getUids());
+			if (updater != null) {
+				updater.setTaskMessage("Initializing WorkList");
+				updater.startCount(membersNidSet.cardinality());
+			}
+			WfInstance instance = new WfInstance();
+			WfComponentProvider prov = new WfComponentProvider();
+			instance.setComponentId(SNOMED.Concept.ROOT.getPrimoridalUid());
+			instance.setState(prov.statusConceptToWfState(Terms.get().getConcept(ArchitectonicAuxiliary.Concept.WORKLIST_ITEM_ASSIGNED_STATUS.getUids())));
+			instance.setWfDefinition(workList.getWorkflowDefinition());
+			instance.setWorkList(workList);
+			user = interpreter.getNextDestination(instance, workList);
+			if (user == null) {
+				throw new Exception("Cannot set next destination\n");
+			}
+			userNid = ts.getNidForUuids(user.getId());
 
-            WfInstance instance = new WfInstance();
-            WfComponentProvider prov = new WfComponentProvider();
-            instance.setComponentId(SNOMED.Concept.ROOT.getPrimoridalUid());
-            instance.setState(prov.statusConceptToWfState(
-                    Terms.get().getConcept(
-                    ArchitectonicAuxiliary.Concept.WORKLIST_ITEM_ASSIGNED_STATUS.getUids())));
-            instance.setWfDefinition(workList.getWorkflowDefinition());
-            instance.setWorkList(workList);
-            user = interpreter.getNextDestination(instance, workList);
-            if (user == null) {
-                throw new Exception("Cannot set next destination\n");
-            }
-            userNid = ts.getNidForUuids(user.getId());
+		} catch (ValidationException e) {
+			AceLog.getAppLog().alertAndLogException(e);
+		} catch (IOException e) {
+			AceLog.getAppLog().alertAndLogException(e);
+		} catch (TerminologyException e) {
+			AceLog.getAppLog().alertAndLogException(e);
+		} catch (Exception e) {
+			AceLog.getAppLog().alertAndLogException(e);
+		}
+	}
 
-        } catch (ValidationException e) {
-            AceLog.getAppLog().alertAndLogException(e);
-        } catch (IOException e) {
-            AceLog.getAppLog().alertAndLogException(e);
-        } catch (TerminologyException e) {
-            AceLog.getAppLog().alertAndLogException(e);
-        } catch (Exception e) {
-            AceLog.getAppLog().alertAndLogException(e);
-        }
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.ihtsdo.tk.api.ContinuationTrackerBI#continueWork()
+	 */
+	@Override
+	public boolean continueWork() {
+		return true;
+	}
 
-    /* (non-Javadoc)
-     * @see org.ihtsdo.tk.api.ContinuationTrackerBI#continueWork()
-     */
-    @Override
-    public boolean continueWork() {
-        return true;
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.ihtsdo.tk.api.ProcessUnfetchedConceptDataBI#getNidSet()
+	 */
+	@Override
+	public NidBitSetBI getNidSet() throws IOException {
+		return membersNidSet;
+	}
 
-    /* (non-Javadoc)
-     * @see org.ihtsdo.tk.api.ProcessUnfetchedConceptDataBI#getNidSet()
-     */
-    @Override
-    public NidBitSetBI getNidSet() throws IOException {
-        return membersNidSet;
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.ihtsdo.tk.api.ProcessUnfetchedConceptDataBI#processUnfetchedConceptData
+	 * (int, org.ihtsdo.tk.api.ConceptFetcherBI)
+	 */
+	@Override
+	public void processUnfetchedConceptData(int cNid, ConceptFetcherBI fetcher) throws Exception {
+		if (membersNidSet.isMember(cNid)) {
+			ConceptVersionBI c = fetcher.fetch(vc);
+			if (processConcept(c)) {
+				// fetcher.update(c.getChronicle());
+				Terms.get().addUncommittedNoChecks((I_GetConceptData) c.getChronicle());
+			}
+		}
 
-    /* (non-Javadoc)
-     * @see org.ihtsdo.tk.api.ProcessUnfetchedConceptDataBI#processUnfetchedConceptData(int, org.ihtsdo.tk.api.ConceptFetcherBI)
-     */
-    @Override
-    public void processUnfetchedConceptData(int cNid, ConceptFetcherBI fetcher)
-            throws Exception {
-        if (membersNidSet.isMember(cNid)) {
-            ConceptVersionBI c = fetcher.fetch(vc);
-            if (processConcept(c)) {
-                //fetcher.update(c.getChronicle());
-                Terms.get().addUncommittedNoChecks((I_GetConceptData) c.getChronicle());
-            }
-        }
+	}
 
-    }
+	/**
+	 * Process concept.
+	 * 
+	 * @param concept
+	 *            the concept
+	 * @return true, if successful
+	 */
+	private boolean processConcept(ConceptVersionBI concept) {
+		boolean update = true;
+		updater.incrementCount();
 
-    /**
-     * Process concept.
-     *
-     * @param concept the concept
-     * @return true, if successful
-     */
-    private boolean processConcept(ConceptVersionBI concept) {
-        boolean update = true;
-        updater.incrementCount();
+		try {
+			RefexCAB newSpec = new RefexCAB(TK_REFEX_TYPE.CID, concept.getNid(), workListNid);
+			newSpec.put(RefexProperty.CNID1, activeNid);
+			RefexChronicleBI<?> newRefex = tc.constructIfNotCurrent(newSpec);
 
-        try {
-            RefexCAB newSpec = new RefexCAB(
-                    TK_REFEX_TYPE.CID,
-                    concept.getNid(),
-                    workListNid);
-            newSpec.put(RefexProperty.CNID1, activeNid);
-            RefexChronicleBI<?> newRefex = tc.constructIfNotCurrent(newSpec);
+			RefexCAB newSpecForProm = new RefexCAB(TK_REFEX_TYPE.CID_CID, concept.getNid(), promRef.getRefsetId());
+			newSpecForProm.put(RefexProperty.CNID1, assignedNid);
+			newSpecForProm.put(RefexProperty.CNID2, userNid);
+			RefexChronicleBI<?> newRefexForProm = tc.constructIfNotCurrent(newSpecForProm);
+			concept.addAnnotation(newRefexForProm);
+		} catch (IOException e) {
+			AceLog.getAppLog().alertAndLogException(e);
+		} catch (Exception e) {
+			AceLog.getAppLog().alertAndLogException(e);
+		}
+		return update;
 
-            RefexCAB newSpecForProm = new RefexCAB(
-                    TK_REFEX_TYPE.CID_CID,
-                    concept.getNid(),
-                    promRef.getRefsetId());
-            newSpecForProm.put(RefexProperty.CNID1, assignedNid);
-            newSpecForProm.put(RefexProperty.CNID2, userNid);
-            RefexChronicleBI<?> newRefexForProm = tc.constructIfNotCurrent(newSpecForProm);
-            concept.addAnnotation(newRefexForProm);
-        } catch (IOException e) {
-            AceLog.getAppLog().alertAndLogException(e);
-        } catch (Exception e) {
-            AceLog.getAppLog().alertAndLogException(e);
-        }
-        return update;
-
-    }
+	}
 }
