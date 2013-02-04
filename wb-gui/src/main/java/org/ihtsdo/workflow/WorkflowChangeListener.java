9f0b5d8b85b89d46541188bbfab9dd7d607667df
alopez <alopez>
2013-02-04 21:47:55 +0000

    Fixes to Workflow listener

--- wb-gui/src/main/java/org/ihtsdo/workflow/WorkflowChangeListener.java
+++ wb-gui/src/main/java/org/ihtsdo/workflow/WorkflowChangeListener.java
@@ -8,14 +8,28 @@ import java.util.concurrent.CancellationException;
 import javax.swing.SwingUtilities;
 
 import org.dwfa.ace.api.I_RepresentIdSet;
+import org.dwfa.ace.api.Terms;
 import org.ihtsdo.tk.Ts;
 import org.ihtsdo.tk.api.NidBitSetItrBI;
+import org.ihtsdo.tk.api.NidSet;
 import org.ihtsdo.tk.api.concept.ConceptChronicleBI;
 
 public class WorkflowChangeListener implements PropertyChangeListener {
+
+	public static NidSet alreadySeen;
+
 	private PropertyChangeEvent myEvt;
+
+
+	public WorkflowChangeListener() {
+		super();
+		alreadySeen = new NidSet();
+	}
+
+
 	@Override
 	public void propertyChange(PropertyChangeEvent arg0) {
+		System.out.println("Change got to listener...");
 		myEvt=arg0;
 		SwingUtilities.invokeLater(new Runnable() {
 
@@ -26,13 +40,16 @@ public class WorkflowChangeListener implements PropertyChangeListener {
 					I_RepresentIdSet idSet=(I_RepresentIdSet)myEvt.getNewValue();
 					if (idSet!=null){
 						NidBitSetItrBI possibleItr = idSet.iterator();
-						
+
 						while (possibleItr.next()) {
-							concept=Ts.get().getConcept(possibleItr.nid());
-							if (concept!=null){
-								WorkflowHelper2.addComponentToDefaultWorklist(concept);
+							if (!alreadySeen.contains(possibleItr.nid())) {
+								alreadySeen.add(possibleItr.nid());
+								concept=Ts.get().getConcept(possibleItr.nid());
+								if (concept!=null){
+									System.out.println("Sending to workflow: " + concept.toString());
+									WorkflowHelper2.addComponentToDefaultWorklist(concept);
+								}
 							}
-							
 						}
 					}
 				} catch (IOException e) {
