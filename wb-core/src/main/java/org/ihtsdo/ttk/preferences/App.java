1b1fd4d34e3db33aada8a8df1c890f580d6d2661
ocarlsen <ocarlsen>
2013-02-05 18:59:28 +0000

    Refactoring to test queue preferences.

--- wb-core/src/main/java/org/ihtsdo/ttk/preferences/App.java
+++ wb-core/src/main/java/org/ihtsdo/ttk/preferences/App.java
@@ -1,80 +1,93 @@
 package org.ihtsdo.ttk.preferences;
 
-//~--- non-JDK imports --------------------------------------------------------
+import org.ihtsdo.ttk.queue.QueueAddress;
+import org.ihtsdo.ttk.queue.QueueList;
+import org.ihtsdo.ttk.queue.QueuePreferences;
+import org.ihtsdo.ttk.queue.QueueType;
 
 import java.io.File;
-import org.ihtsdo.ttk.queue.QueuePreferences;
-
-//~--- JDK imports ------------------------------------------------------------
-
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import java.util.prefs.BackingStoreException;
 import java.util.prefs.InvalidPreferencesFormatException;
 import java.util.prefs.Preferences;
-import org.ihtsdo.ttk.queue.QueueAddress;
-import org.ihtsdo.ttk.queue.QueueList;
-import org.ihtsdo.ttk.queue.QueueType;
 
 /**
  * Hello world!
  *
  */
 public class App {
-    public static void main(String[] args) {
-        try {
-            EnumBasedPreferences pref      = new EnumBasedPreferences("com.ihtsdo.test");
-            pref.removeNode();
-            pref.flush();
-            pref      = new EnumBasedPreferences("com.ihtsdo.test");
-            EnumBasedPreferences prefChild = pref.node("child");
 
-            QueuePreferences     qp        = new QueuePreferences();
-            EnumBasedPreferences queueNode = pref.node("queue");
+    public static void main(String[] args)
+            throws Exception {
 
-            qp.exportFields(queueNode);
-            pref.exportSubtree(new FileOutputStream("Test.xml"));
-            pref.removeNode();
-            pref.flush();
-            Preferences.importPreferences(new FileInputStream("Test.xml"));
-            pref = new EnumBasedPreferences("com.ihtsdo.test");
-            qp   = new QueuePreferences(pref.node("queue"));
-            System.out.println("Queue preferences: " + qp);
-            
-            pref.removeNode();
-            pref.flush();
-            pref = new EnumBasedPreferences("com.ihtsdo.test");
+        final String version = "org.kp:cmt-pa:1.0-SNAPHOT";  // TODO: Get from Maven during build.
+        final String userName = "ocarlsen";  // TODO: Get from application.
+        final String appPrefix = version + ":" + userName;
 
-            QueueList ql = new QueueList();
-            ql.exportFields(pref);
-            pref.exportSubtree(new FileOutputStream("Test.xml"));
-            
-            ql = new QueueList(pref);
-            System.out.println(ql);
-            
-            ql.getQueueList().add(new QueuePreferences());
-            
-            QueuePreferences qp2 = new QueuePreferences();
-            qp2.setQueueDirectory(new File("some other location"));
-            qp2.getServiceItemProperties().clear();
-            qp2.getServiceItemProperties().add(new QueueAddress("qp2 address"));
-            qp2.getServiceItemProperties().add(new QueueType(QueueType.Types.OUTBOX));
-            
-            ql.getQueueList().add(qp2);
-            ql.exportFields(pref);
-            pref.exportSubtree(new FileOutputStream("Test.xml"));
-
-            QueueList ql3 = new QueueList(pref);
-            System.out.println(ql3);
-        } catch (InvalidPreferencesFormatException ex) {
-            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException | BackingStoreException ex) {
-            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        // Check args to try different scenarios.
+        int argCount = args.length;
+        if (argCount == 0) {
+            getEmptyPrefsGetQueueListExportToFileThenRemove(appPrefix, "Test.xml");
+        } else if (argCount == 1) {
+            String fileName = args[0];
+            getEmptyPrefsImportFromFileCreateQueueList(fileName, appPrefix);
         }
-            
-     }
+    }
+
+    private static void getEmptyPrefsGetQueueListExportToFileThenRemove(String appPrefix, String fileName)
+            throws FileNotFoundException, BackingStoreException, IOException {
+        EnumBasedPreferences prefs = getEmptyPrefs(appPrefix);
+        QueueList queueList = getQueueList(prefs);
+
+        queueList.exportFields(prefs);
+        prefs.exportSubtree(new FileOutputStream(fileName));
+
+        prefs.removeNode();
+        prefs.flush();
+    }
+
+    private static void getEmptyPrefsImportFromFileCreateQueueList(String fileName, String appPrefix)
+            throws FileNotFoundException, IOException,
+            InvalidPreferencesFormatException, BackingStoreException {
+        Preferences.importPreferences(new FileInputStream(fileName));
+        EnumBasedPreferences prefs = new EnumBasedPreferences(appPrefix);
+        prefs.sync();  // Bring up-to-date with backing store.
+
+        QueueList queueList = new QueueList(prefs);
+        System.out.println(queueList);
+    }
+
+    private static EnumBasedPreferences getEmptyPrefs(String appPrefix)
+            throws BackingStoreException {
+        EnumBasedPreferences prefs = new EnumBasedPreferences(appPrefix);
+
+        // Remove from backing store.
+        prefs.removeNode();
+        prefs.flush();
+
+        return new EnumBasedPreferences(appPrefix);
+    }
+
+    private static QueueList getQueueList(EnumBasedPreferences prefs)
+            throws BackingStoreException, IOException {
+        QueueList queueList = new QueueList(prefs);
+        System.out.println(queueList);
+
+        QueuePreferences qp1 = new QueuePreferences();
+        queueList.getQueueList().add(qp1);
+        System.out.println(queueList);
+
+        QueuePreferences qp2 = new QueuePreferences();
+        qp2.setQueueDirectory(new File("some other location"));
+        qp2.getServiceItemProperties().clear();
+        qp2.getServiceItemProperties().add(new QueueAddress("qp2 address"));
+        qp2.getServiceItemProperties().add(new QueueType(QueueType.Types.OUTBOX));
+        queueList.getQueueList().add(qp2);
+        System.out.println(queueList);
+
+        return queueList;
+    }
 }
