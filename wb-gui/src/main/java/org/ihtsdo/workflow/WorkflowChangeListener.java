c7ffa2c8f0a802abfa5a938121ddb710403b3727
alopez <alopez>
2013-02-05 15:09:19 +0000

    WorkflowInitiator implementation

--- wb-gui/src/main/java/org/ihtsdo/workflow/WorkflowChangeListener.java
+++ wb-gui/src/main/java/org/ihtsdo/workflow/WorkflowChangeListener.java
@@ -9,26 +9,39 @@ import javax.swing.SwingUtilities;
 
 import org.dwfa.ace.api.I_RepresentIdSet;
 import org.dwfa.ace.api.Terms;
+import org.dwfa.ace.log.AceLog;
+import org.ihtsdo.project.workflow.api.wf2.implementation.WorkflowInitiator;
 import org.ihtsdo.tk.Ts;
 import org.ihtsdo.tk.api.NidBitSetItrBI;
 import org.ihtsdo.tk.api.NidSet;
 import org.ihtsdo.tk.api.concept.ConceptChronicleBI;
+import org.ihtsdo.tk.workflow.api.WorkflowInitiatiorBI;
 
 public class WorkflowChangeListener implements PropertyChangeListener {
 
 	public static NidSet alreadySeen;
 
 	private PropertyChangeEvent myEvt;
+	
+	private WorkflowInitiatiorBI initiator;
 
 
 	public WorkflowChangeListener() {
 		super();
 		alreadySeen = new NidSet();
+		initiator = new WorkflowInitiator();
 	}
-
 
 	@Override
 	public void propertyChange(PropertyChangeEvent arg0) {
+		try {
+			initiator.evaluateForWorkflowInitiation(arg0);
+		} catch (Exception e) {
+			AceLog.getAppLog().alertAndLogException(e);
+		}
+	}
+
+	public void propertyChangeNotUsedLegacy(PropertyChangeEvent arg0) {
 		System.out.println("Change got to listener...");
 		myEvt=arg0;
 		SwingUtilities.invokeLater(new Runnable() {
