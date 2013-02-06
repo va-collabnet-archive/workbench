4c2fbd42837f0f63a5770b6b544b3ad35c219f3d
ocarlsen <ocarlsen>
2013-02-06 03:00:30 +0000

    Removing unused import.

--- wb-core/src/test/java/org/ihtsdo/ttk/preferences/EnumBasedPreferencesTest.java
+++ wb-core/src/test/java/org/ihtsdo/ttk/preferences/EnumBasedPreferencesTest.java
@@ -21,7 +21,6 @@ import org.ihtsdo.ttk.queue.QueuePreferences;
 import org.ihtsdo.ttk.queue.QueueType;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
