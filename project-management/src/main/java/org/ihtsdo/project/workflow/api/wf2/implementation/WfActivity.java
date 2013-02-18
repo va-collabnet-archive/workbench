5b9f45a8cacd2f9cbd1fa7ebe8720d1493a3944c
vmanukyan <vmanukyan>
2013-02-18 22:53:43 +0000

    equals and hash code implemented

--- project-management/src/main/java/org/ihtsdo/project/workflow/api/wf2/implementation/WfActivity.java
+++ project-management/src/main/java/org/ihtsdo/project/workflow/api/wf2/implementation/WfActivity.java
@@ -77,4 +77,29 @@ public class WfActivity implements WfActivityBI {
 		return action.getName();
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((action == null) ? 0 : action.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		WfActivity other = (WfActivity) obj;
+		if (action == null) {
+			if (other.action != null)
+				return false;
+		} else if (!action.equals(other.action))
+			return false;
+		return true;
+	}
+	
 }
--- project-management/src/main/java/org/ihtsdo/project/workflow/model/WfAction.java
+++ project-management/src/main/java/org/ihtsdo/project/workflow/model/WfAction.java
@@ -154,4 +154,47 @@ public class WfAction implements Serializable {
 		this.businessProcess = businessProcess;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((businessProcess == null) ? 0 : businessProcess.hashCode());
+		result = prime * result + ((consequence == null) ? 0 : consequence.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		WfAction other = (WfAction) obj;
+		if (businessProcess == null) {
+			if (other.businessProcess != null)
+				return false;
+		} else if (!businessProcess.equals(other.businessProcess))
+			return false;
+		if (consequence == null) {
+			if (other.consequence != null)
+				return false;
+		} else if (!consequence.equals(other.consequence))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
 }
