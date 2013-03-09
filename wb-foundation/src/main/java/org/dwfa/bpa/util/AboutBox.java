ae40a214553d074e9c2b8722f2c18f6d384e5767
ocarlsen <ocarlsen>
2013-03-09 02:09:12 +0000

    Adding static link to bottom of dialog, with TODO to make dynamic. Adding main method to allow rapid prototyping without having to rebuild bundle. Finally, factored out getTitle and getGraphic methods, created some constants.

--- wb-foundation/src/main/java/org/dwfa/bpa/util/AboutBox.java
+++ wb-foundation/src/main/java/org/dwfa/bpa/util/AboutBox.java
@@ -19,38 +19,51 @@
  */
 package org.dwfa.bpa.util;
 
+import org.dwfa.app.I_ManageStandardAppFunctions;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Desktop;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.File;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
+import java.util.logging.Logger;
 
 import javax.swing.ImageIcon;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
 
 /**
  * @author kec
+ * @author ocarlsen
  * 
  */
 public class AboutBox {
+    
+    private static Logger logger = Logger.getLogger(AboutBox.class.getName());
+
+    private static final String GRAPHIC_PROPERTY = "org.dwfa.AboutBoxGraphic";
+    private static final String TITLE_PROPERTY = "org.dwfa.AboutBoxTitle";
+
     private static JDialog setupAbout(JFrame parent) {
 
-        String title = "About the IHTSDO Editor";
-
-        if (System.getProperty("org.dwfa.AboutBoxTitle") != null
-            && System.getProperty("org.dwfa.AboutBoxTitle").length() > 3) {
-            title = removeQuotes(System.getProperty("org.dwfa.AboutBoxTitle"));
-        }
+        String title = getTitle();
         JDialog aboutBox = new JDialog(parent, title);
 
-        String graphic = "config/about-box/ihtsdo_aboutbox.jpg";
-        if (System.getProperty("org.dwfa.AboutBoxGraphic") != null
-            && System.getProperty("org.dwfa.AboutBoxGraphic").length() > 3) {
-            graphic = removeQuotes(System.getProperty("org.dwfa.AboutBoxGraphic"));
-        }
+        String graphic = getGraphic();
         URL aboutBoxUrl = aboutBox.getClass().getResource(graphic);
         Toolkit tk = aboutBox.getToolkit();
         JLabel aboutLabel;
@@ -70,11 +83,66 @@ public class AboutBox {
         } else {
             aboutLabel = new JLabel("Cannot find " + graphic);
         }
-        aboutBox.getContentPane().setLayout(new GridLayout(1, 1));
-        aboutBox.getContentPane().add(aboutLabel);
+
+        // As a convenience, JDialog adds to the contentPane directly.
+        aboutBox.add(aboutLabel, BorderLayout.CENTER);
+        String version = "1.0-basic-site-SNAPSHOT";  // TODO: Get dynamically.
+        JLabel siteLink = createSiteLink(version);
+        aboutBox.add(siteLink, BorderLayout.SOUTH);
         aboutBox.pack();
         aboutBox.setModal(true);
         return aboutBox;
+    }
+
+    private static String getGraphic() {
+        String graphic = "config/about-box/ihtsdo_aboutbox.jpg";
+        
+        // Look for property override.
+        if (System.getProperty(GRAPHIC_PROPERTY) != null
+            && System.getProperty(GRAPHIC_PROPERTY).length() > 3) {
+            graphic = removeQuotes(System.getProperty(GRAPHIC_PROPERTY));
+        }
+        
+        return graphic;
+    }
+
+    private static String getTitle() {
+        String title = "About the IHTSDO Editor";
+
+        // Look for property override.
+        if (System.getProperty(TITLE_PROPERTY) != null
+            && System.getProperty(TITLE_PROPERTY).length() > 3) {
+            title = removeQuotes(System.getProperty(TITLE_PROPERTY));
+        }
+        
+        return title;
+    }
+
+    private static JLabel createSiteLink(String version) {
+        // TODO: Get from appinfo.properties.
+        final String href = "https://csfe.aceworkspace.net/svn/repos/cmt-rf2-archetypes/branches/basic-site/site/index.html";
+        String text = "Version " + version;
+        JLabel label = new JLabel("<html><a href=\"" + href + "\">" + text + "</a></html>");
+        label.setCursor(new Cursor(Cursor.HAND_CURSOR));
+
+        // Help center within dialog. 
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        label.setVerticalAlignment(SwingConstants.CENTER);
+        label.setBorder(new EmptyBorder(0, 0, 5, 0));  // Padding on bottom.
+        
+        // Open browser on mouse click.
+        label.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                try {
+                    Desktop.getDesktop().browse(new URI(href));
+                } catch (Exception ex) {
+                    
+                }
+            }
+        });
+        
+        return label;
     }
 
     public static String removeQuotes(String str) {
@@ -89,5 +157,28 @@ public class AboutBox {
 
     public static JDialog getAboutBox(JFrame parent) {
         return setupAbout(parent);
+    }
+    
+    /**
+     * Helpful for testing outside an editor bundle.
+     */
+    public static void main(String[] args) {
+        // Configure image from command-line args.
+        String pathToGraphic = args[0];
+        System.setProperty(GRAPHIC_PROPERTY, pathToGraphic);
+        
+        JFrame frame = new JFrame(getTitle());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(400, 400));
+        frame.pack();
+        frame.setVisible(true);
+        
+        // This logic copied from ComponentFrameBean#about().
+        JDialog aboutBox = AboutBox.getAboutBox(frame);
+        aboutBox.pack();
+        aboutBox.setLocation((int) frame.getLocation().getX() + 22, (int) frame.getLocation().getY() + 22);
+        aboutBox.setResizable(false);
+        aboutBox.setVisible(true);
+        aboutBox.toFront();
     }
 }
