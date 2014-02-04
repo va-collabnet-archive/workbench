f02972c2ed7b511c9018a77d9c9c19760ed21058
dgrald <dgrald>
2014-02-04 17:55:05 +0000

    [artf228870] Updates to about box

--- wb-foundation/src/main/java/org/dwfa/bpa/util/AboutBox.java
+++ wb-foundation/src/main/java/org/dwfa/bpa/util/AboutBox.java
@@ -1,33 +1,26 @@
 /**
  * Copyright (c) 2009 International Health Terminology Standards Development
  * Organisation
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 /*
  * Created on Apr 29, 2005
  */
 package org.dwfa.bpa.util;
 
-import static org.dwfa.bpa.util.AppInfoProperties.ARCHETYPE_ARTIFACT_ID;
-import static org.dwfa.bpa.util.AppInfoProperties.ARCHETYPE_GROUP_ID;
-import static org.dwfa.bpa.util.AppInfoProperties.ARCHETYPE_VERSION;
-import static org.dwfa.bpa.util.AppInfoProperties.ARTIFACT_ID;
-import static org.dwfa.bpa.util.AppInfoProperties.GROUP_ID;
-import static org.dwfa.bpa.util.AppInfoProperties.SITE_URL;
-import static org.dwfa.bpa.util.AppInfoProperties.VERSION;
-
-import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.Cursor;
 import java.awt.Desktop;
 import java.awt.Dimension;
@@ -42,19 +35,33 @@ import java.net.URL;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.SwingConstants;
-import javax.swing.SwingUtilities;
+import javax.swing.border.EmptyBorder;
+import static org.dwfa.bpa.util.AppInfoProperties.ARCHETYPE_ARTIFACT_ID;
+import static org.dwfa.bpa.util.AppInfoProperties.ARCHETYPE_GROUP_ID;
+import static org.dwfa.bpa.util.AppInfoProperties.ARCHETYPE_VERSION;
+import static org.dwfa.bpa.util.AppInfoProperties.ARTIFACT_ID;
+import static org.dwfa.bpa.util.AppInfoProperties.BASELINE_DATA_ARTIFACT_ID;
+import static org.dwfa.bpa.util.AppInfoProperties.BASELINE_DATA_GROUP_ID;
+import static org.dwfa.bpa.util.AppInfoProperties.BASELINE_DATA_VERSION;
+import static org.dwfa.bpa.util.AppInfoProperties.GROUP_ID;
+import static org.dwfa.bpa.util.AppInfoProperties.PROJECT_DESCRIPTION;
+import static org.dwfa.bpa.util.AppInfoProperties.PROJECT_NAME;
+import static org.dwfa.bpa.util.AppInfoProperties.SITE_URL;
+import static org.dwfa.bpa.util.AppInfoProperties.SNOMED_CORE_RELEASE_DATE;
+import static org.dwfa.bpa.util.AppInfoProperties.TOOLKIT_VERSION;
+import static org.dwfa.bpa.util.AppInfoProperties.VERSION;
 
 /**
  * @author kec
  * @author ocarlsen
  */
 public class AboutBox {
-    
+
     private static final Logger LOGGER = Logger.getLogger(AboutBox.class.getName());
 
     private static final String GRAPHIC_PROPERTY = "org.dwfa.AboutBoxGraphic";
@@ -64,6 +71,8 @@ public class AboutBox {
 
         String title = getTitle();
         final JDialog aboutBox = new JDialog(parent, title);
+
+        aboutBox.getContentPane().setBackground(Color.WHITE);
 
         String graphic = getGraphic();
         URL aboutBoxUrl = aboutBox.getClass().getResource(graphic);
@@ -85,6 +94,7 @@ public class AboutBox {
         } else {
             aboutLabel = new JLabel("Cannot find " + graphic);
         }
+        aboutLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         // On some platforms, changing the resizable state affects
         // the insets of the Dialog.  As a result, multiple calls
@@ -92,80 +102,65 @@ public class AboutBox {
         // by preemptively calling setResizeable before the first pack().
         aboutBox.setResizable(false);
 
-        // As a convenience, JDialog adds to the contentPane directly.
-        aboutBox.add(aboutLabel, BorderLayout.CENTER);
-        final JLabel versionLabel = createVersionLabel();
-        aboutBox.add(versionLabel, BorderLayout.SOUTH);
-        aboutBox.pack();
-        aboutBox.setModal(true);
+        aboutBox.setLayout(new BoxLayout(aboutBox.getContentPane(), BoxLayout.Y_AXIS));
 
-        // Listen for mouse clicks on aboutLabel to show versionLabel.
-        aboutLabel.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent ev) {
-                // Require Ctrl + "left click".
-                if (ev.isControlDown() && SwingUtilities.isLeftMouseButton(ev)) {
-                    
-                    // Toggle visibility.
-                    boolean visible = versionLabel.isVisible();
-                    versionLabel.setVisible(! visible);
-                    
-                    // Update dialog bounds to accommodate new component.
-                    aboutBox.pack();
-                }
-            }
-        });
+        // As a convenience, JDialog adds to the contentPane directly.
+        aboutBox.add(aboutLabel);
+
+        final JLabel releaseEdition = createReleaseEditionLabel();
+        aboutBox.add(releaseEdition);
+
+        aboutBox.pack();
 
         return aboutBox;
     }
 
     private static String getGraphic() {
         String graphic = "config/about-box/ihtsdo_aboutbox.jpg";
-        
+
         // Look for property override.
         if (System.getProperty(GRAPHIC_PROPERTY) != null
-            && System.getProperty(GRAPHIC_PROPERTY).length() > 3) {
+                && System.getProperty(GRAPHIC_PROPERTY).length() > 3) {
             graphic = removeQuotes(System.getProperty(GRAPHIC_PROPERTY));
         }
-        
+
         return graphic;
     }
 
     private static String getTitle() {
-        String title = "About the IHTSDO Editor";
+        String title = "About the IHTSDO Workbench";
 
         // Look for property override.
         if (System.getProperty(TITLE_PROPERTY) != null
-            && System.getProperty(TITLE_PROPERTY).length() > 3) {
+                && System.getProperty(TITLE_PROPERTY).length() > 3) {
             title = removeQuotes(System.getProperty(TITLE_PROPERTY));
         }
-        
+
         return title;
     }
 
-    private static JLabel createVersionLabel() {
+    private static JLabel createReleaseEditionLabel() {
         Properties appInfoProperties = AppInfoProperties.getProperties();
-        String projectId = buildProjectId(appInfoProperties); 
-        String archetypeId = buildArchetypeId(appInfoProperties); 
+        String projectName = buildProjectName(appInfoProperties);
+        String projectDescription = buildProjectDesc(appInfoProperties);
+        String projectId = buildProjectId(appInfoProperties);
+        String archetypeId = buildArchetypeId(appInfoProperties);
+        String dataId = buildDataId(appInfoProperties);
+        String toolkitVersion = buildToolkitVersion(appInfoProperties);
+        String snomedCoreReleaseDate = buildSnomedCoreReleaseDate(appInfoProperties);
 
         // Get site URL properties from AppInfo.
         // These are not required, and may be null.
         String siteURL = AppInfoProperties.getProperty(SITE_URL);
 
-        // Build a link label if site is specified, a plain old label otherwise.
         JLabel label = null;
         if (siteURL != null) {
-            label = createLinkLabel(siteURL, projectId, archetypeId);           
+            label = createLinkLabel(siteURL, projectName, projectDescription, projectId, archetypeId, dataId, toolkitVersion, snomedCoreReleaseDate);
         } else {
-            label = createPlainLabel(projectId, archetypeId);
+            label = createPlainLabel(projectName, projectDescription, projectId, archetypeId, dataId, toolkitVersion, snomedCoreReleaseDate);
         }
-
-        // Center label within dialog. 
-        label.setVerticalAlignment(SwingConstants.CENTER);
-
-        // Make invisible until activated by user.
-        label.setVisible(false);
-
+        label.setAlignmentX(Component.CENTER_ALIGNMENT);
+        label.setBorder(new EmptyBorder(5, 0, 5, 0));
         return label;
     }
 
@@ -175,7 +170,7 @@ public class AboutBox {
         String groupId = appInfoProperties.getProperty(GROUP_ID);
         String artifactId = appInfoProperties.getProperty(ARTIFACT_ID);
         String version = appInfoProperties.getProperty(VERSION);
-        
+
         return groupId + ":" + artifactId + ":" + version;
     }
 
@@ -185,25 +180,26 @@ public class AboutBox {
         String archetypeGroupId = appInfoProperties.getProperty(ARCHETYPE_GROUP_ID);
         String archetypeArtifactId = appInfoProperties.getProperty(ARCHETYPE_ARTIFACT_ID);
         String archetypeVersion = appInfoProperties.getProperty(ARCHETYPE_VERSION);
-        
+
         // Some older POMs may not have archetype information embedded.
         // If any of these properties are null, abort.
         if ((archetypeGroupId == null) || (archetypeArtifactId == null) || (archetypeVersion == null)) {
             return null;
         }
-        
-        return archetypeGroupId+ ":" + archetypeArtifactId + ":" + archetypeVersion;
+
+        return archetypeGroupId + ":" + archetypeArtifactId + ":" + archetypeVersion;
     }
 
-    private static JLabel createLinkLabel(final String siteURL, String projectId, String archetypeId) {
+    private static JLabel createLinkLabel(final String siteURL, String projectName,
+            String projectDesc, String projectId, String archetypeId, String dataId, String toolkitVersion, String snomedCoreReleaseDate) {
         final String href = buildHref(siteURL);
-        String labelText = buildLabelText(href, projectId, archetypeId);
+        String labelText = buildLabelText(href, projectName, projectDesc, projectId, archetypeId, dataId, toolkitVersion, snomedCoreReleaseDate);
         JLabel label = new JLabel(labelText);
 
         // Simulate hyperlink.
         label.setCursor(new Cursor(Cursor.HAND_CURSOR));
         label.setToolTipText(href);
-        
+
         // Open browser on mouse click, if supported.
         label.addMouseListener(new MouseAdapter() {
             @Override
@@ -220,12 +216,13 @@ public class AboutBox {
                 }
             }
         });
-        
+
         return label;
     }
 
-    private static JLabel createPlainLabel(String projectId, String archetypeId) {
-        String labelText = buildLabelText(null, projectId, archetypeId);
+    private static JLabel createPlainLabel(String projectName, String projectDesc,
+            String projectId, String archetypeId, String dataId, String toolkitVersion, String snomedCoreReleaseDate) {
+        String labelText = buildLabelText(null, projectName, projectDesc, projectId, archetypeId, dataId, toolkitVersion, snomedCoreReleaseDate);
         return new JLabel(labelText);
     }
 
@@ -238,23 +235,70 @@ public class AboutBox {
         }
     }
 
-    static String buildLabelText(String href, String projectId, String archetypeId) {
+    static String buildProjectName(Properties appInfoProperties) {
+        return appInfoProperties.getProperty(PROJECT_NAME);
+    }
+
+    static String buildProjectDesc(Properties appInfoProperties) {
+        return appInfoProperties.getProperty(PROJECT_DESCRIPTION);
+    }
+
+    static String buildSnomedCoreReleaseDate(Properties appInfoProperties) {
+        return appInfoProperties.getProperty(SNOMED_CORE_RELEASE_DATE);
+    }
+
+    static String buildDataId(Properties appInfoProperties) {
+        String dataGroupId = appInfoProperties.getProperty(BASELINE_DATA_GROUP_ID);
+        String dataArtifactId = appInfoProperties.getProperty(BASELINE_DATA_ARTIFACT_ID);
+        String dataVersionId = appInfoProperties.getProperty(BASELINE_DATA_VERSION);
+
+        return dataGroupId + ":" + dataArtifactId + ":" + dataVersionId;
+    }
+
+    static String buildToolkitVersion(Properties appInfoProperties) {
+        return appInfoProperties.getProperty(TOOLKIT_VERSION);
+    }
+
+    static String buildLabelText(String href, String projectName, String projectDesc,
+            String projectId, String archetypeId, String dataId, String toolkitVersion, String snomedCTDate) {
         StringBuilder labelTextBuilder = new StringBuilder("<html><blockquote>");
-        
+
+        if (projectName != null) {
+            labelTextBuilder.append("<br><b>Workbench name: </b>").append(projectName);
+        }
+
+        if (projectName != null) {
+            labelTextBuilder.append("<br><b>Workbench description: </b>").append(projectDesc);
+        }
+
         // Link project version if href is not null.
         if (href != null) {
-            labelTextBuilder.append("Project version <a href=\"\">" + projectId + "</a>");
+            labelTextBuilder.append("<br><b>Workbench version: </b><a href=\"\">").append(projectId).append("</a>");
         } else {
-            labelTextBuilder.append("Project version " + projectId);
+            labelTextBuilder.append("<br><b>Workbench version: </b>").append(projectId);
+        }
+
+        if (snomedCTDate != null){
+            labelTextBuilder.append("<br><b>SNOMED CT version: </b>").append(snomedCTDate);
         }
         
         // Append archetype text if not null.
         if (archetypeId != null) {
-            labelTextBuilder.append("<br>Built from " + archetypeId);
+            labelTextBuilder.append("<br><br><b>Built from: </b>").append(archetypeId);
+        }        
+
+        //Append the data version
+        if (dataId != null) {
+            labelTextBuilder.append("<br><b>Database version: </b>").append(dataId);
+        }
+
+        //Append the toolkit version
+        if (toolkitVersion != null) {
+            labelTextBuilder.append("<br><b>Software version: </b>").append(toolkitVersion);
         }
         
         labelTextBuilder.append("</blockquote></html>");
-       
+
         return labelTextBuilder.toString();
     }
 
@@ -271,7 +315,7 @@ public class AboutBox {
     public static JDialog getAboutBox(JFrame parent) {
         return setupAbout(parent);
     }
-    
+
     /**
      * Helpful for testing outside an editor bundle.
      */
--- wb-foundation/src/test/java/org/dwfa/bpa/util/AboutBoxTest.java
+++ wb-foundation/src/test/java/org/dwfa/bpa/util/AboutBoxTest.java
@@ -12,7 +12,13 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Properties;
-import javax.swing.JLabel;
+import static org.dwfa.bpa.util.AppInfoProperties.BASELINE_DATA_ARTIFACT_ID;
+import static org.dwfa.bpa.util.AppInfoProperties.BASELINE_DATA_GROUP_ID;
+import static org.dwfa.bpa.util.AppInfoProperties.BASELINE_DATA_VERSION;
+import static org.dwfa.bpa.util.AppInfoProperties.PROJECT_DESCRIPTION;
+import static org.dwfa.bpa.util.AppInfoProperties.PROJECT_NAME;
+import static org.dwfa.bpa.util.AppInfoProperties.SNOMED_CORE_RELEASE_DATE;
+import static org.dwfa.bpa.util.AppInfoProperties.TOOLKIT_VERSION;
 
 public class AboutBoxTest {
 
@@ -22,6 +28,13 @@ public class AboutBoxTest {
     private final String archetypeGroupId = "ghi";
     private final String archetypeArtifactId = "jkl";
     private final String archetypeVersion = "2.2";
+    private final String projectName = "project";
+    private final String projectDesc = "project description";
+    private final String baselineDataArtifactId = "baselineArtifactId";
+    private final String baselineDataGroupId = "baselineGroup";
+    private final String baselineDataVersion = "baselineVersion";
+    private final String tkVersion = "1.0-SNAPSHOT";
+    private final String snomedCTVersion = "00-00-00-00.00.00";
 
     private Properties appInfoProperties;
 
@@ -35,91 +48,87 @@ public class AboutBoxTest {
         appInfoProperties.setProperty(ARCHETYPE_GROUP_ID, archetypeGroupId);
         appInfoProperties.setProperty(ARCHETYPE_ARTIFACT_ID, archetypeArtifactId);
         appInfoProperties.setProperty(ARCHETYPE_VERSION, archetypeVersion);
+        appInfoProperties.setProperty(PROJECT_NAME, projectName);
+        appInfoProperties.setProperty(PROJECT_DESCRIPTION, projectDesc);
+        appInfoProperties.setProperty(BASELINE_DATA_ARTIFACT_ID, baselineDataArtifactId);
+        appInfoProperties.setProperty(BASELINE_DATA_GROUP_ID, baselineDataGroupId);
+        appInfoProperties.setProperty(BASELINE_DATA_VERSION, baselineDataVersion);
+        appInfoProperties.setProperty(TOOLKIT_VERSION, tkVersion);
+                appInfoProperties.setProperty(SNOMED_CORE_RELEASE_DATE, snomedCTVersion);
     }
-    
+
     @Test
     public void testBuildProjectId() {
         String projectId = AboutBox.buildProjectId(appInfoProperties);
         assertEquals("abc:def:1.1", projectId);
     }
-    
+
     @Test
     public void testBuildArchetypeId() {
         String archetypeId = AboutBox.buildArchetypeId(appInfoProperties);
         assertEquals("ghi:jkl:2.2", archetypeId);
     }
-    
+
     @Test
     public void testBuildArchetypeId_groupIdNull() {
         // Remove groupId.
         appInfoProperties.remove(ARCHETYPE_GROUP_ID);
-        
+
         String archetypeId = AboutBox.buildArchetypeId(appInfoProperties);
         assertNull(archetypeId);
     }
-    
+
     @Test
     public void testBuildArchetypeId_artifactIdNull() {
         // Remove artifactId.
         appInfoProperties.remove(ARCHETYPE_ARTIFACT_ID);
-        
+
         String archetypeId = AboutBox.buildArchetypeId(appInfoProperties);
         assertNull(archetypeId);
     }
-    
+
     @Test
     public void testBuildArchetypeId_versionNull() {
         // Remove version.
         appInfoProperties.remove(ARCHETYPE_VERSION);
-        
+
         String archetypeId = AboutBox.buildArchetypeId(appInfoProperties);
         assertNull(archetypeId);
     }
-    
+
     @Test
     public void testBuildArchetypeId_allNull() {
         String archetypeId = AboutBox.buildArchetypeId(new Properties());
         assertNull(archetypeId);
     }
-    
+
     @Test
     public void testBuildLabelText() {
         String href = "http://zzz.net";
+        String name = AboutBox.buildProjectName(appInfoProperties);
+        String projectDescription = AboutBox.buildProjectDesc(appInfoProperties);
         String projectId = AboutBox.buildProjectId(appInfoProperties);
         String archetypeId = AboutBox.buildArchetypeId(appInfoProperties);
-        String linkLabelText = AboutBox.buildLabelText(href, projectId, archetypeId);
+        String dataId = AboutBox.buildDataId(appInfoProperties);
+        String toolkitVersion = AboutBox.buildToolkitVersion(appInfoProperties);
+        String snomedCoreReleaseDate = AboutBox.buildSnomedCoreReleaseDate(appInfoProperties);
+        String linkLabelText = AboutBox.buildLabelText(href, name, projectDescription, projectId, archetypeId,
+                dataId, toolkitVersion, snomedCoreReleaseDate);
+
+        String expectedValue = "<html><blockquote>"
+                + "<br><b>Workbench name: </b>" + this.projectName
+                + "<br><b>Workbench description: </b>" + this.projectDesc
+                + "<br><b>Workbench version: </b><a href=\"\">abc:def:1.1</a>"
+                + "<br><b>SNOMED CT version: </b>" + this.snomedCTVersion
+                + "<br><br><b>Built from: </b>" + this.archetypeGroupId + ":"
+                + this.archetypeArtifactId + ":" + this.archetypeVersion
+                + "<br><b>Database version: </b>" + this.baselineDataGroupId
+                + ":" + this.baselineDataArtifactId + ":" + this.baselineDataVersion
+                + "<br><b>Software version: </b>" + this.tkVersion
+                + "</blockquote></html>";
         
-        String expectedValue = "<html><blockquote>" +
-                "Project version <a href=\"\">abc:def:1.1</a>" +
-                "<br>Built from ghi:jkl:2.2" +
-                "</blockquote></html>";
-        assertEquals(expectedValue, linkLabelText);
-    }
-    
-    @Test
-    public void testBuildLinkLabelText_nullHref() {
-        String href = null;
-        String projectId = AboutBox.buildProjectId(appInfoProperties);
-        String archetypeId = AboutBox.buildArchetypeId(appInfoProperties);
-        String linkLabelText = AboutBox.buildLabelText(href, projectId, archetypeId);
-        
-        String expectedValue = "<html><blockquote>" +
-                "Project version abc:def:1.1" +
-                "<br>Built from ghi:jkl:2.2" +
-                "</blockquote></html>";
-        assertEquals(expectedValue, linkLabelText);
-    }
-    
-    @Test
-    public void testBuildLinkLabelText_nullArchetypeId() {
-        String href = "http://zzz.net";
-        String projectId = AboutBox.buildProjectId(appInfoProperties);
-        String archetypeId =  null;
-        String linkLabelText = AboutBox.buildLabelText(href, projectId, archetypeId);
-        
-        String expectedValue = "<html><blockquote>" +
-                "Project version <a href=\"\">abc:def:1.1</a>" +
-                "</blockquote></html>";
+        System.out.println(expectedValue);
+        System.out.println(linkLabelText);
         assertEquals(expectedValue, linkLabelText);
     }
 }
