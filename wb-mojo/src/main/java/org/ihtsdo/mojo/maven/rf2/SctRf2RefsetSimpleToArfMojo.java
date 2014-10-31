f10c67558397b54a42753620103631d23a49fd70
afurber <afurber>
2014-10-31 17:46:23 +0000

    Updating to use same naming as file produced by the release process.

--- wb-mojo/src/main/java/org/ihtsdo/mojo/maven/rf2/SctRf2RefsetSimpleToArfMojo.java
+++ wb-mojo/src/main/java/org/ihtsdo/mojo/maven/rf2/SctRf2RefsetSimpleToArfMojo.java
@@ -125,7 +125,7 @@ public class SctRf2RefsetSimpleToArfMojo extends AbstractMojo implements Seriali
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(
                     outDir + "concept_simple_rf2.refset"), "UTF-8"));
             getLog().info("::: SIMPLE REFSET FILE: " + outDir + "concept_simple_rf2.refset");
-            filesIn = Rf2File.getFiles(wDir, targetSubDir, inputDir, "der2_Refset_Simple", ".txt");
+            filesIn = Rf2File.getFiles(wDir, targetSubDir, inputDir, "der2_cRefset_Simple", ".txt");
             for (Rf2File rf2File : filesIn) {
                 Rf2_RefsetSimpleRecord[] members = Rf2_RefsetSimpleRecord.parseRefset(rf2File, uuidPathStr);
                 for (Rf2_RefsetSimpleRecord m : members) {
