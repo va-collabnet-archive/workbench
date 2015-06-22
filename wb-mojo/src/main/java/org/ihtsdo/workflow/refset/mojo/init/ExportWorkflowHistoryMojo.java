7a0a4e145a345bbe5697a232cf472e059f030826
darmbrust <daniel.armbrust.list@gmail.com>
2015-06-22 06:51:43 +0000

    merging 14620, 14619

--- wb-mojo/src/main/java/org/ihtsdo/workflow/refset/mojo/init/ExportWorkflowHistoryMojo.java
+++ wb-mojo/src/main/java/org/ihtsdo/workflow/refset/mojo/init/ExportWorkflowHistoryMojo.java
@@ -45,7 +45,71 @@ public class ExportWorkflowHistoryMojo extends AbstractMojo {
 	 * 
 	 */
 	private String exportFolder;
-
+	/**
+	 * endpointURL
+	 * 
+	 * @parameter
+	 * 
+	 */
+	private String endpointURL;
+	
+	/**
+	 * username
+	 * 
+	 * @parameter
+	 * 
+	 */
+	private String username;
+	
+	/**
+	 * password
+	 * 
+	 * @parameter
+	 * 
+	 */
+	private String password;
+	
+	
+	/**
+	 * namespaceId
+	 * 
+	 * @parameter default-value="0"
+	 * 
+	 */
+	private String namespaceId;
+	
+	/**
+	 * partitionId
+	 * 
+	 * @parameter default-value="00"
+	 * 
+	 */
+	private String partitionId;
+	
+	/**
+	 * executionId
+	 * 
+	 * @parameter default-value="Daily-build"
+	 * 
+	 */
+	private String executionId;
+	
+	/**
+	 * moduleId
+	 * 
+	 * @parameter default-value="Core Concept Component"
+	 * 
+	 */
+	private String moduleId;
+	
+	/**
+	 * releaseId
+	 * 
+	 * @parameter default-value="20110131"
+	 * 
+	 */
+	private String releaseId;
+	
 	/**
 	 * componentType
 	 * 
@@ -64,7 +128,15 @@ public class ExportWorkflowHistoryMojo extends AbstractMojo {
 			config.setReleaseDate(releaseDate);
 			config.setFlushCount(10000);
 			config.setFileExtension("txt");
-			config.setComponentType(componentType);
+			config.setNamespaceId(namespaceId);
+			config.setPartitionId(partitionId);
+			config.setExecutionId(executionId);
+			config.setModuleId(moduleId);
+			config.setReleaseId(releaseId);
+			config.setComponentType(componentType);			
+			config.setUsername(username);
+			config.setPassword(password);
+			config.setEndPoint(endpointURL);
 			
 			// initialize meta hierarchy
 			ExportUtil.init();
